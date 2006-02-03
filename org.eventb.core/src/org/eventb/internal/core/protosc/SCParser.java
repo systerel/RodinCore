de5b2c816ab62f43b11f98ac0eb934fb44bc605d
Stefan Hallerstede <halstefa@users.sourceforge.net>
2006-02-03 18:50:16 +0000

    improved error messages

--- org.eventb.core/src/org/eventb/internal/core/protosc/SCParser.java
+++ org.eventb.core/src/org/eventb/internal/core/protosc/SCParser.java
@@ -8,7 +8,9 @@
 package org.eventb.internal.core.protosc;
 
 import java.util.Collection;
+import java.util.List;
 
+import org.eventb.core.ast.ASTProblem;
 import org.eventb.core.ast.Assignment;
 import org.eventb.core.ast.FormulaFactory;
 import org.eventb.core.ast.FreeIdentifier;
@@ -17,6 +19,7 @@ import org.eventb.core.ast.IResult;
 import org.eventb.core.ast.ITypeCheckResult;
 import org.eventb.core.ast.ITypeEnvironment;
 import org.eventb.core.ast.Predicate;
+import org.eventb.core.ast.ProblemSeverities;
 import org.rodinp.core.IInternalElement;
 import org.rodinp.core.RodinDBException;
 
@@ -43,13 +46,13 @@ public class SCParser {
 		
 		parseLexResult = factory.parseAssignment(element.getContents());
 		
-		if(parseLexResult.isSuccess()) {
+		if(parseLexResult.isSuccess() && parseLexResult.getProblems().size() == 0) {
 			
 			Assignment assignment = parseLexResult.getParsedAssignment();
 			
 			IResult legibilityResult = assignment.isLegible(declaredIdentifiers);
 			
-			if(legibilityResult.isSuccess()) {
+			if(legibilityResult.isSuccess() && legibilityResult.getProblems().size() == 0) {
 				
 				// TODO the cloning of the type environment should happen in the AST and not here
 				typeCheckResult = assignment.typeCheck(typeEnvironment);
@@ -71,17 +74,44 @@ public class SCParser {
 		}
 		return false;
 	}
+	
+	private void emitProblemMarkers(ISCProblemList problemList, IInternalElement element, String msgHeader, List<ASTProblem> astProblems) {
+		for(ASTProblem astProblem : astProblems) {
+			
+			int severity = astProblem.getSeverity();
+			switch (severity) {
+			case ProblemSeverities.Error:
+				severity = SCProblem.SEVERITY_ERROR;
+				break;
+			case ProblemSeverities.Ignore:
+				severity = SCProblem.SEVERITY_INFO;
+				break;
+			case ProblemSeverities.Warning:
+				severity = SCProblem.SEVERITY_WARNING;
+				break;
+			default:
+				assert false;
+				break;
+			}
+			
+			String message = astProblem.toString();
+			
+			problemList.addProblem(element, msgHeader + message, severity);
+		}
+	}
 
 	protected boolean parsePredicate(IInternalElement element, ISCProblemList problemList) throws RodinDBException {
 		
 		parseLexResult = factory.parsePredicate(element.getContents());
 		
-		if(parseLexResult.isSuccess()) {
+		IResult legibilityResult = null;
+		
+		if(parseLexResult.isSuccess() && parseLexResult.getProblems().size() == 0) {
 			Predicate predicate = parseLexResult.getParsedPredicate();
 			
-			IResult legibilityResult = predicate.isLegible(declaredIdentifiers);
+			legibilityResult = predicate.isLegible(declaredIdentifiers);
 			
-			if(legibilityResult.isSuccess()) {
+			if(legibilityResult.isSuccess() && legibilityResult.getProblems().size() == 0) {
 				
 				// TODO the cloning of the type environment should happen in the AST and not here
 				typeCheckResult = predicate.typeCheck(typeEnvironment);
@@ -89,18 +119,18 @@ public class SCParser {
 				if(typeCheckResult.isSuccess()) {
 					typeEnvironment.addAll(typeCheckResult.getInferredEnvironment());
 					return true;
-				} else {
-					// TODO: proper error messages
-					problemList.addProblem(element, "Type error: " + typeCheckResult.getProblems().get(0).toString(), SCProblem.SEVERITY_ERROR);
 				}
-			} else {
-				// TODO: proper error messages
-				problemList.addProblem(element, "Legibility error: " + legibilityResult.getProblems().get(0).toString(), SCProblem.SEVERITY_ERROR);
 			}
-		} else {
-			// TODO: proper error messages
-			problemList.addProblem(element, "Syntax error: " + parseLexResult.getProblems().get(0).toString(), SCProblem.SEVERITY_ERROR);
 		}
+		// TODO: proper error messages
+		if(parseLexResult != null)
+			emitProblemMarkers(problemList, element, "Syntax error: ", parseLexResult.getProblems());
+		// TODO: proper error messages
+		if(legibilityResult != null)
+			emitProblemMarkers(problemList, element, "Legibility error: ", legibilityResult.getProblems());
+		// TODO: proper error messages
+		if(typeCheckResult != null)
+			emitProblemMarkers(problemList, element, "Type error: ", typeCheckResult.getProblems());
 		return false;
 	}
 
