5229312dcfecbe31bcbd120344acbfc9c9e71db5
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-02-19 18:41:51 +0000

    Renamed the plugin class and added a factory method for creating a new proof tree.

---
+++ org.eventb.core.seqprover/src/org/eventb/core/prover/SequentProver.java
@@ -0,0 +1,61 @@
+package org.eventb.core.prover;
+
+import org.eclipse.core.runtime.Plugin;
+import org.eventb.core.prover.rules.ProofTree;
+import org.eventb.core.prover.sequent.IProverSequent;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class SequentProver extends Plugin {
+
+	//The shared instance.
+	private static SequentProver plugin;
+	
+	/**
+	 * Creates the Sequent Prover plug-in.
+	 * <p>
+	 * The plug-in instance is created automatically by the Eclipse platform.
+	 * Clients must not call.
+	 * </p>
+	 */
+	public SequentProver() {
+		super();
+		plugin = this;
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static SequentProver getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Creates a new proof tree for the given sequent.
+	 * 
+	 * @param sequent
+	 *            the sequent to prove
+	 * @return a new proof tree for the given sequent
+	 */
+	public static IProofTree makeProofTree(IProverSequent sequent) {
+		return new ProofTree(sequent);
+	}
+	
+}
