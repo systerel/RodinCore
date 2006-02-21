48b6c27a77b8baeeed211a6873dc86a3e2cecfba
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-02-21 18:51:47 +0000

    Added utility methods for manipulating event-B file names.

--- org.eventb.core/src/org/eventb/core/EventBPlugin.java
+++ org.eventb.core/src/org/eventb/core/EventBPlugin.java
@@ -15,15 +15,114 @@ import org.osgi.framework.BundleContext;
  */
 public class EventBPlugin extends Plugin {
 
+	//The shared instance.
+	private static EventBPlugin plugin;
+
 	/**
 	 * The plug-in identifier of the Event-B core support (value
 	 * <code>"org.eventb.core"</code>).
 	 */
 	public static final String PLUGIN_ID = "org.eventb.core"; //$NON-NLS-1$
-
-	//The shared instance.
-	private static EventBPlugin plugin;
 	
+	/**
+	 * Returns the name of the component whose data are stored in the file with the given name.
+	 * 
+	 * @param fileName
+	 *            name of the file
+	 * @return the name of the component corresponding to the given file
+	 */
+	public static String getComponentName(String fileName) {
+		final int length = fileName.length() - 4;
+		assert 0 < length;
+		return fileName.substring(0, length);
+	}
+
+	/**
+	 * Returns the name of the Rodin file that contains the context with the
+	 * given name.
+	 * 
+	 * @param bareName
+	 *            name of the context
+	 * @return the name of the file containing that context
+	 */
+	public static String getContextFileName(String bareName) {
+		return bareName + ".buc";
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static EventBPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the name of the Rodin file that contains the machine with the
+	 * given name.
+	 * 
+	 * @param bareName
+	 *            name of the machine
+	 * @return the name of the file containing that machine
+	 */
+	public static String getMachineFileName(String bareName) {
+		return bareName + ".bum";
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static EventBPlugin getPlugin() {
+		return plugin;
+	}
+	
+	/**
+	 * Returns the name of the Rodin file that contains the proof obligations for the component of the
+	 * given name.
+	 * 
+	 * @param bareName
+	 *            name of the component
+	 * @return the name of the file containing POs for that component
+	 */
+	public static String getPOFileName(String bareName) {
+		return bareName + ".bpo";
+	}
+
+	/**
+	 * Returns the name of the Rodin file that contains the proofs for the component of the
+	 * given name.
+	 * 
+	 * @param bareName
+	 *            name of the component
+	 * @return the name of the file containing proofs for that component
+	 */
+	public static String getPRFileName(String bareName) {
+		return bareName + ".bpr";
+	}
+
+	/**
+	 * Returns the name of the Rodin file that contains the checked context with the
+	 * given name.
+	 * 
+	 * @param bareName
+	 *            name of the checked context
+	 * @return the name of the file containing that checked context
+	 */
+	public static String getSCContextFileName(String bareName) {
+		return bareName + ".bcc";
+	}
+
+	/**
+	 * Returns the name of the Rodin file that contains the checked machine with the
+	 * given name.
+	 * 
+	 * @param bareName
+	 *            name of the checked machine
+	 * @return the name of the file containing that checked machine
+	 */
+	public static String getSCMachineFileName(String bareName) {
+		return bareName + ".bcm";
+	}
+
 	/**
 	 * Creates the Event-B core plug-in.
 	 * <p>
@@ -44,20 +143,6 @@ public class EventBPlugin extends Plugin {
 	public void stop(BundleContext context) throws Exception {
 		super.stop(context);
 		plugin = null;
-	}
-
-	/**
-	 * Returns the shared instance.
-	 */
-	public static EventBPlugin getDefault() {
-		return plugin;
-	}
-
-	/**
-	 * Returns the shared instance.
-	 */
-	public static EventBPlugin getPlugin() {
-		return plugin;
 	}
 
 }
