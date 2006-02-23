b1f9a2db6a54193cbcb50fefcb84580b31bc16d9
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-02-23 20:14:09 +0000

    Added timeout in input. Simplified implementation.

--- org.eventb.core.seqprover/src/org/eventb/core/prover/externalReasoners/LegacyProvers.java
+++ org.eventb.core.seqprover/src/org/eventb/core/prover/externalReasoners/LegacyProvers.java
@@ -1,9 +1,9 @@
 package org.eventb.core.prover.externalReasoners;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Set;
 
+import org.eventb.core.ast.ITypeEnvironment;
 import org.eventb.core.ast.Predicate;
 import org.eventb.core.prover.IExtReasonerInput;
 import org.eventb.core.prover.IExtReasonerOutput;
@@ -18,51 +18,85 @@ import org.eventb.core.prover.sequent.ISequent;
 import org.eventb.core.prover.sequent.SimpleSequent;
 
 
-public class LegacyProvers implements IExternalReasoner{
+public class LegacyProvers implements IExternalReasoner {
 	
 	public String name(){
 		return "legacy provers";
 	}
 
-	private static boolean runLegacyProvers(IProverSequent S){
-		Set<Predicate> hyps = new HashSet<Predicate>(S.visibleHypotheses().size());
-//		@SuppressWarnings("unused") String strGoal = S.goal().toString();
-		for (Hypothesis hyp: S.visibleHypotheses()){
-			hyps.add(hyp.getPredicate());
+	private boolean runLegacyProvers(
+			ITypeEnvironment typeEnvironment,
+			Set<Hypothesis> hypotheses,
+			Predicate goal,
+			long timeOutDelay) {
+		
+		final int length = hypotheses.size();
+		final Predicate[] hyps = new Predicate[length];
+		int index = 0;
+		for (Hypothesis hypothesis: hypotheses){
+			hyps[index ++] = hypothesis.getPredicate();
 		}
-		StringBuffer sequent = ClassicB.translateSequent(S.typeEnvironment(), hyps, S.goal());
+		StringBuffer sequent = 
+			ClassicB.translateSequent(typeEnvironment, hyps, goal);
 		try {
-			if(ClassicB.proveWithML(sequent))
+			if (ClassicB.proveWithML(sequent, timeOutDelay))
 				return true;
 			else
-				return ClassicB.proveWithPP(sequent);
+				return ClassicB.proveWithPP(sequent, timeOutDelay);
 		} catch (IOException e) {
-			return false;
-		} catch (InterruptedException e) {
 			return false;
 		}
 	}
 	
-	public IExtReasonerOutput apply(IProverSequent S,IExtReasonerInput I) {	
-		if (runLegacyProvers(S)) {
+	public IExtReasonerOutput apply(IProverSequent sequent,
+			IExtReasonerInput input) {
+		
+		final ITypeEnvironment typeEnvironment = sequent.typeEnvironment();
+		final Set<Hypothesis> hypotheses = sequent.visibleHypotheses();
+		final Predicate goal = sequent.goal();
+		final Input myInput = (Input) input;
+		final long timeOutDelay = myInput.timeOutDelay;
+		if (timeOutDelay < 0) {
+			return new UnSuccessfulExtReasonerOutput(
+					this,
+					input,
+					"Invalid time out delay"
+			);
+		}
+		
+		final boolean success =
+			runLegacyProvers(typeEnvironment, hypotheses, goal, timeOutDelay);
+		if (success) {
 			ISequent outputSequent = 
-				new SimpleSequent(S.typeEnvironment(),S.visibleHypotheses(),S.goal());
-			return new SuccessfullExtReasonerOutput(this,I,new TrustedProof(outputSequent));
+				new SimpleSequent(typeEnvironment, hypotheses, goal);
+			return new SuccessfullExtReasonerOutput(
+					this,
+					input,
+					new TrustedProof(outputSequent)
+			);
 		}
-		else
-		{
-			return new UnSuccessfulExtReasonerOutput(this,I,"Legacy provers failed");
-		}
+		return new UnSuccessfulExtReasonerOutput(
+				this,
+				input,
+				"Legacy provers failed"
+		);
 	}
 	
 	public IExtReasonerInput defaultInput(){
 		return new Input();
 	}
-
 	
 	public static class Input implements IExtReasonerInput{
 		
-		public Input(){
+		public final long timeOutDelay;
+		
+		public Input() {
+			// Defaults to 30 seconds 
+			this.timeOutDelay = 30 * 1000;
+		}
+
+		public Input(long timeOutDelay) {
+			this.timeOutDelay  = timeOutDelay;
 		}
 	}
 
