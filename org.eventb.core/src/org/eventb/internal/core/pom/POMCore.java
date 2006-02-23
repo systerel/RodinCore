12ac2c1d20e9a3494725b508692369bdc3d0cec7
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-02-23 21:09:20 +0000

    Removed method for running POM differently from the builder.

--- org.eventb.core/src/org/eventb/internal/core/pom/POMCore.java
+++ org.eventb.core/src/org/eventb/internal/core/pom/POMCore.java
@@ -8,10 +8,7 @@
 
 package org.eventb.internal.core.pom;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eventb.core.EventBPlugin;
-import org.eventb.core.IPOFile;
-import org.eventb.core.IPRFile;
 
 /**
  * @author halstefa
@@ -19,12 +16,7 @@ import org.eventb.core.IPRFile;
  */
 public class POMCore {
 
-	public static final String AUTO_POM_TOOL_ID = EventBPlugin.PLUGIN_ID + ".autoPOM"; //$NON-NLS-1$
+	public static final String AUTO_POM_TOOL_ID = 
+		EventBPlugin.PLUGIN_ID + ".autoPOM"; //$NON-NLS-1$
 	
-	public static void runAutoPOG(IPOFile poFile, IPRFile prFile) throws CoreException {
-		AutoPOM pom = new AutoPOM();
-		pom.init(poFile, prFile, null, null);
-		pom.writePRFile();
-	}
-
 }
