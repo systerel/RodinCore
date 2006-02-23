556c7814d20e2d82b0091bfce050f59a3a043806
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-02-23 21:07:04 +0000

    Added time out delay and disabling.

--- org.eventb.core/src/org/eventb/internal/core/pom/AutoProver.java
+++ org.eventb.core/src/org/eventb/internal/core/pom/AutoProver.java
@@ -1,19 +1,46 @@
 package org.eventb.internal.core.pom;
 
 import org.eventb.core.prover.IProofTree;
-import org.eventb.core.prover.tactics.ITactic;
 import org.eventb.core.prover.tactics.Tactics;
 
 public class AutoProver {
 	
-	private ITactic tactic;
-
-	public AutoProver(){
-		this.tactic = Tactics.norm();
+	// Default delay for automatic proofs: 2 seconds
+	private static long timeOutDelay = 2 * 1000;
+	
+	private static boolean enabled = true;
+	
+	public AutoProver() {
+		// Nothing to do.
 	}
 	
-	public void run(IProofTree pt){
-		tactic.apply(pt.getRoot());
+	public void run(IProofTree pt) {
+		if (! enabled)
+			return;
+		
+		// First try applying an internal tactic
+		Tactics.norm().apply(pt.getRoot());
+		if (pt.isDischarged())
+			return;
+		
+		// Then, try with the legacy provers.
+		Tactics.legacyProvers(timeOutDelay).apply(pt.getRoot());
 	}
 
+	public static void setTimeOutDelay(long value) {
+		timeOutDelay = value;
+	}
+
+	public static void enable() {
+		enabled = true;
+	}
+	
+	public static void disable() {
+		enabled = false;
+	}
+	
+	public static boolean isEnabled() {
+		return enabled;
+	}
+	
 }
