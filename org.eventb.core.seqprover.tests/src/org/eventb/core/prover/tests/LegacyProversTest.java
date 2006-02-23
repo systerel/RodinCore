78e398e0b45c375f2a114457db59f5a81f1f47b9
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-02-23 20:33:35 +0000

    Updated after changes in prover call interface. Added test for timeout parameter.

--- org.eventb.core.seqprover.tests/src/org/eventb/core/prover/tests/LegacyProversTest.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/prover/tests/LegacyProversTest.java
@@ -11,52 +11,75 @@ import org.eventb.core.prover.externalReasoners.LegacyProvers;
 import org.eventb.core.prover.sequent.IProverSequent;
 
 public class LegacyProversTest extends TestCase {
-	IExternalReasoner legacyProvers = new LegacyProvers();
+	
+	private static IExternalReasoner legacyProvers = new LegacyProvers();
+	
+	/**
+	 * Checks that the legacy provers failed to prove the given sequent.
+	 * 
+	 * @param sequent
+	 *            the sequent to prove
+	 */
+	private static void assertFailure(IProverSequent sequent, LegacyProvers.Input input) {
+		IExtReasonerOutput extReaOut = legacyProvers.apply(sequent, input);
+		assertTrue(extReaOut instanceof UnSuccessfulExtReasonerOutput);
+	}
+
+	/**
+	 * Checks that the legacy provers succeeded in proving the given sequent.
+	 * 
+	 * @param sequent
+	 *            the sequent to prove
+	 */
+	private static void assertSuccess(IProverSequent sequent, LegacyProvers.Input input) {
+		Predicate newGoalPredicate = TestLib.chkProofFormat_getNewGoalPred(
+				sequent, legacyProvers, input);
+		assertTrue(newGoalPredicate.equals(Lib.True));
+	}
+	
+	IProverSequent[] failures = {
+			TestLib.genSeq("1=1 |- 2=1"),
+			TestLib.genSeq("x∈ℤ|- x∈ℕ"),
+	};
 
 	IProverSequent[] success = {
 			TestLib.genSeq("x∈ℕ|- x∈ℕ"),
 			TestLib.genSeq("1=1 |- 1=1"),
 			TestLib.genSeq("1=1 |- 2=2"),
 			TestLib.genSeq("x∈ℕ|- x∈ℤ"),
-			// TestLib.genSeq("x∈ℤ;; x>0 |- x≠0 "),
+			TestLib.genSeq("x∈ℤ;; x>0 |- x≠0 "),
 			TestLib.genSeq("(∀n·n∈ℕ ⇒ n∈A) |- (∃n·n∈ℕ ∧ n∈A) "),
 			TestLib.genSeq("A∈ℙ(ℤ) ;; B∈ℙ(ℤ) ;; x∈A ;; x∈B |- x∈A∩B"),
-			// The next sequent is provable by PP, but not ML
+			// The next sequent is provable by PP, but not by ML
 			TestLib.genSeq("0 ≤ a ∧ 1 < b |- a mod b < b"),
-			TestLib.genSeq("x∈ℕ;; x=z ;; y=z |- x=z")};
-	IProverSequent[] failure = {
-			TestLib.genSeq("1=1 |- 2=1"),
-			TestLib.genSeq("x∈ℤ|- x∈ℕ")
-			};
+			TestLib.genSeq("x∈ℕ;; x=z ;; y=z |- x=z"),
+	};
 
-	@Override
-	public void setUp(){
-	
+	public void testLegacyProverApply() {	
+
+		final LegacyProvers.Input input = new LegacyProvers.Input();
+		for (IProverSequent sequent : success){
+			assertSuccess(sequent, input);
+		}
+		for (IProverSequent sequent : failures){
+			assertFailure(sequent, input);
+		}
 	}
-	
-	public void testApply() {	
 
-		Predicate newGoalPredicate;
-		for (IProverSequent suceed : success){
-			newGoalPredicate = TestLib.chkProofFormat_getNewGoalPred(suceed,legacyProvers,null);
-			assertTrue(newGoalPredicate.equals(Lib.True));
-		}
-		
-		IExtReasonerOutput extReaOut;
-		for (IProverSequent fail : failure){
-			extReaOut = legacyProvers.apply(fail,null);
-			assertTrue (extReaOut instanceof UnSuccessfulExtReasonerOutput);
-		}
-//		
-//		assertTrue(newGoalPredicate.equals(newgoal));
-//		//System.out.println(newGoalPredicate);
-//		
-//		I = new Input(impHyp,true);
-//		newGoalPredicate = TestLib.chkProofFormat_getNewGoalPred(impEseq,impE,I);
-//		
-//		assertTrue(newGoalPredicate.equals(newgoalContrap));
-//		//System.out.println(newGoalPredicate);
-//		
+	public void testTimeOut() {
+		IProverSequent sequent = TestLib.genSeq("0 ≤ a ∧ 1 < b |- a mod b < b");
+
+		// Very short timeout so that the provers are not launched
+		LegacyProvers.Input input = new LegacyProvers.Input(1);
+		assertFailure(sequent, input);
+
+		// Invalid timeout so that the provers are not launched
+		input = new LegacyProvers.Input(-1);
+		assertFailure(sequent, input);
+
+		// Reasonable timeout so that the provers can succeed
+		input = new LegacyProvers.Input(30 * 1000);
+		assertSuccess(sequent, input);
 	}
 	
 }
