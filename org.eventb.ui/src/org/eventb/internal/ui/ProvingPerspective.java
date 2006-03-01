839259ad3f88f2dbf2828e8f74f8863312de293d
Thai Son Hoang <tshoang@users.sourceforge.net>
2006-03-01 12:34:45 +0000

    Added Problems View, Project Explorer and Content Outline views to the perspective.

--- org.eventb.ui/src/org/eventb/internal/ui/ProvingPerspective.java
+++ org.eventb.ui/src/org/eventb/internal/ui/ProvingPerspective.java
@@ -7,6 +7,7 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import org.eventb.internal.ui.obligationexplorer.ObligationExplorer;
+import org.eventb.internal.ui.projectexplorer.ProjectExplorer;
 import org.eventb.internal.ui.prover.ProofControl;
 import org.eventb.internal.ui.prover.ProofInformation;
 import org.eventb.internal.ui.prover.ProofTreeUI;
@@ -36,25 +37,32 @@ public class ProvingPerspective implements IPerspectiveFactory {
         layout.addShowViewShortcut(ProofTreeUI.VIEW_ID);
         layout.addShowViewShortcut(ProofControl.VIEW_ID);
         layout.addShowViewShortcut(ProofInformation.VIEW_ID);
+        layout.addShowViewShortcut(ProjectExplorer.VIEW_ID);
+        layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+        layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
 	}
 	
 	public void defineLayout(IPageLayout layout) {
         // Editors are placed for free.
         String editorArea = layout.getEditorArea();
 
-        // Place the project explorer to left of editor area.
+        // Place the Obligation/Project explorer to left of editor area.
         IFolderLayout left =
                 layout.createFolder("left", IPageLayout.LEFT, 0.20f, editorArea);
         left.addView(ObligationExplorer.VIEW_ID);
+        left.addView(ProjectExplorer.VIEW_ID);
         
-        // Place the outline to right of editor area.
+        // Place the ProofTree / Outline to right of editor area.
         IFolderLayout right = layout.createFolder("right", IPageLayout.RIGHT, 0.7f, editorArea);
         right.addView(ProofTreeUI.VIEW_ID);
+        right.addView(IPageLayout.ID_OUTLINE);
 
-        // Place the Problems / Task to the bottom of the editor area.
+        // Place the Proof Control / Problems to the bottom of the editor area.
         IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.75f, editorArea);
         bottom.addView(ProofControl.VIEW_ID);
+        bottom.addView(IPageLayout.ID_PROBLEM_VIEW);
 
+        // Place the Proof Information to the bottom left of the editor area.
         IFolderLayout bottomLeft =
             layout.createFolder("bottomLeft", IPageLayout.BOTTOM, 0.60f, "left");
         bottomLeft.addView(ProofInformation.VIEW_ID);
