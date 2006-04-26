aa5878543b36a5732313956af294cfdabd12b4c1
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-04-26 10:52:12 +0000

    Extracted out in a new class the substitution which consists in removing some bound identifier declarations.

--- org.eventb.core.ast/src/org/eventb/internal/core/ast/BoundIdentSubstitution.java
+++ org.eventb.core.ast/src/org/eventb/internal/core/ast/BoundIdentSubstitution.java
@@ -8,15 +8,9 @@
 
 package org.eventb.internal.core.ast;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import org.eventb.core.ast.BoundIdentDecl;
-import org.eventb.core.ast.BoundIdentifier;
 import org.eventb.core.ast.Expression;
 import org.eventb.core.ast.FormulaFactory;
-import org.eventb.core.ast.FreeIdentifier;
 
 /**
  * Substitution of some locally bound identifiers by arbitrary expressions. Some
@@ -26,17 +20,8 @@ import org.eventb.core.ast.FreeIdentifier;
  * @author Laurent Voisin
  * 
  */
-public class BoundIdentSubstitution extends Substitution {
+public class BoundIdentSubstitution extends BoundIdentDeclRemover {
 
-	// New declarations to use after substitution
-	protected final List<BoundIdentDecl> newDecls;
-	
-	// Size of the new declarations
-	protected final int newDeclSize;
-	
-	// Reversed array of substitutes
-	protected final Substitute[] substitutes;
-	
 	/**
 	 * Creates a new substitution for bound identifiers.
 	 * <p>
@@ -57,29 +42,14 @@ public class BoundIdentSubstitution extends Substitution {
 	public BoundIdentSubstitution(BoundIdentDecl[] decls, Expression[] exprs,
 			FormulaFactory ff) {
 
-		super(ff);
-		assert decls.length == exprs.length;
+		super(decls, make_keep(exprs), ff);
 
 		final int lastIndex = decls.length - 1;
-		substitutes = new Substitute[exprs.length];
-		newDecls = new ArrayList<BoundIdentDecl>(decls.length);
-		int newIndex = 0;
-		// We need to traverse both arrays backward to compute the new indexes
-		// of the identifiers that are not substituted. The array of new
-		// declarations is built in reverse order.
-		for (int i = 0; i <= lastIndex; i++) {
-			Expression expr = exprs[lastIndex - i];
-			if (expr == null) {
-				substitutes[i] = Substitute.makeSubstitute(newIndex ++, ff);
-				newDecls.add(decls[lastIndex - i]);
-			}
-		}
-		assert newIndex == newDecls.size();
-		this.newDeclSize = newIndex;
-		Collections.reverse(newDecls);
+		int newDeclSize = newDecls.size();
 		
-		// Now we know the systematic bias for the substituted expressions and
-		// can construct them.
+		// Now we build the substituted expressions, applying to them the
+		// systematic bias introduced by the removal of the declarations of
+		// substituted identifiers.
 		for (int i = 0; i <= lastIndex; i++) {
 			Expression expr = exprs[lastIndex - i];
 			if (expr != null) {
@@ -88,31 +58,13 @@ public class BoundIdentSubstitution extends Substitution {
 		}
 	}
 
-	@Override
-	public Expression getReplacement(FreeIdentifier ident) {
-		return ident;
+	private static boolean[] make_keep(Expression[] exprs) {
+		final int length = exprs.length;
+		final boolean[] result = new boolean[length];
+		for (int i = 0; i < length; i++) {
+			result[i] = (exprs[i] == null);
+		}
+		return result;
 	}
 
-	@Override
-	public Expression getReplacement(BoundIdentifier ident) {
-		final int index = ident.getBoundIndex();
-		// Locally bound identifier ?
-		if (index < nbOfInternallyBound)
-			return ident;
-		// Substituted identifier ?
-		final int rootIndex = index - nbOfInternallyBound;
-		if (rootIndex < substitutes.length)
-			return substitutes[rootIndex].getSubstitute(ident, nbOfInternallyBound);
-		// Externally bound identifier
-		final int newIndex = index - substitutes.length + newDecls.size();
-		return ff.makeBoundIdentifier(
-				newIndex, 
-				ident.getSourceLocation(),
-				ident.getType());
-	}
-
-	public List<BoundIdentDecl> getNewDeclarations() {
-		return newDecls;
-	}
-	
 }
