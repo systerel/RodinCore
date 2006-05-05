3ba6e9a6b9567a8956dad7ac2aca7f9427cdc125
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-05-05 09:55:12 +0000

    Reindented code to standard layout. No change in functionality.

--- org.eventb.pp/src/org/eventb/internal/pp/translator/IdentityTranslatorBase.java
+++ org.eventb.pp/src/org/eventb/internal/pp/translator/IdentityTranslatorBase.java
@@ -1,4 +1,5 @@
 package org.eventb.internal.pp.translator;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -6,7 +7,6 @@ import org.eventb.core.ast.AssociativeExpression;
 import org.eventb.core.ast.AssociativePredicate;
 import org.eventb.core.ast.BoundIdentDecl;
 import org.eventb.core.ast.Expression;
-import org.eventb.core.ast.Formula;
 import org.eventb.core.ast.FormulaFactory;
 import org.eventb.core.ast.Predicate;
 import org.eventb.core.ast.QuantifiedExpression;
@@ -19,39 +19,39 @@ public abstract class IdentityTranslatorBase {
 	
 	protected Expression idTransAssociativeExpression(
 			Expression expr, FormulaFactory ff, Expression[] children) {
+
 		SourceLocation loc = expr.getSourceLocation();
-		
 		ArrayList<Expression> newChildren = new ArrayList<Expression>();
 		boolean hasChanged = false;
 		for (Expression child: children) {
 			Expression newChild = translate(child, ff);
-			if(newChild.getTag() == expr.getTag()) {
-				AssociativeExpression assChild = (AssociativeExpression)newChild;
+			if (newChild.getTag() == expr.getTag()) {
+				AssociativeExpression assChild = (AssociativeExpression) newChild;
 				newChildren.addAll(Arrays.asList(assChild.getChildren()));
 				hasChanged = true;
-			}
-			else {
+			} else {
 				newChildren.add(newChild);
-				hasChanged = hasChanged || (newChild != child);
+				hasChanged |= newChild != child;
 			}
 		}
-		if(hasChanged)
-    		return ff.makeAssociativeExpression(expr.getTag(), newChildren, loc);
-    	else
-    		return expr;		    	
+		if (! hasChanged) {
+    		return expr;
+		}
+		return ff.makeAssociativeExpression(expr.getTag(), newChildren, loc);
 	}
 	
 	
 	protected Expression idTransBinaryExpression(
 			Expression expr, FormulaFactory ff, Expression l, Expression r) {
-		SourceLocation loc = expr.getSourceLocation();
 
+		SourceLocation loc = expr.getSourceLocation();
 		Expression nl = translate(l, ff);
 		Expression nr = translate(r, ff);
 
-		if(nl == l && nr == r) return expr;
-		else
-    		return ff.makeBinaryExpression(expr.getTag(), nl, nr, loc);
+		if (nl == l && nr == r) {
+			return expr;
+		}
+		return ff.makeBinaryExpression(expr.getTag(), nl, nr, loc);
 	}
 	
 	protected Expression idTransBoolExpression(
@@ -60,9 +60,10 @@ public abstract class IdentityTranslatorBase {
 		SourceLocation loc = expr.getSourceLocation();
 		Predicate nP = translate(P, ff);
 		
-		if(nP == P) return expr;
-		else 
-			return ff.makeBoolExpression(nP, loc);
+		if (nP == P) {
+			return expr;
+		}
+		return ff.makeBoolExpression(nP, loc);
 	}
 
 	protected Expression idTransQuantifiedExpression(
@@ -72,10 +73,11 @@ public abstract class IdentityTranslatorBase {
 		Predicate nP = translate(P, ff);
 		Expression nE = translate(E, ff);
 
-		if(nP == P && nE == E) return expr;
-		else
-			return ff.makeQuantifiedExpression(
-				expr.getTag(), is, nP, nE, loc, QuantifiedExpression.Form.Explicit);	    			
+		if (nP == P && nE == E) {
+			return expr;
+		}
+		return ff.makeQuantifiedExpression(expr.getTag(),
+				is, nP, nE, loc, QuantifiedExpression.Form.Explicit);	    			
 	}
 	
 	protected Expression idTransSetExtension(
@@ -87,21 +89,23 @@ public abstract class IdentityTranslatorBase {
 		for (Expression child: children) {
 			Expression newChild = translate(child, ff);
 			newChildren.add(newChild);
-			hasChanged = hasChanged || (newChild != child);
+			hasChanged |= newChild != child;
 		}
-		if(hasChanged)
-    		return ff.makeSetExtension(newChildren, loc);
-    	else
-    		return expr;
+		if (! hasChanged) {
+			return expr;
+		}
+		return ff.makeSetExtension(newChildren, loc);
 	}
+	
 	protected Expression idTransUnaryExpression(
 			Expression expr, FormulaFactory ff, Expression child) {
 
 		SourceLocation loc = expr.getSourceLocation();
 		Expression newChild = translate(child, ff);
-		if(newChild == child) return expr;
-		else
-    		return ff.makeUnaryExpression(expr.getTag(), newChild, loc);
+		if (newChild == child) {
+			return expr;
+		}
+		return ff.makeUnaryExpression(expr.getTag(), newChild, loc);
 	}
 	
 	protected Predicate idTransAssociativePredicate(
@@ -113,80 +117,82 @@ public abstract class IdentityTranslatorBase {
 		boolean hasChanged = false;
 		for (Predicate child: children) {
 			Predicate newChild = translate(child, ff);
-			if(newChild.getTag() == pred.getTag()) {
-				AssociativePredicate assPred = (AssociativePredicate)newChild;
+			if (newChild.getTag() == pred.getTag()) {
+				AssociativePredicate assPred = (AssociativePredicate) newChild;
 				newChildren.addAll(Arrays.asList(assPred.getChildren()));
 				hasChanged = true;
 			}
 			else {
 				newChildren.add(newChild);
-				hasChanged = hasChanged || (newChild != child);
+				hasChanged |= newChild != child;
 			}
 		}
-		if(hasChanged)
-			return ff.makeAssociativePredicate(pred.getTag(), newChildren, loc);
-		else
+		if (! hasChanged) {
 			return pred;
+		}
+		return ff.makeAssociativePredicate(pred.getTag(), newChildren, loc);
 	}
 
 	protected Predicate idTransBinaryPredicate(
 			Predicate pred, FormulaFactory ff, Predicate l, Predicate r) {
 
 		SourceLocation loc = pred.getSourceLocation();
-
 		Predicate nl = translate(l, ff);
 		Predicate nr = translate(r, ff);
 		
-		if(nl == l && nr == r) return pred;
-		else
-			return ff.makeBinaryPredicate(pred.getTag(),nl, nr, loc);
+		if (nl == l && nr == r) {
+			return pred;
+		}
+		return ff.makeBinaryPredicate(pred.getTag(),nl, nr, loc);
 	}
 	
 	protected Predicate idTransUnaryPredicate(
 			Predicate pred, FormulaFactory ff, Predicate P) {
 
 		SourceLocation loc = pred.getSourceLocation();
-
   		Predicate nP = translate(P, ff);
-  		if(nP == P) return pred;
-  		else
-			return ff.makeUnaryPredicate(Formula.NOT, nP, loc);
+  		
+  		if (nP == P) {
+  			return pred;
+  		}
+  		return ff.makeUnaryPredicate(pred.getTag(), nP, loc);
 	}
 	
 	protected Predicate idTransSimplePredicate(
 			Predicate pred, FormulaFactory ff, Expression E) {
 
 		SourceLocation loc = pred.getSourceLocation();
-
   		Expression nE = translate(E, ff);
-  		if(nE == E) return pred;
-  		else
-			return ff.makeSimplePredicate(Formula.KFINITE, nE, loc);
+  		
+  		if (nE == E) {
+  			return pred;
+  		}
+  		return ff.makeSimplePredicate(pred.getTag(), nE, loc);
 	}
 	
 	protected Predicate idTransRelationalPredicate(
 			Predicate pred, FormulaFactory ff, Expression l, Expression r) {
 
 		SourceLocation loc = pred.getSourceLocation();
-
 		Expression nl = translate(l, ff);
 		Expression nr = translate(r, ff);
 
-		if(nl == l && nr == r) return pred;
-		else
-			return  ff.makeRelationalPredicate(pred.getTag(), nl, nr, loc);
+		if (nl == l && nr == r) {
+			return pred;
+		}
+		return  ff.makeRelationalPredicate(pred.getTag(), nl, nr, loc);
 	}
 	
 	protected Predicate idTransQuantifiedPredicate(
 			Predicate pred, FormulaFactory ff, BoundIdentDecl[] is, Predicate P) {
 
 		SourceLocation loc = pred.getSourceLocation();
-
 		Predicate nP = translate(P, ff);
   		
-  		if(nP == P) return pred;
-  		else
-			return ff.makeQuantifiedPredicate(pred.getTag(), is, nP, loc);	
+  		if (nP == P) {
+  			return pred;
+  		}
+  		return ff.makeQuantifiedPredicate(pred.getTag(), is, nP, loc);	
 	}
 	
 }
