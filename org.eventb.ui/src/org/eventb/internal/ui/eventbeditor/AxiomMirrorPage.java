d49f40f34e97a1f84c9d51d39fc9c261f468a8fe
Thai Son Hoang <tshoang@users.sourceforge.net>
2006-05-20 16:41:38 +0000

    Code cleaning

--- org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/AxiomMirrorPage.java
+++ org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/AxiomMirrorPage.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2005 ETH-Zurich
+ * Copyright (c) 2005-2006 ETH Zurich.
+ * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     ETH RODIN Group
- *******************************************************************************/
+ *     Rodin @ ETH Zurich
+ ******************************************************************************/
 
 package org.eventb.internal.ui.eventbeditor;
 
@@ -19,49 +20,45 @@ import org.rodinp.core.IInternalElement;
 import org.rodinp.core.IRodinElement;
 import org.rodinp.core.RodinDBException;
 
-
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
- * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
- * <p>
+ * @author htson
+ *         <p>
+ *         This class is an implementation of a Axiom Mirror 'page'.
  */
+public class AxiomMirrorPage extends EventBMirrorPage implements
+		IAxiomMirrorPage {
 
-public class AxiomMirrorPage
-	extends EventBMirrorPage
-	implements	IAxiomMirrorPage
-{
-
+	/**
+	 * Constructor.
+	 * <p>
+	 * 
+	 * @param editor
+	 *            An Event-B Editor.
+	 */
 	public AxiomMirrorPage(EventBEditor editor) {
 		super(editor);
-	}   
-    
-	/**
-	 * Return the form (XML formatted) string that represents the information 
-	 * of the axioms.
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eventb.internal.ui.eventbeditor.EventBMirrorPage#getFormString()
 	 */
 	protected String getFormString() {
 		String formString = "<form>";
 		try {
-			IRodinElement [] axioms = editor.getRodinInput().getChildrenOfType(IAxiom.ELEMENT_TYPE);
+			IRodinElement[] axioms = editor.getRodinInput().getChildrenOfType(
+					IAxiom.ELEMENT_TYPE);
 			for (int i = 0; i < axioms.length; i++) {
-				formString = formString + "<li style=\"bullet\">" + UIUtils.makeHyperlink(axioms[i].getElementName()) + ": ";
-				formString = formString + UIUtils.XMLWrapUp(((IInternalElement) axioms[i]).getContents()); 
+				formString = formString + "<li style=\"bullet\">"
+						+ UIUtils.makeHyperlink(axioms[i].getElementName())
+						+ ": ";
+				formString = formString
+						+ UIUtils.XMLWrapUp(((IInternalElement) axioms[i])
+								.getContents());
 				formString = formString + "</li>";
 			}
-		}
-		catch (RodinDBException e) {
+		} catch (RodinDBException e) {
 			// TODO Exception handle
 			e.printStackTrace();
 		}
@@ -69,24 +66,25 @@ public class AxiomMirrorPage
 
 		return formString;
 	}
-	
-	
-	/**
-	 * Return the hyperlink listener which enable the navigation on the form. 
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eventb.internal.ui.eventbeditor.EventBMirrorPage#createHyperlinkListener()
 	 */
 	protected HyperlinkAdapter createHyperlinkListener() {
 		return (new HyperlinkAdapter() {
 			public void linkActivated(HyperlinkEvent e) {
 				try {
-					IRodinElement [] axioms = editor.getRodinInput().getChildrenOfType(IAxiom.ELEMENT_TYPE);
+					IRodinElement[] axioms = editor.getRodinInput()
+							.getChildrenOfType(IAxiom.ELEMENT_TYPE);
 					for (int i = 0; i < axioms.length; i++) {
 						if (e.getHref().equals(axioms[i].getElementName())) {
 							editor.setSelection(axioms[i]);
 							break;
 						}
 					}
-				}
-				catch (RodinDBException exception) {
+				} catch (RodinDBException exception) {
 					// TODO Exception handle
 					exception.printStackTrace();
 				}
--- org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/AxiomPage.java
+++ org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/AxiomPage.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2005 ETH-Zurich
+ * Copyright (c) 2005-2006 ETH Zurich.
+ * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     ETH RODIN Group
- *******************************************************************************/
+ *     Rodin @ ETH Zurich
+ ******************************************************************************/
 
 package org.eventb.internal.ui.eventbeditor;
 
@@ -20,35 +21,42 @@ import org.eclipse.ui.forms.widgets.Section;
 
 /**
  * @author htson
- * <p>
- * An implementation of the Event-B Form Page
- * for editing Axioms (Rodin elements)
+ *         <p>
+ *         An implementation of the Event-B Form Page for editing Axioms (Rodin
+ *         elements)
  */
-public class AxiomPage
-	extends EventBFormPage
-{
-	
+public class AxiomPage extends EventBFormPage {
+
 	// Title, tab title and ID of the page.
 	public static final String PAGE_ID = "Axioms"; //$NON-NLS-1$
+
 	public static final String PAGE_TITLE = "Axioms";
+
 	public static final String PAGE_TAB_TITLE = "Axioms";
-	
-	
+
 	/**
 	 * Constructor.
-	 * @param editor The form editor that holds the page 
+	 * 
+	 * @param editor
+	 *            The form editor that holds the page
 	 */
 	public AxiomPage(FormEditor editor) {
-		super(editor, PAGE_ID, PAGE_TITLE, PAGE_TAB_TITLE);  //$NON-NLS-1$
+		super(editor, PAGE_ID, PAGE_TITLE, PAGE_TAB_TITLE); //$NON-NLS-1$
 	}
-	
-	
-	/* (non-Javadoc)
-	 * @see org.eventb.internal.ui.eventbeditor.EventBFormPage#createMasterSection(org.eclipse.ui.forms.IManagedForm, org.eclipse.swt.widgets.Composite, int, org.eventb.internal.ui.eventbeditor.EventBEditor)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eventb.internal.ui.eventbeditor.EventBFormPage#createMasterSection(org.eclipse.ui.forms.IManagedForm,
+	 *      org.eclipse.swt.widgets.Composite, int,
+	 *      org.eventb.internal.ui.eventbeditor.EventBEditor)
 	 */
-	@Override
-	protected EventBPartWithButtons createMasterSection(IManagedForm managedForm, Composite parent, int style, EventBEditor editor) {
-		AxiomMasterSection part = new AxiomMasterSection(managedForm, parent, managedForm.getToolkit(), Section.NO_TITLE, (EventBEditor) this.getEditor());
+	protected EventBPartWithButtons createMasterSection(
+			IManagedForm managedForm, Composite parent, int style,
+			EventBEditor editor) {
+		AxiomMasterSection part = new AxiomMasterSection(managedForm, parent,
+				managedForm.getToolkit(), Section.NO_TITLE, (EventBEditor) this
+						.getEditor());
 		GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true);
 		gd.heightHint = 200;
 		gd.minimumHeight = 150;
@@ -56,5 +64,5 @@ public class AxiomPage
 		part.getSection().setLayoutData(gd);
 		return part;
 	}
-	
+
 }
