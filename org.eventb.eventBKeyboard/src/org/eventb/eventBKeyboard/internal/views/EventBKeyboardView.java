a0853f721c31a3cf6bc94c9c465175999d8c89da
Thai Son Hoang <tshoang@users.sourceforge.net>
2006-05-24 13:28:50 +0000

    Code cleaning

--- org.eventb.eventBKeyboard/src/org/eventb/eventBKeyboard/internal/views/EventBKeyboardView.java
+++ org.eventb.eventBKeyboard/src/org/eventb/eventBKeyboard/internal/views/EventBKeyboardView.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2005 ETH-Zurich
+ * Copyright (c) 2005 ETH Zurich.
+ * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     ETH RODIN Group
- *******************************************************************************/
+ *     Rodin @ ETH Zurich
+ ******************************************************************************/
 
 package org.eventb.eventBKeyboard.internal.views;
 
@@ -26,19 +27,17 @@ import org.eventb.eventBKeyboard.EventBTextModifyListener;
 import org.eventb.eventBKeyboard.preferences.PreferenceConstants;
 
 /**
- * @author htson
- * This simple view allow you to enter a mathematical expression of Event-B
- * language and translate that into Unicode.
- * <p>
- * Happy typing !!!
+ * @author htson<p> This simple view allow you to enter a mathematical expression
+ *         of Event-B language and translate that into Unicode.
+ *         <p>
+ *         Happy typing !!!
  */
-public class EventBKeyboardView
-	extends ViewPart 
-	implements IPropertyChangeListener
-{
+public class EventBKeyboardView extends ViewPart implements
+		IPropertyChangeListener {
 	private Text formula;
-	private EventBTextModifyListener listener; 
-	
+
+	private EventBTextModifyListener listener;
+
 	/**
 	 * The constructor.
 	 */
@@ -47,53 +46,63 @@ public class EventBKeyboardView
 	}
 
 	/**
-	 * This is a callback that will allow us
-	 * to create the text area and the check button and initialize it.
+	 * This is a callback that will allow us to create the text area and the
+	 * check button and initialize it.
 	 */
 	public void createPartControl(Composite parent) {
 		GridLayout layout = new GridLayout();
-        layout.marginWidth = 5;
-        layout.marginHeight = 5;
-        layout.numColumns = 3;
-        layout.verticalSpacing = 5;
+		layout.marginWidth = 5;
+		layout.marginHeight = 5;
+		layout.numColumns = 3;
+		layout.verticalSpacing = 5;
 		parent.setLayout(layout);
 
-		Label label = new Label(parent, SWT.NULL); 
-        label.setText("Formula:");
+		Label label = new Label(parent, SWT.NULL);
+		label.setText("Formula:");
 		label.setToolTipText("Enter an Event-B formula");
-		formula = new Text(parent, SWT.MULTI | SWT.V_SCROLL |SWT.BORDER |SWT.H_SCROLL);
+		formula = new Text(parent, SWT.MULTI | SWT.V_SCROLL | SWT.BORDER
+				| SWT.H_SCROLL);
 		GridData gD = new GridData(GridData.FILL_BOTH);
-        formula.setLayoutData(gD);
-        listener = new EventBTextModifyListener();
-        formula.addModifyListener(listener);
-        
-        // Using a special fonts for showing Event-B symbols.
-        Font font = JFaceResources.getFont(PreferenceConstants.EVENTB_MATH_FONT);
-        formula.setFont(font);
+		formula.setLayoutData(gD);
+		listener = new EventBTextModifyListener();
+		formula.addModifyListener(listener);
+
+		// Using a special fonts for showing Event-B symbols.
+		Font font = JFaceResources
+				.getFont(PreferenceConstants.EVENTB_MATH_FONT);
+		formula.setFont(font);
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.util.IPropertyChangeListener#propertyChange(org.eclipse.jface.util.PropertyChangeEvent)
+	 */
 	public void propertyChange(PropertyChangeEvent event) {
 		if (event.getProperty().equals(PreferenceConstants.EVENTB_MATH_FONT)) {
-			Font font = JFaceResources.getFont(PreferenceConstants.EVENTB_MATH_FONT);
+			Font font = JFaceResources
+					.getFont(PreferenceConstants.EVENTB_MATH_FONT);
 			formula.setFont(font);
 		}
 	}
-	
+
 	/**
 	 * Passing the focus request to the viewer's control.
 	 */
 	public void setFocus() {
 		formula.setFocus();
 	}
-	
+
 	/**
-	 * @return The Text inside the View 
+	 * @return The Text inside the View
 	 */
-	public Text getFormula() {return formula;}
+	public Text getFormula() {
+		return formula;
+	}
 
 	/**
 	 * @return The ModifyListener associate with the Text
 	 */
-	public EventBTextModifyListener getListener() {return listener;}
+	public EventBTextModifyListener getListener() {
+		return listener;
+	}
 
 }
--- org.eventb.eventBKeyboard/src/org/eventb/eventBKeyboard/preferences/PreferenceConstants.java
+++ org.eventb.eventBKeyboard/src/org/eventb/eventBKeyboard/preferences/PreferenceConstants.java
@@ -1,22 +1,28 @@
 /*******************************************************************************
- * Copyright (c) 2005 ETH-Zurich
+ * Copyright (c) 2005 ETH Zurich.
+ * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     ETH RODIN Group
- *******************************************************************************/
+ *     Rodin @ ETH Zurich
+ ******************************************************************************/
 
 package org.eventb.eventBKeyboard.preferences;
 
 /**
  * @author htson
- * Constant definitions for plug-in preferences
+ *         <p>
+ *         Constant definitions for Event-B Text font preference.
  */
 public class PreferenceConstants {
 
+	/**
+	 * Property identifier for Event-B Text font, value
+	 * <code>org.eventb.eventBKeyboard.textFont</code>.
+	 */
 	public static final String EVENTB_MATH_FONT = "org.eventb.eventBKeyboard.textFont";
-	
+
 }
