b32adf789e1323eca97633223597efecb7bf2c31
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-05-29 17:09:58 +0000

    Added processing of debugging options.

--- org.eventb.core.seqprover/src/org/eventb/core/prover/SequentProver.java
+++ org.eventb.core.seqprover/src/org/eventb/core/prover/SequentProver.java
@@ -1,6 +1,8 @@
 package org.eventb.core.prover;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Plugin;
+import org.eventb.core.prover.externalReasoners.classicB.ClassicB;
 import org.eventb.core.prover.rules.ProofTree;
 import org.eventb.core.prover.sequent.IProverSequent;
 import org.osgi.framework.BundleContext;
@@ -10,7 +12,10 @@ import org.osgi.framework.BundleContext;
  */
 public class SequentProver extends Plugin {
 
-	public static final String PLUGIN_ID = "org.eventb.core.seqprover";
+	public static final String PLUGIN_ID = "org.eventb.core.seqprover"; //$NON-NLS-1$
+	
+	private static final String B4FREE_TRACE = PLUGIN_ID + "/debug/b4free"; //$NON-NLS-1$
+
 	
 	//The shared instance.
 	private static SequentProver plugin;
@@ -32,6 +37,19 @@ public class SequentProver extends Plugin {
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
+		
+		configureDebugOptions();
+	}
+
+	/**
+	 * Process debugging/tracing options coming from Eclipse.
+	 */
+	private void configureDebugOptions() {
+		if (isDebugging()) {
+			String option = Platform.getDebugOption(B4FREE_TRACE);
+			if (option != null)
+				ClassicB.DEBUG = option.equalsIgnoreCase("true"); //$NON-NLS-1$
+		}
 	}
 
 	/**
