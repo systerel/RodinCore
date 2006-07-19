2494b6dfa5fe2065559542b228fa3591b806eb69
Thai Son Hoang <tshoang@users.sourceforge.net>
2006-07-19 18:14:54 +0000

    Added debug utility methods.

--- org.eventb.eventBKeyboard/src/org/eventb/internal/eventBKeyboard/KeyboardUtils.java
+++ org.eventb.eventBKeyboard/src/org/eventb/internal/eventBKeyboard/KeyboardUtils.java
@@ -6,4 +6,12 @@ public class KeyboardUtils {
 	
 	public static boolean MATH_DEBUG = false;
 	
+	public static void debugText(String str) {
+		if (TEXT_DEBUG) System.out.println(str);
+	}
+	
+	public static void debugMath(String str) {
+		if (MATH_DEBUG) System.out.println(str);
+	}
+	
 }
