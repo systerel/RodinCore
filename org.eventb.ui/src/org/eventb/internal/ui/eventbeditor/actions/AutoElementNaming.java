bd607e7003ccb4d7e7138001d7ad68a456e6696e
Thai Son Hoang <tshoang@users.sourceforge.net>
2006-07-24 19:18:24 +0000

    - Create an abstract class for automatically naming elements. - Added auto theorem naming.

---
+++ org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/actions/AutoElementNaming.java
@@ -0,0 +1,71 @@
+package org.eventb.internal.ui.eventbeditor.actions;
+
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+import org.eventb.internal.ui.UIUtils;
+import org.eventb.internal.ui.eventbeditor.EventBEditor;
+import org.rodinp.core.IInternalElement;
+import org.rodinp.core.IRodinElement;
+import org.rodinp.core.IRodinFile;
+import org.rodinp.core.RodinCore;
+
+public abstract class AutoElementNaming implements IEditorActionDelegate {
+
+	EventBEditor editor;
+
+	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		if (targetEditor instanceof EventBEditor)
+			editor = (EventBEditor) targetEditor;
+	}
+
+	public void rename(final String type, final String prefix) {
+		try {
+			RodinCore.run(new IWorkspaceRunnable() {
+
+				public void run(IProgressMonitor monitor) throws CoreException {
+					IRodinFile file = editor.getRodinInput();
+					IRodinElement[] elements = file
+							.getChildrenOfType(type);
+
+					// Dummy name first
+					for (IRodinElement element : elements) {
+						UIUtils.debugEventBEditor("Rename: "
+								+ element.getElementName() + " to ___"
+								+ element.getElementName());
+
+						((IInternalElement) element).rename("___"
+								+ element.getElementName(), false, null);
+					}
+
+					elements = file
+							.getChildrenOfType(type);
+
+					// Rename to the real desired naming convention
+					for (int counter = 1; counter <= elements.length; counter++) {
+						IRodinElement element = elements[counter-1];
+						UIUtils.debugEventBEditor("Rename: "
+								+ element.getElementName() + " to " + prefix + 
+								+ counter);
+						((IInternalElement) element).rename(prefix + counter, false,
+								null);
+					}
+				}
+
+			}, null);
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		return; // Do nothing
+	}
+
+}
--- org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/actions/AutoThmNaming.java
+++ org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/actions/AutoThmNaming.java
@@ -1,12 +1,12 @@
 package org.eventb.internal.ui.eventbeditor.actions;
 
 import org.eclipse.jface.action.IAction;
-import org.eventb.core.IInvariant;
+import org.eventb.core.ITheorem;
 
-public class AutoInvNaming extends AutoElementNaming {
+public class AutoThmNaming extends AutoElementNaming {
 
 	public void run(IAction action) {
-		rename(IInvariant.ELEMENT_TYPE, "inv");
+		rename(ITheorem.ELEMENT_TYPE, "thm");
 	}
 
 }
