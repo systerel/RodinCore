bc5a8cd2ac1975da780d7d86260835a86598b6eb
Thai Son Hoang <tshoang@users.sourceforge.net>
2006-07-26 12:18:37 +0000

    Implementing the Observer Pattern using synchronized and ISafeRunnable.

--- org.eventb.core/src/org/eventb/core/pm/UserSupportManager.java
+++ org.eventb.core/src/org/eventb/core/pm/UserSupportManager.java
@@ -12,24 +12,26 @@
 
 package org.eventb.core.pm;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 
+import org.eclipse.core.runtime.ISafeRunnable;
+import org.eclipse.core.runtime.Platform;
 import org.eventb.core.IPRFile;
 import org.rodinp.core.RodinDBException;
 
 public class UserSupportManager {
 
-	private static Collection<IUSManagerListener> listeners = new HashSet<IUSManagerListener>();
+	private static Collection<IUSManagerListener> listeners = new ArrayList<IUSManagerListener>();
 
-	private static Collection<UserSupport> userSupports = new HashSet<UserSupport>();
+	private static Collection<UserSupport> userSupports = new ArrayList<UserSupport>();
 
 	public static int REMOVED = 0x1;
-	
+
 	public static int ADDED = 0x2;
-	
+
 	public static int CHANGED = 0x4;
-	
+
 	public static UserSupport newUserSupport() {
 		UserSupport userSupport = new UserSupport();
 		userSupports.add(userSupport);
@@ -39,7 +41,11 @@ public class UserSupportManager {
 
 	public static void disposeUserSupport(UserSupport userSupport) {
 		userSupport.dispose();
-		userSupports.remove(userSupport);
+		synchronized (userSupports) {
+			if (userSupports.contains(userSupport))
+				userSupports.remove(userSupport);
+		}
+
 		notifyUSManagerListener(userSupport, REMOVED);
 	}
 
@@ -48,21 +54,43 @@ public class UserSupportManager {
 	}
 
 	public static void addUSManagerListener(IUSManagerListener listener) {
-		listeners.add(listener);
+		synchronized (listeners) {
+			if (!listeners.contains(listener))
+				listeners.add(listener);
+		}
+
 	}
 
 	public static void removeUSManagerListener(IUSManagerListener listener) {
-		listeners.remove(listener);
-	}
-
-	private static void notifyUSManagerListener(UserSupport userSupport,
-			int status) {
-		for (IUSManagerListener listener : listeners) {
-			listener.USManagerChanged(userSupport, status);
+		synchronized (listeners) {
+			if (listeners.contains(listener))
+				listeners.remove(listener);
 		}
 	}
 
-	public static void setInput(UserSupport userSupport, IPRFile prFile) throws RodinDBException {
+	private static void notifyUSManagerListener(final UserSupport userSupport,
+			final int status) {
+		IUSManagerListener[] safeCopy;
+		synchronized (listeners) {
+			safeCopy = listeners.toArray(new IUSManagerListener[listeners
+					.size()]);
+		}
+		for (final IUSManagerListener listener : safeCopy) {
+			Platform.run(new ISafeRunnable() {
+				public void handleException(Throwable exception) {
+					// do nothing, will be logged by the platform
+				}
+
+				public void run() throws Exception {
+					listener.USManagerChanged(userSupport, status);
+				}
+			});
+		}
+
+	}
+
+	public static void setInput(UserSupport userSupport, IPRFile prFile)
+			throws RodinDBException {
 		userSupport.setInput(prFile);
 		notifyUSManagerListener(userSupport, CHANGED);
 	}
