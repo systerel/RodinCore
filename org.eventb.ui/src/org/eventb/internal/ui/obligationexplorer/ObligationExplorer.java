90be5685c2daf46138531d2bb101bf4f4beca5b1
Thai Son Hoang <tshoang@users.sourceforge.net>
2006-07-27 15:47:38 +0000

    Modified PO has yellow background in the Obligation Explorer view.

--- org.eventb.ui/src/org/eventb/internal/ui/obligationexplorer/ObligationExplorer.java
+++ org.eventb.ui/src/org/eventb/internal/ui/obligationexplorer/ObligationExplorer.java
@@ -20,10 +20,15 @@ import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
+import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.ITableColorProvider;
+import org.eclipse.jface.viewers.ITableFontProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
@@ -32,6 +37,8 @@ import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FormAttachment;
@@ -45,6 +52,8 @@ import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
@@ -84,6 +93,7 @@ import org.rodinp.core.RodinDBException;
 public class ObligationExplorer extends ViewPart implements
 		ISelectionChangedListener, IUSManagerListener,
 		IProofStateChangedListener {
+	private TreeColumn column;
 
 	/**
 	 * The plug-in identifier of the Obligation Explorer (value
@@ -105,27 +115,27 @@ public class ObligationExplorer extends ViewPart implements
 	private boolean byExternal;
 
 	private ToolItem exclude;
-	
+
 	private ToolItem discharge;
 
 	private Text filterText;
 
 	private static int NULL = 0;
-	
+
 	private static int UNATTEMPTED = 1;
-	
+
 	private static int PENDING_BROKEN = 2;
-	
+
 	private static int PENDING = 3;
-	
+
 	private static int REVIEWED_BROKEN = 4;
-	
+
 	private static int REVIEWED = 5;
-	
+
 	private static int DISCHARGED_BROKEN = 6;
-	
+
 	private static int DISCHARGED;
-	
+
 	private class ObligationTextFilter extends ViewerFilter {
 
 		@Override
@@ -135,7 +145,7 @@ public class ObligationExplorer extends ViewPart implements
 			boolean selection = exclude.getSelection();
 			if (element instanceof PRSequent) {
 				PRSequent sequent = (PRSequent) element;
-				
+
 				if (sequent.getName().indexOf(filterText.getText()) == -1)
 					return selection;
 				else
@@ -150,7 +160,8 @@ public class ObligationExplorer extends ViewPart implements
 	private class DischargedFilter extends ViewerFilter {
 
 		@Override
-		public boolean select(Viewer viewer, Object parentElement, Object element) {
+		public boolean select(Viewer viewer, Object parentElement,
+				Object element) {
 			boolean selection = discharge.getSelection();
 			if (element instanceof IPRSequent) {
 				IPRSequent sequent = (IPRSequent) element;
@@ -161,8 +172,7 @@ public class ObligationExplorer extends ViewPart implements
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				
+
 				if (status == DISCHARGED)
 					return !selection;
 				else
@@ -170,11 +180,11 @@ public class ObligationExplorer extends ViewPart implements
 			} else {
 				return true;
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	private int getStatus(IPRSequent sequent) throws RodinDBException {
 		// Try to synchronize with the proof tree in memory
 		Collection<UserSupport> userSupports = UserSupportManager
@@ -182,8 +192,7 @@ public class ObligationExplorer extends ViewPart implements
 		for (UserSupport userSupport : userSupports) {
 			// UIUtils.debugObligationExplorer("Get US: "
 			// + userSupport);
-			Collection<ProofState> proofStates = userSupport
-					.getPOs();
+			Collection<ProofState> proofStates = userSupport.getPOs();
 			for (ProofState proofState : proofStates) {
 				if (proofState.getPRSequent().equals(sequent)) {
 					IProofTree tree = proofState.getProofTree();
@@ -222,7 +231,7 @@ public class ObligationExplorer extends ViewPart implements
 
 		int confidence = sequent.getProofTree().getConfidence();
 		if (sequent.isProofBroken()) {
-			
+
 			if (confidence == IConfidence.PENDING)
 				return PENDING_BROKEN;
 			if (confidence <= IConfidence.REVIEWED_MAX)
@@ -231,7 +240,7 @@ public class ObligationExplorer extends ViewPart implements
 				return DISCHARGED_BROKEN;
 
 		} else {
-	
+
 			if (confidence == IConfidence.PENDING)
 				return PENDING;
 			if (confidence <= IConfidence.REVIEWED_MAX)
@@ -283,57 +292,16 @@ public class ObligationExplorer extends ViewPart implements
 	 *         <p>
 	 *         This class provides the label for object in the tree.
 	 */
-	private class ObligationLabelProvider extends LabelProvider {
+	private class ObligationLabelProvider implements ITableLabelProvider,
+			ITableFontProvider, ITableColorProvider, IPropertyChangeListener {
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.eclipse.jface.viewers.ILabelProvider#getText(java.lang.Object)
-		 */
-		public String getText(Object obj) {
-			if (obj instanceof IRodinProject)
-				return ((IRodinProject) obj).getElementName();
-			if (obj instanceof IRodinFile) {
-				String name = ((IRodinFile) obj).getElementName();
-				return EventBPlugin.getComponentName(name);
-			}
-			if (obj instanceof IPRSequent) {
-				// UIUtils.debugObligationExplorer("Label for: " + obj);
-
-				// Find the label in the list of UserSupport.
-				Collection<UserSupport> userSupports = UserSupportManager
-						.getUserSupports();
-				for (UserSupport userSupport : userSupports) {
-					// UIUtils.debugObligationExplorer("Get US: " +
-					// userSupport);
-					Collection<ProofState> proofStates = userSupport.getPOs();
-					for (ProofState proofState : proofStates) {
-						if (proofState.getPRSequent().equals(obj)) {
-							if (proofState.isDirty())
-								return "* " + ((IPRSequent) obj).getName();
-							else
-								return ((IPRSequent) obj).getName();
-						}
-					}
-				}
-				return ((IPRSequent) obj).getName();
-			}
-
-			return obj.toString();
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.eclipse.jface.viewers.ILabelProvider#getImage(java.lang.Object)
-		 */
-		public Image getImage(Object obj) {
+		public Image getColumnImage(Object obj, int columnIndex) {
 			ImageRegistry registry = EventBUIPlugin.getDefault()
 					.getImageRegistry();
 			if (obj instanceof IPRSequent) {
 				IPRSequent prSequent = (IPRSequent) obj;
 				try {
-					
+
 					// Try to synchronize with the proof tree in memory
 					Collection<UserSupport> userSupports = UserSupportManager
 							.getUserSupports();
@@ -387,7 +355,7 @@ public class ObligationExplorer extends ViewPart implements
 
 					int confidence = prSequent.getProofTree().getConfidence();
 					if (prSequent.isProofBroken()) {
-						
+
 						if (confidence == IConfidence.PENDING)
 							return registry.get(EventBImage.IMG_PENDING_BROKEN);
 						if (confidence <= IConfidence.REVIEWED_MAX)
@@ -398,7 +366,7 @@ public class ObligationExplorer extends ViewPart implements
 									.get(EventBImage.IMG_DISCHARGED_BROKEN);
 
 					} else {
-				
+
 						if (confidence == IConfidence.PENDING)
 							return registry.get(EventBImage.IMG_PENDING);
 						if (confidence <= IConfidence.REVIEWED_MAX)
@@ -429,6 +397,109 @@ public class ObligationExplorer extends ViewPart implements
 			}
 			return UIUtils.getImage(obj);
 		}
+
+		public String getColumnText(Object obj, int columnIndex) {
+			 UIUtils.debugObligationExplorer("Label for: " + obj);
+			 if (obj instanceof IRodinProject) {
+				UIUtils.debugObligationExplorer("Project: "
+						+ ((IRodinProject) obj).getElementName());
+				return ((IRodinProject) obj).getElementName();
+			} else if (obj instanceof IRodinFile) {
+				String name = ((IRodinFile) obj).getElementName();
+				return EventBPlugin.getComponentName(name);
+			} else if (obj instanceof IPRSequent) {
+
+
+				// Find the label in the list of UserSupport.
+				Collection<UserSupport> userSupports = UserSupportManager
+						.getUserSupports();
+				for (UserSupport userSupport : userSupports) {
+					// UIUtils.debugObligationExplorer("Get US: " +
+					// userSupport);
+					Collection<ProofState> proofStates = userSupport.getPOs();
+					for (ProofState proofState : proofStates) {
+						if (proofState.getPRSequent().equals(obj)) {
+							if (proofState.isDirty())
+								return "* " + ((IPRSequent) obj).getName();
+							else
+								return ((IPRSequent) obj).getName();
+						}
+					}
+				}
+				return ((IPRSequent) obj).getName();
+			}
+
+			return obj.toString();
+		}
+
+		public void addListener(ILabelProviderListener listener) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void dispose() {
+			// TODO Auto-generated method stub
+
+		}
+
+		public boolean isLabelProperty(Object element, String property) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		public void removeListener(ILabelProviderListener listener) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public Font getFont(Object element, int columnIndex) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public Color getForeground(Object element, int columnIndex) {
+			Display display = Display.getCurrent();
+			return display.getSystemColor(SWT.COLOR_BLACK);
+		}
+
+		public Color getBackground(Object obj, int columnIndex) {
+			Display display = Display.getCurrent();
+			Color white = display.getSystemColor(SWT.COLOR_WHITE);
+			Color yellow = display.getSystemColor(SWT.COLOR_YELLOW);
+			if (obj instanceof IRodinProject)
+				return white;
+			if (obj instanceof IRodinFile) {
+				return white;
+			}
+			if (obj instanceof IPRSequent) {
+				// UIUtils.debugObligationExplorer("Label for: " + obj);
+
+				// Find the label in the list of UserSupport.
+				Collection<UserSupport> userSupports = UserSupportManager
+						.getUserSupports();
+				for (UserSupport userSupport : userSupports) {
+					// UIUtils.debugObligationExplorer("Get US: " +
+					// userSupport);
+					Collection<ProofState> proofStates = userSupport.getPOs();
+					for (ProofState proofState : proofStates) {
+						if (proofState.getPRSequent().equals(obj)) {
+							if (proofState.isDirty())
+								return yellow;
+							else
+								return white;
+						}
+					}
+				}
+				return white;
+			}
+
+			return white;
+		}
+
+		public void propertyChange(PropertyChangeEvent event) {
+			// TODO Auto-generated method stub
+
+		}
 	}
 
 	CoolItem createToolItem(CoolBar coolBar) {
@@ -443,13 +514,14 @@ public class ObligationExplorer extends ViewPart implements
 				UIUtils.debugObligationExplorer("Status "
 						+ exclude.getSelection());
 				viewer.refresh();
+				column.pack();
 			}
 
 			public void widgetDefaultSelected(SelectionEvent e) {
 				widgetSelected(e);
-			}			
+			}
 		});
-		
+
 		exclude = new ToolItem(toolBar, SWT.CHECK);
 		exclude.setText("ex");
 		exclude.addSelectionListener(new SelectionListener() {
@@ -459,6 +531,7 @@ public class ObligationExplorer extends ViewPart implements
 				UIUtils.debugObligationExplorer("Status "
 						+ exclude.getSelection());
 				viewer.refresh();
+				column.pack();
 			}
 
 			public void widgetDefaultSelected(SelectionEvent e) {
@@ -519,8 +592,7 @@ public class ObligationExplorer extends ViewPart implements
 				| SWT.V_SCROLL);
 		viewer.setContentProvider(new ObligationExplorerContentProvider(this));
 		viewer.setLabelProvider(new ObligationLabelProvider());
-		// viewer.setSorter(new ProjectsSorter());
-		viewer.setInput(EventBUIPlugin.getRodinDatabase());
+//		viewer.setSorter(new ProjectsSorter());
 		viewer.addFilter(new ObligationTextFilter());
 		viewer.addFilter(new DischargedFilter());
 		FormData textData = new FormData();
@@ -529,6 +601,11 @@ public class ObligationExplorer extends ViewPart implements
 		textData.top = new FormAttachment(coolBar);
 		textData.bottom = new FormAttachment(100);
 		viewer.getControl().setLayoutData(textData);
+		Tree tree = viewer.getTree();
+		tree.setHeaderVisible(false);
+		column = new TreeColumn(tree, SWT.LEFT);
+		viewer.setInput(EventBUIPlugin.getRodinDatabase());
+		column.pack();
 
 		// Sync with the current active ProverUI
 		IWorkbenchPage activePage = EventBUIPlugin.getActivePage();
@@ -630,6 +707,7 @@ public class ObligationExplorer extends ViewPart implements
 	 */
 	public void refresh() {
 		viewer.refresh();
+		column.pack();
 	}
 
 	/*
@@ -725,6 +803,7 @@ public class ObligationExplorer extends ViewPart implements
 				final UserSupport userSupport = delta.getSource();
 				if (userSupport.isOutOfDate()) {
 					viewer.refresh(userSupport.getInput(), true);
+					column.pack();
 					return;
 				}
 				if (ps != null) {
@@ -735,6 +814,7 @@ public class ObligationExplorer extends ViewPart implements
 						ProofState state = userSupport.getCurrentPO();
 						final IPRSequent prSequent = state.getPRSequent();
 						viewer.refresh(prSequent, true);
+						column.pack();
 					}
 				}
 			}
