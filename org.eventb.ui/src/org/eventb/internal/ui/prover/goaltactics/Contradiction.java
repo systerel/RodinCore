307a225b936780dc881d20d1e167e1fdfb2f404c
Farhad Mehta <mehta@users.sourceforge.net>
2006-08-11 15:24:46 +0000

    Added more checks for isApplicable

--- org.eventb.ui/src/org/eventb/internal/ui/prover/goaltactics/Contradiction.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/goaltactics/Contradiction.java
@@ -1,6 +1,9 @@
 package org.eventb.internal.ui.prover.goaltactics;
 
+import org.eventb.core.ast.Predicate;
 import org.eventb.core.prover.IProofTreeNode;
+import org.eventb.core.prover.Lib;
+import org.eventb.core.prover.sequent.Hypothesis;
 import org.eventb.core.prover.tactics.ITactic;
 import org.eventb.core.prover.tactics.Tactics;
 import org.eventb.ui.prover.IGoalTactic;
@@ -8,6 +11,13 @@ import org.eventb.ui.prover.IGoalTactic;
 public class Contradiction implements IGoalTactic {
 
 	public boolean isApplicable(IProofTreeNode node) {
+		Predicate goal = node.getSequent().goal();
+		if (goal.equals(Lib.False)) return false;
+		Predicate negGoal = Lib.makeNeg(goal);
+		if (negGoal.equals(Lib.True)) return false;
+		if (Hypothesis.containsPredicate(
+				node.getSequent().selectedHypotheses(),
+				negGoal));
 		return true;
 	}
 
