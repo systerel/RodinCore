5eab5a0f0c271d50c21ed9ee2bae48a506ce51d5
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-08-12 12:45:13 +0000

    This interface is now deprecated.

--- org.rodinp.core/src/org/rodinp/core/IUnnamedInternalElement.java
+++ org.rodinp.core/src/org/rodinp/core/IUnnamedInternalElement.java
@@ -4,8 +4,9 @@ package org.rodinp.core;
  * Common protocol for all unnamed internal elements.
  * 
  * @author Laurent Voisin
+ * @deprecated Don't use this interface anymore.
  */
-// TODO document IUnnamedInternalElement
+@Deprecated
 public interface IUnnamedInternalElement extends IInternalElement {
 	// No additional methods
 }
