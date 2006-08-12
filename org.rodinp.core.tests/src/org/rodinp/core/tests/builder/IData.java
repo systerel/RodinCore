04ee85526e2af1b6f4058e86d438b10a43fbb933
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-08-12 16:01:27 +0000

    Removed any unnamed internal element from the tests.

--- org.rodinp.core.tests/src/org/rodinp/core/tests/builder/IData.java
+++ org.rodinp.core.tests/src/org/rodinp/core/tests/builder/IData.java
@@ -1,8 +1,8 @@
 package org.rodinp.core.tests.builder;
 
-import org.rodinp.core.IUnnamedInternalElement;
+import org.rodinp.core.IInternalElement;
 
-public interface IData extends IUnnamedInternalElement {
+public interface IData extends IInternalElement {
 
 	public static final String ELEMENT_TYPE = "org.rodinp.core.tests.data";
 
--- org.rodinp.core.tests/src/org/rodinp/core/tests/builder/basis/Data.java
+++ org.rodinp.core.tests/src/org/rodinp/core/tests/builder/basis/Data.java
@@ -1,13 +1,18 @@
 package org.rodinp.core.tests.builder.basis;
 
 import org.rodinp.core.IRodinElement;
-import org.rodinp.core.basis.UnnamedInternalElement;
+import org.rodinp.core.basis.InternalElement;
 import org.rodinp.core.tests.builder.IData;
 
-public class Data extends UnnamedInternalElement implements IData {
+public class Data extends InternalElement implements IData {
 	
-	public Data(IRodinElement parent) {
-		super(ELEMENT_TYPE, parent);
+	public Data(String name, IRodinElement parent) {
+		super(name, parent);
+	}
+
+	@Override
+	public String getElementType() {
+		return ELEMENT_TYPE;
 	}
 
 }
