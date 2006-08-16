1ed460dc805999e852ea8d7743d36f495f27025a
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-08-16 18:41:59 +0000

    Made internal class static.

--- org.rodinp.core.tests/src/org/rodinp/core/tests/AbstractRodinDBTests.java
+++ org.rodinp.core.tests/src/org/rodinp/core/tests/AbstractRodinDBTests.java
@@ -93,7 +93,7 @@ public abstract class AbstractRodinDBTests extends TestCase {
 	/**
 	 * Delta listener
 	 */
-	protected class DeltaListener implements IElementChangedListener {
+	protected static class DeltaListener implements IElementChangedListener {
 		/**
 		 * Deltas received from the Rodin database. See 
 		 * <code>#startDeltas</code> and
