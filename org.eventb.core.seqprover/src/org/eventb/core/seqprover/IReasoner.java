bc120afaad66d9f471c3c804958e021e72a1de81
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-10-07 09:32:46 +0000

    Application of a reasoner to a sequent now takes a proof monitor (so that it can be cancelled).

--- org.eventb.core.seqprover/src/org/eventb/core/seqprover/IReasoner.java
+++ org.eventb.core.seqprover/src/org/eventb/core/seqprover/IReasoner.java
@@ -1,14 +1,16 @@
 package org.eventb.core.seqprover;
 
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eventb.core.seqprover.IReasonerInputSerializer.SerializeException;
 
 public interface IReasoner {
-	
+
 	String getReasonerID();
-	
-	IReasonerInput deserializeInput(IReasonerInputSerializer reasonerInputSerializer) throws SerializeException;
-	
-	IReasonerOutput apply(IProverSequent seq,IReasonerInput input, IProgressMonitor progressMonitor);
+
+	IReasonerInput deserializeInput(
+			IReasonerInputSerializer reasonerInputSerializer)
+			throws SerializeException;
+
+	IReasonerOutput apply(IProverSequent seq, IReasonerInput input,
+			IProofMonitor pm);
 
 }
