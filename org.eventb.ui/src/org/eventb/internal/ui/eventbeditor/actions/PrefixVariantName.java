b54465f29476afd4a9dc70a5c460716389a059ab
Thai Son Hoang <tshoang@users.sourceforge.net>
2006-10-12 18:59:48 +0000

    Created action for adding variant.

---
+++ org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/actions/PrefixVariantName.java
@@ -0,0 +1,19 @@
+package org.eventb.internal.ui.eventbeditor.actions;
+
+import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.jface.action.IAction;
+import org.eventb.internal.ui.EventBUIPlugin;
+
+public class PrefixVariantName extends PrefixElementName {
+
+	public static final QualifiedName QUALIFIED_NAME = new QualifiedName(
+			EventBUIPlugin.PLUGIN_ID, "prefix-variant-name");
+
+	public static final String DEFAULT_PREFIX = "variant";
+	
+	public void run(IAction action) {
+		setPrefix(QUALIFIED_NAME, DEFAULT_PREFIX, "Variant Name Prefix",
+				"Please specify the prefix for variant names");
+	}
+
+}
