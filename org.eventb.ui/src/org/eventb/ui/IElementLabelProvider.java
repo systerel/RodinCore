9b6f74da9db20d4493b0a15952ce7f204cf4146d
Thai Son Hoang <tshoang@users.sourceforge.net>
2006-10-19 14:08:03 +0000

    Commented the interface and the default implementation for element label provider

--- org.eventb.ui/src/org/eventb/ui/IElementLabelProvider.java
+++ org.eventb.ui/src/org/eventb/ui/IElementLabelProvider.java
@@ -1,7 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2006 ETH Zurich.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Rodin @ ETH Zurich
+ ******************************************************************************/
+
 package org.eventb.ui;
 
+/**
+ * @author htson
+ *         <p>
+ *         A common interface for proving label of an object
+ */
 public interface IElementLabelProvider {
 
+	/**
+	 * Getting the label of an object, this must NOT be <code>null</code>
+	 * <p>
+	 * 
+	 * @param obj
+	 *            an object
+	 * @return the label of the input object
+	 */
 	public String getLabel(Object obj);
-	
+
 }
--- org.eventb.ui/src/org/eventb/ui/NullElementLabelProvider.java
+++ org.eventb.ui/src/org/eventb/ui/NullElementLabelProvider.java
@@ -1,7 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2006 ETH Zurich.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Rodin @ ETH Zurich
+ ******************************************************************************/
 package org.eventb.ui;
 
+/**
+ * @author htson
+ *         <p>
+ *         A default implementation of and
+ *         <code>org.eventb.ui.IElementLabelProvider</code> which returns
+ *         empty string always
+ */
 public class NullElementLabelProvider implements IElementLabelProvider {
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eventb.ui.IElementLabelProvider#getLabel(java.lang.Object)
+	 */
 	public String getLabel(Object obj) {
 		return "";
 	}
