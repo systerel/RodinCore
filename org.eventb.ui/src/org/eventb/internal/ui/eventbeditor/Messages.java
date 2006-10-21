af6914ba1815b8b553a57e7d96122759e78ca368
Thai Son Hoang <tshoang@users.sourceforge.net>
2006-10-21 21:14:08 +0000

    Fixed the Axiom Page title + Log the error when there are mismatches in getting resource information

--- org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/Messages.java
+++ org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/Messages.java
@@ -3,6 +3,8 @@ package org.eventb.internal.ui.eventbeditor;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
+import org.eventb.internal.ui.UIUtils;
+
 public class Messages {
 	private static final String BUNDLE_NAME = "org.eventb.internal.ui.eventbeditor.messages"; //$NON-NLS-1$
 
@@ -13,10 +15,15 @@ public class Messages {
 	}
 
 	public static String getString(String key) {
-		// TODO Auto-generated method stub
 		try {
 			return RESOURCE_BUNDLE.getString(key);
 		} catch (MissingResourceException e) {
+			// Log the error. Report the error if in debug mode
+			String message = "Cannot find the externalised string for key "
+					+ key;
+			if (EventBEditorUtils.DEBUG)
+				EventBEditorUtils.debug(message);
+			UIUtils.log(e, message);
 			return '!' + key + '!';
 		}
 	}
