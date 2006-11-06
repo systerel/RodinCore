0e442d74d828bcdca6e51029b9163684cc2d6589
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-11-06 16:13:26 +0000

    Moved the abstract test class to a common package.

--- org.eventb.core.tests/src/org/eventb/core/tests/BuilderTest.java
+++ org.eventb.core.tests/src/org/eventb/core/tests/BuilderTest.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 
-package org.eventb.core.testscpog;
+package org.eventb.core.tests;
 
 import junit.framework.TestCase;
 
