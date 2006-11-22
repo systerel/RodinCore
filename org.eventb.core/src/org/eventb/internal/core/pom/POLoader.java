42db6a72d78fc42c5d9628fa0eb41005429385a5
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-11-22 20:28:43 +0000

    Refactored code to work with new version of the database API for PO files.

--- org.eventb.core/src/org/eventb/internal/core/pom/POLoader.java
+++ org.eventb.core/src/org/eventb/internal/core/pom/POLoader.java
@@ -1,11 +1,8 @@
 package org.eventb.internal.core.pom;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
-import org.eventb.core.IPOFile;
 import org.eventb.core.IPOIdentifier;
 import org.eventb.core.IPOPredicate;
 import org.eventb.core.IPOPredicateSet;
@@ -13,10 +10,10 @@ import org.eventb.core.IPOSequent;
 import org.eventb.core.ast.FormulaFactory;
 import org.eventb.core.ast.ITypeEnvironment;
 import org.eventb.core.ast.Predicate;
-import org.eventb.core.ast.Type;
 import org.eventb.core.seqprover.Hypothesis;
 import org.eventb.core.seqprover.IProverSequent;
 import org.eventb.core.seqprover.ProverFactory;
+import org.eventb.internal.core.Util;
 import org.rodinp.core.RodinDBException;
 
 public final class POLoader {
@@ -27,60 +24,95 @@ public final class POLoader {
 		super();
 	}
 
-	// Run from the builder. Does not lock the pofile
-	public static Map<String, IProverSequent> readPOs(final IPOFile poFile) throws RodinDBException {
-		final IPOSequent[] poSequents = poFile.getSequents();
-		Map<String, IProverSequent> result = new HashMap<String, IProverSequent>(poSequents.length);
-		for (IPOSequent poSeq:poSequents){
-			String name = poSeq.getElementName();
-			ITypeEnvironment typeEnv = factory.makeTypeEnvironment();
-			getTypeEnvironment(poFile, poSeq.getHypothesis(), typeEnv);
-			Set<Hypothesis> hypotheses = readPredSet(poSeq.getHypothesis(), typeEnv);
-			Predicate goal = 
-				poSeq.getGoal().getPredicate(factory, typeEnv);
-			IProverSequent seq = ProverFactory.makeSequent(typeEnv,hypotheses,goal);
-			result.put(name,seq);
-		}
-		
-			return result;			
-	}
-	
-	// lock the PO file before reading
+	/**
+	 * Returns the sequent associated to the given proof obligation.
+	 * <p>
+	 * The PO file containing the proof obligation to read should be locked
+	 * before running this method, so that the PO doesn't change while reading
+	 * it.
+	 * </p>
+	 * 
+	 * @param poSeq
+	 *            the proof obligation to read
+	 * @return the sequent of the given proof obligation
+	 * @throws RodinDBException
+	 */
 	public static IProverSequent readPO(IPOSequent poSeq) throws RodinDBException {
-		if (! poSeq.exists()) return null;
-		IPOFile poFile = (IPOFile) poSeq.getOpenable();
-		ITypeEnvironment typeEnv = factory.makeTypeEnvironment();
-		getTypeEnvironment(poFile, poSeq.getHypothesis(), typeEnv);
-		Set<Hypothesis> hypotheses = readPredSet(poSeq.getHypothesis(),typeEnv);
-		Predicate goal = poSeq.getGoal().getPredicate(factory, typeEnv);
-		IProverSequent seq = ProverFactory.makeSequent(typeEnv,hypotheses,goal);
-		return seq;
+		final ITypeEnvironment typeEnv = factory.makeTypeEnvironment();
+		final Set<Hypothesis> hypotheses = new HashSet<Hypothesis>();
+		loadHypotheses(poSeq, hypotheses, typeEnv);
+		final Predicate goal = readGoal(poSeq, typeEnv);
+		return ProverFactory.makeSequent(typeEnv,hypotheses,goal);
 	}
 
-	private static void getTypeEnvironment(
-			final IPOFile poFile, IPOPredicateSet predSet, ITypeEnvironment env) throws RodinDBException {
-		for (IPOIdentifier identifier : predSet.getIdentifiers()) {
-			String name = identifier.getElementName();
-			Type type =  identifier.getType(factory);
-			assert (name!=null && type !=null);
-			env.addName(name,type);
-		}
-		IPOPredicateSet parentSet = predSet.getParentPredicateSet();
-		if (parentSet == null)
+	/**
+	 * Loads the hypotheses of the given PO and appends them to the given set of
+	 * hypotheses. The given type environment is enriched with the types of the
+	 * free identifiers that occur in the loaded hypotheses.
+	 * 
+	 * @param poSeq
+	 *            PO to read
+	 * @param hypotheses
+	 *            set of hypotheses where to store the loaded hypotheses
+	 * @param typeEnv
+	 *            type environment to enrich at the same time
+	 * @throws RodinDBException
+	 */
+	private static void loadHypotheses(IPOSequent poSeq,
+			Set<Hypothesis> hypotheses, ITypeEnvironment typeEnv)
+			throws RodinDBException {
+
+		IPOPredicateSet[] dbHyps = poSeq.getHypotheses();
+		if (dbHyps.length == 0) {
+			Util.log(null, "No predicate set in PO " + poSeq);
 			return;
-		else
-			getTypeEnvironment(poFile, parentSet, env);
-	}
-	
-	private static Set<Hypothesis> readPredSet(IPOPredicateSet poPredSet, ITypeEnvironment typeEnv) throws RodinDBException {
-		Set<Hypothesis> result = new HashSet<Hypothesis>();
-		for (IPOPredicate poPred:poPredSet.getPredicates()){
-			result.add(new Hypothesis(
-					poPred.getPredicate(factory, typeEnv)));
 		}
-		if (poPredSet.getParentPredicateSet() != null) 
-			result.addAll(readPredSet(poPredSet.getParentPredicateSet(),typeEnv));
-		return result;
+		if (dbHyps.length != 1) {
+			Util.log(null, "More than one predicate set in PO " + poSeq);
+		}
+		loadPredicateSet(dbHyps[0], hypotheses, typeEnv);
 	}
 	
+	private static void loadPredicateSet(IPOPredicateSet poPredSet,
+			Set<Hypothesis> hypotheses, ITypeEnvironment typeEnv)
+			throws RodinDBException {
+
+		final IPOPredicateSet parentSet = poPredSet.getParentPredicateSet();
+		if (parentSet != null) {
+			loadPredicateSet(parentSet, hypotheses, typeEnv);
+		}
+		for (final IPOIdentifier poIdent: poPredSet.getIdentifiers()) {
+			typeEnv.add(poIdent.getIdentifier(factory));
+		}
+		for (final IPOPredicate poPred : poPredSet.getPredicates()) {
+			final Predicate predicate = poPred.getPredicate(factory, typeEnv);
+			final Hypothesis hypothesis = new Hypothesis(predicate);
+			hypotheses.add(hypothesis);
+		}
+	}
+	
+	/**
+	 * Reads the goal of the given proof obligation.
+	 * 
+	 * @param poSeq
+	 *            PO to read
+	 * @param typeEnv
+	 *            type environment to use
+	 * @return the goal of the given PO
+	 * @throws RodinDBException
+	 */
+	private static Predicate readGoal(IPOSequent poSeq, ITypeEnvironment typeEnv)
+			throws RodinDBException {
+		
+		IPOPredicate[] dbGoals = poSeq.getGoals();
+		if (dbGoals.length == 0) {
+			Util.log(null, "No goal for PO " + poSeq);
+			return null;
+		}
+		if (dbGoals.length != 1) {
+			Util.log(null, "More than one goal for PO " + poSeq);
+		}
+		return dbGoals[0].getPredicate(factory, typeEnv);
+	}
+
 }
