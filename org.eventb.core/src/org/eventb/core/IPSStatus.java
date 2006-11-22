20313784fa15eccf392debc4b16f747ac6f65741
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-11-22 21:22:43 +0000

    Updated comments.

--- org.eventb.core/src/org/eventb/core/IPSStatus.java
+++ org.eventb.core/src/org/eventb/core/IPSStatus.java
@@ -8,7 +8,6 @@
 
 package org.eventb.core;
 
-
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.rodinp.core.IInternalElement;
 import org.rodinp.core.IInternalElementType;
@@ -17,41 +16,39 @@ import org.rodinp.core.RodinDBException;
 
 /**
  * Common protocol for Proof Status elements in Event-B Proof Status (PS) files.
- *
+ * 
  * <p>
- * The convention used for associating proof obligations (IPOSequent) in the PO file to 
- * proof status elements (IPSStatus) in the PS file, and proofs (in the PR file) is that 
- * they all have the identical element name.
+ * The convention used for associating proof obligations (IPOSequent) in the PO
+ * file to proof status elements (IPSStatus) in the PS file, and proofs (in the
+ * PR file) is that they all have the identical element name.
  * </p>
  * 
  * <p>
  * This interface is not intended to be implemented by clients.
  * </p>
  * 
- * @see #getElementName() , IPOSequent, IPRProof 
+ * @see #getElementName() , IPOSequent, IPRProof
  * 
  * @author Farhad Mehta
- *
+ * 
  */
 public interface IPSStatus extends IInternalElement {
-	
-	IInternalElementType ELEMENT_TYPE =
-		RodinCore.getInternalElementType(EventBPlugin.PLUGIN_ID + ".psStatus"); //$NON-NLS-1$
-	
+
+	IInternalElementType ELEMENT_TYPE = RodinCore
+			.getInternalElementType(EventBPlugin.PLUGIN_ID + ".psStatus"); //$NON-NLS-1$
+
 	/**
-	 * Returns the proof associated to this proof obligation from the
-	 * RODIN database.
+	 * Returns the proof associated to this proof obligation from the RODIN
+	 * database.
 	 * <p>
 	 * This is a handle-only method. The proof element may or may not be
 	 * present.
 	 * </p>
 	 * 
-	 * @return a handle to the proof associated to this status element
-	 * 
-	 * @throws RodinDBException
+	 * @return the proof associated to this status element
 	 */
-	IPRProof getProof() throws RodinDBException;
-	
+	IPRProof getProof();
+
 	/**
 	 * Returns the proof obligation associated to this status element from the
 	 * RODIN database.
@@ -59,134 +56,133 @@ public interface IPSStatus extends IInternalElement {
 	 * This is a handle-only method. The returned element may or may not be
 	 * present.
 	 * </p>
-	 *  
-	 * @return the IPOSequent associated to this proof obligation from the
-	 * RODIN database.
-	 *
-	 * @throws RodinDBException
+	 * 
+	 * @return the sequent associated to this proof obligation
 	 */
-	IPOSequent getPOSequent() throws RodinDBException;
-	
+	IPOSequent getPOSequent();
+
 	/**
-	 * Returns the value stored in the proof validity attribute of this 
-	 * proof status element.
+	 * Returns the value stored in the proof validity attribute of this proof
+	 * status element.
 	 * <p>
-	 * When consistent, the proof validity attribute is <code>true</code>
-	 * iff the proof stored in the associated proof element is valid (or 
-	 * applicable) for the associated proof obligation.
+	 * When consistent, the proof validity attribute is <code>true</code> iff
+	 * the proof stored in the associated proof element is valid (or applicable)
+	 * for the associated proof obligation.
 	 * </p>
-	 *
-	 * @return The value stored in the proof validity attribute of this 
-	 * proof status element.
+	 * 
+	 * @return The value stored in the proof validity attribute of this proof
+	 *         status element.
 	 * 
 	 * @throws RodinDBException
 	 */
 	boolean getProofValidAttribute() throws RodinDBException;
-	
+
 	/**
-	 * Sets the value stored in the proof validity attribute of this 
-	 * proof status element.
+	 * Sets the value stored in the proof validity attribute of this proof
+	 * status element.
 	 * <p>
-	 * When consistent, the proof validity attribute is <code>true</code>
-	 * iff the proof stored in the associated proof element is valid (or 
-	 * applicable) for the associated proof obligation.
+	 * When consistent, the proof validity attribute is <code>true</code> iff
+	 * the proof stored in the associated proof element is valid (or applicable)
+	 * for the associated proof obligation.
 	 * </p>
-	 *
+	 * 
 	 * @param valid
-	 * 			The value to set to
+	 *            The value to set to
 	 * @param monitor
 	 *            a progress monitor, or <code>null</code> if progress
 	 *            reporting is not desired
 	 * 
 	 * @throws RodinDBException
 	 */
-	void setProofValidAttribute(boolean valid, IProgressMonitor monitor) throws RodinDBException;
-	
+	void setProofValidAttribute(boolean valid, IProgressMonitor monitor)
+			throws RodinDBException;
+
 	/**
-	 * Returns the value stored in the confidence attribute of this 
-	 * proof status element.
+	 * Returns the value stored in the confidence attribute of this proof status
+	 * element.
 	 * <p>
-	 * When consistent, the confidence attribute is identical to the
-	 * confidence attribute stored in the associated proof element.
+	 * When consistent, the confidence attribute is identical to the confidence
+	 * attribute stored in the associated proof element.
 	 * </p>
-	 *
-	 * @return The value stored in the confidence attribute of this 
-	 * proof status element.
+	 * 
+	 * @return The value stored in the confidence attribute of this proof status
+	 *         element.
 	 * 
 	 * @throws RodinDBException
 	 */
 	int getProofConfidence() throws RodinDBException;
 
 	/**
-	 * Sets value stored in the confidence attribute of this 
-	 * proof status element.
+	 * Sets value stored in the confidence attribute of this proof status
+	 * element.
 	 * <p>
-	 * When consistent, the confidence attribute is identical to the
-	 * confidence attribute stored in the associated proof element.
+	 * When consistent, the confidence attribute is identical to the confidence
+	 * attribute stored in the associated proof element.
 	 * </p>
-	 *
+	 * 
 	 * @param confidence
-	 * 			The confidence value to set
-	 *
+	 *            The confidence value to set
+	 * 
 	 * @param monitor
 	 *            a progress monitor, or <code>null</code> if progress
 	 *            reporting is not desired
 	 * 
 	 * @throws RodinDBException
 	 */
-	void setProofConfidence(int confidence, IProgressMonitor monitor) throws RodinDBException;
+	void setProofConfidence(int confidence, IProgressMonitor monitor)
+			throws RodinDBException;
 
-	
 	/**
-	 * Returns whether an auto proof attribute is present in this 
-	 * proof status element.
+	 * Returns whether an auto proof attribute is present in this proof status
+	 * element.
 	 * <p>
-	 * The presence of an auto proof attribute means that the auto prover
-	 * has been attemptd on the associated proof obligation
+	 * The presence of an auto proof attribute means that the auto prover has
+	 * been attemptd on the associated proof obligation
 	 * </p>
-	 *
-	 * @return <code>true</code> iff an auto proof attribute is present in this 
-	 * proof status element.
+	 * 
+	 * @return <code>true</code> iff an auto proof attribute is present in
+	 *         this proof status element.
 	 * @throws RodinDBException
 	 */
 	boolean hasAutoProofAttribute() throws RodinDBException;
-	
+
 	/**
-	 * Returns the value stored in the auto proof attribute of this 
-	 * proof status element.
+	 * Returns the value stored in the auto proof attribute of this proof status
+	 * element.
 	 * <p>
 	 * This attribute may be absent if the auto prover has not been attempted.
-	 * In this case a check with {@link #hasAutoProofAttribute()} 
-	 * is needed to avoid a RodinDBException.
+	 * In this case a check with {@link #hasAutoProofAttribute()} is needed to
+	 * avoid a RodinDBException.
 	 * </p>
 	 * <p>
-	 * The value of this attribute is <code>true</code> iff the associated proof 
-	 * element was generated by the auto prover.
+	 * The value of this attribute is <code>true</code> iff the associated
+	 * proof element was generated by the auto prover.
 	 * </p>
-	 *
-	 * @return The value stored in the auto proof attribute of this 
-	 * proof status element.
+	 * 
+	 * @return The value stored in the auto proof attribute of this proof status
+	 *         element.
 	 * 
 	 * @throws RodinDBException
 	 */
 	boolean getAutoProofAttribute() throws RodinDBException;
-	
+
 	/**
-	 * Sets the value stored in the auto proof attribute of this 
-	 * proof status element.
+	 * Sets the value stored in the auto proof attribute of this proof status
+	 * element.
 	 * <p>
-	 * The value of this attribute is <code>true</code> iff the associated proof 
-	 * element was generated by the auto prover.
+	 * The value of this attribute is <code>true</code> iff the associated
+	 * proof element was generated by the auto prover.
 	 * </p>
-	 *
+	 * 
 	 * @param autoProof
-	 * 			The value to set to
+	 *            The value to set to
 	 * @param monitor
 	 *            a progress monitor, or <code>null</code> if progress
 	 *            reporting is not desired
 	 * 
 	 * @throws RodinDBException
 	 */
-	void setAutoProofAttribute(boolean autoProof, IProgressMonitor monitor) throws RodinDBException;
-	
+	void setAutoProofAttribute(boolean autoProof, IProgressMonitor monitor)
+			throws RodinDBException;
+
 }
