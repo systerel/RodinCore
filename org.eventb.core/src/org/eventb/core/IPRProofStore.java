fd186f2095c3a18dfedc9249a320914c3b9ac03a
Laurent Voisin <lvoisin@users.sourceforge.net>
2006-11-28 15:43:26 +0000

    Added access methods to this database element.

--- org.eventb.core/src/org/eventb/core/IPRProofStore.java
+++ org.eventb.core/src/org/eventb/core/IPRProofStore.java
@@ -8,17 +8,125 @@
 
 package org.eventb.core;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.rodinp.core.IInternalElement;
 import org.rodinp.core.IInternalElementType;
 import org.rodinp.core.RodinCore;
+import org.rodinp.core.RodinDBException;
 
 /**
+ * A proof store gathers together all predicates and expressions used in a
+ * persisted proof. The children of this element are:
+ * <ul>
+ * <li>Identifiers occurring free in a predicate or expression in the proof.</li>
+ * <li>Predicates used in the proof.</li>
+ * <li>Expressions used in the proof.</li>
+ * </ul>
+ * Moreover, the store also carries an attribute called
+ * <code>org.eventb.core.prSets</code> which contains a list of all carrier
+ * sets used in the proof (set names separated by commas).
+ * 
  * @author Farhad Mehta
- *
  */
 public interface IPRProofStore extends IInternalElement {
-	
-	IInternalElementType ELEMENT_TYPE =
-		RodinCore.getInternalElementType(EventBPlugin.PLUGIN_ID + ".prProofStore"); //$NON-NLS-1$
+
+	IInternalElementType ELEMENT_TYPE = RodinCore
+			.getInternalElementType(EventBPlugin.PLUGIN_ID + ".prProofStore"); //$NON-NLS-1$
+
+	/**
+	 * Returns a handle to the identifier child with the given name.
+	 * <p>
+	 * This is a handle-only method. The identifier element may or may not be
+	 * present.
+	 * </p>
+	 * 
+	 * @param name
+	 *            name of the child
+	 * 
+	 * @return a handle to the child identifier with the given name
+	 * @see #getIdentifiers()
+	 */
+	IPRIdentifier getIdentifier(String name);
+
+	/**
+	 * Returns all children identifier elements.
+	 * 
+	 * @return an array of all chidren element of type identifier
+	 * @throws RodinDBException
+	 * @see #getIdentifier(String)
+	 */
+	IPRIdentifier[] getIdentifiers() throws RodinDBException;
+
+	/**
+	 * Returns a handle to the expression child with the given name.
+	 * <p>
+	 * This is a handle-only method. The expression element may or may not be
+	 * present.
+	 * </p>
+	 * 
+	 * @param name
+	 *            element name of the child
+	 * 
+	 * @return a handle to the child expression with the given element name
+	 * @see #getExpressions()
+	 */
+	IPRStoredExpr getExpression(String name);
+
+	/**
+	 * Returns all children expression elements.
+	 * 
+	 * @return an array of all chidren element of type expression
+	 * @throws RodinDBException
+	 * @see #getExpression(String)
+	 */
+	IPRStoredExpr[] getExpressions() throws RodinDBException;
+
+	/**
+	 * Returns a handle to the predicate child with the given name.
+	 * <p>
+	 * This is a handle-only method. The predicate element may or may not be
+	 * present.
+	 * </p>
+	 * 
+	 * @param name
+	 *            element name of the child
+	 * 
+	 * @return a handle to the child predicate with the given element name
+	 * @see #getPredicates()
+	 */
+	IPRStoredPred getPredicate(String name);
+
+	/**
+	 * Returns all children predicate elements.
+	 * 
+	 * @return an array of all chidren element of type predicate
+	 * @throws RodinDBException
+	 * @see #getPredicate(String)
+	 */
+	IPRStoredPred[] getPredicates() throws RodinDBException;
+
+	/**
+	 * Returns the carrier sets that are used in this store.
+	 * 
+	 * @return an array of the carrier set names of this store
+	 * @throws RodinDBException
+	 * @see #setSets(String[], IProgressMonitor)
+	 */
+	String[] getSets() throws RodinDBException;
+
+	/**
+	 * Sets the carrier sets that are used in this store.
+	 * 
+	 * @param sets
+	 *            the carrier set names to set
+	 * @param monitor
+	 *            a progress monitor, or <code>null</code> if progress
+	 *            reporting is not desired
+	 * 
+	 * @throws RodinDBException
+	 * @see #getSets()
+	 */
+	void setSets(String[] sets, IProgressMonitor monitor)
+			throws RodinDBException;
 
 }
--- org.eventb.core/src/org/eventb/core/basis/PRProofStore.java
+++ org.eventb.core/src/org/eventb/core/basis/PRProofStore.java
@@ -7,9 +7,16 @@
  *******************************************************************************/
 package org.eventb.core.basis;
 
+import static org.eventb.core.EventBAttributes.PR_SETS_ATTRIBUTE;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eventb.core.IPRIdentifier;
 import org.eventb.core.IPRProofStore;
+import org.eventb.core.IPRStoredExpr;
+import org.eventb.core.IPRStoredPred;
 import org.rodinp.core.IInternalElementType;
 import org.rodinp.core.IRodinElement;
+import org.rodinp.core.RodinDBException;
 import org.rodinp.core.basis.InternalElement;
 
 /**
@@ -18,6 +25,8 @@ import org.rodinp.core.basis.InternalElement;
  */
 public class PRProofStore extends InternalElement implements IPRProofStore {
 
+	private static final String[] NO_STRINGS = new String[0];
+
 	public PRProofStore(String name, IRodinElement parent) {
 		super(name, parent);
 	}
@@ -25,6 +34,58 @@ public class PRProofStore extends InternalElement implements IPRProofStore {
 	@Override
 	public IInternalElementType getElementType() {
 		return ELEMENT_TYPE;
+	}
+
+	public IPRStoredExpr getExpression(String name) {
+		return (IPRStoredExpr) getInternalElement(IPRStoredExpr.ELEMENT_TYPE, name);
+	}
+
+	public IPRStoredExpr[] getExpressions() throws RodinDBException {
+		return (IPRStoredExpr[]) getChildrenOfType(IPRStoredExpr.ELEMENT_TYPE);
+	}
+
+	public IPRIdentifier getIdentifier(String name) {
+		return (IPRIdentifier) getInternalElement(IPRIdentifier.ELEMENT_TYPE, name);
+	}
+
+	public IPRIdentifier[] getIdentifiers() throws RodinDBException {
+		return (IPRIdentifier[]) getChildrenOfType(IPRIdentifier.ELEMENT_TYPE);
+	}
+
+	public IPRStoredPred getPredicate(String name) {
+		return (IPRStoredPred) getInternalElement(IPRStoredPred.ELEMENT_TYPE, name);
+	}
+
+	public IPRStoredPred[] getPredicates() throws RodinDBException {
+		return (IPRStoredPred[]) getChildrenOfType(IPRStoredPred.ELEMENT_TYPE);
+	}
+
+	public String[] getSets() throws RodinDBException {
+		if (hasAttribute(PR_SETS_ATTRIBUTE)) {
+			String value = getAttributeValue(PR_SETS_ATTRIBUTE);
+			return value.split(",");
+		}
+		return NO_STRINGS;
+	}
+
+	public void setSets(String[] sets, IProgressMonitor monitor) throws RodinDBException {
+		final int length = sets.length;
+		if (length == 0) {
+			removeAttribute(PR_SETS_ATTRIBUTE, monitor);
+			return;
+		}
+		if (length == 1) {
+			setAttributeValue(PR_SETS_ATTRIBUTE, sets[0], monitor);
+			return;
+		}
+		final StringBuilder builder = new StringBuilder();
+		String sep = "";
+		for (String name: sets) {
+			builder.append(sep);
+			sep = ",";
+			builder.append(name);
+		}
+		setAttributeValue(PR_SETS_ATTRIBUTE, builder.toString(), monitor);
 	}
 	
 }
