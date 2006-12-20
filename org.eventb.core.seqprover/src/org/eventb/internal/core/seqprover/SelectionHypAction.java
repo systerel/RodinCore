5b217d1dde517b952abf8d45e8dbfd766f5ea97f
Farhad Mehta <mehta@users.sourceforge.net>
2006-12-20 17:20:38 +0000

    Minor fix, replacing == with equals()

--- org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/SelectionHypAction.java
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/SelectionHypAction.java
@@ -14,7 +14,7 @@ public class SelectionHypAction implements IInternalHypAction, ISelectionHypActi
 
 	/**
 	 * @param actionType
-	 * 			Must be IDENTICAL to the action types defined in {@link ISelectionHypAction}
+	 * 			Must be the same as the action types defined in {@link ISelectionHypAction}
 	 * @param hyps
 	 */
 	public SelectionHypAction(final String actionType, final Collection<Predicate> hyps) {
@@ -28,13 +28,13 @@ public class SelectionHypAction implements IInternalHypAction, ISelectionHypActi
 	}
 
 	public IProverSequent perform(IProverSequent seq) {
-		if (actionType == ISelectionHypAction.SELECT_ACTION_TYPE)
+		if (actionType.equals(ISelectionHypAction.SELECT_ACTION_TYPE))
 			return seq.selectHypotheses(hyps);
-		if (actionType == ISelectionHypAction.DESELECT_ACTION_TYPE)
+		if (actionType.equals(ISelectionHypAction.DESELECT_ACTION_TYPE))
 			return seq.deselectHypotheses(hyps);
-		if (actionType == ISelectionHypAction.HIDE_ACTION_TYPE)
+		if (actionType.equals(ISelectionHypAction.HIDE_ACTION_TYPE))
 			return seq.hideHypotheses(hyps);
-		if (actionType == ISelectionHypAction.SHOW_ACTION_TYPE)
+		if (actionType.equals(ISelectionHypAction.SHOW_ACTION_TYPE))
 			return seq.showHypotheses(hyps);
 		return seq;
 	}
