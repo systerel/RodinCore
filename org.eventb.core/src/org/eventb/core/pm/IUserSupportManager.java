cd4d67e82d0f1600529f2dab7ebe4cbeab2e46a6
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-01-02 00:28:34 +0000

    Comments added for methos run(IRunnable)

--- org.eventb.core/src/org/eventb/core/pm/IUserSupportManager.java
+++ org.eventb.core/src/org/eventb/core/pm/IUserSupportManager.java
@@ -48,6 +48,30 @@ public interface IUserSupportManager {
 	 */
 	public abstract IProvingMode getProvingMode();
 
+	/**
+	 * Run the give action as an atomic User Support Manager operation.
+	 * <p>
+	 * After running a method that changes user supportss,
+	 * registered listeners receive after-the-fact notification of
+	 * what just transpired, in the form of an user support manager changed delta.
+	 * This method allows clients to call a number of
+	 * methods that changes user supports and only have user support manager
+	 * changed notifications reported at the end of the entire
+	 * batch.
+	 * </p>
+	 * <p>
+	 * If this method is called outside the dynamic scope of another such
+	 * call, this method runs the action and then reports a single
+	 * user support manager changed notification describing the net effect of all changes
+	 * done to user supports by the action.
+	 * </p>
+	 * <p>
+	 * If this method is called in the dynamic scope of another such
+	 * call, this method simply runs the action.
+	 * </p>
+	 * @param op the operation to perform
+	 * @throws RodinDBException if the operation failed.
+	 */
 	public void run(Runnable op) throws RodinDBException;
 
 }
