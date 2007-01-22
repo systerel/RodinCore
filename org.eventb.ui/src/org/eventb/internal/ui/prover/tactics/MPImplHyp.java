c6a1aa5b3d50735886ac9edf12aeb4cae9063c8f
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-01-22 14:33:36 +0000

    Added an interactive rewrite rule:   P => Q  == not(Q) => not(P)

---
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/MPImplHyp.java
@@ -0,0 +1,50 @@
+package org.eventb.internal.ui.prover.tactics;
+
+import java.util.List;
+
+import org.eventb.core.ast.BinaryPredicate;
+import org.eventb.core.ast.IPosition;
+import org.eventb.core.ast.Predicate;
+import org.eventb.core.seqprover.IProofTreeNode;
+import org.eventb.core.seqprover.ITactic;
+import org.eventb.core.seqprover.eventbExtensions.Tactics;
+import org.eventb.internal.ui.prover.DefaultFormularFilter;
+import org.eventb.ui.prover.DefaultTacticProvider;
+
+public class MPImplHyp extends DefaultTacticProvider {
+
+	private List<IPosition> positions = null;
+
+	@Override
+	public ITactic getTactic(IProofTreeNode node, Predicate hyp,
+			IPosition position, String[] inputs) {
+		return Tactics.mpImpHyp(hyp, position); // Second level
+	}
+
+	@Override
+	public List<IPosition> getApplicablePositions(IProofTreeNode node,
+			Predicate hyp, String input) {
+		if (node == null)
+			return null;
+
+		internalGetPositions(hyp);
+		if (positions.size() == 0)
+			return null;
+		return positions;
+	}
+
+	private void internalGetPositions(Predicate hyp) {
+		positions = hyp.getPositions(new DefaultFormularFilter() {
+
+			@Override
+			public boolean select(BinaryPredicate predicate) {
+				if (predicate.getTag() == Predicate.LIMP) {
+					return true;
+				}
+				return false;
+			}
+		});
+
+	}
+
+}
