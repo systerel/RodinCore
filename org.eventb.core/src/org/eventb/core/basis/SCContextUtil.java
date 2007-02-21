4789efe9dcce7fbc8fee3256520413797fad144c
Laurent Voisin <lvoisin@users.sourceforge.net>
2007-02-21 14:15:52 +0000

    Resurrected this file to implement the common algorithm for computing the type environment of a context.

--- org.eventb.core/src/org/eventb/core/basis/SCContextUtil.java
+++ org.eventb.core/src/org/eventb/core/basis/SCContextUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 ETH Zurich.
+ * Copyright (c) 2006-2007 ETH Zurich.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,40 +7,46 @@
  *******************************************************************************/
 package org.eventb.core.basis;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eventb.core.ISCAxiom;
 import org.eventb.core.ISCCarrierSet;
 import org.eventb.core.ISCConstant;
 import org.eventb.core.ISCContext;
-import org.eventb.core.ISCTheorem;
+import org.eventb.core.ast.FormulaFactory;
+import org.eventb.core.ast.ITypeEnvironment;
 import org.rodinp.core.RodinDBException;
-import org.rodinp.core.basis.RodinElement;
 
 /**
  * This class contains some utility methods for SC contexts.
- * <p>
- * These methods correspond (roughly) to the interface  {@link ISCContext}.
- * </p>
  * 
- * @author Stefan Hallerstede
+ * @author Laurent Voisin
  */
-@Deprecated
-abstract class SCContextUtil {
+/* package */ class SCContextUtil {
 
-	static public ISCCarrierSet[] getSCCarrierSets(RodinElement element, IProgressMonitor monitor) throws RodinDBException {
-		return element.getChildrenOfType(ISCCarrierSet.ELEMENT_TYPE); 
+	private SCContextUtil() {
+		// Disabled constructor
 	}
 
-	static public ISCConstant[] getSCConstants(RodinElement element, IProgressMonitor monitor) throws RodinDBException {
-		return element.getChildrenOfType(ISCConstant.ELEMENT_TYPE); 
-	}
+	/**
+	 * Adds the carrier sets and constants of the given context to the given
+	 * type environment. Operates by side-effect on the given type environment.
+	 * 
+	 * @param ctx
+	 *            the contributing context (internal or file)
+	 * @param typenv
+	 *            the type environment to enrich
+	 * @param factory
+	 *            the formula factory to use
+	 * @throws RodinDBException
+	 *             if there was a problem accessing the database
+	 */
+	static void augmentTypeEnvironment(ISCContext ctx, ITypeEnvironment typenv,
+			FormulaFactory factory) throws RodinDBException {
 
-	static public ISCAxiom[] getSCAxioms(RodinElement element, IProgressMonitor monitor) throws RodinDBException {
-		return element.getChildrenOfType(ISCAxiom.ELEMENT_TYPE);
-	}
-
-	static public ISCTheorem[] getSCTheorems(RodinElement element, IProgressMonitor monitor) throws RodinDBException {
-		return element.getChildrenOfType(ISCTheorem.ELEMENT_TYPE); 
+		for (ISCCarrierSet set : ctx.getSCCarrierSets()) {
+			typenv.add(set.getIdentifier(factory));
+		}
+		for (ISCConstant cst : ctx.getSCConstants()) {
+			typenv.add(cst.getIdentifier(factory));
+		}
 	}
 
 }
