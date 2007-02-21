c7bfb14d85ee9f14ab4155230c11c1e7393022f2
Laurent Voisin <lvoisin@users.sourceforge.net>
2007-02-21 14:17:08 +0000

    Made the formula factory static.

--- org.eventb.core.tests/src/org/eventb/core/tests/BuilderTest.java
+++ org.eventb.core.tests/src/org/eventb/core/tests/BuilderTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 ETH Zurich.
+ * Copyright (c) 2006-2007 ETH Zurich.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -39,7 +39,7 @@ import org.rodinp.core.RodinMarkerUtil;
  */
 public abstract class BuilderTest extends TestCase {
 	
-	protected FormulaFactory factory = FormulaFactory.getDefault();
+	protected static FormulaFactory factory = FormulaFactory.getDefault();
 
 	protected IRodinProject rodinProject;
 
