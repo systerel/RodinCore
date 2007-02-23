7716bd68d20229819135da21bfe24cce9f77f00e
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-02-23 17:20:47 +0000

    Search View should be shown after searching.

--- org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/SearchHypotheses.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/SearchHypotheses.java
@@ -10,11 +10,11 @@ import org.rodinp.core.RodinDBException;
 
 public class SearchHypotheses implements IProofCommand {
 
-	public void apply(IUserSupport us, Predicate hyp, String [] inputs, IProgressMonitor monitor)
-			throws RodinDBException {
+	public void apply(IUserSupport us, Predicate hyp, String[] inputs,
+			IProgressMonitor monitor) throws RodinDBException {
 		us.searchHyps(inputs[0]);
 		// Trying to show the Search View
-		UIUtils.activateView(SearchHypothesis.VIEW_ID);
+		UIUtils.showView(SearchHypothesis.VIEW_ID);
 	}
 
 	public boolean isApplicable(IUserSupport us, Predicate hyp, String input) {
