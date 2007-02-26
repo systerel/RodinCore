f89ce23de5273eff2933941b36ee977536ba9254
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-02-26 15:44:23 +0000

    Added Properties action.

---
+++ org.eventb.ui/src/org/eventb/internal/ui/propertiesView/Properties.java
@@ -0,0 +1,41 @@
+package org.eventb.internal.ui.propertiesView;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eventb.internal.ui.UIUtils;
+
+public class Properties implements IObjectActionDelegate {
+
+	/**
+	 * Constructor.
+	 */
+	public Properties() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		// Do nothing
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		UIUtils.showView(IPageLayout.ID_PROP_SHEET);
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection sel) {
+		// Do nothing
+	}
+
+}
