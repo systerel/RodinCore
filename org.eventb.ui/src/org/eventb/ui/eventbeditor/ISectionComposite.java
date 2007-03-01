9e899c666b734ad037dd4a7d6249e9c6eb39e682
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-03-01 15:35:40 +0000

    Added getSelectedElement method to the interface.

--- org.eventb.ui/src/org/eventb/ui/eventbeditor/ISectionComposite.java
+++ org.eventb.ui/src/org/eventb/ui/eventbeditor/ISectionComposite.java
@@ -1,15 +1,19 @@
 package org.eventb.ui.eventbeditor;
 
+import java.util.List;
+
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eventb.internal.ui.eventbeditor.editpage.EditPage;
 import org.rodinp.core.IElementChangedListener;
-import org.rodinp.core.IRodinFile;
+import org.rodinp.core.IInternalElement;
 
 public interface ISectionComposite extends IElementChangedListener {
 
-	ISectionComposite create(
-			IEventBEditor editor, FormToolkit toolkit, ScrolledForm form,
-			Composite parent, IRodinFile rInput);
+	ISectionComposite create(EditPage page, FormToolkit toolkit,
+			ScrolledForm form, Composite parent);
+
+	List<IInternalElement> getSelectedElements();
 
 }
