7bc38e17993c52b1a42c483d276a905af05b48db
Farhad Mehta <mehta@users.sourceforge.net>
2007-03-30 16:54:58 +0000

    cleaning imports

--- org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/tests/ProverSequentTests.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/tests/ProverSequentTests.java
@@ -8,13 +8,6 @@
 
 package org.eventb.core.seqprover.tests;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
-
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
