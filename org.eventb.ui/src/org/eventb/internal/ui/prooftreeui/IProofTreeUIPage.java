21524d5a0770618087720db4f2bd44e6c35f8e09
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-04-16 15:17:06 +0000

    IProofTreeUIPage extends IPageBookViewPage instead of IPage.

--- org.eventb.ui/src/org/eventb/internal/ui/prooftreeui/IProofTreeUIPage.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prooftreeui/IProofTreeUIPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 ETH Zurich.
+ * Copyright (c) 2005-2007 ETH Zurich.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -13,13 +13,13 @@
 package org.eventb.internal.ui.prooftreeui;
 
 import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.ui.part.IPage;
+import org.eclipse.ui.part.IPageBookViewPage;
 
 /**
  * @author htson
  *         <p>
  *         This is the interface for Proof Tree UI pages.
  */
-public interface IProofTreeUIPage extends ISelectionProvider, IPage {
+public interface IProofTreeUIPage extends ISelectionProvider, IPageBookViewPage {
 	// Only extends IPage and ISelectionProvider
 }
