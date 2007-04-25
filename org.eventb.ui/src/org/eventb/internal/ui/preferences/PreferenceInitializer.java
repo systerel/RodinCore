4e56752603d8f71b16b3fac1b635da649339c866
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-04-25 17:46:06 +0000

    Added default value for PROVINGMODE.

--- org.eventb.ui/src/org/eventb/internal/ui/preferences/PreferenceInitializer.java
+++ org.eventb.ui/src/org/eventb/internal/ui/preferences/PreferenceInitializer.java
@@ -2,7 +2,7 @@ package org.eventb.internal.ui.preferences;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
-
+import org.eventb.core.seqprover.SequentProver;
 import org.eventb.ui.EventBUIPlugin;
 
 /**
@@ -20,9 +20,21 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		IPreferenceStore store = EventBUIPlugin.getDefault()
 				.getPreferenceStore();
 		store.setDefault(PreferenceConstants.P_PROOFPAGE_AUTOLAYOUT, true);
-//		store.setDefault(PreferenceConstants.P_CHOICE, "choice2");
-//		store.setDefault(PreferenceConstants.P_STRING,
-//				"Default value");
+		
+		// Default value for tactic registry
+		String[] registeredIDs = SequentProver.getTacticRegistry().getRegisteredIDs();
+		StringBuffer buffer = new StringBuffer();
+		boolean sep = false;
+		for (Object item : registeredIDs) {
+			if (sep) {
+				sep = true;
+			}
+			else {
+				buffer.append(",");
+			}
+			buffer.append(item);
+		}
+		store.setDefault(PreferenceConstants.P_PROVINGMODE, buffer.toString());
 	}
 
 }
