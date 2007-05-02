e230959114417de80e1eb4375f896560ea81fd81
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-05-02 19:11:28 +0000

    Replace Global tactic Normalise by more general Post Tactic.

--- org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/PostTactic.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/PostTactic.java
@@ -3,19 +3,20 @@ package org.eventb.internal.ui.prover.tactics;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eventb.core.EventBPlugin;
 import org.eventb.core.ast.IPosition;
 import org.eventb.core.ast.Predicate;
 import org.eventb.core.seqprover.IProofTreeNode;
 import org.eventb.core.seqprover.ITactic;
-import org.eventb.core.seqprover.eventbExtensions.Tactics;
 import org.eventb.ui.prover.DefaultTacticProvider;
 
-public class Normalise extends DefaultTacticProvider {
+public class PostTactic extends DefaultTacticProvider {
 
 	@Override
 	public ITactic getTactic(IProofTreeNode node, Predicate hyp,
 			IPosition position, String[] inputs) {
-		return Tactics.norm();
+		return EventBPlugin.getDefault().getUserSupportManager()
+				.getProvingMode().getPostTactic();
 	}
 
 	@Override
