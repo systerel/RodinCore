33bdc913afa698730200cec0aed1b325ff17b854
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-05-13 19:24:20 +0000

    Allow disable/enable the translation.

--- org.eventb.ui/src/org/eventb/internal/ui/EventBMath.java
+++ org.eventb.ui/src/org/eventb/internal/ui/EventBMath.java
@@ -26,6 +26,10 @@ import org.eventb.eventBKeyboard.Text2EventBMathTranslator;
  */
 public class EventBMath extends EventBControl implements IEventBInputText {
 
+	boolean translate;
+	
+	EventBTextModifyListener listener;
+	
 	/**
 	 * Constructor.
 	 * <p>
@@ -35,7 +39,8 @@ public class EventBMath extends EventBControl implements IEventBInputText {
 	 */
 	public EventBMath(final Text text) {
 		super(text);
-		text.addModifyListener(new EventBTextModifyListener());
+		listener = new EventBTextModifyListener();
+		text.addModifyListener(listener);
 		text.addFocusListener(new FocusListener() {
 
 			public void focusGained(FocusEvent e) {
@@ -43,11 +48,11 @@ public class EventBMath extends EventBControl implements IEventBInputText {
 			}
 
 			public void focusLost(FocusEvent e) {
-				if (text.getEditable()) {
-					String translate = Text2EventBMathTranslator.translate(text
+				if (text.getEditable() && translate) {
+					String translateStr = Text2EventBMathTranslator.translate(text
 							.getText());
-					if (!text.getText().equals(translate)) {
-						text.setText(translate);
+					if (!text.getText().equals(translateStr)) {
+						text.setText(translateStr);
 					}
 				}
 			}
@@ -62,6 +67,11 @@ public class EventBMath extends EventBControl implements IEventBInputText {
 	 */
 	public Text getTextWidget() {
 		return (Text) getControl();
+	}
+
+	public void setTranslate(boolean translate) {
+		this.translate = translate;
+		listener.setEnable(translate);
 	}
 
 }
