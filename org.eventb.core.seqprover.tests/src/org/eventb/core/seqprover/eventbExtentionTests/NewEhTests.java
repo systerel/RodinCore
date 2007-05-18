6a464737edcb0a73d81d6f9141c8c2b2c6b637cb
Farhad Mehta <mehta@users.sourceforge.net>
2007-05-18 19:50:47 +0000

    Added new support for reasoner tests

---
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtentionTests/NewEhTests.java
@@ -0,0 +1,44 @@
+package org.eventb.core.seqprover.eventbExtentionTests;
+
+import org.eventb.core.seqprover.reasonerExtentionTests.AbstractReasonerTests;
+import org.eventb.core.seqprover.reasonerInputs.SinglePredInput;
+import org.eventb.core.seqprover.tests.TestLib;
+import org.eventb.internal.core.seqprover.eventbExtensions.Eq;
+
+/**
+ * Unit tests for the Eq reasoner
+ * 
+ * @author Farhad Mehta
+ *
+ */
+public class NewEhTests extends AbstractReasonerTests {
+
+	@Override
+	public String getReasonerID() {
+		return (new Eq()).getReasonerID();
+	}
+	
+	@Override
+	public SuccessfullReasonerApplication[] getSuccessfulReasonerApplications() {
+		return new SuccessfullReasonerApplication[]{
+				new SuccessfullReasonerApplication(
+						TestLib.genSeq(" 1=2 ;; 1+1 = 2 |- 1+1+1 = 3 "),
+						new SinglePredInput(TestLib.genPred("1=2")),
+						"[{}[][1+1=2][1=2, 2+2=2] |- 2+2+2=3]"
+						)
+		};
+	}
+
+	@Override
+	public UnsuccessfullReasonerApplication[] getUnsuccessfullReasonerApplications() {
+		return new UnsuccessfullReasonerApplication[]{
+				// eqHyp not present
+				new UnsuccessfullReasonerApplication(TestLib.genSeq(" ⊤ |- ⊤ "), new SinglePredInput(TestLib.genPred("1=2"))),
+				// eqHyp not an equality
+				new UnsuccessfullReasonerApplication(TestLib.genSeq(" ⊤ |- ⊥ "), new SinglePredInput(TestLib.genPred("⊤"))),
+				// nothing to do
+				new UnsuccessfullReasonerApplication(TestLib.genSeq(" 1=2 ;; ⊤ |- ⊤ "), new SinglePredInput(TestLib.genPred("1=2")))
+		};
+	}
+
+}
---
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/reasonerExtentionTests/ConcreteReasonerTestsExample.java
@@ -0,0 +1,46 @@
+package org.eventb.core.seqprover.reasonerExtentionTests;
+
+import org.eventb.core.seqprover.IReasonerInput;
+import org.eventb.core.seqprover.ITactic;
+import org.eventb.core.seqprover.eventbExtensions.Tactics;
+import org.eventb.core.seqprover.reasonerInputs.EmptyInput;
+import org.eventb.core.seqprover.tests.TestLib;
+
+/**
+ * This class demonstrates how to extend {@link AbstractReasonerTests} to test a particular 
+ * registered reasoner implementation (in this case, {@link TrueGoal}).
+ * 
+ * @author Farhad Mehta
+ *
+ */
+public class ConcreteReasonerTestsExample extends AbstractReasonerTests {
+
+	private static final IReasonerInput input = new EmptyInput();
+
+	@Override
+	public String getReasonerID() {
+		return "org.eventb.core.seqprover.tests.trueGoal";
+	}
+	
+	@Override
+	public ITactic getJustDischTactic() {
+		return Tactics.tautology();
+	}
+
+	@Override
+	public SuccessfullReasonerApplication[] getSuccessfulReasonerApplications() {
+		return new SuccessfullReasonerApplication[]{
+				new SuccessfullReasonerApplication(TestLib.genSeq(" ⊤ |- ⊤ "), input),
+				new SuccessfullReasonerApplication(TestLib.genSeq(" ⊤ |- ⊤ "), input, "[]")
+		};
+	}
+
+	@Override
+	public UnsuccessfullReasonerApplication[] getUnsuccessfullReasonerApplications() {
+		return new UnsuccessfullReasonerApplication[]{
+				new UnsuccessfullReasonerApplication(TestLib.genSeq(" ⊤ |- ⊥ "), input),
+				new UnsuccessfullReasonerApplication(TestLib.genSeq(" ⊤ |- ⊥ "), input, "Goal is not a tautology")
+		};
+	}
+
+}
