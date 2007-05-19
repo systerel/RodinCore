88f393b8ed335a4636a3a7639733fca68f6fee1f
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-05-19 11:35:09 +0000

    Used the utility method to get applicable positions from Tactics class.

--- org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/FunOvrGoal.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/FunOvrGoal.java
@@ -1,12 +1,10 @@
 package org.eventb.internal.ui.prover.tactics;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.swt.graphics.Point;
 import org.eventb.core.ast.AssociativeExpression;
 import org.eventb.core.ast.BinaryExpression;
-import org.eventb.core.ast.DefaultFilter;
 import org.eventb.core.ast.Expression;
 import org.eventb.core.ast.Formula;
 import org.eventb.core.ast.IPosition;
@@ -17,8 +15,6 @@ import org.eventb.core.seqprover.eventbExtensions.Tactics;
 import org.eventb.ui.prover.DefaultTacticProvider;
 
 public class FunOvrGoal extends DefaultTacticProvider {
-
-	private List<IPosition> positions = null;
 
 	@Override
 	public ITactic getTactic(IProofTreeNode node, Predicate hyp,
@@ -32,36 +28,17 @@ public class FunOvrGoal extends DefaultTacticProvider {
 			Predicate hyp, String input) {
 		if (node == null)
 			return null;
-
-		internalGetPositions(node.getSequent().goal());
+		List<IPosition> positions = Tactics.funOvrGetPositions(node.getSequent()
+				.goal());
+		
 		if (positions.size() == 0)
 			return null;
 		return positions;
 	}
-
-	private void internalGetPositions(final Predicate pred) {
-		positions = pred.getPositions(new DefaultFilter() {
-
-			@Override
-			public boolean select(BinaryExpression expression) {
-				if (Tactics.isFunOvrApp(expression))
-					return true;
-				return false;
-			}
-		});
-
-		List<IPosition> toBeRemoved = new ArrayList<IPosition>();
-		for (IPosition pos : positions) {
-			if (!Tactics.isParentTopLevelPredicate(pred, pos)) {
-				toBeRemoved.add(pos);
-			}
-		}
-
-		positions.removeAll(toBeRemoved);
-	}
 	
 	@Override
-	public Point getOperatorPosition(Predicate predicate, String predStr, IPosition position) {
+	public Point getOperatorPosition(Predicate predicate, String predStr,
+			IPosition position) {
 		assert Tactics.isFunOvrApp(predicate.getSubFormula(position));
 		Formula subFormula = predicate.getSubFormula(position);
 		Expression left = ((BinaryExpression) subFormula).getLeft();
--- org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/FunOvrHyp.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/FunOvrHyp.java
@@ -1,12 +1,10 @@
 package org.eventb.internal.ui.prover.tactics;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.swt.graphics.Point;
 import org.eventb.core.ast.AssociativeExpression;
 import org.eventb.core.ast.BinaryExpression;
-import org.eventb.core.ast.DefaultFilter;
 import org.eventb.core.ast.Expression;
 import org.eventb.core.ast.Formula;
 import org.eventb.core.ast.IPosition;
@@ -17,8 +15,6 @@ import org.eventb.core.seqprover.eventbExtensions.Tactics;
 import org.eventb.ui.prover.DefaultTacticProvider;
 
 public class FunOvrHyp extends DefaultTacticProvider {
-
-	private List<IPosition> positions = null;
 
 	@Override
 	public ITactic getTactic(IProofTreeNode node, Predicate hyp,
@@ -33,44 +29,24 @@ public class FunOvrHyp extends DefaultTacticProvider {
 		if (node == null)
 			return null;
 
-		internalGetPositions(hyp);
+		List<IPosition> positions = Tactics.funOvrGetPositions(hyp);
+
 		if (positions.size() == 0)
 			return null;
 		return positions;
 	}
 
-	private void internalGetPositions(final Predicate pred) {
-		positions = pred.getPositions(new DefaultFilter() {
-
-			@Override
-			public boolean select(BinaryExpression expression) {
-				if (Tactics.isFunOvrApp(expression))
-					return true;
-				return false;
-			}
-		});
-
-		List<IPosition> toBeRemoved = new ArrayList<IPosition>();
-		for (IPosition pos : positions) {
-			if (!Tactics.isParentTopLevelPredicate(pred, pos)) {
-				toBeRemoved.add(pos);
-			}
-		}
-
-		positions.remove(toBeRemoved);
-	}
-
 	@Override
-	public Point getOperatorPosition(Predicate predicate, String predStr, IPosition position) {
+	public Point getOperatorPosition(Predicate predicate, String predStr,
+			IPosition position) {
 		assert Tactics.isFunOvrApp(predicate.getSubFormula(position));
 		Formula subFormula = predicate.getSubFormula(position);
 		Expression left = ((BinaryExpression) subFormula).getLeft();
-		Expression[] children = ((AssociativeExpression) left)
-				.getChildren();
+		Expression[] children = ((AssociativeExpression) left).getChildren();
 		Expression last = children[children.length - 1];
 		Expression secondLast = children[children.length - 2];
-		return getOperatorPosition(predStr, secondLast.getSourceLocation().getEnd() + 1, last
-					.getSourceLocation().getStart());
+		return getOperatorPosition(predStr, secondLast.getSourceLocation()
+				.getEnd() + 1, last.getSourceLocation().getStart());
 	}
 
 }
