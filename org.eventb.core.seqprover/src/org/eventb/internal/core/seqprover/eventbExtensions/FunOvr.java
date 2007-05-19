bd695aed3b02f4a68a165f37b2dcc9c04fc0f40f
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-05-19 10:46:19 +0000

    Added more general rule for ov (f <+ ... <+ h)(G).

--- org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/FunOvr.java
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/FunOvr.java
@@ -2,7 +2,7 @@ package org.eventb.internal.core.seqprover.eventbExtensions;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -19,11 +19,15 @@ import org.eventb.core.seqprover.IProverSequent;
 import org.eventb.core.seqprover.ProverFactory;
 import org.eventb.core.seqprover.SequentProver;
 import org.eventb.core.seqprover.IProofRule.IAntecedent;
+import org.eventb.core.seqprover.eventbExtensions.Lib;
+import org.eventb.core.seqprover.eventbExtensions.Tactics;
 
 public class FunOvr extends AbstractManualInference {
 
 	public static String REASONER_ID = SequentProver.PLUGIN_ID + ".funOvr";
 
+	private static FormulaFactory ff = FormulaFactory.getDefault();
+	
 	public String getReasonerID() {
 		return REASONER_ID;
 	}
@@ -31,84 +35,152 @@ public class FunOvr extends AbstractManualInference {
 	@Override
 	protected IAntecedent[] getAntecedents(IProverSequent seq, Predicate pred,
 			IPosition position) {
-		// There will be 2 antecidents
-		IAntecedent[] antecidents = new IAntecedent[2];
-
 		Predicate predicate = pred;
 		if (predicate == null)
 			predicate = seq.goal();
 
 		Formula subFormula = predicate.getSubFormula(position);
 
-		Expression G = ((BinaryExpression) subFormula).getRight();
-		AssociativeExpression left = (AssociativeExpression) ((BinaryExpression) subFormula)
-				.getLeft();
-		Expression[] children = left.getChildren();
-		Expression[] members = ((SetExtension) children[children.length - 1])
-				.getMembers();
+		// "subFormula" should have the form (f <+ ... <+ h)(G)
+		if (!Tactics.isFunOvrApp(subFormula))
+			return null;
+		
+		// There will be 2 antecidents
+		IAntecedent[] antecidents = new IAntecedent[2];
 
-		Expression F = ((BinaryExpression) members[0]).getRight();
-		Expression E = ((BinaryExpression) members[0]).getLeft();
+		BinaryExpression funAppExp = (BinaryExpression) subFormula;
+		Expression G = funAppExp.getRight();
+		Expression left = funAppExp.getLeft();
+		
+		AssociativeExpression funOvr = (AssociativeExpression) left;
+		Expression[] children = funOvr.getChildren();
+		Expression h = children[children.length - 1];
+		
+		if (Lib.isSetExtension(h)
+				&& ((SetExtension) h).getMembers().length == 1) {
+			// g is {E |-> F}
+			Expression[] members = ((SetExtension) h).getMembers();
+			Expression F = ((BinaryExpression) members[0]).getRight();
+			Expression E = ((BinaryExpression) members[0]).getLeft();
 
-		FormulaFactory ff = FormulaFactory.getDefault();
-
-		Predicate newPred0 = predicate.rewriteSubFormula(position, F, ff);
-		Predicate newhyp0 = ff.makeRelationalPredicate(Expression.EQUAL, G,
-				E, null);
-		Set<Predicate> addedHyps0 = new HashSet<Predicate>();
-		addedHyps0.add(newhyp0);
-		if (pred == null) {
-			antecidents[0] = ProverFactory.makeAntecedent(newPred0, addedHyps0,
-				null);
+			// Generate the first antecedent
+			antecidents[0] = createEqualAntecident(pred, predicate, position,
+					E, F, G);
+			
+			antecidents[1] = createNotEqualAntecident(pred, predicate,
+					position, children, E, G);
 		}
 		else {
-			addedHyps0.add(newPred0); // Added the new hyp
-			
-			// Hide the old hyp
-			Collection<Predicate> hideHyps = new ArrayList<Predicate>();
-			hideHyps.add(pred);
-			IHypAction hypAction = ProverFactory.makeHideHypAction(hideHyps);
-			
-			antecidents[0] = ProverFactory.makeAntecedent(null, addedHyps0, hypAction);
+			antecidents[0] = createInDomAntecident(pred, predicate, position,
+					h, G);
+			antecidents[1] = createNotInDomAntecident(pred, predicate, position, children,
+					h, G);
 		}
 		
+		return antecidents;
+	}
+
+	private IAntecedent createNotInDomAntecident(Predicate sourcePred,
+			Predicate predicate, IPosition position, Expression[] children,
+			Expression h, Expression G) {
+		Expression fG = makeExpressionFOfG(children, G);
+
+		Predicate inferredPred = predicate.rewriteSubFormula(position, fG, ff);
+		
+		// Make predicate not(G : dom(h))
+		Expression domH = ff.makeUnaryExpression(Expression.KDOM, h, null);
+		Predicate gInDomH = ff.makeRelationalPredicate(Predicate.IN, G,
+				domH, null);
+		Predicate gNotInDomH = ff.makeUnaryPredicate(Predicate.NOT, gInDomH,
+				null);
+
+		return makeAntecedent(sourcePred, inferredPred, gNotInDomH);
+	}
+
+	private Expression makeExpressionFOfG(Expression[] children, Expression G) {
 		List<Expression> newChildren = new ArrayList<Expression>();
 		for (int i = 0; i < children.length - 1; ++i) {
 			newChildren.add(children[i]);
 		}
-		
 		Expression f;
 		if (newChildren.size() != 1) {
-			f = ff.makeAssociativeExpression(Expression.OVR,
-				newChildren, null);
-		}
-		else {
+			f = ff.makeAssociativeExpression(Expression.OVR, newChildren,
+					null);
+		} else {
 			f = newChildren.get(0);
 		}
-		Expression fG = ff.makeBinaryExpression(Expression.FUNIMAGE, f, G, null);
-		
-		Predicate newPred1 = predicate.rewriteSubFormula(position, fG, ff);
-		Predicate newhyp1 = ff.makeUnaryPredicate(Expression.NOT, newhyp0, null);
-		Set<Predicate> addedHyps1 = new HashSet<Predicate>();
-		addedHyps1.add(newhyp1);
-	
-		if (pred == null) {
-			// Function overriding in goal
-			antecidents[1] = ProverFactory.makeAntecedent(newPred1, addedHyps1,
+		return ff.makeBinaryExpression(Expression.FUNIMAGE, f, G,
 				null);
+	}
+
+	private IAntecedent createInDomAntecident(Predicate sourcePred,
+			Predicate predicate, IPosition position, Expression h, Expression G) {
+		// Generate the new predicate
+		//        P((f <+ ... <+ h)(G)) == P(h(G))
+		Expression F = ff.makeBinaryExpression(Expression.FUNIMAGE, h, G, null);
+		Predicate inferredPred = predicate.rewriteSubFormula(position, F, ff);
+
+		// Make predicate G : dom(h)
+		Expression domH = ff.makeUnaryExpression(Expression.KDOM, h, null);
+		Predicate gInDomH = ff.makeRelationalPredicate(Predicate.IN, G,
+				domH, null);
+		
+		return makeAntecedent(sourcePred, inferredPred, gInDomH);
+	}
+
+	
+	private IAntecedent createNotEqualAntecident(Predicate sourcePred,
+			Predicate predicate, IPosition position, Expression[] children,
+			Expression E, Expression G) {
+		Expression fG = makeExpressionFOfG(children, G);
+
+		Predicate inferredPred = predicate.rewriteSubFormula(position, fG, ff);
+		
+		// Make predicate not(G = E)
+		Predicate gEqualsE = ff.makeRelationalPredicate(Predicate.EQUAL, G,
+				E, null);
+		Predicate gNotEqualsE = ff.makeUnaryPredicate(Predicate.NOT, gEqualsE,
+				null);
+
+		return makeAntecedent(sourcePred, inferredPred, gNotEqualsE);
+	}
+
+	private IAntecedent createEqualAntecident(Predicate sourcePred,
+			Predicate predicate, IPosition position, Expression E,
+			Expression F, Expression G) {
+		// Generate the new predicate
+		//        P((f <+ ... <+ {E |-> F})(G)) == P(F)
+		Predicate inferredPred = predicate.rewriteSubFormula(position, F, ff);
+
+		// Make predicate G = E
+		Predicate gEqualsE = ff.makeRelationalPredicate(Predicate.EQUAL, G,
+				E, null);
+		
+		return makeAntecedent(sourcePred, inferredPred, gEqualsE);
+	}
+
+	private IAntecedent makeAntecedent(Predicate sourcePred,
+			Predicate inferredPred, Predicate newHyp) {
+		Set<Predicate> addedHyps = new LinkedHashSet<Predicate>();
+		addedHyps.add(newHyp);
+
+		if (sourcePred == null) {
+			// Function overriding in goal
+			return ProverFactory.makeAntecedent(inferredPred,
+					addedHyps, null);
 		} else {
 			// Function overriding in hypothesis
-			addedHyps1.add(newPred1); // Added the new hyp
-			
+			addedHyps.add(inferredPred); // Added the new hyp
+
 			// Hide the old hyp
 			Collection<Predicate> hideHyps = new ArrayList<Predicate>();
-			hideHyps.add(pred);
-			IHypAction hypAction = ProverFactory.makeHideHypAction(hideHyps);
-			
-			antecidents[1] = ProverFactory.makeAntecedent(null, addedHyps1, hypAction);			
-		}
-		return antecidents;
+			hideHyps.add(sourcePred);
+			IHypAction hypAction = ProverFactory
+					.makeHideHypAction(hideHyps);
 
+			return ProverFactory.makeAntecedent(null, addedHyps,
+					hypAction);
+		}
 	}
 
 	@Override
@@ -120,13 +192,5 @@ public class FunOvr extends AbstractManualInference {
 			return "ovr goal";
 		}
 	}
-//
-//	@Override
-//	public boolean isApplicable(Formula orgFormula, Formula formula) {
-//		if (formula instanceof Expression) {
-//			return Tactics.isFunOvrApp(orgFormula, (Expression) formula);
-//		}
-//		return false;
-//	}
 
 }
