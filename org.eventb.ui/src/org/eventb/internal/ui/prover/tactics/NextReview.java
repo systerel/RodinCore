896720bfef2dc9112952bc9a4106339ac7fb7989
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-05-24 16:00:34 +0000

    Added the proof command next review to the proof control.

---
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/NextReview.java
@@ -0,0 +1,34 @@
+package org.eventb.internal.ui.prover.tactics;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eventb.core.ast.Predicate;
+import org.eventb.core.pm.IProofState;
+import org.eventb.core.pm.IUserSupport;
+import org.eventb.core.seqprover.IConfidence;
+import org.eventb.core.seqprover.IProofTreeNode;
+import org.eventb.core.seqprover.IProofTreeNodeFilter;
+import org.eventb.ui.prover.IProofCommand;
+import org.rodinp.core.RodinDBException;
+
+public class NextReview implements IProofCommand {
+
+	public void apply(IUserSupport us, Predicate hyp, String[] inputs,
+			IProgressMonitor monitor) throws RodinDBException {
+		us.selectNextSubgoal(false, new IProofTreeNodeFilter() {
+
+			public boolean select(IProofTreeNode node) {
+				int confidence = node.getConfidence();
+				return !node.isOpen() && !node.hasChildren()
+						&& confidence <= IConfidence.REVIEWED_MAX
+						&& IConfidence.PENDING <= confidence;
+			}
+			
+		});
+	}
+
+	public boolean isApplicable(IUserSupport us, Predicate hyp, String input) {
+		IProofState currentPO = us.getCurrentPO();
+		return (currentPO != null);
+	}
+
+}
