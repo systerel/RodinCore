e41846a022fb289cde8e6926c584b0911a51352b
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-05-29 17:55:51 +0000

    Added reasoner for rewriting   P => ( Q & .. & R)  == (P => Q) & ... (P => R)

---
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/reasonerExtentionTests/ImpAndTests.java
@@ -0,0 +1,162 @@
+package org.eventb.core.seqprover.reasonerExtentionTests;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.eventb.core.ast.FormulaFactory;
+import org.eventb.core.ast.Predicate;
+import org.eventb.core.seqprover.IReasonerInput;
+//import org.eventb.core.seqprover.ITactic;
+import org.eventb.core.seqprover.eventbExtensions.Lib;
+import org.eventb.core.seqprover.tests.TestLib;
+import org.eventb.internal.core.seqprover.eventbExtensions.rewriters.AbstractManualRewrites;
+
+//import com.b4free.rodin.core.B4freeCore;
+
+public class ImpAndTests extends AbstractReasonerTests {
+
+	@Override
+	public String getReasonerID() {
+		return "org.eventb.core.seqprover.impAndRewrites";
+	}
+
+	@Override
+	public SuccessfullReasonerApplication[] getSuccessfulReasonerApplications() {
+		Collection<SuccessfullReasonerApplication> successfullReasonerApps = new ArrayList<SuccessfullReasonerApplication>();
+		IReasonerInput input;
+		Predicate pred;
+		FormulaFactory ff = FormulaFactory.getDefault();
+		pred = Lib.parsePredicate("x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3");
+		pred.typeCheck(ff.makeTypeEnvironment());
+		input = new AbstractManualRewrites.Input(null, ff
+				.makePosition(""));
+		successfullReasonerApps.add(new SuccessfullReasonerApplication(TestLib
+				.genSeq(" ⊤ |- x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3 "), input));
+		successfullReasonerApps.add(new SuccessfullReasonerApplication(TestLib
+				.genSeq(" ⊤ |- x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3 "), input,
+				"[{x=ℤ}[][][⊤] |- (x=0⇒x=1)∧(x=0⇒x=2)∧(x=0⇒x=3)]"));
+		input = new AbstractManualRewrites.Input(pred, ff
+				.makePosition(""));
+		successfullReasonerApps.add(new SuccessfullReasonerApplication(TestLib
+				.genSeq(" x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3 |- ⊤ "), input));
+		successfullReasonerApps.add(new SuccessfullReasonerApplication(TestLib
+				.genSeq(" x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3 |- ⊤ "), input,
+				"[{x=ℤ}[x=0⇒x=1∧x=2∧x=3][][(x=0⇒x=1)∧(x=0⇒x=2)∧(x=0⇒x=3)] |- ⊤]"));
+		
+		pred = Lib.parsePredicate("x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3)");
+		pred.typeCheck(ff.makeTypeEnvironment());
+		input = new AbstractManualRewrites.Input(null, ff
+				.makePosition("1"));
+		successfullReasonerApps.add(new SuccessfullReasonerApplication(TestLib
+				.genSeq(" ⊤ |- x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3) "), input));
+		successfullReasonerApps.add(new SuccessfullReasonerApplication(TestLib
+				.genSeq(" ⊤ |- x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3) "), input,
+				"[{x=ℤ}[][][⊤] |- x=4⇒(x=0⇒x=1)∧(x=0⇒x=2)∧(x=0⇒x=3)]"));
+		input = new AbstractManualRewrites.Input(pred, ff
+				.makePosition("1"));
+		successfullReasonerApps.add(new SuccessfullReasonerApplication(TestLib
+				.genSeq(" x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3) |- ⊤ "), input));
+		successfullReasonerApps.add(new SuccessfullReasonerApplication(TestLib
+				.genSeq(" x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3) |- ⊤ "), input,
+				"[{x=ℤ}[x=4⇒(x=0⇒x=1∧x=2∧x=3)][][x=4⇒(x=0⇒x=1)∧(x=0⇒x=2)∧(x=0⇒x=3)] |- ⊤]"));
+
+		pred = Lib.parsePredicate("∀x·x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3)");
+		pred.typeCheck(ff.makeTypeEnvironment());
+		input = new AbstractManualRewrites.Input(null, ff
+				.makePosition("1.1"));
+		successfullReasonerApps.add(new SuccessfullReasonerApplication(TestLib
+				.genSeq(" ⊤ |- ∀x·x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3) "), input));
+		successfullReasonerApps.add(new SuccessfullReasonerApplication(TestLib
+				.genSeq(" ⊤ |- ∀x·x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3) "), input,
+				"[{}[][][⊤] |- ∀x·x=4⇒(x=0⇒x=1)∧(x=0⇒x=2)∧(x=0⇒x=3)]"));
+		input = new AbstractManualRewrites.Input(pred, ff
+				.makePosition("1.1"));
+		successfullReasonerApps.add(new SuccessfullReasonerApplication(TestLib
+				.genSeq(" ∀x·x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3) |- ⊤ "), input));
+		successfullReasonerApps.add(new SuccessfullReasonerApplication(TestLib
+				.genSeq(" ∀x·x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3) |- ⊤ "), input,
+				"[{}[∀x·x=4⇒(x=0⇒x=1∧x=2∧x=3)][][∀x·x=4⇒(x=0⇒x=1)∧(x=0⇒x=2)∧(x=0⇒x=3)] |- ⊤]"));
+
+		return successfullReasonerApps
+				.toArray(new SuccessfullReasonerApplication[successfullReasonerApps
+						.size()]); 
+	}
+
+	@Override
+	public UnsuccessfullReasonerApplication[] getUnsuccessfullReasonerApplications() {
+		Collection<UnsuccessfullReasonerApplication> unsuccessfullReasonerApps = new ArrayList<UnsuccessfullReasonerApplication>();
+		IReasonerInput input;
+		Predicate pred;
+		FormulaFactory ff = FormulaFactory.getDefault();
+		pred = Lib.parsePredicate("x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3");
+		pred.typeCheck(ff.makeTypeEnvironment());
+		input = new AbstractManualRewrites.Input(pred, ff
+				.makePosition(""));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" x = 0 ⇒ x = 1 |- ⊤ "), input));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" x = 0 ⇒ x = 1 |- ⊤ "), input,
+				"Nonexistent hypothesis: x=0⇒x=1∧x=2∧x=3"));
+
+		pred = Lib.parsePredicate("x = 0 ⇒ x = 1");
+		pred.typeCheck(ff.makeTypeEnvironment());
+		input = new AbstractManualRewrites.Input(null, ff
+				.makePosition(""));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" ⊤ |- x = 0 ⇒ x = 1 "), input));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" ⊤ |- x = 0 ⇒ x = 1"), input,
+				"Rewriter "	+ getReasonerID() + " is inapplicable for goal x=0⇒x=1"));
+		input = new AbstractManualRewrites.Input(pred, ff
+				.makePosition(""));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" x = 0 ⇒ x = 1 |- ⊤ "), input));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" x = 0 ⇒ x = 1 |- ⊤ "), input,
+				"Rewriter "	+ getReasonerID() + " is inapplicable for hypothesis x=0⇒x=1"));
+		
+		pred = Lib.parsePredicate("x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3");
+		pred.typeCheck(ff.makeTypeEnvironment());
+		input = new AbstractManualRewrites.Input(null, ff
+				.makePosition("0"));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" ⊤ |- x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3 "), input));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" ⊤ |- x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3"), input,
+				"Rewriter "	+ getReasonerID() + " is inapplicable for goal x=0⇒x=1∧x=2∧x=3"));
+		input = new AbstractManualRewrites.Input(pred, ff
+				.makePosition("0"));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3 |- ⊤ "), input));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3 |- ⊤ "), input,
+				"Rewriter "	+ getReasonerID() + " is inapplicable for hypothesis x=0⇒x=1∧x=2∧x=3"));
+		
+		pred = Lib.parsePredicate("∀x·x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3)");
+		pred.typeCheck(ff.makeTypeEnvironment());
+		input = new AbstractManualRewrites.Input(null, ff
+				.makePosition("1.0"));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" ⊤ |- ∀x·x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3) "), input));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" ⊤ |- ∀x·x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3) "), input,
+				"Rewriter "	+ getReasonerID() + " is inapplicable for goal ∀x·x=4⇒(x=0⇒x=1∧x=2∧x=3)"));
+		input = new AbstractManualRewrites.Input(pred, ff
+				.makePosition("1.0"));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" ∀x·x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3) |- ⊤ "), input));
+		unsuccessfullReasonerApps.add(new UnsuccessfullReasonerApplication(TestLib
+				.genSeq(" ∀x·x = 4 ⇒ (x = 0 ⇒ x = 1 ∧ x = 2 ∧ x = 3) |- ⊤ "), input,
+				"Rewriter "	+ getReasonerID() + " is inapplicable for hypothesis ∀x·x=4⇒(x=0⇒x=1∧x=2∧x=3)"));
+
+		return unsuccessfullReasonerApps
+				.toArray(new UnsuccessfullReasonerApplication[unsuccessfullReasonerApps
+						.size()]); 
+	}
+
+//	@Override
+//	public ITactic getJustDischTactic() {
+//		return B4freeCore.externalPP(false);
+//	}
+
+}
---
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/rewriters/ImpAndRewriterImpl.t
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2006 ETH Zurich.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eventb.internal.core.seqprover.eventbExtensions.rewriters;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.eventb.core.ast.AssociativeExpression;
+import org.eventb.core.ast.AssociativePredicate;
+import org.eventb.core.ast.AtomicExpression;
+import org.eventb.core.ast.BinaryExpression;
+import org.eventb.core.ast.BinaryPredicate;
+import org.eventb.core.ast.BoolExpression;
+import org.eventb.core.ast.BoundIdentDecl;
+import org.eventb.core.ast.BoundIdentifier;
+import org.eventb.core.ast.DefaultRewriter;
+import org.eventb.core.ast.Expression;
+import org.eventb.core.ast.Formula;
+import org.eventb.core.ast.FormulaFactory;
+import org.eventb.core.ast.FreeIdentifier;
+import org.eventb.core.ast.Identifier;
+import org.eventb.core.ast.IntegerLiteral;
+import org.eventb.core.ast.LiteralPredicate;
+import org.eventb.core.ast.Predicate;
+import org.eventb.core.ast.QuantifiedExpression;
+import org.eventb.core.ast.QuantifiedPredicate;
+import org.eventb.core.ast.RelationalPredicate;
+import org.eventb.core.ast.SetExtension;
+import org.eventb.core.ast.SimplePredicate;
+import org.eventb.core.ast.UnaryExpression;
+import org.eventb.core.ast.UnaryPredicate;
+import org.eventb.core.seqprover.eventbExtensions.Lib;
+
+/**
+ * Basic automated rewriter for the Event-B sequent prover.
+ */
+@SuppressWarnings("unused")
+public class ImpAndRewriterImpl extends DefaultRewriter {
+
+	public ImpAndRewriterImpl() {
+		super(true, FormulaFactory.getDefault());
+	}
+		
+	%include {Formula.tom}
+	
+	@Override
+	public Predicate rewrite(BinaryPredicate predicate) {
+	    %match (Predicate predicate) {
+
+			/**
+	    	 * Implication : P ⇒ Q ∧ R  ==  P ⇒ Q  ∧  P ⇒ R
+	    	 */
+			Limp(P, Land(children)) -> {
+				Collection<Predicate> newChildren = new ArrayList<Predicate>(
+						`children.length);
+				FormulaFactory ff = FormulaFactory.getDefault();
+				for (Predicate child : `children) {
+					newChildren.add(ff.makeBinaryPredicate(Predicate.LIMP, `P,
+							child, null));
+				}
+				return ff.makeAssociativePredicate(
+						Predicate.LAND, newChildren, null);
+			}
+			
+	    }
+	    return predicate;
+	}
+
+}
