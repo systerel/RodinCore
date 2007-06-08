662f628d54c37574a15d80055ff57bd5b88c898a
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-06-08 12:31:10 +0000

    Updates the tests.

--- org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtentionTests/FunConvInterImgTests.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtentionTests/FunConvInterImgTests.java
@@ -21,9 +21,25 @@ public class FunConvInterImgTests extends AbstractManualInferenceTests {
 
 	String P3 = "f∼[{x,3} ∩ {2}] = {3}";
 
+	String resultP3GoalA = "{f=ℙ(ℤ×ℤ), x=ℤ}[][][⊤] |- f∈ℤ ⇸ ℤ";
+
+	String resultP3GoalB = "{f=ℙ(ℤ×ℤ), x=ℤ}[][][⊤] |- f∼[{x,3}]∩f∼[{2}]={3}";
+
+	String resultP3HypA = "{f=ℙ(ℤ×ℤ), x=ℤ}[][][f∼[{x,3}∩{2}]={3}] |- f∈ℤ ⇸ ℤ";
+
+	String resultP3HypB = "{f=ℙ(ℤ×ℤ), x=ℤ}[f∼[{x,3}∩{2}]={3}][][f∼[{x,3}]∩f∼[{2}]={3}] |- ⊤";
+
 	String P4 = "{3} = f∼[{x,3} ∩ {2}]";
 
-	public String [] getTestGetPositions() {
+	String resultP4GoalA = "{f=ℙ(ℤ×ℤ), x=ℤ}[][][⊤] |- f∈ℤ ⇸ ℤ";
+
+	String resultP4GoalB = "{f=ℙ(ℤ×ℤ), x=ℤ}[][][⊤] |- {3}=f∼[{x,3}]∩f∼[{2}]";
+
+	String resultP4HypA = "{f=ℙ(ℤ×ℤ), x=ℤ}[][][{3}=f∼[{x,3}∩{2}]] |- f∈ℤ ⇸ ℤ";
+
+	String resultP4HypB = "{f=ℙ(ℤ×ℤ), x=ℤ}[{3}=f∼[{x,3}∩{2}]][][{3}=f∼[{x,3}]∩f∼[{2}]] |- ⊤";
+
+	protected String [] getTestGetPositions() {
 		return new String [] {
 				P1, "",
 				P2, "",
@@ -44,22 +60,17 @@ public class FunConvInterImgTests extends AbstractManualInferenceTests {
 	protected SuccessfulTest[] getSuccessfulTests() {
 		return new SuccessfulTest[] {
 				// P3 in goal
-				new SuccessfulTest(" ⊤ |- " + P3, null, "0",
-						"{f=ℙ(ℤ×ℤ), x=ℤ}[][][⊤] |- f∈ℤ ⇸ ℤ",
-						"{f=ℙ(ℤ×ℤ), x=ℤ}[][][⊤] |- f∼[{x,3}]∩f∼[{2}]={3}"),
+				new SuccessfulTest(" ⊤ |- " + P3, null, "0", resultP3GoalA,
+						resultP3GoalB),
 				// P3 in hyp
-				new SuccessfulTest(P3 + " |- ⊤ ", P3, "0",
-						"{f=ℙ(ℤ×ℤ), x=ℤ}[][][f∼[{x,3}∩{2}]={3}] |- f∈ℤ ⇸ ℤ",
-						"{f=ℙ(ℤ×ℤ), x=ℤ}[f∼[{x,3}∩{2}]={3}][][f∼[{x,3}]∩f∼[{2}]={3}] |- ⊤"),
+				new SuccessfulTest(P3 + " |- ⊤ ", P3, "0", resultP3HypA,
+						resultP3HypB),
 				// P4 in goal
-				new SuccessfulTest(" ⊤ |- " + P4, null, "1",
-						"{f=ℙ(ℤ×ℤ), x=ℤ}[][][⊤] |- f∈ℤ ⇸ ℤ",
-						"{f=ℙ(ℤ×ℤ), x=ℤ}[][][⊤] |- {3}=f∼[{x,3}]∩f∼[{2}]"),
+				new SuccessfulTest(" ⊤ |- " + P4, null, "1", resultP4GoalA,
+						resultP4GoalB),
 				// P4 in hyp
-				new SuccessfulTest(P4 + " |- ⊤ ", P4, "1",
-						"{f=ℙ(ℤ×ℤ), x=ℤ}[][][{3}=f∼[{x,3}∩{2}]] |- f∈ℤ ⇸ ℤ",
-						"{f=ℙ(ℤ×ℤ), x=ℤ}[{3}=f∼[{x,3}∩{2}]][][{3}=f∼[{x,3}]∩f∼[{2}]] |- ⊤")
-		};
+				new SuccessfulTest(P4 + " |- ⊤ ", P4, "1", resultP4HypA,
+						resultP4HypB) };
 	}
 
 	protected String[] getUnsuccessfulTests() {
