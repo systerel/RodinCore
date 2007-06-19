e6b4fb8ba56de3595f8335b8c5a5bcf4e7965679
Farhad Mehta <mehta@users.sourceforge.net>
2007-06-19 11:34:28 +0000

    Fixed bug related to free identifier renaming when types change

--- org.eventb.core.seqprover/src/org/eventb/core/seqprover/proofBuilder/ReplayHints.java
+++ org.eventb.core.seqprover/src/org/eventb/core/seqprover/proofBuilder/ReplayHints.java
@@ -52,7 +52,8 @@ public class ReplayHints {
 		for (int i = 0; i < old.getAddedFreeIdents().length; i++) {
 			if
 			((i < current.getAddedFreeIdents().length) &&
-			(! old.getAddedFreeIdents()[i].equals(current.getAddedFreeIdents()[i])))
+			(! old.getAddedFreeIdents()[i].equals(current.getAddedFreeIdents()[i])) &&
+			( old.getAddedFreeIdents()[i].getType().equals(current.getAddedFreeIdents()[i].getType())))
 			{
 				this.freeVarRename.put(old.getAddedFreeIdents()[i],current.getAddedFreeIdents()[i]);
 			}
