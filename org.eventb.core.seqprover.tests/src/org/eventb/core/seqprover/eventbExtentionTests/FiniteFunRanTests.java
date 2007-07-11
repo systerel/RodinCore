6da7bbf74508bbb8cf3f0d5daadee34d9165ea02
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-07-11 15:54:37 +0000

    - Added manual inference rule for finiteness of relational image of a function:      H |- f : S +-> T      H |- finite(s)  ------------------------------------------                H |- finite(f[s])
    - Added manual inference rule for finiteness of domain of a function:
         H |- f~ : S +-> T      H |- finite(S)
     ------------------------------------------
                   H |- finite(dom(f))
    
    - Added manual inference rule for finiteness of range of a function:
         H |- f: S +-> T      H |- finite(S)
     ------------------------------------------
                   H |- finite(ran(f))

---
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtentionTests/FiniteFunRanTests.java
@@ -0,0 +1,77 @@
+package org.eventb.core.seqprover.eventbExtentionTests;
+
+import java.util.List;
+
+import org.eventb.core.ast.IPosition;
+import org.eventb.core.ast.Predicate;
+import org.eventb.core.seqprover.eventbExtensions.Tactics;
+import org.eventb.internal.core.seqprover.eventbExtensions.FiniteFunRan;
+
+/**
+ * Unit tests for the Finite of range of a function reasoner {@link FiniteFunRan}
+ * 
+ * @author htson
+ */
+public class FiniteFunRanTests extends AbstractSingleExpressionInputReasonerTests {
+
+	String P1 = "finite(ran({0 ↦ (3 ↦ 2),1 ↦ (3 ↦ x),1 ↦ (2 ↦ 3)}))";
+
+	String resultP1GoalA = "{x=ℤ}[][][⊤] |- {0 ↦ (3 ↦ 2),1 ↦ (3 ↦ x),1 ↦ (2 ↦ 3)}∈ℕ ⇸ ℕ × ℕ";
+
+	String resultP1GoalB = "{x=ℤ}[][][⊤] |- finite(ℕ)";
+		
+	String P2 = "x = 1 ⇒ finite(ran({0 ↦ (3 ↦ 2),1 ↦ (3 ↦ x),1 ↦ (2 ↦ 3)}))";
+
+	String P3 = "finite({0 ↦ 3,1 ↦ x,1 ↦ 2}[{x}])";
+
+	protected String [] getTestGetPositions() {
+		return new String [] {
+				P1, "",
+				P2, "",
+				P3, ""
+		};
+	}
+
+	protected List<IPosition> getPositions(Predicate predicate) {
+		return Tactics.finiteFunRanGetPositions(predicate);
+	}
+	
+	@Override
+	public String getReasonerID() {
+		return "org.eventb.core.seqprover.finiteFunRan";
+	}
+
+	protected SuccessfulTest[] getSuccessfulTests() {
+		return new SuccessfulTest[] {
+				// P1 in goal
+				new SuccessfulTest(" ⊤ |- " + P1, null, "ℕ⇸ℕ × ℕ", resultP1GoalA,
+						resultP1GoalB),
+		};
+	}
+
+	protected String[] getUnsuccessfulTests() {
+		return new String[] {
+				// P1 in goal
+				" ⊤ |- " + P1,
+				null,
+				"ℕ ↔ BOOL × ℕ",
+				"Expected a set of all partial functions S ⇸ T",
+				// P1 in goal
+				" ⊤ |- " + P1,
+				null,
+				"ℕ ⇸ BOOL × ℕ",
+				"Type check failed for " + "{0 ↦ (3 ↦ 2),1 ↦ (3 ↦ x),1 ↦ (2 ↦ 3)}∈ℕ ⇸ BOOL × ℕ",
+				// P2 in goal
+				" ⊤ |- " + P2,
+				null,
+				"ℕ",
+				"Goal is not a finiteness",
+				// P3 in goal
+				" ⊤ |- " + P3,
+				null,
+				"ℕ",
+				"Goal is not a finiteness of range of a relation"
+		};
+	}
+
+}
---
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtentionTests/FiniteFunRelImgTests.java
@@ -0,0 +1,62 @@
+package org.eventb.core.seqprover.eventbExtentionTests;
+
+import java.util.List;
+
+import org.eventb.core.ast.IPosition;
+import org.eventb.core.ast.Predicate;
+import org.eventb.core.seqprover.eventbExtensions.Tactics;
+import org.eventb.internal.core.seqprover.eventbExtensions.FiniteFunRelImg;
+
+/**
+ * Unit tests for the Finite of relational image of a function reasoner
+ * {@link FiniteFunRelImg}
+ * 
+ * @author htson
+ */
+public class FiniteFunRelImgTests extends AbstractEmptyInputReasonerTests {
+
+	String P1 = "(x = 2) ⇒ finite({0 ↦ (3 ↦ 2),1 ↦ (3 ↦ x),1 ↦ (2 ↦ 3)}[S])";
+
+	String P2 = "∀x· x = 2 ⇒ finite({0 ↦ (3 ↦ 2),1 ↦ (3 ↦ x),1 ↦ (2 ↦ 3)}[S])";
+
+	String P3 = "finite({0 ↦ (3 ↦ 2),1 ↦ (3 ↦ x),1 ↦ (2 ↦ 3)}[S])";
+
+	String resultP3GoalA = "{S=ℙ(ℤ), x=ℤ}[][][⊤] |- {0 ↦ (3 ↦ 2),1 ↦ (3 ↦ x),1 ↦ (2 ↦ 3)}∈ℤ ⇸ ℤ × ℤ";
+
+	String resultP3GoalB = "{S=ℙ(ℤ), x=ℤ}[][][⊤] |- finite(S)";
+	
+	protected List<IPosition> getPositions(Predicate predicate) {
+		return Tactics.finiteFunRelImgGetPositions(predicate);
+	}
+
+	@Override
+	public String getReasonerID() {
+		return "org.eventb.core.seqprover.finiteFunRelImg";
+	}
+
+	protected SuccessfulTest[] getSuccessfulTests() {
+		return new SuccessfulTest[] {
+				// P3 in goal
+				new SuccessfulTest(" ⊤ |- " + P3, resultP3GoalA, resultP3GoalB)
+		};
+	}
+
+	protected String[] getUnsuccessfulTests() {
+		return new String[] {
+				// P1 in goal
+				" ⊤ |- " + P1,
+				// P2 in goal
+				" ⊤ |- " + P2
+		};
+	}
+
+	@Override
+	protected String[] getTestGetPositions() {
+		return new String[] {
+				P1, "",
+				P2, "",
+				P3, "",
+		};
+	}
+
+}
