344766e994b4494e15885144d053646a84cf542e
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-08-07 15:47:59 +0000

    Added up and down buttons to the before and after hyperlinks.

--- org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/editpage/AbstractHyperlinkComposite.java
+++ org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/editpage/AbstractHyperlinkComposite.java
@@ -4,8 +4,13 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.events.HyperlinkAdapter;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.ImageHyperlink;
+import org.eventb.internal.ui.EventBImage;
 import org.eventb.internal.ui.eventbeditor.EventBEditorUtils;
+import org.eventb.ui.IEventBSharedImages;
 import org.rodinp.core.IInternalElement;
 import org.rodinp.core.IInternalElementType;
 import org.rodinp.core.IInternalParent;
@@ -22,6 +27,10 @@ public abstract class AbstractHyperlinkComposite {
 	
 	IInternalElementType<? extends IInternalElement> type;
 
+	ImageHyperlink upHyperlink;
+	
+	ImageHyperlink downHyperlink;
+	
 	public AbstractHyperlinkComposite(EditPage page, IInternalParent parent,
 			IInternalElementType<? extends IInternalElement> type,
 			FormToolkit toolkit, Composite compParent) {
@@ -37,7 +46,7 @@ public abstract class AbstractHyperlinkComposite {
 		GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
 		composite.setLayoutData(gridData);
 		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
+		gridLayout.numColumns = 4;
 		gridLayout.marginWidth = 0;
 		gridLayout.marginHeight = 0;
 		composite.setLayout(gridLayout);
@@ -58,5 +67,33 @@ public abstract class AbstractHyperlinkComposite {
 
 	public void createHyperlinks(FormToolkit toolkit, int level) {
 		initialised = true;
+		upHyperlink = toolkit.createImageHyperlink(
+				composite, SWT.TOP);
+		upHyperlink.setImage(EventBImage
+				.getImage(IEventBSharedImages.IMG_UP));
+		upHyperlink.addHyperlinkListener(new HyperlinkAdapter() {
+
+			@Override
+			public void linkActivated(HyperlinkEvent e) {
+				page.move(true);
+			}
+
+		});
+		upHyperlink.setLayoutData(new GridData());
+		
+		downHyperlink = toolkit.createImageHyperlink(
+				composite, SWT.TOP);
+		downHyperlink.setImage(EventBImage
+				.getImage(IEventBSharedImages.IMG_DOWN));
+		downHyperlink.addHyperlinkListener(new HyperlinkAdapter() {
+
+			@Override
+			public void linkActivated(HyperlinkEvent e) {
+				page.move(false);
+			}
+
+		});
+		downHyperlink.setLayoutData(new GridData());
 	}
+
 }
