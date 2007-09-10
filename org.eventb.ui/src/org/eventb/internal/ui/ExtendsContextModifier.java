79d652a8c17a54ba3c2ef7d5ca11b0215c9dd55f
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-09-10 15:37:15 +0000

    Comment added.

--- org.eventb.ui/src/org/eventb/internal/ui/ExtendsContextModifier.java
+++ org.eventb.ui/src/org/eventb/internal/ui/ExtendsContextModifier.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2006 ETH Zurich.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Rodin @ ETH Zurich
+ ******************************************************************************/
 package org.eventb.internal.ui;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -6,10 +17,21 @@ import org.eventb.ui.IElementModifier;
 import org.rodinp.core.IRodinElement;
 import org.rodinp.core.RodinDBException;
 
+/**
+ * @author htson
+ *         <p>
+ *         A modifier class for extends context elements.
+ *         </p>
+ */
 public class ExtendsContextModifier implements IElementModifier {
 
+	/* (non-Javadoc)
+	 * @see org.eventb.ui.IElementModifier#modify(org.rodinp.core.IRodinElement, java.lang.String)
+	 */
 	public void modify(IRodinElement element, String text)
 			throws RodinDBException {
+		// Try to set the abstract context name if the element is an extends
+		// context element.
 		if (element instanceof IExtendsContext) {
 			IExtendsContext extendsContext = (IExtendsContext) element;
 			String abstractContextName = null;
@@ -19,10 +41,14 @@ public class ExtendsContextModifier implements IElementModifier {
 			catch (RodinDBException e) {
 				// Do nothing
 			}
-			if (abstractContextName == null || !abstractContextName.equals(text))
+			// Try to set the abstract context name if it does not exists
+			// or not equal to the input text.
+			if (abstractContextName == null
+					|| !abstractContextName.equals(text))
 				extendsContext.setAbstractContextName(text,
 						new NullProgressMonitor());
 		}
+		// Do nothing if the element is not an extends context element.
 		return;
 	}
 
