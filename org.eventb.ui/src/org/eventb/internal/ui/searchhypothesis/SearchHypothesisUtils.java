0e8e4af1f3b6e18384832b9f6fc92f814cb6f165
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-10-03 14:55:31 +0000

    Fixed debug message.

--- org.eventb.ui/src/org/eventb/internal/ui/searchhypothesis/SearchHypothesisUtils.java
+++ org.eventb.ui/src/org/eventb/internal/ui/searchhypothesis/SearchHypothesisUtils.java
@@ -1,7 +1,5 @@
 package org.eventb.internal.ui.searchhypothesis;
 
-import org.eventb.internal.ui.eventbeditor.EventBEditorUtils;
-
 public class SearchHypothesisUtils {
 
 	public static boolean DEBUG = false;
@@ -9,7 +7,7 @@ public class SearchHypothesisUtils {
 	public final static String DEBUG_PREFIX = "*** SearchHypothesis *** ";
 
 	public static void debug(String message) {
-		System.out.println(EventBEditorUtils.DEBUG_PREFIX + message);
+		System.out.println(DEBUG_PREFIX + message);
 	}
 
 }
