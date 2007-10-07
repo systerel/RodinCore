8f37ec60e00286f249d2fa8b94eba76928c0857d
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-10-07 18:23:56 +0000

    - Externalized the strings. - Comment added.

--- org.eventb.ui/src/org/eventb/internal/ui/searchhypothesis/SearchHypothesisComposite.java
+++ org.eventb.ui/src/org/eventb/internal/ui/searchhypothesis/SearchHypothesisComposite.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2007 ETH Zurich.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Rodin @ ETH Zurich
+ ******************************************************************************/
+
 package org.eventb.internal.ui.searchhypothesis;
 
 import java.util.ArrayList;
@@ -20,41 +32,73 @@ import org.eventb.core.seqprover.eventbExtensions.Tactics;
 import org.eventb.internal.ui.EventBImage;
 import org.eventb.internal.ui.prover.HypothesisComposite;
 import org.eventb.internal.ui.prover.ProverUI;
+import org.eventb.internal.ui.utils.Messages;
 import org.eventb.ui.IEventBSharedImages;
 import org.rodinp.core.RodinDBException;
 
 public class SearchHypothesisComposite extends HypothesisComposite {
 
+	// Tool item for adding the selected (searched) hypotheses to the set of
+	// selected hypotheses.
 	ToolItem addItem;
 	
+	// Tool item for removing the selected (searched) hypotheses out of the
+	// searched hypotheses.
 	ToolItem removeItem;
 
+	// Tool item for inverse the current selection.
 	ToolItem inverseSelection;
 
+	// Tool item for select all searched hypotheses.
 	ToolItem selectAll;
 	
+	// Tool item for de-select all searched hypothesis 
 	ToolItem selectNone;
-	
+
+	/**
+	 * Constructor.
+	 * <p>
+	 * 
+	 * @param userSupport
+	 *            the User Support associated with this searched hypothesis
+	 *            composite.
+	 * @param proverUI
+	 *            the main prover editor associated with this searched
+	 *            hypothesis composite.
+	 */
 	public SearchHypothesisComposite(IUserSupport userSupport,
 			ProverUI proverUI) {
+		// Create a hypothesis composite which listens to the changes for:
+		// Current Proof Tree Node, changes in the Proof Tree itself and changes
+		// for the search.
 		super(userSupport, IProofStateDelta.F_NODE
 				| IProofStateDelta.F_PROOFTREE | IProofStateDelta.F_SEARCH,
 				proverUI);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eventb.internal.ui.prover.HypothesisComposite#createItems(org.eclipse.swt.widgets.ToolBar)
+	 */
 	@Override
 	public void createItems(ToolBar toolBar) {
+		// Create item for adding hypotheses.
 		addItem = new ToolItem(toolBar, SWT.PUSH);
 		addItem.setImage(EventBImage.getImage(IEventBSharedImages.IMG_ADD));
-		addItem.setToolTipText("Select hypotheses");
+		addItem
+				.setToolTipText(Messages.searchHypothesis_toolItem_add_toolTipText);
 		addItem.addSelectionListener(new SelectionListener() {
 
 			public void widgetDefaultSelected(SelectionEvent e) {
-				IUserSupport userSupport = SearchHypothesisComposite.this.getUserSupport();
+				IUserSupport userSupport = SearchHypothesisComposite.this
+						.getUserSupport();
 				assert userSupport != null;
 				
-				Set<Predicate> selected = SearchHypothesisComposite.this.getSelectedHyps();
-				ITactic t = Tactics.mngHyp(ProverFactory.makeSelectHypAction(selected));
+				Set<Predicate> selected = SearchHypothesisComposite.this
+						.getSelectedHyps();
+				ITactic t = Tactics.mngHyp(ProverFactory
+						.makeSelectHypAction(selected));
 				try {
 					userSupport.applyTacticToHypotheses(t, selected, true,
 							new NullProgressMonitor());
@@ -70,16 +114,21 @@ public class SearchHypothesisComposite extends HypothesisComposite {
 			
 		});
 
+		// Create item for remove hypotheses.
 		removeItem = new ToolItem(toolBar, SWT.PUSH);
-		removeItem.setImage(EventBImage.getImage(IEventBSharedImages.IMG_REMOVE));
-		removeItem.setToolTipText("Remove hypotheses");
+		removeItem.setImage(EventBImage
+				.getImage(IEventBSharedImages.IMG_REMOVE));
+		removeItem
+				.setToolTipText(Messages.searchHypothesis_toolItem_remove_toolTipText);
 		removeItem.addSelectionListener(new SelectionListener() {
 
 			public void widgetDefaultSelected(SelectionEvent e) {
-				IUserSupport userSupport = SearchHypothesisComposite.this.getUserSupport();
+				IUserSupport userSupport = SearchHypothesisComposite.this
+						.getUserSupport();
 				assert userSupport != null;
-				
-				Set<Predicate> deselected = SearchHypothesisComposite.this.getSelectedHyps();
+
+				Set<Predicate> deselected = SearchHypothesisComposite.this
+						.getSelectedHyps();
 				userSupport.removeSearchedHypotheses(deselected);
 			}
 
@@ -89,9 +138,12 @@ public class SearchHypothesisComposite extends HypothesisComposite {
 			
 		});
 		
+		// Create item for select all (cached) hypotheses.
 		selectAll = new ToolItem(toolBar, SWT.PUSH);
-		selectAll.setImage(EventBImage.getImage(IEventBSharedImages.IMG_SELECT_ALL));
-		selectAll.setToolTipText("Select all");
+		selectAll.setImage(EventBImage
+				.getImage(IEventBSharedImages.IMG_SELECT_ALL));
+		selectAll
+				.setToolTipText(Messages.searchHypothesis_toolItem_selectAll_toolTipText);
 		selectAll.addSelectionListener(new SelectionListener() {
 
 			public void widgetDefaultSelected(SelectionEvent e) {
@@ -104,9 +156,11 @@ public class SearchHypothesisComposite extends HypothesisComposite {
 			
 		});
 
+		// Create item for inverting the current selection.
 		inverseSelection = new ToolItem(toolBar, SWT.PUSH);
 		inverseSelection.setImage(EventBImage.getImage(IEventBSharedImages.IMG_INVERSE));
-		inverseSelection.setToolTipText("Inverse selection");
+		inverseSelection
+				.setToolTipText(Messages.searchHypothesis_toolItem_inverseSelection_toolTipText);
 		inverseSelection.addSelectionListener(new SelectionListener() {
 
 			public void widgetDefaultSelected(SelectionEvent e) {
@@ -119,9 +173,12 @@ public class SearchHypothesisComposite extends HypothesisComposite {
 			
 		});
 
+		// Create item for de-select all cached hypotheses.
 		selectNone = new ToolItem(toolBar, SWT.PUSH);
-		selectNone.setImage(EventBImage.getImage(IEventBSharedImages.IMG_SELECT_NONE));
-		selectNone.setToolTipText("Deselect all");
+		selectNone.setImage(EventBImage
+				.getImage(IEventBSharedImages.IMG_SELECT_NONE));
+		selectNone
+				.setToolTipText(Messages.searchHypothesis_toolItem_selectNone_toolTipText);
 		selectNone.addSelectionListener(new SelectionListener() {
 
 			public void widgetDefaultSelected(SelectionEvent e) {
@@ -136,12 +193,20 @@ public class SearchHypothesisComposite extends HypothesisComposite {
 
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eventb.internal.ui.prover.HypothesisComposite#getHypotheses(org.eventb.core.pm.IProofState)
+	 */
 	@Override
 	public Iterable<Predicate> getHypotheses(IProofState ps) {
+		// Get the searched hypotheses associated with the proof state.
 		Collection<Predicate> searched = new ArrayList<Predicate>();
 		if (ps != null) {
 			searched = ps.getSearched();
 		}
+
+		// Return the valid searched hypotheses only.
 		Collection<Predicate> validSearched = new ArrayList<Predicate>();
 		for (Predicate search : searched) {
 			if (ps.getCurrentNode().getSequent().containsHypothesis(search))
@@ -150,17 +215,36 @@ public class SearchHypothesisComposite extends HypothesisComposite {
 		return validSearched;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eventb.internal.ui.prover.HypothesisComposite#updateToolbarItems()
+	 */
 	@Override
 	public void updateToolbarItems() {
+		if (SearchHypothesisUtils.DEBUG)
+			SearchHypothesisUtils.debug("Update Tool Item: Add, Remove");
 		addItem.setEnabled(!this.getSelectedHyps().isEmpty());
 		removeItem.setEnabled(!this.getSelectedHyps().isEmpty());
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.swt.events.SelectionListener#widgetDefaultSelected(org.eclipse.swt.events.SelectionEvent)
+	 */
 	public void widgetDefaultSelected(SelectionEvent e) {
+		// Update the status of the toolbar items.
 		updateToolbarItems();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.swt.events.SelectionListener#widgetSelected(org.eclipse.swt.events.SelectionEvent)
+	 */
 	public void widgetSelected(SelectionEvent e) {
+		// Behave as the default selected.
 		widgetDefaultSelected(e);
 	}
 
