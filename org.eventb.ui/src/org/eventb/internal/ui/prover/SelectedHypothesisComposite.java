44ee4297de66e281f92211f41c574cfab465d78a
Thai Son Hoang <tshoang@users.sourceforge.net>
2007-10-08 14:04:24 +0000

    Comment added.

--- org.eventb.ui/src/org/eventb/internal/ui/prover/SelectedHypothesisComposite.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/SelectedHypothesisComposite.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2007 ETH Zurich.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Rodin @ ETH Zurich
+ ******************************************************************************/
+
 package org.eventb.internal.ui.prover;
 
 import java.util.ArrayList;
@@ -19,20 +31,41 @@ import org.eventb.core.seqprover.ITactic;
 import org.eventb.core.seqprover.ProverFactory;
 import org.eventb.core.seqprover.eventbExtensions.Tactics;
 import org.eventb.internal.ui.EventBImage;
+import org.eventb.internal.ui.utils.Messages;
 import org.eventb.ui.IEventBSharedImages;
 import org.rodinp.core.RodinDBException;
 
 public class SelectedHypothesisComposite extends HypothesisComposite {
 
-	ToolItem removeItem;
-	
-	ToolItem inverseSelection;
+	// Tool item for removing the selected hypotheses.
+	private ToolItem removeItem;
 
-	ToolItem selectAll;
+	// Tool item for inverse the current selection.
+	private ToolItem inverseSelection;
+
+	// Tool item for select all selected hypotheses.
+	private ToolItem selectAll;
 	
-	ToolItem selectNone;
+	// Tool item for de-select all selected hypothesis 
+	private ToolItem selectNone;
 	
-	ScrolledForm parentForm;
+	// The parent scrolled form of this selected hypothesis composite.
+	private ScrolledForm parentForm;
+
+	/**
+	 * Constructor.
+	 * <p>
+	 * 
+	 * @param userSupport
+	 *            the User Support associated with this selected hypothesis
+	 *            composite.
+	 * @param parentForm
+	 *            the parent scrolled form of this selected hypothesis
+	 *            composite.
+	 * @param proverUI
+	 *            the main prover editor associated with this selected
+	 *            hypothesis composite.
+	 */
 	public SelectedHypothesisComposite(IUserSupport userSupport,
 			ScrolledForm parentForm, ProverUI proverUI) {
 		super(userSupport, IProofStateDelta.F_NODE
@@ -40,17 +73,27 @@ public class SelectedHypothesisComposite extends HypothesisComposite {
 		this.parentForm = parentForm;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eventb.internal.ui.prover.HypothesisComposite#createItems(org.eclipse.swt.widgets.ToolBar)
+	 */
 	@Override
 	public void createItems(ToolBar toolBar) {
+		// Create item for remove hypotheses.
 		removeItem = new ToolItem(toolBar, SWT.PUSH);
-		removeItem.setImage(EventBImage.getImage(IEventBSharedImages.IMG_REMOVE));
-		removeItem.setToolTipText("Remove hypotheses");
+		removeItem.setImage(EventBImage
+				.getImage(IEventBSharedImages.IMG_REMOVE));
+		removeItem
+				.setToolTipText(Messages.selectedHypothesis_toolItem_remove_toolTipText);
 		removeItem.addSelectionListener(new SelectionListener() {
 
 			public void widgetDefaultSelected(SelectionEvent e) {
 				assert userSupport != null;
-				Set<Predicate> deselected = SelectedHypothesisComposite.this.getSelectedHyps();
-				ITactic t = Tactics.mngHyp(ProverFactory.makeDeselectHypAction(deselected));
+				Set<Predicate> deselected = SelectedHypothesisComposite.this
+						.getSelectedHyps();
+				ITactic t = Tactics.mngHyp(ProverFactory
+						.makeDeselectHypAction(deselected));
 				try {
 					userSupport.applyTacticToHypotheses(t, deselected, true,
 							new NullProgressMonitor());
@@ -66,9 +109,12 @@ public class SelectedHypothesisComposite extends HypothesisComposite {
 			
 		});
 
+		// Create item for select all (cached) hypotheses.
 		selectAll = new ToolItem(toolBar, SWT.PUSH);
-		selectAll.setImage(EventBImage.getImage(IEventBSharedImages.IMG_SELECT_ALL));
-		selectAll.setToolTipText("Select all");
+		selectAll.setImage(EventBImage
+				.getImage(IEventBSharedImages.IMG_SELECT_ALL));
+		selectAll
+				.setToolTipText(Messages.selectedHypothesis_toolItem_selectAll_toolTipText);
 		selectAll.addSelectionListener(new SelectionListener() {
 
 			public void widgetDefaultSelected(SelectionEvent e) {
@@ -80,10 +126,13 @@ public class SelectedHypothesisComposite extends HypothesisComposite {
 			}
 			
 		});
-
+		
+		// Create item for inverting the current selection.
 		inverseSelection = new ToolItem(toolBar, SWT.PUSH);
-		inverseSelection.setImage(EventBImage.getImage(IEventBSharedImages.IMG_INVERSE));
-		inverseSelection.setToolTipText("Inverse selection");
+		inverseSelection.setImage(EventBImage
+				.getImage(IEventBSharedImages.IMG_INVERSE));
+		inverseSelection
+				.setToolTipText(Messages.selectedHypothesis_toolItem_inverseSelection_toolTipText);
 		inverseSelection.addSelectionListener(new SelectionListener() {
 
 			public void widgetDefaultSelected(SelectionEvent e) {
@@ -96,10 +145,13 @@ public class SelectedHypothesisComposite extends HypothesisComposite {
 			}
 			
 		});
-
+		
+		// Create item for de-select all cached hypotheses.
 		selectNone = new ToolItem(toolBar, SWT.PUSH);
-		selectNone.setImage(EventBImage.getImage(IEventBSharedImages.IMG_SELECT_NONE));
-		selectNone.setToolTipText("Deselect all");
+		selectNone.setImage(EventBImage
+				.getImage(IEventBSharedImages.IMG_SELECT_NONE));
+		selectNone
+				.setToolTipText(Messages.selectedHypothesis_toolItem_selectNone_toolTipText);
 		selectNone.addSelectionListener(new SelectionListener() {
 
 			public void widgetDefaultSelected(SelectionEvent e) {
@@ -114,6 +166,11 @@ public class SelectedHypothesisComposite extends HypothesisComposite {
 
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eventb.internal.ui.prover.HypothesisComposite#getHypotheses(org.eventb.core.pm.IProofState)
+	 */
 	@Override
 	public Iterable<Predicate> getHypotheses(IProofState ps) {
 		if (ps != null) {
@@ -124,25 +181,48 @@ public class SelectedHypothesisComposite extends HypothesisComposite {
 		return new ArrayList<Predicate>();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eventb.internal.ui.prover.HypothesisComposite#updateToolbarItems()
+	 */
 	@Override
 	public void updateToolbarItems() {
 		removeItem.setEnabled(!this.getSelectedHyps().isEmpty());
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.swt.events.SelectionListener#widgetDefaultSelected(org.eclipse.swt.events.SelectionEvent)
+	 */
 	public void widgetDefaultSelected(SelectionEvent e) {
+		// Update the status of the toolbar items.
 		updateToolbarItems();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.swt.events.SelectionListener#widgetSelected(org.eclipse.swt.events.SelectionEvent)
+	 */
 	public void widgetSelected(SelectionEvent e) {
+		// Behave as the default selected.
 		widgetDefaultSelected(e);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eventb.internal.ui.prover.HypothesisComposite#refresh()
+	 */
 	@Override
-	void refresh() {
+	protected void refresh() {
+		// Override this method to re-layout the parent scrolled form and scroll
+		// to the bottom of the hypothesis rows.
 		super.refresh();
 		parentForm.reflow(true);
 		scrollToBottom();
 	}
 
-	
 }
