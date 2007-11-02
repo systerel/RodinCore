39ece5640214e34fc91d65cf8b428bb55d99a310
Laurent Voisin <lvoisin@users.sourceforge.net>
2007-11-02 22:36:16 +0000

    Fixed typo in comment + rerun formatter.

--- org.eventb.core/src/org/eventb/core/sc/state/IContextPointerArray.java
+++ org.eventb.core/src/org/eventb/core/sc/state/IContextPointerArray.java
@@ -16,91 +16,100 @@ import org.eventb.core.tool.state.IStateType;
 import org.rodinp.core.IInternalElement;
 
 /**
- * A context pointer array serves to manage references to contexts.
- * The structure of context relationships is a directed acyclic graph
- * which is flattened by the static checker. The context pointer array
- * is used to keep track of conclicts caused by multiple sees in a machine
- * or extends clauses in a context. Conflicts are caused whenever an identifier,
- * carrier set or constant, is declared in two <b>different</b> contexts that
- * are (transitively) referenced by a context or machine.
+ * A context pointer array serves to manage references to contexts. The
+ * structure of context relationships is a directed acyclic graph which is
+ * flattened by the static checker. The context pointer array is used to keep
+ * track of conflicts caused by multiple sees in a machine or extends clauses in
+ * a context. Conflicts are caused whenever an identifier, carrier set or
+ * constant, is declared in two <b>different</b> contexts that are
+ * (transitively) referenced by a context or machine.
  * <p>
  * This interface is not intended to be implemented by clients.
  * </p>
  * 
  * @author Stefan Hallerstede
- *
+ * 
  */
 public interface IContextPointerArray extends ISCState {
 
-	final static IStateType<IContextPointerArray> STATE_TYPE = 
-		SCCore.getToolStateType(EventBPlugin.PLUGIN_ID + ".contextPointerArray");
+	final static IStateType<IContextPointerArray> STATE_TYPE = SCCore
+			.getToolStateType(EventBPlugin.PLUGIN_ID + ".contextPointerArray");
 
 	/**
-	 * The enumerated type <code>PointerType</code> is used determine whether this
-	 * context pointer array is used with sees clauses in a machine or extends
-	 * clauses in a context.
+	 * The enumerated type <code>PointerType</code> is used determine whether
+	 * this context pointer array is used with sees clauses in a machine or
+	 * extends clauses in a context.
 	 * 
 	 */
 	public enum PointerType {
-		EXTENDS_POINTER,
-		SEES_POINTER
+		EXTENDS_POINTER, SEES_POINTER
 	}
-	
+
 	/**
-	 * Returns the type of context pointers used with this context pointer array.
+	 * Returns the type of context pointers used with this context pointer
+	 * array.
 	 * 
 	 * @return the type of context pointers used with this context pointer array
 	 */
 	PointerType getContextPointerType();
-	
+
 	/**
-	 * Returns whether the error flag of the context pointer with the specified index is set.
+	 * Returns whether the error flag of the context pointer with the specified
+	 * index is set.
 	 * 
-	 * @param index the index of the context pointer
-	 * @return whether the error flag of the context pointer with the specified index is set
+	 * @param index
+	 *            the index of the context pointer
+	 * @return whether the error flag of the context pointer with the specified
+	 *         index is set
 	 */
 	boolean hasError(int index);
-	
+
 	/**
-	 * Returns the number of context pointers contained in this context pointer array.
+	 * Returns the number of context pointers contained in this context pointer
+	 * array.
 	 * 
-	 * @return the number of context pointers contained in this context pointer array
+	 * @return the number of context pointers contained in this context pointer
+	 *         array
 	 */
 	int size();
-	
+
 	/**
-	 * Returns the index in this context pointer array of the context with the specified name.
+	 * Returns the index in this context pointer array of the context with the
+	 * specified name.
 	 * 
-	 * @param contextName the name of the context
+	 * @param contextName
+	 *            the name of the context
 	 * @return the index of the context
 	 */
 	int getPointerIndex(String contextName);
-	
+
 	/**
-	 * Returns the context pointer associated with the specified index.
-	 * This is either {@link IExtendsContext} or {@link ISeesContext} 
-	 * depending on the pointer type ({@link PointerType}) of this context pointer array.
+	 * Returns the context pointer associated with the specified index. This is
+	 * either {@link IExtendsContext} or {@link ISeesContext} depending on the
+	 * pointer type ({@link PointerType}) of this context pointer array.
 	 * 
-	 * @param index the index of the context pointer
+	 * @param index
+	 *            the index of the context pointer
 	 * @return the context pointer with the specified index
 	 */
 	IInternalElement getContextPointer(int index);
-	
+
 	/**
-	 * Returns a reference to the statically checked context file associated with the 
-	 * specified index.
-	 * This is the statically checked context retrieved by means of 
-	 * {@link IExtendsContext} or {@link ISeesContext}. This is a convenience method
-	 * to facilitate access to the referenced (statically checked) contexts.
+	 * Returns a reference to the statically checked context file associated
+	 * with the specified index. This is the statically checked context
+	 * retrieved by means of {@link IExtendsContext} or {@link ISeesContext}.
+	 * This is a convenience method to facilitate access to the referenced
+	 * (statically checked) contexts.
 	 * <p>
-	 * If the returned value is <code>null</code> for some index, this means that 
-	 * no file has been specified and the index should be ignored.
+	 * If the returned value is <code>null</code> for some index, this means
+	 * that no file has been specified and the index should be ignored.
 	 * </p>
 	 * 
-	 * @param index the index of the statically checked context file
-	 * @return a reference to the statically checked context file file associated with the 
-	 * specified index
+	 * @param index
+	 *            the index of the statically checked context file
+	 * @return a reference to the statically checked context file file
+	 *         associated with the specified index
 	 */
 	ISCContextFile getSCContextFile(int index);
-	
+
 }
