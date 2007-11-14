7429f3036f19ad8c64a18aa52810661cf3982ac4
Laurent Voisin <lvoisin@users.sourceforge.net>
2007-11-14 17:15:30 +0000

    Removed use of timer task that was never canceled and caused a memory leak.

--- org.eventb.pp/src/org/eventb/internal/pp/PPProverCall.java
+++ org.eventb.pp/src/org/eventb/internal/pp/PPProverCall.java
@@ -10,37 +10,37 @@ package org.eventb.internal.pp;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import org.eventb.core.ast.Predicate;
 import org.eventb.core.seqprover.IProofMonitor;
 import org.eventb.core.seqprover.xprover.XProverCall;
 import org.eventb.core.seqprover.xprover.XProverInput;
+import org.eventb.pp.IPPMonitor;
 import org.eventb.pp.PPProof;
+import org.eventb.pp.PPResult;
 import org.eventb.pp.PPResult.Result;
 
 /**
  * Implementation of {@link XProverCall} for PP.
- *
+ * 
  * @author François Terrier
- *
+ * 
  */
-public class PPProverCall extends XProverCall {
+public class PPProverCall extends XProverCall implements IPPMonitor {
 
-	private PPProof prover; 
-	private int maxSteps;
-	
-	public PPProverCall(XProverInput input, Iterable<Predicate> hypothesis, Predicate goal, IProofMonitor pm) {
-		super(hypothesis,goal,pm);
-		
-		maxSteps = ((PPInput)input).getMaxSteps();
-		this.prover = new PPProof(hypotheses,this.goal);
+	private final int maxSteps;
+	private PPResult result;
+
+	public PPProverCall(XProverInput input, Iterable<Predicate> hypothesis,
+			Predicate goal, IProofMonitor pm) {
+		super(hypothesis, goal, pm);
+		maxSteps = ((PPInput) input).getMaxSteps();
 	}
-	
+
 	@Override
 	public void cleanup() {
-		// nothing
+		// Run the garbage collector
+		System.gc();
 	}
 
 	@Override
@@ -50,38 +50,41 @@ public class PPProverCall extends XProverCall {
 
 	@Override
 	public boolean isValid() {
-		return prover.getResult().getResult()==Result.valid;
+		if (result == null) {
+			throw new IllegalStateException("isValid() called before run().");
+		}
+		return result.getResult() == Result.valid;
 	}
-
-	private static final long DEFAULT_PERIOD = 317;
 
 	@Override
 	public void run() {
-		final PPProof prover = this.prover;
-		
-		Timer timer = new Timer(true);
-		timer.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				if (isCancelled()) {
-					prover.cancel();
-				}
-			}
-		}, DEFAULT_PERIOD, DEFAULT_PERIOD);
-		
+		final PPProof prover = new PPProof(hypotheses, goal);
 		prover.translate();
 		prover.load();
-		prover.prove(maxSteps);
+		prover.prove(maxSteps, this);
+		result = prover.getResult();
 	}
-	
+
 	@Override
 	public boolean isGoalNeeded() {
-		return prover.getResult().getTracer().isGoalNeeded();
+		if (result == null) {
+			throw new IllegalStateException(
+					"isGoalNeeded() called before run().");
+		}
+		return result.getTracer().isGoalNeeded();
 	}
 
 	@Override
 	public Set<Predicate> neededHypotheses() {
-		return new HashSet<Predicate>(prover.getResult().getTracer().getNeededHypotheses());
+		if (result == null) {
+			throw new IllegalStateException(
+					"neededHypotheses() called before run().");
+		}
+		return new HashSet<Predicate>(result.getTracer().getNeededHypotheses());
+	}
+
+	public boolean isCanceled() {
+		return isCancelled();
 	}
 
 }
