771fc538c4db17b56046e855b8d5509fad458eda
Laurent Voisin <lvoisin@users.sourceforge.net>
2007-12-26 16:53:58 +0000

    Fully refactored TermBuilder so that it doesn't use the Visitor design pattern. Here this design pattern makes things more complicated than they should be.

--- org.eventb.pp/src/org/eventb/internal/pp/loader/predicate/TermBuilder.java
+++ org.eventb.pp/src/org/eventb/internal/pp/loader/predicate/TermBuilder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 ETH Zurich.
+ * Copyright (c) 2006,2007 ETH Zurich.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,7 +16,6 @@ import org.eventb.core.ast.AssociativeExpression;
 import org.eventb.core.ast.AtomicExpression;
 import org.eventb.core.ast.BinaryExpression;
 import org.eventb.core.ast.BoundIdentifier;
-import org.eventb.core.ast.DefaultVisitor;
 import org.eventb.core.ast.Expression;
 import org.eventb.core.ast.FreeIdentifier;
 import org.eventb.core.ast.IntegerLiteral;
@@ -37,154 +36,137 @@ import org.eventb.internal.pp.loader.formula.terms.VariableSignature;
 
 /**
  * This class is the builder for terms.
- *
+ * 
  * @author François Terrier
- *
+ * 
  */
-public class TermBuilder extends DefaultVisitor {
-	
-	private Stack<TermSignature> terms;
-	private Stack<NormalizedFormula> results;
-	
+public class TermBuilder {
+
+	private final Stack<NormalizedFormula> results;
+
 	public TermBuilder(Stack<NormalizedFormula> results) {
 		assert results != null;
-
 		this.results = results;
 	}
-	
-	public TermSignature buildTerm(Expression expression) {
-		assert expression != null;
-		
-		this.terms = new Stack<TermSignature>();
-		
-		expression.accept(this);
-		
-		assert terms.size() == 1;
-		return terms.pop();
-	}
-	
-	@Override
-	public boolean exitDIV(BinaryExpression expr) {
-		assert terms.size() == 2;
-		
-		TermSignature right = terms.pop();
-		TermSignature left = terms.pop();
-		TermSignature div = new DivideSignature(left, right);
-		terms.push(div);
-		return true;
+
+	public TermSignature buildTerm(Expression expr) {
+		return process(expr);
 	}
 
-	@Override
-	public boolean exitEXPN(BinaryExpression expr) {
-		assert terms.size() == 2;
-		
-		TermSignature right = terms.pop();
-		TermSignature left = terms.pop();
-		TermSignature expn = new ExpnSignature(left, right);
-		terms.push(expn);
-		return true;
+	public TermSignature process(Expression expr) {
+		switch (expr.getTag()) {
+		case Expression.DIV:
+			return processDIV((BinaryExpression) expr);
+		case Expression.EXPN:
+			return processEXPN((BinaryExpression) expr);
+		case Expression.MINUS:
+			return processMINUS((BinaryExpression) expr);
+		case Expression.MOD:
+			return processMOD((BinaryExpression) expr);
+		case Expression.MUL:
+			return processMUL((AssociativeExpression) expr);
+		case Expression.PLUS:
+			return processPLUS((AssociativeExpression) expr);
+		case Expression.UNMINUS:
+			return processUNMINUS((UnaryExpression) expr);
+		case Expression.BOUND_IDENT:
+			return processBOUND_IDENT((BoundIdentifier) expr);
+		case Expression.FREE_IDENT:
+			return processFREE_IDENT((FreeIdentifier) expr);
+		case Expression.INTLIT:
+			return processINTLIT((IntegerLiteral) expr);
+		case Expression.TRUE:
+			return processTRUE((AtomicExpression) expr);
+			// TODO case of predefined sets BOOL and INTEGER
+		default:
+			throw new IllegalArgumentException("Invalid term: " + expr);
+		}
 	}
 
-	@Override
-	public boolean exitMINUS(BinaryExpression expr) {
-		assert terms.size() == 2;
-		
-		TermSignature right = terms.pop();
-		TermSignature left = terms.pop();
-		TermSignature minus = new MinusSignature(left, right);
-		terms.push(minus);
-		return true;
+	public TermSignature processDIV(BinaryExpression expr) {
+		final TermSignature left = process(expr.getLeft());
+		final TermSignature right = process(expr.getRight());
+		return new DivideSignature(left, right);
 	}
 
-	@Override
-	public boolean exitMOD(BinaryExpression expr) {
-		assert terms.size() == 2;
-		
-		TermSignature right = terms.pop();
-		TermSignature left = terms.pop();
-		TermSignature mod = new ModSignature(left, right);
-		terms.push(mod);
-		return true;
+	public TermSignature processEXPN(BinaryExpression expr) {
+		final TermSignature left = process(expr.getLeft());
+		final TermSignature right = process(expr.getRight());
+		return new ExpnSignature(left, right);
 	}
 
-	@Override
-	public boolean exitMUL(AssociativeExpression expr) {
-		List<TermSignature> temp = new ArrayList<TermSignature>();
-		temp.addAll(terms);
-		TermSignature times = new TimesSignature(temp);
-		terms.removeAllElements();
-		terms.push(times);
-		return true;
+	public TermSignature processMINUS(BinaryExpression expr) {
+		final TermSignature left = process(expr.getLeft());
+		final TermSignature right = process(expr.getRight());
+		return new MinusSignature(left, right);
 	}
 
-	@Override
-	public boolean exitPLUS(AssociativeExpression expr) {
-		List<TermSignature> temp = new ArrayList<TermSignature>();
-		temp.addAll(terms);
-		TermSignature plus = new PlusSignature(temp);
-		terms.removeAllElements();
-		terms.push(plus);
-		return true;
+	public TermSignature processMOD(BinaryExpression expr) {
+		final TermSignature left = process(expr.getLeft());
+		final TermSignature right = process(expr.getRight());
+		return new ModSignature(left, right);
 	}
 
-	@Override
-	public boolean exitUNMINUS(UnaryExpression expr) {
-		assert terms.size() == 1;
-		
-		TermSignature minus = new UnaryMinusSignature(terms.pop());
-		terms.push(minus);
-		return true;
+	public TermSignature processMUL(AssociativeExpression expr) {
+		final List<TermSignature> children = new ArrayList<TermSignature>();
+		for (Expression child : expr.getChildren()) {
+			children.add(process(child));
+		}
+		return new TimesSignature(children);
 	}
 
-	@Override
-	public boolean visitBOUND_IDENT(BoundIdentifier ident) {
-		int index = getIndex(ident.getBoundIndex());
-		TermSignature boundIdent = new VariableSignature(getStartIndex(ident.getBoundIndex()), index, new Sort(ident.getType()));
-		terms.push(boundIdent);
-		return true;
+	public TermSignature processPLUS(AssociativeExpression expr) {
+		final List<TermSignature> children = new ArrayList<TermSignature>();
+		for (Expression child : expr.getChildren()) {
+			children.add(process(child));
+		}
+		return new PlusSignature(children);
 	}
 
-	@Override
-	public boolean visitFREE_IDENT(FreeIdentifier ident) {
-		TermSignature freeIdent = new ConstantSignature(ident.getName(), new Sort(ident.getType()));
-		terms.push(freeIdent);
-		return true;
+	public TermSignature processUNMINUS(UnaryExpression expr) {
+		final TermSignature child = process(expr.getChild());
+		return new UnaryMinusSignature(child);
 	}
 
-	@Override
-	public boolean visitINTLIT(IntegerLiteral lit) {
+	public TermSignature processBOUND_IDENT(BoundIdentifier ident) {
+		final int index = getIndex(ident.getBoundIndex());
+		return new VariableSignature(getStartIndex(ident.getBoundIndex()),
+				index, new Sort(ident.getType()));
+	}
+
+	public TermSignature processFREE_IDENT(FreeIdentifier ident) {
+		return new ConstantSignature(ident.getName(), new Sort(ident.getType()));
+	}
+
+	public TermSignature processINTLIT(IntegerLiteral lit) {
 		assert new Sort(lit.getType()).equals(Sort.NATURAL);
-		TermSignature intlit = new IntegerSignature(lit.getValue());
-		terms.push(intlit);
-		return true;
+		return new IntegerSignature(lit.getValue());
 	}
 
-	@Override
-	public boolean visitTRUE(AtomicExpression expr) {
-		TermSignature tr = new TrueConstantSignature(new Sort(expr.getType()));
-		terms.push(tr);
-		return true;
+	public TermSignature processTRUE(AtomicExpression expr) {
+		return new TrueConstantSignature(new Sort(expr.getType()));
 	}
 
 	private int getIndex(int boundIndex) {
 		int tmp = 0;
-		int i = results.size()-1;
-		
+		int i = results.size() - 1;
+
 		while (i > 0) {
-			tmp = tmp+results.get(i).getBoundIdentDecls().length;
+			tmp = tmp + results.get(i).getBoundIdentDecls().length;
 			i--;
 		}
-		return tmp-1-boundIndex;
+		return tmp - 1 - boundIndex;
 	}
-	
+
 	private int getStartIndex(int boundIndex) {
 		int tmp = boundIndex;
-		int i = results.size()-1;
-		
+		int i = results.size() - 1;
+
 		while (tmp >= results.get(i).getBoundIdentDecls().length) {
 			tmp = tmp - results.get(i).getBoundIdentDecls().length;
 			i--;
 		}
-		return (results.get(i).getBoundIdentDecls().length-tmp-1)+results.get(i).getStartAbsolute();
+		return (results.get(i).getBoundIdentDecls().length - tmp - 1)
+				+ results.get(i).getStartAbsolute();
 	}
 }
