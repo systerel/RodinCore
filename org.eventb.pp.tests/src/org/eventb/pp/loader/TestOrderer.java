588fc112533478e056d183ba79b1d5bc60a7a8df
Laurent Voisin <lvoisin@users.sourceforge.net>
2007-12-28 09:31:07 +0000

    Simplified the testing machinery. Restored some commented out tests.

--- org.eventb.pp.tests/src/org/eventb/pp/loader/TestOrderer.java
+++ org.eventb.pp.tests/src/org/eventb/pp/loader/TestOrderer.java
@@ -1,138 +1,65 @@
 package org.eventb.pp.loader;
 
-import java.util.Arrays;
-import java.util.List;
-
-import junit.framework.TestCase;
-
-import org.eventb.core.ast.FormulaFactory;
 import org.eventb.core.ast.ITypeEnvironment;
+import org.eventb.core.ast.Predicate;
+import org.eventb.internal.pp.core.elements.terms.AbstractPPTest;
 import org.eventb.internal.pp.core.elements.terms.Util;
 import org.eventb.internal.pp.loader.predicate.INormalizedFormula;
 import org.eventb.internal.pp.loader.predicate.PredicateBuilder;
 
+public class TestOrderer extends AbstractPPTest {
 
-public class TestOrderer extends TestCase {
-	private static FormulaFactory ff = FormulaFactory.getDefault();
-	
-	class TestPair {
-		List<String> predicate;
-		
-		TestPair (String... predicate) {
-			this.predicate = Arrays.asList(predicate);
-		}
-	}
-	
-//	private static Term a = cCons("a");
-//	private static Term b = cCons("b");
-//	private static Term c = cCons("c");
-//	private static Term d = cCons("d");
-//	private static Term e = cCons("e");
-//	private static Term n = cCons("n");
-//	private static Term D = cCons("D");
-//	private static Term M = cCons("M");
-//	private static Term P = cCons("P");
-//	private static Term Q = cCons("Q");
-//	private static Term R = cCons("R");
-//	private static Term S = cCons("S");
-//	private static Term SS = cCons("SS");
-//	private static Term T = cCons("T");
-//	private static Term U = cCons("U");
-//	private static Term one = cCons("1");
-//	private static Term TRUE = cCons("TRUE");
-
-	private static ITypeEnvironment env = ff.makeTypeEnvironment();
+	private static ITypeEnvironment typenv = ff.makeTypeEnvironment();
 	static {
-		env.addName("x0", ff.makeGivenType("A"));
-		env.addName("x1", ff.makeGivenType("B"));
-		env.addName("a", ff.makeGivenType("S"));
-//		env.addName("b", ff.makeGivenType("S"));
-		
-//		env.addName("d", ff.makeIntegerType());
-		env.addName("e", ff.makeBooleanType());
-		env.addName("f", ff.makePowerSetType(ff.makeProductType(ff.makeGivenType("A"), ff.makeGivenType("B"))));
-		env.addName("n", ff.makeIntegerType());
-		env.addName("N", ff.makePowerSetType(ff.makeIntegerType()));
-		env.addName("S", ff.makePowerSetType(ff.makeGivenType("S")));
-		env.addName("P", ff.makePowerSetType(ff.makeGivenType("B")));
-		env.addName("Q", ff.makePowerSetType(ff.makeGivenType("A")));
-		env.addName("R", ff.makePowerSetType(ff.makeGivenType("A")));
-		env.addName("U", ff.makePowerSetType(ff.makeGivenType("U")));
-		env.addName("M", ff.makePowerSetType(ff.makeProductType(ff.makeProductType(ff.makeGivenType("B"),ff.makeGivenType("A")),ff.makeGivenType("A"))));
-		env.addName("SS", ff.makePowerSetType(ff.makeGivenType("S")));
-		env.addName("T", ff.makePowerSetType(ff.makeProductType(ff.makeIntegerType(), ff.makeIntegerType())));
-		env.addName("TT", ff.makePowerSetType(ff.makeProductType(ff.makeProductType(ff.makeIntegerType(),ff.makeIntegerType()), ff.makeIntegerType())));
+		typenv.addName("x0", ty_A);
+		typenv.addName("x1", ty_B);
+		typenv.addName("a", ty_S);
+		typenv.addName("e", ty_BOOL);
+		typenv.addName("f", REL(ty_A, ty_B));
+		typenv.addName("n", INT);
+		typenv.addName("N", POW(INT));
+		typenv.addName("S", POW(ty_S));
+		typenv.addName("P", POW(ty_B));
+		typenv.addName("Q", POW(ty_A));
+		typenv.addName("R", POW(ty_A));
+		typenv.addName("U", POW(ty_U));
+		typenv.addName("M", REL(CPROD(ty_B, ty_A), ty_A));
+		typenv.addName("SS", POW(ty_S));
+		typenv.addName("T", REL(INT, INT));
+		typenv.addName("TT", REL(CPROD(INT, INT), INT));
 	}
-	
-//	private static List<Literal<?,?>> noLit = new ArrayList<Literal<?,?>>();
-	
-	TestPair[] testOrdering = new TestPair[] {
-			new TestPair(
-					"a ∈ S ∨ d ∈ U","d ∈ U ∨ a ∈ S"
-			),
-			new TestPair(
-					"a ∈ S ∨ ¬(a ∈ S)","¬(a ∈ S) ∨ a ∈ S"
-			),
-			new TestPair(
-					"a = b ∨ a ∈ S", "a ∈ S ∨ a = b"
-			),
-//			new TestPair(
-//					"a = b ∨ n = 1", "n = 1 ∨ a = b"
-//			),
-			new TestPair(
-					"a = b ∨ ¬(a = b)", "¬(a = b) ∨ a = b"
-			),
-			new TestPair(
-					"a = b ∨ ¬(a ∈ S)", "¬(a ∈ S) ∨ a = b"
-			),
-			new TestPair(
-					"¬(a = b) ∨ a ∈ S", "a ∈ S ∨ ¬(a = b)"
-			),
-//			new TestPair(
-//					"n < 1 ∨ a = b", "a = b ∨ n < 1"
-//			),
-			new TestPair(
-					"¬(a ∈ S ∨ d ∈ U) ∨ a = b", "a = b ∨ ¬(a ∈ S ∨ d ∈ U)"
-			),
-			new TestPair(
-					"¬(a ∈ S ∨ d ∈ U) ∨ b ∈ S", "b ∈ S ∨ ¬(a ∈ S ∨ d ∈ U)"
-			),
-			new TestPair(
-					"(∀x·x ∈ S) ∨ (∀x·x ∈ U)", "(∀x·x ∈ U) ∨ (∀x·x ∈ S)"
-			),
-			new TestPair(
-					"a ∈ S ∨ (∀x·x ∈ U)", "(∀x·x ∈ U) ∨ a ∈ S"
-			),
-			new TestPair(
-					"a = b ∨ (∀x·x ∈ U)", "(∀x·x ∈ U) ∨ a = b"
-			),
-	};
-	
-	TestPair[] testTermOrdering = new TestPair[] {
-			
-	};
-	
-	public void doTest(List<String> strPredicate) {
+
+	public static void doTest(String... images) {
+		final PredicateBuilder builder = new PredicateBuilder();
 		INormalizedFormula formula = null;
-		PredicateBuilder builder = new PredicateBuilder();
 		int index = 0;
-		for (String predicate : strPredicate) {
-			builder.build(Util.parsePredicate(predicate, env),false);
-			INormalizedFormula newFormula = builder.getContext().getResults().get(index++);
+		for (String image : images) {
+			final Predicate pred = Util.parsePredicate(image, typenv);
+			builder.build(pred, false);
+			INormalizedFormula newFormula = builder.getContext().getResults()
+					.get(index++);
 			if (formula == null) {
 				formula = newFormula;
 				continue;
 			}
 			assertEquals(formula, newFormula);
 		}
-		
 	}
-	
-	
+
 	public void testOrdering() {
-		for (TestPair test : testOrdering) {
-			doTest(test.predicate);
-		}
+		doTest("a ∈ S ∨ d ∈ U", "d ∈ U ∨ a ∈ S");
+		doTest("a ∈ S ∨ ¬(a ∈ S)", "¬(a ∈ S) ∨ a ∈ S");
+		doTest("a = b ∨ a ∈ S", "a ∈ S ∨ a = b");
+		doTest("a = b ∨ n = 1", "n = 1 ∨ a = b");
+		doTest("a = b ∨ ¬(a = b)", "¬(a = b) ∨ a = b");
+		doTest("a = b ∨ ¬(a ∈ S)", "¬(a ∈ S) ∨ a = b");
+		doTest("¬(a = b) ∨ a ∈ S", "a ∈ S ∨ ¬(a = b)");
+		doTest("n < 1 ∨ a = b", "a = b ∨ n < 1");
+		doTest("¬(a ∈ S ∨ d ∈ U) ∨ a = b", "a = b ∨ ¬(a ∈ S ∨ d ∈ U)");
+		doTest("¬(a ∈ S ∨ d ∈ U) ∨ b ∈ S", "b ∈ S ∨ ¬(a ∈ S ∨ d ∈ U)");
+		doTest("(∀x·x ∈ S) ∨ (∀x·x ∈ U)", "(∀x·x ∈ U) ∨ (∀x·x ∈ S)");
+		doTest("a ∈ S ∨ (∀x·x ∈ U)", "(∀x·x ∈ U) ∨ a ∈ S");
+		doTest("a = b ∨ (∀x·x ∈ U)", "(∀x·x ∈ U) ∨ a = b");
 	}
 
 }
