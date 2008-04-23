35b58f4129c59da457af492f3b5138a5bb09d16a
Laurent Voisin <lvoisin@users.sourceforge.net>
2008-04-23 11:25:28 +0000

    Updated Javadoc.

--- org.eventb.core.seqprover/src/org/eventb/core/seqprover/IProofTreeNode.java
+++ org.eventb.core.seqprover/src/org/eventb/core/seqprover/IProofTreeNode.java
@@ -2,36 +2,36 @@ package org.eventb.core.seqprover;
 
 import org.eventb.core.seqprover.tactics.BasicTactics;
 
-
 /**
  * Common protocol for a proof tree node.
  * 
  * <p>
- * Each proof tree node contains a proof sequent (see {@link IProofSequent}). A node has 
- * children proof tree nodes iff a rule has been applied to it. Nodes that have no 
- * rule applied to them are termed "open". Note that there is a difference between a 
- * proof tree node having no children (no rule applied, open), and a proof tree node 
- * having 0 children (rule with 0 anticidents applied, closed).
- * 
+ * Each proof tree node contains a proof sequent (see {@link IProofSequent}). A
+ * node has children proof tree nodes iff a rule has been applied to it. Nodes
+ * that have no rule applied to them are termed "open". Note that there is a
+ * difference between a proof tree node having no children (no rule applied,
+ * open), and a proof tree node having 0 children (rule with 0 antecedents
+ * applied, closed).
+ * </p>
+ * <p>
  * Proof Nodes are either "pending" or "closed" :
  * <ul>
- * <li>Pending - This node is either open, or has at least one open descendent. The 
- * proof attempt for its sequent is not yet complete</li>
- * <li>Closed - This node has no open descendents. The proof attempt for its sequent 
- * is complete.
+ * <li>Pending - This node is either open, or has at least one open descendant.
+ * The proof attempt for its sequent is not yet complete</li>
+ * <li>Closed - This node has no open descendants. The proof attempt for its
+ * sequent is complete.</li>
  * </ul>
  * </p>
  * <p>
- * Each node in addition has a confidence level associated to it 
- * (see {@see org.eventb.core.prover.IConfidence}):
+ * Each node in addition has a confidence level associated to it (see
+ * {@see org.eventb.core.prover.IConfidence}):
  * <ul>
- * <li>Pending nodes have their confidence levels set to PENDING, which is the 
+ * <li>Pending nodes have their confidence levels set to PENDING, which is the
  * minimum confidence level and is reserved for this purpose. </li>
- * <li>The confidence level for a closed node is the minimum of the confidence level 
- * of the rule associated to it, and the confidence levels of its children.
- * </li>
- * <ul>
- * 
+ * <li>The confidence level for a closed node is the minimum of the confidence
+ * level of the rule associated to it, and the confidence levels of its
+ * children. </li>
+ * </ul>
  * </p>
  * <p>
  * This interface is not intended to be implemented by clients.
@@ -76,16 +76,16 @@ public interface IProofTreeNode extends IProofSkeleton {
 	 * this node in preorder.
 	 * <p>
 	 * If this node is already open, it will be returned. Otherwise, this method
-	 * will traverse the proof tree to which this node belongs, in pre-order and
+	 * will traverse the proof tree to which this node belongs, in preorder and
 	 * return the first open node encountered after traversing this node.
 	 * </p>
 	 * 
-	 * @return the next open node after this node in pre-order or
+	 * @return the next open node after this node in preorder or
 	 *         <code>null</code> if this node has been discharged
 	 * @see #getFirstOpenDescendant()
 	 */
 	IProofTreeNode getNextOpenNode();
-	
+
 	/**
 	 * Returns all open descendants of this node.
 	 * <p>
@@ -95,7 +95,7 @@ public interface IProofTreeNode extends IProofSkeleton {
 	 * </p>
 	 * <p>
 	 * This node is considered a descendant of itself. Hence, calling this
-	 * method on an open node returns an array containg one element: the open
+	 * method on an open node returns an array containing one element: the open
 	 * node itself.
 	 * </p>
 	 * <p>
@@ -169,22 +169,24 @@ public interface IProofTreeNode extends IProofSkeleton {
 	 * @return <code>true</code> iff this node is open (no rule applied to it)
 	 */
 	boolean isOpen();
-	
+
 	/**
-	 * Copies the sub proof tree corresping to the current node and returns a new
-	 * proof tree corresponding to this sub tree
+	 * Copies the sub proof tree rooted at the current node and returns a
+	 * new proof tree corresponding to this sub tree
 	 * 
 	 * @return A new proof tree with the root identical to the current node
 	 */
 	IProofTree copySubTree();
-	
+
 	/**
-	 * Extracts the proof skeleton rooted in the current node and returns it
+	 * Extracts the proof skeleton rooted in the current node and returns it.
+	 * The returned skeleton is then independent of the proof tree of this node:
+	 * it will not be affected by modifications of this proof tree.
 	 * 
 	 * @return A new proof skeleton rooted at this node
 	 */
 	IProofSkeleton copyProofSkeleton();
-	
+
 	/**
 	 * Returns the comment field of the current proof tree node.
 	 * 
@@ -192,22 +194,21 @@ public interface IProofTreeNode extends IProofSkeleton {
 	 * @see #setComment()
 	 */
 	String getComment();
-	
-	
+
 	/**
 	 * Returns the confidence of this proof tree node.
 	 * 
 	 * @return the confidence of this proof tree node (see {@see IConfidence})
 	 */
 	int getConfidence();
-	
+
 	/**
 	 * Applies the given rule to this node.
 	 * 
 	 * <p>
 	 * Note that outside the sequent prover, tactics provide a more uniform way
 	 * to modify proof tree nodes and should be used instead of directly calling
-	 * this method. 
+	 * this method.
 	 * </p>
 	 * 
 	 * @param rule
@@ -223,46 +224,46 @@ public interface IProofTreeNode extends IProofSkeleton {
 	 * <p>
 	 * Note that outside the sequent prover, tactics provide a more uniform way
 	 * to modify proof tree nodes and should be used instead of directly calling
-	 * this method. 
+	 * this method.
 	 * </p>
 	 * 
 	 * @see ITactic
 	 * 
-	 * @return the child subtrees that have been pruned, or <code>null</code> if no rule 
-	 * was applied to this node. 
+	 * @return the child subtrees that have been pruned, or <code>null</code>
+	 *         if no rule was applied to this node.
 	 */
 	IProofTree[] pruneChildren();
-	
+
 	/**
 	 * Sets the comment field of the current proof tree node.
 	 * 
 	 * @param comment
-	 * 	 The new comment for the current proof tree node
+	 *            The new comment for the current proof tree node
 	 * 
 	 * @see #getComment()
 	 */
 	void setComment(String comment);
-	
+
 	/**
-	 * Grafts a proof tree to this proof node if it is open, and its sequent is identical
-	 * to the root sequent of the given proof tree. 
+	 * Grafts a proof tree to this proof node if it is open, and its sequent is
+	 * identical to the root sequent of the given proof tree.
 	 * 
 	 * A successful graft has the effect of pruning the input proof tree.
 	 * 
 	 * <p>
 	 * Note that outside the sequent prover, tactics provide a more uniform way
 	 * to modify proof tree nodes and should be used instead of directly calling
-	 * this method. 
+	 * this method.
 	 * </p>
 	 * 
 	 * @see ITactic
 	 * 
-	 * @return <code>true</code> iff the operation suceeded.
+	 * @return <code>true</code> iff the operation succeeded.
 	 * 
-	 * @deprecated
-	 * Instead first extract the proof skeleton using {@link #copyProofSkeleton()} and
-	 * then use one of the proof reconstruction tactics in {@link BasicTactics}.
-	 *  
+	 * @deprecated Instead first extract the proof skeleton using
+	 *             {@link #copyProofSkeleton()} and then use one of the proof
+	 *             reconstruction tactics in {@link BasicTactics}.
+	 * 
 	 */
 	@Deprecated
 	boolean graft(IProofTree tree);
@@ -275,7 +276,7 @@ public interface IProofTreeNode extends IProofSkeleton {
 	 * <p>
 	 * If <code>rootIncluded</code> is true and if this node satisfies the
 	 * filter, it will be returned. Otherwise, this method will traverse the
-	 * proof tree to which this node belongs, in pre-order and return the first
+	 * proof tree to which this node belongs, in preorder and return the first
 	 * node satisfies the filter encountered after traversing this node.
 	 * </p>
 	 * 
@@ -286,7 +287,7 @@ public interface IProofTreeNode extends IProofSkeleton {
 	 *            a proof tree node filter
 	 * 
 	 * @return the next node satisfies the input filter after this node in
-	 *         pre-order or <code>null</code> if there is no such node.
+	 *         preorder or <code>null</code> if there is no such node.
 	 */
 	IProofTreeNode getNextNode(boolean rootIncluded, IProofTreeNodeFilter filter);
 
