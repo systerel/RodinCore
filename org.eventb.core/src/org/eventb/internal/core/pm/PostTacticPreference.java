0cf167939814b10f5193c3d234113e93df04762b
Laurent Voisin <lvoisin@users.sourceforge.net>
2008-04-24 19:18:41 +0000

    Implementation of FR #1949932: add auto tactic for case split on override: added FunOvrGoalTac tactic to auto and post tactic preferences (incl. default).

--- org.eventb.core/src/org/eventb/internal/core/pm/PostTacticPreference.java
+++ org.eventb.core/src/org/eventb/internal/core/pm/PostTacticPreference.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2008 ETH Zurich and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     ETH Zurich - initial API and implementation
+ *     Systerel - added funOvrGoalTac to the default
+ *******************************************************************************/
 package org.eventb.internal.core.pm;
 
 import org.eventb.core.EventBPlugin;
@@ -41,6 +52,7 @@ public class PostTacticPreference extends AutoTacticPreference {
 	            "org.eventb.core.seqprover.eqHypTac",
 	            "org.eventb.core.seqprover.shrinkImpHypTac",
 	            "org.eventb.core.seqprover.shrinkEnumHypTac",
+	            "org.eventb.core.seqprover.funOvrGoalTac",
 	            "org.eventb.core.seqprover.clarifyGoalTac"
 		};
 
