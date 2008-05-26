383b2f5ac063c2ec2b2b4a18197e26b1022a81ec
Laurent Voisin <lvoisin@users.sourceforge.net>
2008-05-26 16:25:29 +0000

    Quick fix for bug #1965795: NAT in the proof command.

--- org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/SearchHypotheses.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/SearchHypotheses.java
@@ -3,6 +3,7 @@ package org.eventb.internal.ui.prover.tactics;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eventb.core.ast.Predicate;
 import org.eventb.core.pm.IUserSupport;
+import org.eventb.eventBKeyboard.Text2EventBMathTranslator;
 import org.eventb.internal.ui.UIUtils;
 import org.eventb.internal.ui.searchhypothesis.SearchHypothesis;
 import org.eventb.ui.prover.IProofCommand;
@@ -12,7 +13,9 @@ public class SearchHypotheses implements IProofCommand {
 
 	public void apply(IUserSupport us, Predicate hyp, String[] inputs,
 			IProgressMonitor monitor) throws RodinDBException {
-		us.searchHyps(inputs[0]);
+		// TODO should have been fixed at the root (the text widget) rather than here
+		final String input = Text2EventBMathTranslator.translate(inputs[0]);
+		us.searchHyps(input);
 		// Trying to show the Search View
 		UIUtils.showView(SearchHypothesis.VIEW_ID);
 	}
