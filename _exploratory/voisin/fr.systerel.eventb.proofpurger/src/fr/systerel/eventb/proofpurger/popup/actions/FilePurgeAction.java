840f837ee5fba3c6cca3f3c754b74638607e53bb
Laurent Voisin <lvoisin@users.sourceforge.net>
2008-06-30 15:02:15 +0000

    Added analysis of the current selection as a list of PR files.

--- _exploratory/voisin/fr.systerel.eventb.proofpurger/src/fr/systerel/eventb/proofpurger/popup/actions/FilePurgeAction.java
+++ _exploratory/voisin/fr.systerel.eventb.proofpurger/src/fr/systerel/eventb/proofpurger/popup/actions/FilePurgeAction.java
@@ -1,5 +1,10 @@
 package fr.systerel.eventb.proofpurger.popup.actions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
@@ -9,15 +14,17 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchPartSite;
+import org.eventb.core.IEventBFile;
+import org.eventb.core.IPRFile;
 import org.rodinp.core.IRodinElement;
+import org.rodinp.core.IRodinProject;
+import org.rodinp.core.RodinDBException;
 
 public class FilePurgeAction implements IObjectActionDelegate {
 	
-	private static final Object[] NO_OBJECTS = new Object[0];
-	
 	private IWorkbenchPartSite site;
 
-	private ISelection selection;
+	private IStructuredSelection selection;
 	
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
 		site = targetPart.getSite();
@@ -29,15 +36,27 @@ public class FilePurgeAction implements IObjectActionDelegate {
 
 	public void run(IAction action) {
 		
-		final Object[] sel = getSelectedObjects();
-		final StringBuilder b = new StringBuilder();
-		for (Object o: sel) {
-			final IRodinElement e = ((IRodinElement) o);
-			b.append('\t');
-			b.append(e.getCorrespondingResource().getFullPath());
-			b.append('\n');
+		final IPRFile[] files;
+		try {
+			files = getSelectedPRFiles();
+		} catch (RodinDBException e) {
+			// TODO Display the DB error to the user
+			return;
 		}
 		
+		if (files == null) {
+			// TODO Indicate invalid selection to the user
+			return;
+		}
+		
+		final StringBuilder b = new StringBuilder();
+		for (IPRFile file: files) {
+			b.append('\t');
+			b.append(file.getPath());
+			b.append('\n');
+		}
+
+		// TODO Implement a progress monitor here.
 		BusyIndicator.showWhile(site.getShell().getDisplay(), new Runnable() {
 			public void run() {
 				collectInformation();
@@ -53,16 +72,48 @@ public class FilePurgeAction implements IObjectActionDelegate {
 			"Purge unused proofs was executed on\n" + b.toString());
 	}
 
-	private Object[] getSelectedObjects() {
-		if (selection instanceof IStructuredSelection) {
-			final IStructuredSelection ss = (IStructuredSelection) selection;
-			return ss.toArray();
+	private IPRFile[] getSelectedPRFiles() throws RodinDBException {
+		if (selection == null) {
+			return null;
 		}
-		return NO_OBJECTS;
+		final List<IPRFile> result = new ArrayList<IPRFile>(selection.size());
+		for (Object o : selection.toList()) {
+			final IRodinElement elem = asRodinElement(o);
+			if (elem instanceof IRodinProject) {
+				addProjectPRFiles(result, (IRodinProject) elem);
+			} else if (elem instanceof IEventBFile) {
+				result.add(((IEventBFile) elem).getPRFile());
+			} else {
+				// Unexpected openable or null
+				return null;
+			}
+		}
+		return result.toArray(new IPRFile[result.size()]);
+	}
+
+	private IRodinElement asRodinElement(Object o) {
+		if (o instanceof IRodinElement) {
+			return (IRodinElement) o;
+		}
+		if (o instanceof IAdaptable) {
+			final IAdaptable adaptable = (IAdaptable) o;
+			return (IRodinElement) adaptable.getAdapter(IRodinElement.class); 
+		}
+		return null;
+	}
+
+	private void addProjectPRFiles(List<IPRFile> list, IRodinProject prj)
+			throws RodinDBException {
+		final IPRFile[] prjFiles = prj.getChildrenOfType(IPRFile.ELEMENT_TYPE);
+		list.addAll(Arrays.asList(prjFiles));
 	}
 
 	public void selectionChanged(IAction action, ISelection s) {
-		selection = s;
+		if (s instanceof IStructuredSelection) {
+			selection = (IStructuredSelection) s;
+		} else {
+			selection = null;
+		}
 	}
 
 }
