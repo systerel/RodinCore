1ff35c834990edf4c3849ea839b63b876cbd6e39
Laurent Voisin <lvoisin@users.sourceforge.net>
2008-08-18 19:47:58 +0000

    added constant DEFAULT_CONFIGURATION to make life easier for clients.

--- org.eventb.core/src/org/eventb/core/IConfigurationElement.java
+++ org.eventb.core/src/org/eventb/core/IConfigurationElement.java
@@ -1,11 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2008 University of Southampton.
+ * Copyright (c) 2008 University of Southampton and others.
+ * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Soton - initial API and implementation
+ *     Systerel - added DEFAULT_CONFIGURATION
  *******************************************************************************/
 package org.eventb.core;
+
+import static org.eventb.core.EventBPlugin.PLUGIN_ID;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eventb.internal.core.tool.types.IModule;
@@ -18,16 +25,18 @@ import org.rodinp.core.RodinDBException;
 
 /**
  * The contents of Event-B models is interpreted with respect to configurations:
- * a configuration describes a certain composition of modules of the static checker 
- * and the proof obligation generator.
+ * a configuration describes a certain composition of modules of the static
+ * checker and the proof obligation generator.
  * <p>
- * The value of the configuration, a string, is used to retrieve instances of static checkers 
- * proof obligation generators. They are declared in extensions of type 
- * <code>configuration</code> contributed to the extension point <code>configurations</code>.
+ * The value of the configuration, a string, is used to retrieve instances of
+ * static checkers proof obligation generators. They are declared in extensions
+ * of type <code>configuration</code> contributed to the extension point
+ * <code>configurations</code>.
  * </p>
  * <p>
  * Each configuration selects a set of processor and filter modules.
  * </p>
+ * 
  * @see IModule
  * @see ISCFilterModule
  * @see ISCProcessorModule
@@ -35,22 +44,30 @@ import org.rodinp.core.RodinDBException;
  * @see IPOGProcessorModule
  * 
  * @author Stefan Hallerstede
- *
+ * 
  */
 public interface IConfigurationElement extends IRodinElement {
 
 	/**
-	 * Set the configuration of this element.
+	 * Default configuration of the Static Checker and POG tools which
+	 * implements forward simulation.
+	 */
+	String DEFAULT_CONFIGURATION = PLUGIN_ID + ".fwd";
+
+	/**
+	 * Sets the configuration of this element.
 	 * 
-	 * @param configuration the specified configuration
+	 * @param configuration
+	 *            the specified configuration
 	 * @param monitor
 	 *            a progress monitor, or <code>null</code> if progress
 	 *            reporting is not desired
 	 * @throws RodinDBException
 	 *             if there was a problem accessing the database
 	 */
-	void setConfiguration(String configuration, IProgressMonitor monitor) throws RodinDBException;
-	
+	void setConfiguration(String configuration, IProgressMonitor monitor)
+			throws RodinDBException;
+
 	/**
 	 * Returns the configuration of this element.
 	 * 
@@ -59,7 +76,7 @@ public interface IConfigurationElement extends IRodinElement {
 	 *             if there was a problem accessing the database
 	 */
 	public String getConfiguration() throws RodinDBException;
-	
+
 	/**
 	 * Returns whether this element has a configuration.
 	 * 
