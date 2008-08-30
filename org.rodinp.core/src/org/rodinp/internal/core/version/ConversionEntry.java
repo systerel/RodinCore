67e220573c4db6d4ac4f50c22a01f64735ca26a6
Laurent Voisin <lvoisin@users.sourceforge.net>
2008-08-30 11:25:13 +0000

    Moved code specific to an entry into the entry as public methods.

--- org.rodinp.core/src/org/rodinp/internal/core/version/ConversionEntry.java
+++ org.rodinp.core/src/org/rodinp/internal/core/version/ConversionEntry.java
@@ -1,39 +1,79 @@
 /*******************************************************************************
- * Copyright (c) 2008 Systerel and others.
+ * Copyright (c) 2007, 2008 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     Systerel - initial API and implementation
+ *     ETH Zurich - initial API and implementation
+ *     Systerel - refactoring and various improvements
  *******************************************************************************/
 package org.rodinp.internal.core.version;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubProgressMonitor;
+import org.rodinp.core.IFileElementType;
+import org.rodinp.core.IRodinDBStatusConstants;
 import org.rodinp.core.IRodinFile;
+import org.rodinp.core.RodinDBException;
 import org.rodinp.core.IConversionResult.IEntry;
 import org.rodinp.core.basis.RodinFile;
+import org.rodinp.internal.core.Buffer;
+import org.rodinp.internal.core.util.Messages;
 
 class ConversionEntry implements IEntry {
 
-	@Override
-	public String toString() {
-		StringBuffer sb = new StringBuffer();
-		for (byte b : buffer) {
-			sb.append((char) b);
-		}
-		return sb.toString();
-	}
-
 	protected final RodinFile file;
+
 	protected long version;
 	protected long reqVersion;
 	protected Exception error;
 	protected String message;
 	protected byte[] buffer;
 
+	public ConversionEntry(IRodinFile file) {
+		this.file = (RodinFile) file;
+	}
+
+	// Consumes one tick of given monitor
+	public void accept(boolean force, boolean keepHistory,
+			IProgressMonitor monitor) throws RodinDBException {
+		if (success() && buffer != null) {
+			file.revert();
+			final ByteArrayInputStream s = new ByteArrayInputStream(buffer);
+			final SubProgressMonitor spm = new SubProgressMonitor(monitor, 1);
+			try {
+				file.getResource().setContents(s, force, keepHistory, spm);
+			} catch (CoreException e) {
+				throw new RodinDBException(e);
+			}
+		} else {
+			monitor.worked(1);
+		}
+	}
+
 	public IRodinFile getFile() {
 		return file;
+	}
+
+	private long getFileVersion(IProgressMonitor pm) throws RodinDBException {
+		final Buffer fileBuffer = new Buffer(file);
+		try {
+			fileBuffer.load(pm);
+			return fileBuffer.getVersion();
+		} catch (RodinDBException e) {
+			final long v = fileBuffer.getVersion();
+			if (v != -1) {
+				return v;
+			} else {
+				throw e;
+			}
+		}
 	}
 
 	public String getMessage() {
@@ -52,7 +92,37 @@ class ConversionEntry implements IEntry {
 		return error == null;
 	}
 
-	public ConversionEntry(IRodinFile file) {
-		this.file = (RodinFile) file;
+	@Override
+	public String toString() {
+		return new String(buffer);
 	}
+
+	// Consumes two ticks of given monitor
+	public void upgrade(VersionManager vManager, boolean force,
+			IProgressMonitor pm) {
+		final IFileElementType<?> type = file.getElementType();
+		try {
+			version = getFileVersion(new SubProgressMonitor(pm, 1));
+			reqVersion = vManager.getVersion(type);
+			if (version == reqVersion) {
+				message = Messages.converter_fileUnchanged;
+				return;
+			}
+			if (version > reqVersion) {
+				error = new RodinDBException(null,
+						IRodinDBStatusConstants.FUTURE_VERSION);
+				message = Messages.converter_failedConversion;
+				return;
+			}
+			final Converter converter = vManager.getConverter(type);
+			final InputStream contents = file.getResource().getContents(force);
+			buffer = converter.convert(contents, version, reqVersion);
+		} catch (Exception e) {
+			error = e;
+			message = Messages.converter_failedConversion;
+		} finally {
+			pm.worked(1);
+		}
+	}
+
 }
