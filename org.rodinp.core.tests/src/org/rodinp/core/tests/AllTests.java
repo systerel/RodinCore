b10c58109c2768f7d0fcf0b71623b71486a9d21d
Michael Jastram <jastram@users.sourceforge.net>
2008-09-08 10:03:09 +0000

    Test suite for automated testing with CruiseControl

---
+++ org.rodinp.core.tests/src/org/rodinp/core/tests/AllTests.java
@@ -0,0 +1,43 @@
+package org.rodinp.core.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.rodinp.core.tests.builder.CBuilderTest;
+import org.rodinp.core.tests.builder.MBuilderTest;
+import org.rodinp.core.tests.version.BasicVersionTest;
+import org.rodinp.core.tests.version.FaultyVersionTest;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for org.rodinp.core.tests.*");
+		// $JUnit-BEGIN$
+		suite.addTestSuite(TestInternalManipulation.class);
+		suite.addTestSuite(DeleteTests.class);
+		suite.addTestSuite(ClearTests.class);
+		suite.addTestSuite(MementoTests.class);
+		suite.addTestSuite(OpenableTests.class);
+		suite.addTestSuite(CopyMoveResourcesTests.class);
+		suite.addTestSuite(RodinElementDeltaTests.class);
+		suite.addTestSuite(TestRodinDB.class);
+		suite.addTestSuite(AttributeTests.class);
+		suite.addTestSuite(MarkerTests.class);
+		suite.addTestSuite(ElementTypeTests.class);
+		suite.addTestSuite(CopyMoveElementsTests.class);
+		suite.addTestSuite(SnapshotTests.class);
+		suite.addTestSuite(TestFileCreation.class);
+		suite.addTestSuite(AdapterFactoryTests.class);
+		suite.addTestSuite(SameContentsTests.class);
+
+		suite.addTestSuite(MBuilderTest.class);
+		suite.addTestSuite(CBuilderTest.class);
+
+		suite.addTestSuite(BasicVersionTest.class);
+		suite.addTestSuite(FaultyVersionTest.class);
+
+		// $JUnit-END$
+		return suite;
+	}
+
+}
