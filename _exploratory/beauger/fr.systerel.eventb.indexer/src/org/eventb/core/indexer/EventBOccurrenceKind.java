09b06472c4aa777a70735febba028f03236a7f22
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2008-09-15 19:01:05 +0000

    Changed OccurrenceKind to an interface. Implemented that interface with enum types.

--- _exploratory/beauger/fr.systerel.eventb.indexer/src/org/eventb/core/indexer/EventBOccurrenceKind.java
+++ _exploratory/beauger/fr.systerel.eventb.indexer/src/org/eventb/core/indexer/EventBOccurrenceKind.java
@@ -1,32 +1,7 @@
 package org.eventb.core.indexer;
 
-import java.io.ObjectStreamException;
-
 import org.rodinp.core.index.OccurrenceKind;
 
-public class EventBOccurrenceKind extends OccurrenceKind {
-
-	protected EventBOccurrenceKind(String name) {
-		super(name);
-	}
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4034811166410265505L;
-
-
-	public static final EventBOccurrenceKind DECLARATION = new EventBOccurrenceKind(
-	"declaration");
-
-	public static final EventBOccurrenceKind REFERENCE = new EventBOccurrenceKind(
-	"reference");
-
-	private static final EventBOccurrenceKind[] PRIVATE_VALUES = { DECLARATION, REFERENCE };
-	
-	private Object readResolve() throws ObjectStreamException {
-		return PRIVATE_VALUES[ordinal];
-	}
-
-
+public enum EventBOccurrenceKind implements OccurrenceKind {
+	DECLARATION, REFERENCE
 }
