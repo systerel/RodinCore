6ac0d93b3bf5575366a3ff1135d316aed4f132a4
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2008-10-06 11:15:01 +0000

    Added missing copyrights.

--- _exploratory/beauger/fr.systerel.indexer.tests/src/org/rodinp/internal/core/index/tables/tests/NodeTests.java
+++ _exploratory/beauger/fr.systerel.indexer.tests/src/org/rodinp/internal/core/index/tables/tests/NodeTests.java
@@ -8,6 +8,16 @@ import org.rodinp.internal.core.index.tables.Node;
 
 import junit.framework.TestCase;
 
+/*******************************************************************************
+ * Copyright (c) 2008 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
 public class NodeTests extends TestCase {
 
 	private static final Integer ONE = new Integer(1);
