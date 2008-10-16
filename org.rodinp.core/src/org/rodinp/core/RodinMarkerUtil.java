8173c550a86fa94108a13904f88583b22776ce4c
Laurent Voisin <lvoisin@users.sourceforge.net>
2008-10-16 17:08:03 +0000

    Fixed copyright.

--- org.rodinp.core/src/org/rodinp/core/RodinMarkerUtil.java
+++ org.rodinp.core/src/org/rodinp/core/RodinMarkerUtil.java
@@ -1,12 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2006 ETH Zurich.
- * Strongly inspired by org.eclipse.jdt.core.IJavaModel.java which is
- * 
- * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation as
+ *     		org.eclipse.jdt.core.IJavaModel
+ *     ETH Zurich - adaptation from JDT to Rodin
  *******************************************************************************/
 package org.rodinp.core;
 
