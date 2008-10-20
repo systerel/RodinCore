967359d8b68cb2f5027bbb022bf32dccb017e8a7
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2008-10-20 09:55:55 +0000

    Added missing class for exception tests.

---
+++ _exploratory/beauger/fr.systerel.indexer.tests/src/org/rodinp/internal/core/index/tests/FakeExceptionIndexer.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
+package org.rodinp.internal.core.index.tests;
+
+import org.rodinp.core.IInternalElement;
+import org.rodinp.core.IRodinFile;
+import org.rodinp.core.index.IIndexer;
+import org.rodinp.core.index.IIndexingToolkit;
+
+public class FakeExceptionIndexer implements IIndexer {
+
+	private static final String ID = "fr.systerel.indexer.tests.fakeexceptionindexer";
+	
+	private static final IRodinFile[] NO_FILES = new IRodinFile[0];
+
+	public IRodinFile[] getDependencies(IInternalElement root) {
+		return NO_FILES;
+	}
+
+	public String getId() {
+		return ID;
+	}
+
+	public void index(IIndexingToolkit index) {
+		int z = 1 / 0;
+		z++;
+	}
+
+}
