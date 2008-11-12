9b3a616f7a7d04057c33cb67c6cfb2edb28c29db
Laurent Voisin <lvoisin@users.sourceforge.net>
2008-11-12 14:57:17 +0000

    Added acceptance test for project build.

---
+++ org.eventb.core.tests/src/org/eventb/core/tests/ProjectBuildTests.java
@@ -0,0 +1,127 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
+package org.eventb.core.tests;
+
+import static org.eclipse.core.resources.IResource.DEPTH_INFINITE;
+
+import java.io.PrintStream;
+import java.util.Map;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eventb.core.IContextRoot;
+import org.eventb.core.IEventBRoot;
+import org.eventb.core.IMachineRoot;
+import org.rodinp.core.IInternalElement;
+import org.rodinp.core.IRodinElement;
+import org.rodinp.core.IRodinProject;
+import org.rodinp.core.RodinDBException;
+
+/**
+ * Tests exercising build on whole projects.
+ * 
+ * @author Laurent Voisin
+ */
+public class ProjectBuildTests extends EventBTest {
+
+	private static final String FWD_CONFIG = "org.eventb.core.fwd";
+
+	private static void assertExists(IRodinElement element) {
+		if (!element.exists()) {
+			fail("Element " + element.getHandleIdentifier() + " should exist");
+		}
+	}
+
+	private static void assertGenerated(IEventBRoot... roots) {
+		for (IEventBRoot root : roots) {
+			assertGenerated(root);
+		}
+	}
+
+	private static void assertGenerated(IEventBRoot root) {
+		if (root instanceof IContextRoot)
+			assertExists(root.getSCContextRoot());
+		if (root instanceof IMachineRoot)
+			assertExists(root.getSCMachineRoot());
+		assertExists(root.getPORoot());
+		assertExists(root.getPSRoot());
+	}
+
+	private static void assertNoMarker(IRodinProject rp) throws CoreException {
+		final IResource res = rp.getResource();
+		final IMarker[] markers = res.findMarkers(null, true, DEPTH_INFINITE);
+		if (markers.length != 0) {
+			System.out.println("Unexpected markers found:");
+			for (IMarker marker: markers) {
+				printMarker(marker, System.out);
+			}
+			fail("Project shouldn't contain any marker");
+		}
+	}
+
+	private static void printMarker(IMarker marker, PrintStream stream)
+			throws CoreException {
+		stream.println(marker.getResource());
+		stream.println(marker.getType());
+		stream.println("Attributes:");
+		final Map<?, ?> attrs = marker.getAttributes();
+		for (Map.Entry<?, ?> e : attrs.entrySet()) {
+			stream.println(e.getKey() + ": " + e.getValue());
+		}
+		stream.println();
+	}
+
+	private static void saveRodinFileFor(IInternalElement element)
+			throws RodinDBException {
+		element.getRodinFile().save(null, true);
+	}
+
+	public ProjectBuildTests() {
+		super();
+	}
+
+	public ProjectBuildTests(String name) {
+		super(name);
+	}
+
+	@Override
+	protected IMachineRoot createMachine(String bareName) throws RodinDBException {
+		IMachineRoot root = super.createMachine(bareName);
+		root.setConfiguration(FWD_CONFIG, null);
+		return root;
+	}
+
+	/**
+	 * Regression test for bug # TODO. Two components depend on a third one, but
+	 * PO are generated only for one of them.
+	 */
+	public void testTwoConcrete() throws Exception {
+		final IMachineRoot abs = createMachine("M1");
+		addInitialisation(abs);
+		saveRodinFileFor(abs);
+
+		final IMachineRoot con1 = createMachine("A1");
+		addMachineRefines(con1, abs.getElementName());
+		addInitialisation(con1);
+		saveRodinFileFor(con1);
+
+		final IMachineRoot con2 = createMachine("A2");
+		addMachineRefines(con2, abs.getElementName());
+		addInitialisation(con2);
+		saveRodinFileFor(con2);
+
+		runBuilder();
+		assertNoMarker(abs.getRodinProject());
+		assertGenerated(abs, con1, con2);
+	}
+
+}
