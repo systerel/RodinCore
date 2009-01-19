41dec1f0670994ca40b12f5bd4a66ba1fa8eaeb8
Laurent Voisin <lvoisin@users.sourceforge.net>
2009-01-19 11:31:22 +0000

    Added tests for illegal internal element types.

--- org.rodinp.core.tests/src/org/rodinp/core/tests/ElementTypeTests.java
+++ org.rodinp.core.tests/src/org/rodinp/core/tests/ElementTypeTests.java
@@ -14,6 +14,7 @@ package org.rodinp.core.tests;
 import junit.framework.TestCase;
 
 import org.rodinp.core.IElementType;
+import org.rodinp.core.IInternalElementType;
 import org.rodinp.core.IRodinDB;
 import org.rodinp.core.IRodinProject;
 import org.rodinp.core.RodinCore;
@@ -30,13 +31,13 @@ public class ElementTypeTests extends TestCase {
 	static void assertElementTypeId(IElementType<?> type, String id) {
 		assertEquals("Wrong id", id, type.getId());
 	}
-	
+
 	static void assertETypePos(IElementType<?> expected) {
-		String id = expected.getId();
-		IElementType<?> actual = RodinCore.getElementType(id);
+		final String id = expected.getId();
+		final IElementType<?> actual = RodinCore.getElementType(id);
 		assertSame("Wrong elementType", expected, actual);
 	}
-	
+
 	static void assertETypeNeg(String id) {
 		try {
 			RodinCore.getElementType(id);
@@ -45,40 +46,76 @@ public class ElementTypeTests extends TestCase {
 			// success
 		}
 	}
-	
+
+	static void assertInternalETypePos(IInternalElementType<?> expected) {
+		final String id = expected.getId();
+		final IElementType<?> actual = RodinCore.getInternalElementType(id);
+		assertSame("Wrong elementType", expected, actual);
+	}
+
+	static void assertInternalETypeNeg(String id) {
+		try {
+			RodinCore.getInternalElementType(id);
+			fail("Should have raised an exception.");
+		} catch (IllegalArgumentException e) {
+			// success
+		}
+	}
+
 	/**
 	 * Ensures that the Rodin database element type can be retrieved.
 	 */
-	public void testDBElementType() throws Exception {
+	public void testElementTypeDB() throws Exception {
 		assertETypePos(IRodinDB.ELEMENT_TYPE);
 	}
 
 	/**
 	 * Ensures that the Rodin project element type can be retrieved.
 	 */
-	public void testProjectElementType() throws Exception {
+	public void testElementTypeProject() throws Exception {
 		assertETypePos(IRodinProject.ELEMENT_TYPE);
 	}
 
 	/**
 	 * Ensures that the test file element type can be retrieved.
 	 */
-	public void testFileElementType() throws Exception {
+	public void testElementTypeFile() throws Exception {
 		assertETypePos(RodinTestFile.ELEMENT_TYPE);
 	}
 
 	/**
 	 * Ensures that the test internal element type can be retrieved.
 	 */
-	public void testInternalElementType() throws Exception {
+	public void testElementTypeInternal() throws Exception {
 		assertETypePos(NamedElement.ELEMENT_TYPE);
 	}
 
 	/**
 	 * Ensures that an unknown element type can't be retrieved.
 	 */
-	public void testUnknownElementType() throws Exception {
+	public void testElementTypeUnknown() throws Exception {
 		assertETypeNeg("unknown element type");
 	}
 
+	/**
+	 * Ensures that the test internal element type can be retrieved.
+	 */
+	public void testInternalElementTypeInternal() throws Exception {
+		assertInternalETypePos(NamedElement.ELEMENT_TYPE);
+	}
+
+	/**
+	 * Ensures that an unknown element type can't be retrieved.
+	 */
+	public void testInternalElementTypeUnknown() throws Exception {
+		assertInternalETypeNeg("unknown element type");
+	}
+
+	/**
+	 * Ensures that a project element type can't be retrieved as an internal
+	 * element type.
+	 */
+	public void testInternalElementTypeWrong() throws Exception {
+		assertInternalETypeNeg(IRodinProject.ELEMENT_TYPE.getId());
+	}
 }
