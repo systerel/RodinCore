f7a0309b52bd6e4f916c2f3caffc600e21ffc25c
Laurent Voisin <lvoisin@users.sourceforge.net>
2009-01-21 17:46:19 +0000

    Fully rewritten all tests.

--- _exploratory/tshoang/org.eventb.contributer.seqprover.fr1798741.tests/src/org/eventb/contributer/seqprover/fr1798741/tests/AutoFormulaRewriterTests.java
+++ _exploratory/tshoang/org.eventb.contributer.seqprover.fr1798741.tests/src/org/eventb/contributer/seqprover/fr1798741/tests/AutoFormulaRewriterTests.java
@@ -1,46 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
 package org.eventb.contributer.seqprover.fr1798741.tests;
 
-import org.eventb.core.ast.Expression;
-import org.eventb.core.ast.IFormulaRewriter;
 import org.eventb.contributer.seqprover.fr1798741.AutoRewriterImpl;
+import org.eventb.core.ast.IFormulaRewriter;
 import org.eventb.core.seqprover.rewriterTests.AbstractFormulaRewriterTests;
 import org.junit.Test;
 
-public class AutoFormulaRewriterTests extends AbstractFormulaRewriterTests 
-{
+public class AutoFormulaRewriterTests extends AbstractFormulaRewriterTests {
+
 	private static final IFormulaRewriter rewriter = new AutoRewriterImpl();
-	public AutoFormulaRewriterTests()
-	{
+
+	public AutoFormulaRewriterTests() {
 		super(rewriter);
 	}
-	
-	@Test
-	public void test_overwriting()
-	{
-		Expression emptySetInteger =
-			ff.makeEmptySet(ff.makePowerSetType(ff.makeIntegerType()), null);
-		Expression emptySetIntegerProduct = ff.makeEmptySet(ff.makePowerSetType(
-				ff.makeProductType(ff.makeIntegerType(), ff.makeIntegerType())), null);
-		
-		Expression inputExp = makeInputExpression("{1↦2}[∅]");
-		expressionTest(emptySetInteger, inputExp);
 
-		inputExp = makeInputExpression("{1↦2, 1↦-2}[∅]");
-		expressionTest(emptySetInteger, inputExp);
-		
-		inputExp = makeInputExpression("{-4↦2, -3↦-2, -1↦1}[∅]");
-		expressionTest(emptySetInteger, inputExp);
-		
-		// ∅[{1, 2, 3}]
-		inputExp = ff.makeBinaryExpression(Expression.RELIMAGE,
-				emptySetIntegerProduct, makeInputExpression("{1, 2, 3}"), null);
-		expressionTest(emptySetInteger, inputExp);
-		
-		// ∅[∅]
-		inputExp = ff.makeBinaryExpression(Expression.RELIMAGE,
-				emptySetIntegerProduct, emptySetInteger, null);
-		expressionTest(emptySetInteger, inputExp);
-		
+	@Test
+	public void test_overwriting() {
+		expressionTest("(∅ ⦂ ℙ(T))", "f[(∅ ⦂ ℙ(S))]", "f", "ℙ(S×T)");
+		expressionTest("(∅ ⦂ ℙ(T))", "(∅ ⦂ ℙ(S×T))[s]", "s", "ℙ(S)");
+		expressionTest("(∅ ⦂ ℙ(T))", "(∅ ⦂ ℙ(S×T))[(∅ ⦂ ℙ(S))]");
 	}
 
 }
