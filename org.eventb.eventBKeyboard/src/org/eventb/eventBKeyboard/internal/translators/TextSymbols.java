583b2c1a690037633f408063a7cae79686a922e0
Thai Son Hoang <tshoang@users.sourceforge.net>
2009-03-23 12:57:50 +0000

    Added "oftype" keyword for U+2982.
    svn path=/trunk/RodinCore/org.eventb.eventBKeyboard/; revision=6933

--- org.eventb.eventBKeyboard/src/org/eventb/eventBKeyboard/internal/translators/TextSymbols.java
+++ org.eventb.eventBKeyboard/src/org/eventb/eventBKeyboard/internal/translators/TextSymbols.java
@@ -4,12 +4,12 @@ package org.eventb.eventBKeyboard.internal.translators;
 public class TextSymbols extends AbstractSymbols {
 	// Combos input which are "text".
 	private static final String[] textCombo = { "NAT1", "NAT", "POW1", "POW",
-			"INT", "INTER", "UNION", "or", "not", "true", "false", "circ" };
+			"INT", "INTER", "UNION", "or", "not", "true", "false", "circ", "oftype" };
 
 	// The translation of the above "text" combos
 	private static final String[] textTranslation = { "\u2115\u0031",
 			"\u2115", "\u2119\u0031", "\u2119", "\u2124", "\u22c2", "\u22c3",
-			"\u2228", "\u00ac", "\u22a4", "\u22a5", "\u2218" };
+			"\u2228", "\u00ac", "\u22a4", "\u22a5", "\u2218", "\u2982" };
 
 
 	@Override
