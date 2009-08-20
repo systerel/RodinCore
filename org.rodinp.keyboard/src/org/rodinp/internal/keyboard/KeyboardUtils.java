b99d6c75f5191e822f00b7146fb2b2da5e5d5867
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2009-08-20 12:55:56 +0000

    Fixed copyright texts.
    svn path=/trunk/RodinCore/org.rodinp.keyboard/; revision=7481

--- org.rodinp.keyboard/src/org/rodinp/internal/keyboard/KeyboardUtils.java
+++ org.rodinp.keyboard/src/org/rodinp/internal/keyboard/KeyboardUtils.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2009 ETH Zurich and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * This used to be abstract class AbstractSymbols. 
+ * 
+ * Contributors:
+ *     ETH Zurich - initial API and implementation
+ *******************************************************************************/
 package org.rodinp.internal.keyboard;
 
 public class KeyboardUtils {
--- org.rodinp.keyboard/src/org/rodinp/internal/keyboard/translators/RodinKeyboardMathTranslator.java
+++ org.rodinp.keyboard/src/org/rodinp/internal/keyboard/translators/RodinKeyboardMathTranslator.java
@@ -1,15 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2005 ETH Zurich.
- * 
+ * Copyright (c) 2006, 2009 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
+ * This used to be abstract class AbstractSymbols. 
+ * 
  * Contributors:
- *     Rodin @ ETH Zurich
- ******************************************************************************/
-
+ *     ETH Zurich - initial API and implementation
+ *******************************************************************************/
 package org.rodinp.internal.keyboard.translators;
 
 import java.util.Collection;
