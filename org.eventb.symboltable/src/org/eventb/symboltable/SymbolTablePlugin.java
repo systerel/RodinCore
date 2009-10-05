40178debc3841d45671e9a4cfe1ac433d26e58de
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2009-10-05 14:52:47 +0000

    Symbol table plug-in: initial source import.
    svn path=/trunk/RodinCore/org.eventb.symboltable/; revision=7578

---
+++ org.eventb.symboltable/src/org/eventb/symboltable/SymbolTablePlugin.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Lehrstuhl fuer Softwaretechnik und Programmiersprachen, 
+ * Heinrich Heine Universitaet Duesseldorf and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Heinrich Heine Universitaet Duesseldorf - initial API and implementation
+ *     Systerel - used Eclipse Command Framework
+ *******************************************************************************/
+
+package org.eventb.symboltable;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class SymbolTablePlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eventb.symboltable";
+	public static final String SYMBOL_TABLE_VIEW_ID = PLUGIN_ID + ".view";
+
+	// The shared instance
+	private static SymbolTablePlugin plugin;
+
+	/**
+	 * The constructor
+	 */
+	public SymbolTablePlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	@Override
+	public void start(final BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	@Override
+	public void stop(final BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static SymbolTablePlugin getDefault() {
+		return plugin;
+	}
+
+}
---
+++ org.eventb.symboltable/src/org/eventb/symboltable/internal/Messages.java
@@ -0,0 +1,159 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
+package org.eventb.symboltable.internal;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eventb.symboltable.internal.messages"; //$NON-NLS-1$
+
+	public static String view_tooltip;
+
+	public static String errorReport_cannotInsertSymbol;
+	
+	public static String symbol_101;
+
+	public static String symbol_104;
+
+	public static String symbol_107;
+
+	public static String symbol_11;
+
+	public static String symbol_110;
+
+	public static String symbol_113;
+
+	public static String symbol_116;
+
+	public static String symbol_119;
+
+	public static String symbol_122;
+
+	public static String symbol_125;
+
+	public static String symbol_128;
+
+	public static String symbol_131;
+
+	public static String symbol_134;
+
+	public static String symbol_137;
+
+	public static String symbol_14;
+
+	public static String symbol_140;
+
+	public static String symbol_143;
+
+	public static String symbol_146;
+
+	public static String symbol_149;
+
+	public static String symbol_152;
+
+	public static String symbol_155;
+
+	public static String symbol_158;
+
+	public static String symbol_161;
+
+	public static String symbol_164;
+
+	public static String symbol_167;
+
+	public static String symbol_17;
+
+	public static String symbol_170;
+
+	public static String symbol_173;
+
+	public static String symbol_176;
+
+	public static String symbol_179;
+
+	public static String symbol_182;
+
+	public static String symbol_185;
+
+	public static String symbol_188;
+
+	public static String symbol_191;
+
+	public static String symbol_194;
+
+	public static String symbol_2;
+
+	public static String symbol_20;
+
+	public static String symbol_23;
+
+	public static String symbol_26;
+
+	public static String symbol_29;
+
+	public static String symbol_32;
+
+	public static String symbol_35;
+
+	public static String symbol_38;
+
+	public static String symbol_41;
+
+	public static String symbol_44;
+
+	public static String symbol_47;
+
+	public static String symbol_5;
+
+	public static String symbol_50;
+
+	public static String symbol_53;
+
+	public static String symbol_56;
+
+	public static String symbol_59;
+
+	public static String symbol_62;
+
+	public static String symbol_65;
+
+	public static String symbol_68;
+
+	public static String symbol_71;
+
+	public static String symbol_74;
+
+	public static String symbol_77;
+
+	public static String symbol_8;
+
+	public static String symbol_80;
+
+	public static String symbol_83;
+
+	public static String symbol_86;
+
+	public static String symbol_89;
+
+	public static String symbol_92;
+
+	public static String symbol_95;
+
+	public static String symbol_98;
+
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
