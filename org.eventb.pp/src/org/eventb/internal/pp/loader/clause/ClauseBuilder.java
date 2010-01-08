5c248280a78c0af5ac65274ca225299730f190ee
Laurent Voisin <lvoisin@users.sourceforge.net>
2010-01-08 17:08:05 +0000

    Added "final" keyword to constant declarations in PP.
    svn path=/trunk/RodinCore/org.eventb.pp/; revision=8027

--- org.eventb.pp/src/org/eventb/internal/pp/loader/clause/ClauseBuilder.java
+++ org.eventb.pp/src/org/eventb/internal/pp/loader/clause/ClauseBuilder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2009 ETH Zurich and others.
+ * Copyright (c) 2006, 2010 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -57,7 +57,7 @@ public final class ClauseBuilder {
 	public static void debug(String message){
 		System.out.println(prefix+message);
 	}
-	private static StringBuilder prefix = new StringBuilder("");
+	private static final StringBuilder prefix = new StringBuilder("");
 	public static void debugEnter(AbstractFormula<?> pred) {
 		if (DEBUG) debug("Entering "+pred+": "+pred.getStringDeps());
 		prefix.append("  ");
