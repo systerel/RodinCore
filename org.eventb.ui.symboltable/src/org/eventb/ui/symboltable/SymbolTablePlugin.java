120182939bd9125b3da0c35c3c1912547fcb8e66
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2010-01-12 17:28:06 +0000

    Symbol Table: added missing @since tag and removed empty package.
    svn path=/trunk/RodinCore/org.eventb.ui.symboltable/; revision=8036

--- org.eventb.ui.symboltable/src/org/eventb/ui/symboltable/SymbolTablePlugin.java
+++ org.eventb.ui.symboltable/src/org/eventb/ui/symboltable/SymbolTablePlugin.java
@@ -18,6 +18,7 @@ import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
+ * @since 1.0
  */
 public class SymbolTablePlugin extends AbstractUIPlugin {
 
