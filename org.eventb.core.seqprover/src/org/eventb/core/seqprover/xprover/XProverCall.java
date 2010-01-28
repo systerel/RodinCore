fea367a3df6d49835e64e05cbb46cf6f24838c7f
Laurent Voisin <lvoisin@users.sourceforge.net>
2010-01-28 16:07:43 +0000

    In reference to bug #2941295: Race condition in XProverCall and descendants   - improved JavaDoc of abstract class XProverCall
    svn path=/trunk/RodinCore/org.eventb.core.seqprover/; revision=8132

--- org.eventb.core.seqprover/src/org/eventb/core/seqprover/xprover/XProverCall.java
+++ org.eventb.core.seqprover/src/org/eventb/core/seqprover/xprover/XProverCall.java
@@ -1,9 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2007 ETH Zurich.
+ * Copyright (c) 2007, 2010 ETH Zurich.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     ETH Zurich - initial API and implementation
+ *     Systerel - improved documentation about concurrency
  *******************************************************************************/
 package org.eventb.core.seqprover.xprover;
 
@@ -23,9 +27,15 @@ import org.eventb.core.seqprover.IProofMonitor;
  * method.
  * </p>
  * <p>
- * Once the instance created, the <code>run()</code> method will be called
- * once to run the external prover. Then, the <code>cleanup()</code> method
- * will be called eventually.
+ * Once the instance created, the <code>run()</code> method will be called once
+ * to run the external prover. The <code>cleanup()</code> method will be called
+ * eventually.
+ * </p>
+ * <p>
+ * Beware that method <code>run()</code> is called in a separate thread. All
+ * other methods are called in the same thread as the constructor. This can lead
+ * to subtle concurrency bugs and subclasses should take care to ensure proper
+ * synchronization between <code>run()</code> and other methods, if needed.
  * </p>
  * <p>
  * Clients that wish to implement a call to an external prover should extend
@@ -85,13 +95,19 @@ public abstract class XProverCall implements Runnable, Cancellable {
 	
 	/**
 	 * Run the external prover on the sequent given at instance creation.
+	 * <p>
+	 * Implementors of this method must take great care to check very often for
+	 * cancellation using {@link #isCancelled()} and to respond to thread
+	 * interruption in a timely manner. If either cancellation or interruption
+	 * has been requested, this method shall return as soon as possible.
+	 * </p>
 	 */
 	public abstract void run();
 
 	/**
-	 * Tells whether the user cancelled this prover call.
+	 * Tells whether the user has canceled this prover call.
 	 * 
-	 * @return <code>true</code> iff the user cancelled this call
+	 * @return <code>true</code> iff the user has canceled this call
 	 */
 	public final boolean isCancelled() {
 		if (proofMonitor != null && proofMonitor.isCanceled()) {
@@ -113,8 +129,13 @@ public abstract class XProverCall implements Runnable, Cancellable {
 	}
 	
 	/**
-	 * Tells whether the sequent passed to the constructor is proved valid by
-	 * the external prover.
+	 * Tells whether the sequent passed to the constructor has been proved valid
+	 * by the external prover.
+	 * <p>
+	 * This method and {@link #run()} may be called concurrently. Implementors
+	 * should not make any assumption on the respective call orders of these two
+	 * methods.
+	 * </p>
 	 * 
 	 * @return <code>true</code> if the sequent to prove is valid
 	 */
@@ -175,10 +196,15 @@ public abstract class XProverCall implements Runnable, Cancellable {
 	
 	/**
 	 * Cleans up this prover call. This is the place to put any code that does
-	 * some clean up after a prover call. This method will always be called
-	 * after <code>run()</code>. For instance, a client could put here the
-	 * code to delete any intermediate file that was created by this prover
-	 * call.
+	 * some clean up. For instance, a client could put here the code to delete
+	 * any intermediate file that was created by this prover call.
+	 * <p>
+	 * This method will always be called after the task running
+	 * <code>run()</code> has been canceled. However, as task synchronization is
+	 * only done cooperatively in Java, there is no guarantee that method
+	 * <code>run()</code> has actually finished execution when this method is
+	 * called.
+	 * </p>
 	 */
 	public abstract void cleanup();
 
