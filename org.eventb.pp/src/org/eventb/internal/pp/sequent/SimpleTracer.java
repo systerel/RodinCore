a7a3d5a8ae11e2f8197cc707d333b806d4a8b71f
Laurent Voisin <lvoisin@users.sourceforge.net>
2010-03-13 09:16:39 +0000

    Cleanup of class SimpleTrace.  No functional change.
    svn path=/trunk/RodinCore/org.eventb.pp/; revision=8439

--- org.eventb.pp/src/org/eventb/internal/pp/sequent/SimpleTracer.java
+++ org.eventb.pp/src/org/eventb/internal/pp/sequent/SimpleTracer.java
@@ -7,37 +7,43 @@
  * 
  * Contributors:
  *     ETH Zurich - initial API and implementation
+ *     Systerel - cleanup
  *******************************************************************************/
 package org.eventb.internal.pp.sequent;
 
-import java.util.Collections;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+
 import java.util.List;
 
 import org.eventb.core.ast.Predicate;
 import org.eventb.pp.ITracer;
 
+/**
+ * Simple tracer for a sequent which is found to be trivially true during the
+ * loading phase.
+ */
 public class SimpleTracer implements ITracer {
-	private final Predicate hypothesis;
+
+	private final List<Predicate> hypotheses;
 	private final boolean goalNeeded;
 
 	public SimpleTracer(InputPredicate ip) {
 		if (ip.isGoal) {
-			this.hypothesis = null;
+			this.hypotheses = emptyList();
 			this.goalNeeded = true;
 		} else {
-			this.hypothesis = ip.originalPredicate;
+			this.hypotheses = singletonList(ip.originalPredicate);
 			this.goalNeeded = false;
 		}
 	}
 
 	public List<Predicate> getNeededHypotheses() {
-		if (hypothesis != null) {
-			return Collections.singletonList(hypothesis);
-		}
-		return Collections.emptyList();
+		return hypotheses;
 	}
 
 	public boolean isGoalNeeded() {
 		return goalNeeded;
 	}
+
 }
