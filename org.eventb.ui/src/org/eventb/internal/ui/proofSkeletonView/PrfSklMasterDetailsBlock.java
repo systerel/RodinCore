1e38e774075723804f36ee973709b8d23621d630
Aurélien Gilles <a_gilles@users.sourceforge.net>
2010-03-16 15:19:12 +0000

    Set the orientation of the Proof Skeleton vertically.
    svn path=/trunk/RodinCore/org.eventb.ui/; revision=8452

--- org.eventb.ui/src/org/eventb/internal/ui/proofSkeletonView/PrfSklMasterDetailsBlock.java
+++ org.eventb.ui/src/org/eventb/internal/ui/proofSkeletonView/PrfSklMasterDetailsBlock.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 Systerel and others.
+ * Copyright (c) 2008, 2010 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -36,6 +36,7 @@ public class PrfSklMasterDetailsBlock extends MasterDetailsBlock {
 		masterPart = new PrfSklMasterPart(parent);
 		managedForm.addPart(masterPart);
 		managedForm.setInput(DefaultInput.getDefault());
+		sashForm.setOrientation(SWT.VERTICAL);
 	}
 
 	@Override
