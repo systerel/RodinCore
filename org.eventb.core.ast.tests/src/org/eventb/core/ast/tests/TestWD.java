c1bbc392b9f082544d283f899f5e2f69d5769dd8
Laurent Voisin <lvoisin@users.sourceforge.net>
2010-06-02 13:04:34 +0000

    Merged r8333 through r8662    and r8898 through r8969 from trunk into branch 2.0 for plug-in org.eventb.core.ast.tests Next merge for this plug-in shall start from r8970.
    svn path=/branches/RodinCore/2.0/; revision=9008

--- org.eventb.core.ast.tests/src/org/eventb/core/ast/tests/TestWD.java
+++ org.eventb.core.ast.tests/src/org/eventb/core/ast/tests/TestWD.java
@@ -27,16 +27,11 @@ import org.eventb.core.ast.Predicate;
  */
 public class TestWD extends AbstractTests {
 	
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
-	
 	private static IntegerType INTEGER = ff.makeIntegerType();
 	private static BooleanType BOOL = ff.makeBooleanType();
 	private static GivenType S = ff.makeGivenType("S");
 
-	ITypeEnvironment defaultTEnv = mTypeEnvironment(
+	static ITypeEnvironment defaultTEnv = mTypeEnvironment(
 			"x", INTEGER,
 			"y", INTEGER,
 			"A", POW(INTEGER),
@@ -46,13 +41,13 @@ public class TestWD extends AbstractTests {
 			"S", POW(S)
 	);
 	
-	private abstract class TestFormula {
+	private static abstract class TestFormula {
 		// base class of various tests
 		TestFormula() { super(); }
 		public abstract void test();
 	}
 	
-	private class TestPredicate extends TestFormula {
+	private static class TestPredicate extends TestFormula {
 		String input;
 		String expected;
 		ITypeEnvironment env;
@@ -81,7 +76,7 @@ public class TestWD extends AbstractTests {
 		}
 	}
 	
-	private class TestAssignment extends TestFormula {
+	private static class TestAssignment extends TestFormula {
 		String input;
 		String expected;
 		ITypeEnvironment env;
@@ -107,127 +102,74 @@ public class TestWD extends AbstractTests {
 		}
 	}
 	
-	private TestFormula[] formulas = new TestFormula[] {
-			new TestPredicate(
-					"x≠y ∧ y=1",
-					"⊤"
-			), new TestPredicate(
-					"x+y+x+1=0 ⇒ y<x",
-					"⊤"
-			), new TestPredicate(
-					"x+1=0 ∨ x<y",
-					"⊤"
-			), new TestPredicate(
-					"(∃x \u00b7 0<x ⇒ (∀y \u00b7 y+x=0))",
-					"⊤"
-			), new TestPredicate(
-					"(B×Y)(x) ∈ Y",
-					"x∈dom(B × Y) ∧ B × Y ∈ ℤ ⇸ BOOL"
-			), new TestPredicate(
-					"x=f(f(y))",
-					"((y∈dom(f) ∧ f ∈ ℤ ⇸ ℤ)" +
-					"∧ f(y)∈dom(f)) ∧ f ∈ ℤ ⇸ ℤ"
-			), new TestPredicate(
-					"(x÷y=y) ⇔ (y mod x=0)",
-					"y≠0 ∧ 0 ≤ y ∧ 0 < x"
-			), new TestPredicate(
-					"∀z \u00b7 x^z>y",
-					"∀z \u00b7 0≤x ∧ 0≤z"
-			), new TestPredicate(
-					"card(A)>x",
-					"finite(A)"
-			), new TestPredicate(
-					"inter({A,B}) ⊆ A∩B",
-					"{A,B}≠∅"
-			), new TestPredicate(
-					"(λ m↦n \u00b7 m>n \u2223 y)(1↦x) = y",
-					"1 ↦ x∈dom(λm ↦ n\u00b7m>n ∣ y) " +
-					"∧ (λm ↦ n\u00b7m>n ∣ y) ∈ (ℤ×ℤ) ⇸ ℤ"
-			), new TestPredicate(
-					"{m,n \u00b7 m=f(n) \u2223 m↦n}[A] ⊂ B",
-					"∀n \u00b7 n∈dom(f) ∧ f ∈ ℤ ⇸ ℤ"
-			), new TestPredicate(
-					"{f(n)↦m \u2223 x=n ∧ y+x=m ∧ f ∈ ℤ→A} = A×B",
-					"∀f,n,m \u00b7 x=n ∧ y+x=m ∧ f ∈ ℤ→A ⇒ n∈dom(f) ∧ f ∈ ℤ ⇸ ℤ"
-			), new TestPredicate(
-					"{1, 2, x, x+y, 4, 6} = B",
-					"⊤"
-			), new TestPredicate(
-					"(⋂ m,n \u00b7 m∈A ∧ n∈B \u2223 {m÷n}) = B",
-					"(∀m,n \u00b7 (m∈A ∧ n∈B) ⇒ n≠0) ∧ (∃m,n \u00b7 (m∈A ∧ n∈B))"
-			), new TestPredicate(
-					"(⋂{m+n} \u2223 m+n∈A)=B",
-					"∃m,n\u00b7m+n∈A"
-			), new TestPredicate(
-					"bool(⊤)=bool(⊥)",
-					"⊤"
-			), new TestPredicate(
-					"x+y+(x mod y)\u2217x+1=0 ⇒ y<x",
-					"0 ≤ x ∧ 0 < y"
-			), new TestAssignment(
-					"x≔y",
-					"⊤"
-			), new TestAssignment(
-					"x :\u2223 x'>x",
-					"⊤"
-			), new TestAssignment(
-					"x :∈ {x,y}",
-					"⊤"
-			), new TestAssignment(
-					"x :∈ {x÷y, y}",
-					"y≠0"
-			), new TestAssignment(
-					"f(x)≔f(x)",
-					"x∈dom(f)∧f ∈ ℤ ⇸ ℤ"
-			), new TestAssignment(
-					"x :\u2223 x'=card(A∪{x'})",
-					"∀x' \u00b7 finite(A∪{x'})"
-			), new TestPredicate(
-					"a = {x∣x≤card(A)}",
-					"finite(A)"
-			), new TestPredicate(
-					"a = min(A)",
-					"A ≠ ∅ ∧ (∃b·∀x·x∈A ⇒ b≤x)"
-			), new TestPredicate(
-					"a = max(A)",
-					"A ≠ ∅ ∧ (∃b·∀x·x∈A ⇒ b≥x)"
-			// Ensure that a type name doesn't get captured
-			// when computing a WD lemma
-			), new TestPredicate(
-					"T ⊆ S ∧ g ∈ ℤ → T ⇒ (∃S·g(S) ∈ T)",
-					"T ⊆ S ∧ g ∈ ℤ → T ⇒ (∀S0·S0 ∈ dom(g) ∧ g ∈ ℤ ⇸ S)"
-			// Example from the Mobile model
-			), new TestPredicate(
-					"   a ∈ S ↔ S" +
-					" ∧ b ∈ S ↔ (ℤ ↔ S)" +
-					" ∧ (∀s·s ∈ dom(a) ⇒ a(s) = b(s)(max(dom(b(s)))))",
-					//---------------------------
-					"  a∈S ↔ S ∧ b∈S ↔ (ℤ ↔ S)" +
-					"⇒ (∀s·s∈dom(a)" +
-					"    ⇒ s∈dom(a) ∧ a ∈ S ⇸ S" +
-					"    ∧ s∈dom(b) ∧ b ∈ S ⇸ ℙ(ℤ × S)" +
-					"    ∧ s∈dom(b) ∧ b ∈ S ⇸ ℙ(ℤ × S)" +
-					"    ∧ dom(b(s))≠∅ ∧ (∃b0·∀x·x∈dom(b(s))⇒b0≥x)" +
-					"    ∧ max(dom(b(s)))∈dom(b(s))" +
-					"    ∧ b(s) ∈ ℤ ⇸ S)"
-			// Reduced example extracted from the preceding one
-			), new TestPredicate(
-					"∀s·max(s) ∈ s",
-					"∀s·s≠∅ ∧ (∃b·∀x·x ∈ s  ⇒  b ≥ x)"
-			// Case where a bound variable disappears
-			), new TestPredicate(
-					"∀y·∃x·x = f(y)",
-					"∀y·y∈dom(f) ∧ f∈ℤ ⇸ ℤ"
-			), new TestPredicate(
-					"∀f,y·f∈ℤ → ℤ ⇒ (∃x·x = f(y))",
-					"∀f,y·f∈ℤ → ℤ ⇒ y∈dom(f) ∧ f∈ℤ ⇸ ℤ"
-			),
-	};
-	
+	private static void assertWDLemma(String in, String expected) {
+		new TestPredicate(in, expected).test();
+	}
+
+	private static void assertWDLemmaAssignment(String in, String expected) {
+		new TestAssignment(in, expected).test();
+	}
+
 	public void testWD() {
-		for (int i=0; i<formulas.length; i++) {
-			formulas[i].test();
-		}
+		assertWDLemma("x≠y ∧ y=1", "⊤");
+		assertWDLemma("x+y+x+1=0 ⇒ y<x", "⊤");
+		assertWDLemma("x+1=0 ∨ x<y", "⊤");
+		assertWDLemma("(∃x \u00b7 0<x ⇒ (∀y \u00b7 y+x=0))", "⊤");
+		assertWDLemma("(B×Y)(x) ∈ Y", "x∈dom(B × Y) ∧ B × Y ∈ ℤ ⇸ BOOL");
+		assertWDLemma("x=f(f(y))", //
+				"((y∈dom(f) ∧ f ∈ ℤ ⇸ ℤ) ∧ f(y)∈dom(f)) ∧ f ∈ ℤ ⇸ ℤ");
+		assertWDLemma("(x÷y=y) ⇔ (y mod x=0)", "y≠0 ∧ 0 ≤ y ∧ 0 < x");
+		assertWDLemma("∀z \u00b7 x^z>y", "∀z \u00b7 0≤x ∧ 0≤z");
+		assertWDLemma("card(A)>x", "finite(A)");
+		assertWDLemma("inter({A,B}) ⊆ A∩B", "{A,B}≠∅");
+		assertWDLemma("(λ m↦n \u00b7 m>n \u2223 y)(1↦x) = y",
+				"1 ↦ x∈dom(λm ↦ n\u00b7m>n ∣ y) "
+						+ "∧ (λm ↦ n\u00b7m>n ∣ y) ∈ (ℤ×ℤ) ⇸ ℤ");
+		assertWDLemma("{m,n \u00b7 m=f(n) \u2223 m↦n}[A] ⊂ B",
+				"∀n \u00b7 n∈dom(f) ∧ f ∈ ℤ ⇸ ℤ");
+		assertWDLemma("{f(n)↦m \u2223 x=n ∧ y+x=m ∧ f ∈ ℤ→A} = A×B",
+				"∀f,n,m \u00b7 x=n ∧ y+x=m ∧ f ∈ ℤ→A ⇒ n∈dom(f) ∧ f ∈ ℤ ⇸ ℤ");
+		assertWDLemma("{1, 2, x, x+y, 4, 6} = B", "⊤");
+		assertWDLemma("(⋂ m,n \u00b7 m∈A ∧ n∈B \u2223 {m÷n}) = B",
+				"(∀m,n \u00b7 (m∈A ∧ n∈B) ⇒ n≠0) ∧ (∃m,n \u00b7 (m∈A ∧ n∈B))");
+		assertWDLemma("(⋂{m+n} \u2223 m+n∈A)=B", "∃m,n\u00b7m+n∈A");
+		assertWDLemma("bool(⊤)=bool(⊥)", "⊤");
+		assertWDLemma("x+y+(x mod y)\u2217x+1=0 ⇒ y<x", "0 ≤ x ∧ 0 < y");
+		assertWDLemmaAssignment("x≔y", "⊤");
+		assertWDLemmaAssignment("x :\u2223 x'>x", "⊤");
+		assertWDLemmaAssignment("x :∈ {x,y}", "⊤");
+		assertWDLemmaAssignment("x :∈ {x÷y, y}", "y≠0");
+		assertWDLemmaAssignment("f(x)≔f(x)", "x∈dom(f)∧f ∈ ℤ ⇸ ℤ");
+		assertWDLemmaAssignment("x :\u2223 x'=card(A∪{x'})",
+				"∀x' \u00b7 finite(A∪{x'})");
+		assertWDLemma("a = {x∣x≤card(A)}", "finite(A)");
+		assertWDLemma("a = min(A)", "A ≠ ∅ ∧ (∃b·∀x·x∈A ⇒ b≤x)");
+		assertWDLemma("a = max(A)", "A ≠ ∅ ∧ (∃b·∀x·x∈A ⇒ b≥x)");
+
+		// Ensure that a type name doesn't get captured
+		// when computing a WD lemma
+		assertWDLemma("T ⊆ S ∧ g ∈ ℤ → T ⇒ (∃S·g(S) ∈ T)",
+				"T ⊆ S ∧ g ∈ ℤ → T ⇒ (∀S0·S0 ∈ dom(g) ∧ g ∈ ℤ ⇸ S)");
+
+		// Example from the Mobile model
+		assertWDLemma("   a ∈ S ↔ S" + " ∧ b ∈ S ↔ (ℤ ↔ S)"
+				+ " ∧ (∀s·s ∈ dom(a) ⇒ a(s) = b(s)(max(dom(b(s)))))",
+		// ---------------------------
+				"  a∈S ↔ S ∧ b∈S ↔ (ℤ ↔ S)" + "⇒ (∀s·s∈dom(a)"
+						+ "    ⇒ s∈dom(a) ∧ a ∈ S ⇸ S"
+						+ "    ∧ s∈dom(b) ∧ b ∈ S ⇸ ℙ(ℤ × S)"
+						+ "    ∧ s∈dom(b) ∧ b ∈ S ⇸ ℙ(ℤ × S)"
+						+ "    ∧ dom(b(s))≠∅ ∧ (∃b0·∀x·x∈dom(b(s))⇒b0≥x)"
+						+ "    ∧ max(dom(b(s)))∈dom(b(s))"
+						+ "    ∧ b(s) ∈ ℤ ⇸ S)");
+
+		// Reduced example extracted from the preceding one
+		assertWDLemma("∀s·max(s) ∈ s", "∀s·s≠∅ ∧ (∃b·∀x·x ∈ s  ⇒  b ≥ x)");
+
+		// Case where a bound variable disappears
+		assertWDLemma("∀y·∃x·x = f(y)", "∀y·y∈dom(f) ∧ f∈ℤ ⇸ ℤ");
+		assertWDLemma("∀f,y·f∈ℤ → ℤ ⇒ (∃x·x = f(y))",
+				"∀f,y·f∈ℤ → ℤ ⇒ y∈dom(f) ∧ f∈ℤ ⇸ ℤ");
 	}
 	
 }
