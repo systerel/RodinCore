6a5483b0ab7fd88b2f4c22cdcea3170e2abfcb55
Laurent Voisin <lvoisin@users.sourceforge.net>
2010-06-09 14:31:06 +0000

    Refactored Search Hypothesis view: moved code from the View class to the page class where it should have been in the first place.
    svn path=/trunk/RodinCore/org.eventb.ui/; revision=9115

--- org.eventb.ui/src/org/eventb/internal/ui/searchhypothesis/ISearchHypothesisPage.java
+++ org.eventb.ui/src/org/eventb/internal/ui/searchhypothesis/ISearchHypothesisPage.java
@@ -1,25 +1,28 @@
 /*******************************************************************************
- * Copyright (c) 2005-2007 ETH Zurich.
- * 
+ * Copyright (c) 2005, 2010 ETH Zurich.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Rodin @ ETH Zurich
+ *     ETH Zurich - initial API and implementation
+ *     Systerel - added dropdown list, buttons and search field
  ******************************************************************************/
-
 package org.eventb.internal.ui.searchhypothesis;
 
 import org.eventb.internal.ui.prover.IHypothesisPage;
 
 /**
+ * Common protocol for a page displaying searched hypotheses.
+ *
  * @author htson
- *         <p>
- *         This is the interface for the Search Hypothesis Page which is just a
- *         direct extension of the general {@link IHypothesisPage}.
  */
 public interface ISearchHypothesisPage extends IHypothesisPage {
-	// Direct extension of IHypothesisPage
+
+	/**
+	 * Sets the string pattern to use for searching relevant hypotheses.
+	 */
+	public void setPattern(String input);
+
 }
