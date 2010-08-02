5a54e244d005facca32e4769ba54706d6cec279f
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2010-08-02 12:43:21 +0000

    AST datatypes: added javadoc for published API, no code change.
    svn path=/trunk/RodinCore/org.eventb.core.ast/; revision=9519

--- org.eventb.core.ast/src/org/eventb/core/ast/extension/datatype/IConstructorMediator.java
+++ org.eventb.core.ast/src/org/eventb/core/ast/extension/datatype/IConstructorMediator.java
@@ -16,6 +16,14 @@ import org.eventb.core.ast.Type;
 import org.eventb.core.ast.extension.ITypeMediator;
 
 /**
+ * Common protocol for constructor mediators.
+ * <p>
+ * Instances of this interface are passed as argument to
+ * {@link IDatatypeExtension#addConstructors(IConstructorMediator)}. It provides
+ * methods for declaring constructors and destructors of a datatype, based upon
+ * the definition of their arguments.
+ * </p>
+ * 
  * @author Nicolas Beauger
  * @since 2.0
  * @noimplement This interface is not intended to be implemented by clients.
@@ -23,34 +31,103 @@ import org.eventb.core.ast.extension.ITypeMediator;
  */
 public interface IConstructorMediator extends ITypeMediator {
 
+	/**
+	 * Returns the type parameter with the given name, or <code>null</code> if
+	 * not found.
+	 * 
+	 * @param name
+	 *            a String
+	 * @return a type parameter or <code>null</code>
+	 */
 	ITypeParameter getTypeParameter(String name);
 
+	/**
+	 * Makes a new argument type whose type is the given type parameter.
+	 * 
+	 * @param type
+	 *            a type parameter
+	 * @return a new argument type
+	 */
 	IArgumentType newArgumentType(ITypeParameter type);
 
+	/**
+	 * Makes a new argument type whose type is the given type.
+	 * 
+	 * @param type
+	 *            a type
+	 * @return a new argument type
+	 */
 	IArgumentType newArgumentType(Type type);
 
+	/**
+	 * Makes a new argument with the given argument type and no associated
+	 * destructor.
+	 * 
+	 * @param type
+	 *            a new argument
+	 * @return a new argument
+	 */
 	IArgument newArgument(IArgumentType type);
 
+	/**
+	 * Makes a new argument with the given argument type and an associated
+	 * destructor of the given name (operator symbol).
+	 * 
+	 * @param destructorName
+	 *            the name of the destructor
+	 * @param type
+	 *            a new argument
+	 * @return a new argument
+	 */
 	IArgument newArgument(String destructorName, IArgumentType type);
-	
-	IArgumentType makePowerSetType(IArgumentType arg);
-	
+
+	/**
+	 * Makes a new argument type whose type is the powerset of the given
+	 * argument type.
+	 * 
+	 * @param type
+	 *            an argument type
+	 * @return a new argument type
+	 */
+	IArgumentType makePowerSetType(IArgumentType type);
+
+	/**
+	 * Makes a new argument type whose type is the cartesian product of the
+	 * given argument types.
+	 * 
+	 * @param left
+	 *            an argument type
+	 * @param right
+	 *            an argument type
+	 * 
+	 * @return a new argument type
+	 */
 	IArgumentType makeProductType(IArgumentType left, IArgumentType right);
-	
+
+	/**
+	 * Makes a new argument type whose type is the relation (powerset of the
+	 * cartesian product) of the given argument types.
+	 * 
+	 * @param left
+	 *            an argument type
+	 * @param right
+	 *            an argument type
+	 * @return a new argument type
+	 */
 	IArgumentType makeRelationalType(IArgumentType left, IArgumentType right);
-	
+
 	/**
 	 * @param typeParams
 	 *            type parameters
 	 * @return a new type constructor argument type
 	 */
 	IArgumentType newArgumentTypeConstr(List<IArgumentType> typeParams);
-	
+
 	/**
-	 * Instance with no arguments.
+	 * Adds a new constructor with the given name, id but no arguments.
 	 * <p>
-	 * Same as calling {@link #addConstructor(String, String, List)}
-	 * with an empty list.
+	 * Same as calling {@link #addConstructor(String, String, List)} with an
+	 * empty list.
 	 * </p>
 	 * 
 	 * @param name
@@ -59,7 +136,7 @@ public interface IConstructorMediator extends ITypeMediator {
 	void addConstructor(String name, String id);
 
 	/**
-	 * Instance with arguments, whose types are given.
+	 * Adds a new constructor with the given name, id and arguments.
 	 * <p>
 	 * The number of arguments is determined from the size of the given list.
 	 * </p>
--- org.eventb.core.ast/src/org/eventb/core/ast/extension/datatype/IDatatype.java
+++ org.eventb.core.ast/src/org/eventb/core/ast/extension/datatype/IDatatype.java
@@ -13,10 +13,17 @@ package org.eventb.core.ast.extension.datatype;
 import java.util.List;
 import java.util.Set;
 
+import org.eventb.core.ast.FormulaFactory;
 import org.eventb.core.ast.extension.IExpressionExtension;
 import org.eventb.core.ast.extension.IFormulaExtension;
 
 /**
+ * Common protocol for datatypes.
+ * <p>
+ * Instances of this interface are intended to be made through
+ * {@link FormulaFactory#makeDatatype(IDatatypeExtension)}.
+ * </p>
+ * 
  * @author Nicolas Beauger
  * @since 2.0
  * @noimplement This interface is not intended to be implemented by clients.
@@ -24,14 +31,78 @@ import org.eventb.core.ast.extension.IFormulaExtension;
  */
 public interface IDatatype {
 
+	/**
+	 * Returns the type parameters of this datatype.
+	 * 
+	 * @return a list of type parameters (possibly empty)
+	 */
 	List<ITypeParameter> getTypeParameters();
 
+	/**
+	 * Returns the type constructor extension for this datatype.
+	 * <p>
+	 * Returned extension can be used as parameter in
+	 * {@link FormulaFactory#makeParametricType(List, IExpressionExtension)} as
+	 * well as makeExtendedExpression(IExpressionExtension, ...) methods.
+	 * </p>
+	 * 
+	 * @return an expression extension
+	 */
 	IExpressionExtension getTypeConstructor();
 
+	/**
+	 * Returns the constructor extension associated to the given id, or
+	 * <code>null</code> if not found.
+	 * 
+	 * <p>
+	 * Given id refers to the same id parameter given through
+	 * {@link IConstructorMediator#addConstructor(String, String, List)}.
+	 * </p>
+	 * 
+	 * @param constructorId
+	 *            a String identifier
+	 * @return an expression extension or <code>null</code>
+	 */
 	IExpressionExtension getConstructor(String constructorId);
 
+	/**
+	 * Returns the destructor extension that corresponds to the given
+	 * constructor id and the given argument number, or <code>null</code> if not
+	 * found.
+	 * <p>
+	 * The argument number is the zero-based index of the constructor argument
+	 * that corresponds to the destructor target. For instance, in constructor
+	 * definition <b>cons(head, tail)</b>, <b>head</b> has argument number 0 and
+	 * <b>tail</b> has argument number 1.
+	 * </p>
+	 * <p>
+	 * <code>null</code> is returned when:
+	 * <li>the given id is not associated to a constructor</li>
+	 * <li>the given argument number is out of the bounds of the constructor
+	 * arguments</li>
+	 * <li>the argument corresponding to the given argument number has no
+	 * associated destructor</li>
+	 * </p>
+	 * 
+	 * @param constructorId
+	 *            a String id
+	 * @param argNumber
+	 *            a natural number
+	 * @return an expression extension, or <code>null</code>
+	 */
 	IExpressionExtension getDestructor(String constructorId, int argNumber);
 
+	/**
+	 * Returns a set of all formula extensions defined by this datatype.
+	 * <p>
+	 * Defined extensions are:
+	 * <li>the type constructor</li>
+	 * <li>all constructors</li>
+	 * <li>all destructors</li>
+	 * </p>
+	 * 
+	 * @return a set of formula extensions
+	 */
 	Set<IFormulaExtension> getExtensions();
 
 }
