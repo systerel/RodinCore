658479fce201084314a2c784db1384523519d720
Laurent Voisin <lvoisin@users.sourceforge.net>
2010-12-09 19:26:32 +0000

    Refactored LambdaCheck.
    svn path=/branches/RodinCore/AutoRewriterL2/; revision=10438

--- org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/rewriters/LambdaCheck.java
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/rewriters/LambdaCheck.java
@@ -1,74 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
 package org.eventb.internal.core.seqprover.eventbExtensions.rewriters;
 
+import static java.util.Arrays.asList;
 import static org.eventb.core.ast.Formula.BOUND_IDENT;
 import static org.eventb.core.ast.Formula.MAPSTO;
 
-import java.util.Arrays;
+import java.util.List;
 
 import org.eventb.core.ast.BinaryExpression;
-import org.eventb.core.ast.BoundIdentDecl;
 import org.eventb.core.ast.BoundIdentifier;
 import org.eventb.core.ast.Expression;
 import org.eventb.core.ast.QuantifiedExpression;
 
+// TODO Benoît Present this algorithm here
 public class LambdaCheck {
 
 	public static boolean lambdaCheck(QuantifiedExpression expression) {
 		return new LambdaCheck(expression).verify();
 	}
 
-	private final QuantifiedExpression original;
-	private final BoundIdentDecl[] quantifiedIdentifiers;
+	private final QuantifiedExpression qExpr;
+	private final int nbBound;
 
-	private LambdaCheck(QuantifiedExpression expression) {
-		original = expression;
-		quantifiedIdentifiers = original.getBoundIdentDecls();
+	private LambdaCheck(QuantifiedExpression qExpr) {
+		this.qExpr = qExpr;
+		this.nbBound = qExpr.getBoundIdentDecls().length;
 	}
 
 	private boolean verify() {
-		final Expression expression = original.getExpression();
-		if (expression.getTag() == MAPSTO) {
-			final Expression left = ((BinaryExpression) expression).getLeft();
-			final Expression right = ((BinaryExpression) expression).getRight();
-			if (isWellFormed(left)) {
-				return checkIdents(left, right);
+		final Expression child = qExpr.getExpression();
+		if (child.getTag() != MAPSTO) {
+			return false;
+		}
+		final BinaryExpression maplet = (BinaryExpression) child;
+		final Expression left = maplet.getLeft();
+		final Expression right = maplet.getRight();
+		return isInjective(left) && checkIdents(left, right);
+	}
+
+	private boolean isInjective(Expression expr) {
+		switch (expr.getTag()) {
+		case MAPSTO:
+			final BinaryExpression maplet = (BinaryExpression) expr;
+			return isInjective(maplet.getLeft())
+					&& isInjective(maplet.getRight());
+		case BOUND_IDENT:
+			return true;
+		default:
+			return !containsLocallyBound(expr);
+		}
+	}
+
+	private boolean containsLocallyBound(Expression expression) {
+		for (final BoundIdentifier bi : expression.getBoundIdentifiers()) {
+			if (isLocallyBound(bi)) {
+				return true;
 			}
 		}
 		return false;
 	}
 
-	private boolean isWellFormed(Expression pattern) {
-		switch (pattern.getTag()) {
-		case MAPSTO:
-			final BinaryExpression maplet = (BinaryExpression) pattern;
-			return isWellFormed(maplet.getLeft())
-					&& isWellFormed(maplet.getRight());
-		case BOUND_IDENT:
-			// TODO Benoit : something else to do ?
-			return true;
-		default:
-			return !containsQuantifiedIdent(pattern);
-		}
+	private boolean isLocallyBound(final BoundIdentifier bi) {
+		return bi.getBoundIndex() < nbBound;
 	}
 
-	private boolean containsQuantifiedIdent(Expression expression) {
-		final BoundIdentifier[] boundIdents = expression.getBoundIdentifiers();
-
-		for (int i = 0; i < boundIdents.length; i++) {
-			if (boundIdents[i].getBoundIndex() < quantifiedIdentifiers.length) {
-				return true;
-			}
-		}
-			return false;
-		}
-
 	private boolean checkIdents(Expression left, Expression right) {
-		final BoundIdentifier[] lbids = left.getBoundIdentifiers();
-		final BoundIdentifier[] rbids = right.getBoundIdentifiers();
-		for (int i = 0; i < rbids.length; i++) {
-			if (Arrays.asList(lbids).contains(rbids[i])) {
-				// ok, identifier is in both expressions
-			} else {
+		final List<BoundIdentifier> lbis = asList(left.getBoundIdentifiers());
+		for (final BoundIdentifier rbi : right.getBoundIdentifiers()) {
+			if (isLocallyBound(rbi) && ! lbis.contains(rbi)) {
 				return false;
 			}
 		}
