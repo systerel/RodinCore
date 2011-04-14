af36d052fb964ec0f1e62bf90f0081e139b7f13a
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2011-04-14 14:14:00 +0000

    Used topological sort for refinement participants.
    svn path=/trunk/RodinCore/org.rodinp.core/; revision=11245

--- org.rodinp.core/src/org/rodinp/internal/core/RefinementRegistry.java
+++ org.rodinp.core/src/org/rodinp/internal/core/RefinementRegistry.java
@@ -11,6 +11,8 @@
 package org.rodinp.internal.core;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -20,6 +22,8 @@ import java.util.Set;
 
 import org.rodinp.core.IInternalElementType;
 import org.rodinp.core.IRefinementParticipant;
+import org.rodinp.internal.core.indexer.sort.DefaultNode;
+import org.rodinp.internal.core.indexer.sort.Sorter;
 
 /**
  * @author Nicolas Beauger
@@ -43,6 +47,21 @@ public class RefinementRegistry {
 
 	public static RefinementRegistry getDefault() {
 		return DEFAULT_INSTANCE;
+	}
+
+	private static class Node extends DefaultNode<IRefinementParticipant, Node> {
+
+		public Node(IRefinementParticipant refinePart) {
+			super(refinePart);
+		}
+
+		public void setSuccsPreds(List<Node> succs, List<Node> preds) {
+			successors.clear();
+			predecessors.clear();
+			successors.addAll(succs);
+			predecessors.addAll(preds);
+		}
+
 	}
 
 	private static class Refinement {
@@ -72,8 +91,70 @@ public class RefinementRegistry {
 		}
 
 		private void orderParticipants() {
-			// FIXME order
-			orderedRefParts.addAll(participants.values());
+			orderedRefParts.clear();
+			final Map<IRefinementParticipant, List<IRefinementParticipant>> succs = new HashMap<IRefinementParticipant, List<IRefinementParticipant>>();
+			final Map<IRefinementParticipant, List<IRefinementParticipant>> preds = new HashMap<IRefinementParticipant, List<IRefinementParticipant>>();
+
+			computeSuccPreds(succs, preds);
+			final Collection<Node> nodes = toNodes(succs, preds);
+			final Sorter<IRefinementParticipant, Node> sorter = new Sorter<IRefinementParticipant, Node>(
+					nodes);
+			final List<Node> sorted = sorter.sort();
+			for (Node node : sorted) {
+				orderedRefParts.add(node.getLabel());
+			}
+		}
+
+		private void computeSuccPreds(
+				Map<IRefinementParticipant, List<IRefinementParticipant>> succMap,
+				Map<IRefinementParticipant, List<IRefinementParticipant>> predMap) {
+			for (Entry<String, Set<String>> entry : order.entrySet()) {
+				final IRefinementParticipant part = participants.get(entry
+						.getKey());
+				final List<IRefinementParticipant> succs = new ArrayList<IRefinementParticipant>();
+				for (String succId : entry.getValue()) {
+					final IRefinementParticipant succ = participants
+							.get(succId);
+					succs.add(succ);
+					List<IRefinementParticipant> preds = predMap.get(succId);
+					if (preds == null) {
+						preds = new ArrayList<IRefinementParticipant>();
+						predMap.put(succ, preds);
+					}
+					preds.add(part);
+				}
+				succMap.put(part, succs);
+			}
+		}
+
+		private Collection<Node> toNodes(
+				Map<IRefinementParticipant, List<IRefinementParticipant>> succMap,
+				Map<IRefinementParticipant, List<IRefinementParticipant>> predMap) {
+			final Map<IRefinementParticipant, Node> nodes = new HashMap<IRefinementParticipant, Node>();
+			for (IRefinementParticipant part : participants.values()) {
+				nodes.put(part, new Node(part));
+			}
+			for (Entry<IRefinementParticipant, Node> entry : nodes.entrySet()) {
+				final IRefinementParticipant participant = entry.getKey();
+				final List<Node> succN = toNodeList(participant, succMap, nodes);
+				final List<Node> predN = toNodeList(participant, predMap, nodes);
+				entry.getValue().setSuccsPreds(succN, predN);
+			}
+			return nodes.values();
+		}
+
+		private List<Node> toNodeList(IRefinementParticipant participant,
+				Map<IRefinementParticipant, List<IRefinementParticipant>> map,
+				Map<IRefinementParticipant, Node> nodes) {
+			List<IRefinementParticipant> succs = map.get(participant);
+			if (succs == null) {
+				return Collections.emptyList();
+			}
+			final List<Node> succN = new ArrayList<Node>();
+			for (IRefinementParticipant part : succs) {
+				succN.add(nodes.get(part));
+			}
+			return succN;
 		}
 
 		public void setOrder(String before, String after)
@@ -169,7 +250,7 @@ public class RefinementRegistry {
 					"A refinement with the same id already exists: failed to add "
 							+ refinementId);
 		}
-		
+
 		final Refinement refinement = new Refinement();
 		refinementIds.put(refinementId, refinement);
 		refinements.put(rootType, refinement);
