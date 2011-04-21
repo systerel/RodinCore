1f0ae2bfc3be0f6aa0c93b730ad03b7d7bd49688
Thomas Muller <thomasmuller@users.sourceforge.net>
2011-04-21 16:55:57 +0000

    Implemented the AddSibling() handler.
    svn path=/trunk/_exploratory/hmaria/; revision=11354

--- _exploratory/hmaria/fr.systerel.editor/src/fr/systerel/editor/handlers/AddSiblingHandler.java
+++ _exploratory/hmaria/fr.systerel.editor/src/fr/systerel/editor/handlers/AddSiblingHandler.java
@@ -1,8 +1,23 @@
 package fr.systerel.editor.handlers;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eventb.internal.ui.eventbeditor.elementdesc.ElementDescRegistry;
+import org.rodinp.core.IInternalElement;
+import org.rodinp.core.RodinDBException;
+import org.rodinp.core.emf.api.itf.ILElement;
+
+import fr.systerel.editor.EditorPlugin;
+import fr.systerel.editor.documentModel.Interval;
+import fr.systerel.editor.documentModel.RodinDocumentProvider;
+import fr.systerel.editor.editors.RodinEditor;
 
 /*******************************************************************************
  * Copyright (c) 2011 Systerel and others.
@@ -16,17 +31,43 @@ import org.eclipse.core.commands.IHandler;
  *******************************************************************************/
 
 /**
- * @author tommy
- *
+ * Handler to add siblings.
  */
 public class AddSiblingHandler extends AbstractHandler implements IHandler {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
-	 */
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		System.out.println("Executed ADD_SIBLING");
+		final IEditorPart activeEditor = EditorPlugin.getActivePage()
+				.getActiveEditor();
+		if (!(activeEditor instanceof RodinEditor)) {
+			return null;
+		}
+		final RodinEditor rEditor = (RodinEditor) activeEditor;
+		final ISelection currentSelection = HandlerUtil
+				.getActiveMenuSelection(event);
+		if (currentSelection instanceof TextSelection) {
+			final int offset = ((TextSelection) currentSelection).getOffset();
+			final Interval inter = rEditor.getDocumentMapper()
+					.findEditableIntervalAfter(offset);
+			if (inter == null)
+				return null;
+			final ILElement element = inter.getElement();
+			if (element == null) {
+				return null;
+			}
+			try {
+				final IInternalElement localParent = element.getParent().getElement();
+				ElementDescRegistry.getInstance().createElement(
+						localParent.getRoot(), localParent, element.getElementType(),
+						element.getElement());
+				((RodinDocumentProvider) rEditor.getDocumentProvider())
+						.doSynchronize(element.getRoot(), null);
+			} catch (RodinDBException e1) {
+				e1.printStackTrace();
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
 		return null;
 	}
 
