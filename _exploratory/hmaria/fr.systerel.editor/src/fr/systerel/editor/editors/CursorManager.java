d0b5cc76e6724372d14f43cc15d4a2b6e088f353
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2011-05-11 16:01:50 +0000

    avoided potential NPE
    svn path=/trunk/_exploratory/hmaria/; revision=11593

--- _exploratory/hmaria/fr.systerel.editor/src/fr/systerel/editor/editors/CursorManager.java
+++ _exploratory/hmaria/fr.systerel.editor/src/fr/systerel/editor/editors/CursorManager.java
@@ -60,6 +60,7 @@ public class CursorManager implements MouseMoveListener {
 			final int offset = styledText.getOffsetAtLocation(p);
 			final Interval inter = editor.getDocumentMapper().findInterval(
 					offset);
+			if(inter == null) return;
 			if (inter.getContentType() == RodinConfiguration.LEFT_PRESENTATION_TYPE
 					&& styledText.getCursor() != original) {
 				styledText.setCursor(aCursor);
