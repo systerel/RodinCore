systerel/RodinCore

commit 841af33970f108499eeda75b4ba3423f50820519

Created a ILFileFactory to hide the RodinResource implementation.
svn path=/trunk/_exploratory/hmaria/; revision=11870

_exploratory/tmuller/org.rodinp.core.emf/src/org/rodinp/core/emf/lightcore/RodinResource.java
@@ -26,6 +26,7 @@ import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
+import org.eclipse.emf.ecore.util.EContentAdapter;
 import org.rodinp.core.IRodinFile;
 import org.rodinp.core.IRodinProject;
 import org.rodinp.core.RodinCore;
@@ -209,4 +210,11 @@ public class RodinResource extends ResourceImpl implements ILFile {
 		root.eSetDeliver(false);
 		root.delete();
 	}
+
+	@Override
+	public void addEContentAdapter(EContentAdapter adapter) {
+		if (eAdapters != null && !eAdapters.contains(adapter))
+			eAdapters.add(adapter);
+	}
+	
 }

_exploratory/hmaria/fr.systerel.editor/src/fr/systerel/editor/documentModel/RodinDocumentProvider.java
@@ -18,8 +18,6 @@ import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jface.operation.IRunnableContext;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
@@ -35,7 +33,7 @@ import org.rodinp.core.IRodinElement;
 import org.rodinp.core.RodinMarkerUtil;
 import org.rodinp.core.emf.api.itf.ILElement;
 import org.rodinp.core.emf.api.itf.ILFile;
-import org.rodinp.core.emf.lightcore.RodinResource;
+import org.rodinp.core.emf.api.itf.ILFileFactory;
 
 import fr.systerel.editor.editors.RodinEditor;
 import fr.systerel.editor.presentation.RodinConfiguration;
@@ -71,18 +69,14 @@ public class RodinDocumentProvider extends AbstractDocumentProvider {
 	 * This is the method called to load a resource into the editing domain's
 	 * resource set based on the editor's input.
 	 */
-	public Resource getResource(IFile file) {
-		final String projectName = file.getProject().getName();
-		final URI resourceURI = URI.createPlatformResourceURI(projectName + "/"
-				+ file.getName(), true);
-		final Resource resource = new RodinResource();
-		resource.setURI(resourceURI);
+	public ILFile getResource(IFile file) {
+		final ILFile resource = ILFileFactory.INSTANCE.createILFile(file);
 		try {
 			resource.load(null);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		resource.eAdapters().add(elementPresentationChangeAdapter);
+		resource.addEContentAdapter(elementPresentationChangeAdapter);
 		return resource;
 	}
 

_exploratory/tmuller/org.rodinp.core.emf/src/org/rodinp/core/emf/api/itf/ILFile.java
@@ -10,18 +10,26 @@
  *******************************************************************************/
 package org.rodinp.core.emf.api.itf;
 
+import java.io.IOException;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.util.EContentAdapter;
 
 /**
  * @author Nicolas Beauger
  */
 public interface ILFile {
 
+	void load(final Map<?, ?> options) throws IOException;
+
 	ILElement getRoot();
-	
+
 	boolean isEmpty();
-	
+
 	void save();
 
 	void unloadResource();
 	
+	void addEContentAdapter(EContentAdapter adapter);
+
 }

_exploratory/tmuller/org.rodinp.core.emf/src/org/rodinp/core/emf/api/itf/ILFileFactory.java
@@ -1,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Systerel and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     Systerel - Initial API and implementation
+ *******************************************************************************/
+package org.rodinp.core.emf.api.itf;
+
+import org.eclipse.core.resources.IFile;
+import org.rodinp.core.emf.lightcore.RodinResourceFactory;
+
+/**
+ * @author "Thomas Muller"
+ */
+public interface ILFileFactory {
+	
+	ILFileFactory INSTANCE = new RodinResourceFactory();
+	
+	ILFile createILFile(IFile file);
+	
+	void removeILFile(ILFile file);
+	
+}

_exploratory/tmuller/org.rodinp.core.emf/src/org/rodinp/core/emf/lightcore/RodinResourceFactory.java
@@ -18,11 +18,14 @@ import java.util.HashSet;
 import java.util.Map;
 
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.rodinp.core.emf.api.itf.ILFile;
+import org.rodinp.core.emf.api.itf.ILFileFactory;
 
-public class RodinResourceFactory implements Resource.Factory {
+public class RodinResourceFactory implements Resource.Factory, ILFileFactory {
 	/**
 	 * A map of {@link Resource}s which are handled, i.e., created by this
 	 * factory.
@@ -93,6 +96,19 @@ public class RodinResourceFactory implements Resource.Factory {
 		}
 
 		return null;
+	}
+
+	@Override
+	public ILFile createILFile(IFile file) {
+		final String projectName = file.getProject().getName();
+		final URI resourceURI = URI.createPlatformResourceURI(projectName + "/"
+				+ file.getName(), true);
+		return (ILFile) createResource(resourceURI);
+	}
+
+	@Override
+	public void removeILFile(ILFile file) {
+		removeResource((RodinResource) file);
 	}
 	
 }

commit bda38819dc3c7cfe754378ebd662b4d33ebce2e9

Fixed a bug introduced in r11870 and which prevented the editor to  get the notifications upon change. (e.g. it blocked the refreshment of the editor in case of attribute modification)
svn path=/trunk/_exploratory/tmuller/org.rodinp.core.emf/; revision=12107

_exploratory/tmuller/org.rodinp.core.emf/src/org/rodinp/core/emf/lightcore/RodinResource.java
@@ -213,8 +213,8 @@ public class RodinResource extends ResourceImpl implements ILFile {
 
 	@Override
 	public void addEContentAdapter(EContentAdapter adapter) {
-		if (eAdapters != null && !eAdapters.contains(adapter))
-			eAdapters.add(adapter);
+		if (!eAdapters().contains(adapter))
+			eAdapters().add(adapter);
 	}
 	
 }