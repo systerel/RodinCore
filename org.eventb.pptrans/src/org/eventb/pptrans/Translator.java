ed338c7016f060e73fd14a8e5e3ebe5fc0b13139
Laurent Voisin <lvoisin@users.sourceforge.net>
2011-07-07 10:36:31 +0000

    Reindent code and improve API Javadoc
    No change in the actual source code.
    
    svn path=/trunk/RodinCore/org.eventb.pptrans/; revision=12369

--- org.eventb.pptrans/src/org/eventb/pptrans/Translator.java
+++ org.eventb.pptrans/src/org/eventb/pptrans/Translator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2010 ETH Zurich and others.
+ * Copyright (c) 2006, 2011 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,51 +18,76 @@ import org.eventb.internal.pptrans.translator.GoalChecker;
 import org.eventb.internal.pptrans.translator.PredicateSimplification;
 
 /**
- * Provides a facade to the org.eventb.pptrans package.
- * 
+ * Provides access to the Translator of the Predicate Prover. The intended use
+ * of this class is to call the first three provided methods in sequence, as
+ * follows
+ * <ol>
+ * <li>{@link #decomposeIdentifiers(Predicate, FormulaFactory)}</li>
+ * <li>{@link #reduceToPredicateCalulus(Predicate, FormulaFactory)}</li>
+ * <li>{@link #simplifyPredicate(Predicate, FormulaFactory)}</li>
+ * </ol>
+ * <p>
+ * The additional method {@link #isInGoal(Predicate)} is provided for test
+ * purposes. It allows to check that the result of a translation is indeed in
+ * the target sub-language.
+ * </p>
  * 
  * @author Matthias Konrad
  * @since 0.2
  */
-
 public class Translator {
-	
+
 	private Translator() {
 		// Non-instantiable class
 	}
-	
+
 	/**
-	 * Decomposes every free or bound identifier of a Cartesian product type. Decomposed
-	 * predicates can be reduced with reduceToPredicateCalculus.
-	 * @param predicate the predicate to be decomposed
-	 * @param ff the formula factory to be used during decomposition
-	 * @return Returns a new predicate that has no identifiers of a Cartesian product type,
-	 * except for the quantification of the free identifiers.
+	 * Decomposes every free or bound identifier of Cartesian product type in
+	 * the given predicate.
+	 * 
+	 * @param predicate
+	 *            the predicate whose identifiers are to be decomposed
+	 * @param ff
+	 *            the formula factory to use
+	 * @return an equivalent predicate that contains no identifier of Cartesian
+	 *         product type, except for the quantification of the free
+	 *         identifiers
 	 */
-	public static Predicate decomposeIdentifiers(Predicate predicate, FormulaFactory ff) {
-		predicate = FreeIdentifierDecomposition.decomposeIdentifiers(predicate, ff);
-		return BoundIdentifierDecomposition.decomposeBoundIdentifiers(predicate, ff);
+	public static Predicate decomposeIdentifiers(Predicate predicate,
+			FormulaFactory ff) {
+		predicate = FreeIdentifierDecomposition.decomposeIdentifiers(predicate,
+				ff);
+		return BoundIdentifierDecomposition.decomposeBoundIdentifiers(
+				predicate, ff);
 	}
-		
+
 	/**
-	 * Transforms a predicate from Set-Theory to Predicate Calculus
-	 * @param predicate the predicate to be reduced. This predicate needs to be
-	 * in a decomposed form. Use decomposeIdentifier if this is not yet the case. 
-	 * @param ff the formula factory that should be used during the reduction
-	 * @return Returns a new reduced predicate 
+	 * Transforms a predicate from Set-Theory to Predicate Calculus.
+	 * 
+	 * @param predicate
+	 *            the predicate to reduce. This predicate must be in a
+	 *            decomposed form
+	 * @param ff
+	 *            the formula factory to use
+	 * @return a reduced predicate equivalent to the input predicate
 	 */
-	public static Predicate reduceToPredicateCalulus(Predicate predicate, FormulaFactory ff) {
-		return org.eventb.internal.pptrans.translator.Translator.reduceToPredCalc(predicate, ff);
+	public static Predicate reduceToPredicateCalulus(Predicate predicate,
+			FormulaFactory ff) {
+		return org.eventb.internal.pptrans.translator.Translator
+				.reduceToPredCalc(predicate, ff);
 	}
-	
-	
+
 	/**
-	 * Simplifies predicates with some basic rules.
-	 * @param predicate the predicate to be simplified.
-	 * @param ff the formula factory that should be used during the simplification
-	 * @return A new simplified predicate
+	 * Simplifies the given predicate using some basic simplification rules.
+	 * 
+	 * @param predicate
+	 *            the predicate to simplify
+	 * @param ff
+	 *            the formula factory to use
+	 * @return a simplified predicate equivalent to the input predicate
 	 */
-	public static Predicate simplifyPredicate(Predicate predicate, FormulaFactory ff) {
+	public static Predicate simplifyPredicate(Predicate predicate,
+			FormulaFactory ff) {
 		return PredicateSimplification.simplifyPredicate(predicate, ff);
 	}
 
