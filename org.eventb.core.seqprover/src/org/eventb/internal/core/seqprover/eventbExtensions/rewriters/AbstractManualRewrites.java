f168e7c23ec9f7deff1fa8b632c0ca7a14fdee09
Laurent Voisin <lvoisin@users.sourceforge.net>
2011-07-08 18:15:56 +0000

    Fix TODO in Javadoc
    svn path=/trunk/RodinCore/org.eventb.core.seqprover/; revision=12408

--- org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/rewriters/AbstractManualRewrites.java
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/rewriters/AbstractManualRewrites.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2010 ETH Zurich and others.
+ * Copyright (c) 2007, 2011 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -192,10 +192,12 @@ public abstract class AbstractManualRewrites implements IReasoner {
 	 * 
 	 * @param pred
 	 *            predicate to rewrite
-	 * @param ff TODO
+	 * @param ff
+	 *            formula factory to use
 	 * @return the predicate which is the result of rewriting
 	 */
-	public abstract Predicate rewrite(Predicate pred, IPosition position, FormulaFactory ff);
+	public abstract Predicate rewrite(Predicate pred, IPosition position,
+			FormulaFactory ff);
 
 	/**
 	 * Returns the name to display in the generated rule.
