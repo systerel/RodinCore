8804429a241238ac22a5a7c5173a472be29c5fdf
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2011-07-21 16:26:45 +0000

    added broken input repair ability to ae
    svn path=/trunk/RodinCore/org.eventb.core.seqprover/; revision=12542

--- org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/AbstrExpr.java
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/AbstrExpr.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2010 ETH Zurich and others.
+ * Copyright (c) 2007, 2011 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  * 
  * Contributors:
  *     ETH Zurich - initial API and implementation
+ *     Systerel - added broken input repair mechanism
  *******************************************************************************/
 package org.eventb.internal.core.seqprover.eventbExtensions;
 
@@ -14,15 +15,19 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.eventb.core.ast.Expression;
+import org.eventb.core.ast.Formula;
 import org.eventb.core.ast.FormulaFactory;
 import org.eventb.core.ast.FreeIdentifier;
 import org.eventb.core.ast.ITypeEnvironment;
 import org.eventb.core.ast.Predicate;
+import org.eventb.core.ast.RelationalPredicate;
 import org.eventb.core.seqprover.IProofMonitor;
 import org.eventb.core.seqprover.IProofRule.IAntecedent;
 import org.eventb.core.seqprover.IProverSequent;
 import org.eventb.core.seqprover.IReasonerInput;
+import org.eventb.core.seqprover.IReasonerInputReader;
 import org.eventb.core.seqprover.IReasonerOutput;
+import org.eventb.core.seqprover.IRepairableInputReasoner;
 import org.eventb.core.seqprover.ProverFactory;
 import org.eventb.core.seqprover.SequentProver;
 import org.eventb.core.seqprover.eventbExtensions.DLib;
@@ -33,13 +38,14 @@ import org.eventb.core.seqprover.reasonerInputs.SingleExprInputReasoner;
 /**
  * This reasoner abstracts a given expression with a fresh free identifier.
  * 
- * It does this by intruducing a new free variable and an equality hypothesis that can be
- * used to later rewrite all occurances of the expression by the free variable.
+ * It does this by introducing a new free variable and an equality hypothesis that can be
+ * used to later rewrite all occurrences of the expression by the free variable.
  * 
  * @author Farhad Mehta
  *
  */
-public class AbstrExpr extends SingleExprInputReasoner {
+public class AbstrExpr extends SingleExprInputReasoner implements
+		IRepairableInputReasoner {
 
 	public static final String REASONER_ID = SequentProver.PLUGIN_ID + ".ae";
 	
@@ -115,4 +121,26 @@ public class AbstrExpr extends SingleExprInputReasoner {
 		return identName;
 	}
 	
+	@Override
+	public IReasonerInput repair(IReasonerInputReader reader) {
+		// might be caused by bug 3370087 => infer input
+		final IAntecedent[] antecedents = reader.getAntecedents();
+		if (antecedents.length != 2) return null;
+		
+		final FreeIdentifier[] addedFreeIdents = antecedents[1].getAddedFreeIdents();
+		if (addedFreeIdents.length != 1) return null;
+		
+		final FreeIdentifier ident = addedFreeIdents[0];
+		final Set<Predicate> addedHyps = antecedents[1].getAddedHyps();
+		if (addedHyps.size() != 1) return null;
+		
+		final Predicate hyp = addedHyps.iterator().next();
+		if (hyp.getTag() != Formula.EQUAL) return null;
+		
+		final RelationalPredicate eq = (RelationalPredicate) hyp;
+		if (!eq.getLeft().equals(ident)) return null;
+		
+		final Expression expr = eq.getRight();
+		return new SingleExprInput(expr);
+	}
 }
