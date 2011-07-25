36b5cc581c851e998dde58532c44a335959dc022
Laurent Voisin <lvoisin@users.sourceforge.net>
2011-07-25 17:44:11 +0000

    Refactor tests of FormulaInspector
    Refactor these tests to use the pre-defined extensions for tests.
    
    svn path=/trunk/RodinCore/org.eventb.core.ast.tests/; revision=12589

--- org.eventb.core.ast.tests/src/org/eventb/core/ast/tests/TestFormulaInspector.java
+++ org.eventb.core.ast.tests/src/org/eventb/core/ast/tests/TestFormulaInspector.java
@@ -12,6 +12,9 @@ package org.eventb.core.ast.tests;
 
 import static java.util.Arrays.asList;
 import static org.eventb.core.ast.FormulaFactory.makePosition;
+import static org.eventb.core.ast.tests.ExtendedFormulas.EFF;
+import static org.eventb.core.ast.tests.ExtendedFormulas.barS;
+import static org.eventb.core.ast.tests.ExtendedFormulas.fooS;
 import static org.eventb.core.ast.tests.FastFactory.mAssociativeExpression;
 import static org.eventb.core.ast.tests.FastFactory.mAssociativePredicate;
 import static org.eventb.core.ast.tests.FastFactory.mAtomicExpression;
@@ -30,11 +33,9 @@ import static org.eventb.core.ast.tests.FastFactory.mRelationalPredicate;
 import static org.eventb.core.ast.tests.FastFactory.mSimplePredicate;
 import static org.eventb.core.ast.tests.FastFactory.mUnaryExpression;
 import static org.eventb.core.ast.tests.FastFactory.mUnaryPredicate;
-import static org.eventb.internal.core.parser.BMath.StandardGroup.ATOMIC_PRED;
 
 import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -63,7 +64,6 @@ import org.eventb.core.ast.IAccumulator;
 import org.eventb.core.ast.IFormulaInspector;
 import org.eventb.core.ast.IPosition;
 import org.eventb.core.ast.IntegerLiteral;
-import org.eventb.core.ast.IntegerType;
 import org.eventb.core.ast.LiteralPredicate;
 import org.eventb.core.ast.MultiplePredicate;
 import org.eventb.core.ast.Predicate;
@@ -73,19 +73,8 @@ import org.eventb.core.ast.QuantifiedPredicate;
 import org.eventb.core.ast.RelationalPredicate;
 import org.eventb.core.ast.SetExtension;
 import org.eventb.core.ast.SimplePredicate;
-import org.eventb.core.ast.Type;
 import org.eventb.core.ast.UnaryExpression;
 import org.eventb.core.ast.UnaryPredicate;
-import org.eventb.core.ast.extension.ICompatibilityMediator;
-import org.eventb.core.ast.extension.IExpressionExtension;
-import org.eventb.core.ast.extension.IExtendedFormula;
-import org.eventb.core.ast.extension.IExtensionKind;
-import org.eventb.core.ast.extension.IFormulaExtension;
-import org.eventb.core.ast.extension.IPredicateExtension;
-import org.eventb.core.ast.extension.IPriorityMediator;
-import org.eventb.core.ast.extension.ITypeCheckMediator;
-import org.eventb.core.ast.extension.ITypeMediator;
-import org.eventb.core.ast.extension.IWDMediator;
 
 /**
  * Ensures that finding accumulators work as expected for addition of new
@@ -379,177 +368,6 @@ public class TestFormulaInspector extends TestCase {
 	public static final FormulaFactory ff = FormulaFactory.getDefault();
 	protected static final IntegerLiteral ONE = ff.makeIntegerLiteral(BigInteger.ONE, null);
 
-	private static final IPredicateExtension EXT_PRIME = new IPredicateExtension() {
-		private static final String SYMBOL = "prime";
-		private static final String ID = "Ext Prime";
-		
-		@Override
-		public Predicate getWDPredicate(IExtendedFormula formula,
-				IWDMediator wdMediator) {
-			return wdMediator.makeTrueWD();
-		}
-		
-		@Override
-		public String getSyntaxSymbol() {
-			return SYMBOL;
-		}
-		
-		@Override
-		public IExtensionKind getKind() {
-			return PARENTHESIZED_UNARY_PREDICATE;
-		}
-		
-		@Override
-		public String getId() {
-			return ID;
-		}
-		
-		@Override
-		public String getGroupId() {
-			return ATOMIC_PRED.getId();
-		}
-		
-		@Override
-		public void addPriorities(IPriorityMediator mediator) {
-			// no priority
-		}
-		
-		@Override
-		public void addCompatibilities(ICompatibilityMediator mediator) {
-			// no compatibility			
-		}
-		
-		@Override
-		public void typeCheck(ExtendedPredicate predicate,
-				ITypeCheckMediator tcMediator) {
-			final Expression child = predicate.getChildExpressions()[0];
-			final Type childType = tcMediator.makePowerSetType(tcMediator.makeIntegerType());
-			tcMediator.sameType(child.getType(), childType);
-		}
-
-		@Override
-		public boolean conjoinChildrenWD() {
-			return true;
-		}
-
-		@Override
-		public Object getOrigin() {
-			return null;
-		}
-	};
-
-	private static final FormulaFactory PRIME_FAC = FormulaFactory
-			.getInstance(Collections.<IFormulaExtension> singleton(EXT_PRIME));
-	
-	private static class Emax implements IExpressionExtension {
-		private static final String SYNTAX_SYMBOL = "emax";
-		private static final String OPERATOR_ID = "org.eventb.core.ast.tests.extMax";
-		private final boolean conjoinChildrenWD;
-
-		public Emax(boolean conjoinChildrenWD) {
-			this.conjoinChildrenWD = conjoinChildrenWD;
-		}
-
-		@Override
-		public Type synthesizeType(Expression[] childExprs,
-				Predicate[] childPreds, ITypeMediator mediator) {
-			return childExprs[0].getType();
-		}
-
-		@Override
-		public boolean verifyType(Type proposedType,
-				Expression[] childExprs, Predicate[] childPreds) {
-			for (Expression child : childExprs) {
-				final Type childType = child.getType();
-				if (!(childType instanceof IntegerType)) {
-					return false;
-				}
-			}
-			return true;
-		}
-		
-		@Override
-		public Type typeCheck(ExtendedExpression expression,
-				ITypeCheckMediator tcMediator) {
-			final Expression[] children = expression.getChildExpressions();
-			final Type resultType = tcMediator.makeIntegerType();
-			for (int i = 0; i < children.length; i++) {
-				tcMediator.sameType(children[i].getType(), resultType);
-			}
-			return resultType;
-		}
-
-		@Override
-		public void addCompatibilities(ICompatibilityMediator mediator) {
-			mediator.addCompatibility(getId(), getId());
-		}
-
-		@Override
-		public void addPriorities(IPriorityMediator mediator) {
-			// no priority to add
-		}
-
-		@Override
-		public String getGroupId() {
-			return "Arithmetic";
-		}
-
-		@Override
-		public String getId() {
-			return OPERATOR_ID + (conjoinChildrenWD ? ".conj" : ".noConj");
-		}
-
-		@Override
-		public IExtensionKind getKind() {
-			return PARENTHESIZED_BINARY_EXPRESSION;
-		}
-
-		@Override
-		public String getSyntaxSymbol() {
-			return SYNTAX_SYMBOL;
-		}
-
-		// BTRUE if the first child is an integer literal
-		// else BFALSE 
-		@Override
-		public Predicate getWDPredicate(IExtendedFormula formula,
-				IWDMediator wdMediator) {
-			final Expression firstChild = formula.getChildExpressions()[0];
-			
-			final FormulaFactory factory = wdMediator.getFormulaFactory();
-			if (firstChild.getTag() == Formula.INTLIT) {
-				return factory.makeLiteralPredicate(Formula.BTRUE, null);
-			} else {
-				return factory.makeLiteralPredicate(Formula.BFALSE, null);
-			}
-		}
-
-		@Override
-		public boolean conjoinChildrenWD() {
-			return conjoinChildrenWD;
-		}
-
-		@Override
-		public boolean isATypeConstructor() {
-			return false;
-		}
-
-		@Override
-		public Object getOrigin() {
-			return null;
-		}
-
-	}
-	
-	private static final IExpressionExtension EMAX = new Emax(true);
-
-	private static IntegerType INTEGER = ff.makeIntegerType();
-	private static final Set<Predicate> NO_PREDICATE = Collections.emptySet();
-	private static final FreeIdentifier FRID_A = ff.makeFreeIdentifier("A", null, INTEGER);
-	private static final FreeIdentifier FRID_B = ff.makeFreeIdentifier("B",	null, INTEGER);
-	final FormulaFactory extFac = FormulaFactory.getInstance(Collections
-			.<IFormulaExtension> singleton(EMAX));
-
 	/**
 	 * Ensures that skip methods work as expected on associative expressions
 	 */
@@ -602,11 +420,14 @@ public class TestFormulaInspector extends TestCase {
 	 * Ensures that skip methods work as expected on extended predicates
 	 */
 	public void testExtendedPredicate() throws Exception {
-		final Predicate predicate = PRIME_FAC.makeExtendedPredicate(EXT_PRIME,
-				Arrays.<Expression> asList(ONE),
-				Collections.<Predicate> emptySet(), null);
+		final Predicate predicate = EFF.makeExtendedPredicate(fooS,
+				Arrays.<Expression> asList(eA, eB),
+				Arrays.<Predicate> asList(pA, pB), null);
 		assertTrace(predicate, "");
 		assertTrace(predicate, "0");
+		assertTrace(predicate, "1");
+		assertTrace(predicate, "2");
+		assertTrace(predicate, "3");
 		assertSkipChildrenOnce(predicate);
 	}
 
@@ -614,13 +435,14 @@ public class TestFormulaInspector extends TestCase {
 	 * Ensures that skip methods work as expected on extended expression
 	 */
 	public void testExtendedExpression() throws Exception {
-		final Expression expression = extFac.makeExtendedExpression(EMAX, Arrays
-				.<Expression> asList(
-						FRID_A,
-						FRID_B),
-				NO_PREDICATE, null);
+		final Expression expression = EFF.makeExtendedExpression(barS,
+				Arrays.<Expression> asList(eA, eB),
+				Arrays.<Predicate> asList(pA, pB), null);
 		assertTrace(expression, "");
 		assertTrace(expression, "0");
+		assertTrace(expression, "1");
+		assertTrace(expression, "2");
+		assertTrace(expression, "3");
 		assertSkipChildrenOnce(expression);
 	}
 
