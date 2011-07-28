cf471dad5df4e23e0fc6abf4cdc5df33d3ba0162
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2011-07-28 16:14:34 +0000

    adapted ae tests to common framework
    svn path=/trunk/RodinCore/org.eventb.core.seqprover.tests/; revision=12630

--- org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtentionTests/AbstrExprTests.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtentionTests/AbstrExprTests.java
@@ -1,16 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2011 ETH Zurich and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ETH Zurich - initial API and implementation
+ *     Systerel - adapted to common test framework
+ *******************************************************************************/
 package org.eventb.core.seqprover.eventbExtentionTests;
 
-import static org.junit.Assert.assertTrue;
-
-import org.eventb.core.seqprover.IProofRule;
 import org.eventb.core.seqprover.IProverSequent;
-import org.eventb.core.seqprover.IReasoner;
-import org.eventb.core.seqprover.IReasonerFailure;
-import org.eventb.core.seqprover.IReasonerOutput;
+import org.eventb.core.seqprover.reasonerExtentionTests.AbstractReasonerTests;
 import org.eventb.core.seqprover.reasonerInputs.SingleExprInput;
 import org.eventb.core.seqprover.tests.TestLib;
 import org.eventb.internal.core.seqprover.eventbExtensions.AbstrExpr;
-import org.junit.Test;
 
 /**
  * Unit tests for the Abstract expression reasoner
@@ -19,50 +24,40 @@ import org.junit.Test;
  * 
  * @author Farhad Mehta
  */
-public class AbstrExprTests {
+public class AbstrExprTests extends AbstractReasonerTests {
 
-	private static final IReasoner reasoner = new AbstrExpr();
-	
-	/**
-	 * Tests for correct reasoner failure
-	 */
-	@Test
-	public void testFailure(){
-		IProverSequent seq;
-		IReasonerOutput output;
-				
-		// Expression not parsable
-		seq = TestLib.genSeq(" ⊤ |- ⊤ ");
-		output = reasoner.apply(seq, new SingleExprInput("@unparsable@",seq.typeEnvironment()), null);
-		assertTrue(output instanceof IReasonerFailure);
-		
-		// Expression not typecheckable
-		seq = TestLib.genSeq(" ⊤ |- ⊤ ");
-		output = reasoner.apply(seq, new SingleExprInput("x",seq.typeEnvironment()), null);
-		assertTrue(output instanceof IReasonerFailure);
-		
+	@Override
+	public String getReasonerID() {
+		return new AbstrExpr().getReasonerID();
 	}
-	
-	/**
-	 * Tests for reasoner success
-	 */
-	@Test
-	public void testSuccess(){
-		IProverSequent seq;
-		IProverSequent[] newSeqs;
-		IReasonerOutput output;
-				
-		
-		seq = TestLib.genSeq(" x=1 ;; x+1 = 2 |- (x+1)+1 = 3 ");
-		output = reasoner.apply(seq, new SingleExprInput("x+1",seq.typeEnvironment()), null);
-		assertTrue(output instanceof IProofRule);
-		newSeqs = ((IProofRule)output).apply(seq);
-		assertTrue(newSeqs.length == 2);
-		// Note: this test is pretty printer dependent. Change string (after inspection)
-		// in case pretty printer is modified
-		assertTrue(newSeqs[0].toString().equals("{x=ℤ}[][][x=1, x+1=2] |- ⊤"));
-		assertTrue(newSeqs[1].toString().equals("{ae=ℤ, x=ℤ}[][][x=1, x+1=2, ae=x+1] |- (x+1)+1=3"));
-		
+
+	@Override
+	public UnsuccessfullReasonerApplication[] getUnsuccessfullReasonerApplications() {
+		return new UnsuccessfullReasonerApplication[] {
+				// Expression not parsable
+				new UnsuccessfullReasonerApplication(
+						TestLib.genSeq(" ⊤ |- ⊤ "),
+						new SingleExprInput("@unparsable@",lib.makeTypeEnvironment())
+				),
+				// Expression not typecheckable
+				new UnsuccessfullReasonerApplication(
+						TestLib.genSeq(" ⊤ |- ⊤ "),
+						new SingleExprInput("x",lib.makeTypeEnvironment())
+				),	
+		};
+	}
+
+	@Override
+	public SuccessfullReasonerApplication[] getSuccessfulReasonerApplications() {
+		final IProverSequent seq = TestLib.genSeq(" x=1 ;; x+1 = 2 |- (x+1)+1 = 3 ");
+		return new SuccessfullReasonerApplication[]{
+				// hyp not univ quantified implication, but still univ quantified
+				new SuccessfullReasonerApplication(seq, 
+						new SingleExprInput("x+1",seq.typeEnvironment()),
+						"{x=ℤ}[][][x=1;; x+1=2] |- ⊤",
+						"{ae=ℤ, x=ℤ}[][][x=1;; x+1=2;; ae=x+1] |- (x+1)+1=3"
+				),
+		};
 	}
 	
 }
