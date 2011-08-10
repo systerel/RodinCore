4b41c5b13c4eaaa6b9cbec806614075af73de6db
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2011-08-10 12:40:48 +0000

    added a test about equality rewrite in an associative expression
    svn path=/trunk/RodinCore/org.eventb.core.seqprover.tests/; revision=12717

--- org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtentionTests/NewEhTests.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtentionTests/NewEhTests.java
@@ -35,6 +35,11 @@ public class NewEhTests extends AbstractReasonerTests {
 						TestLib.genSeq(" 1=2 ;; 1+1 = 2 |- 1+1+1 = 3 "),
 						new HypothesisReasoner.Input(TestLib.genPred("1=2")),
 						"{}[][1+1=2][1=2;; 2+2=2] |- 2+2+2=3"
+						),
+				new SuccessfullReasonerApplication(
+						TestLib.genSeq(" 1+2 = 12 |- 0+1+2+3 = 123 "),
+						new HypothesisReasoner.Input(TestLib.genPred("1+2=12")),
+						"{}[][][1+2=12] |- 0+12+3=123"
 						)
 		};
 	}
