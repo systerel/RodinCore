68fce744e5bf120136b79774a6aa0f0edb9e1f2a
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2011-08-25 08:52:02 +0000

    fixed equality of parameter settings / valuations by implementing hashcode/equals in AbstractParameterValue
    svn path=/branches/RodinCore/CustomTactics/; revision=12830

--- org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/paramTactics/ParameterValues.java
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/paramTactics/ParameterValues.java
@@ -39,6 +39,36 @@ public class ParameterValues {
 		public String toString() {
 			return value.toString();
 		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((value == null) ? 0 : value.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (!(obj instanceof AbstractParameterValue)) {
+				return false;
+			}
+			AbstractParameterValue<?> other = (AbstractParameterValue<?>) obj;
+			if (value == null) {
+				if (other.value != null) {
+					return false;
+				}
+			} else if (!value.equals(other.value)) {
+				return false;
+			}
+			return true;
+		}
 	}
 
 	public static class BoolParameterValue extends AbstractParameterValue<Boolean> {
