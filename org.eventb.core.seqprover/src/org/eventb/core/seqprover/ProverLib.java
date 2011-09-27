d8686e6496ed8e150ea5dd15c74ccdf5cf9c06ba
Emmanuel Billaud <billaude@users.sourceforge.net>
2011-09-27 13:14:55 +0000

    Adding a ProofMonitor constant in ProverLib. This ProofMonitor does not do anything.
    svn path=/trunk/RodinCore/org.eventb.core.seqprover/; revision=13256

--- org.eventb.core.seqprover/src/org/eventb/core/seqprover/ProverLib.java
+++ org.eventb.core.seqprover/src/org/eventb/core/seqprover/ProverLib.java
@@ -423,4 +423,30 @@ public class ProverLib {
 		}
 	}
 	
+	/**
+	 * Simple proof monitor.
+	 * @since 2.3
+	 */
+	public static ProofMonitor PM = new ProofMonitor();
+
+	private static class ProofMonitor implements IProofMonitor {
+
+		private boolean canceled;
+
+		@Override
+		public boolean isCanceled() {
+			return canceled;
+		}
+
+		@Override
+		public void setCanceled(boolean value) {
+			canceled = value;
+		}
+
+		@Override
+		public void setTask(String name) {
+			// Ignore
+		}
+	}
+
 }
