ba5c32eeb49e578dc8533f6cfcc75693d99cefef
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2011-09-28 14:18:15 +0000

    changed 'conjunction introduction' application point to root instead of predicate (red link instead of left button).
    svn path=/trunk/RodinCore/org.eventb.ui/; revision=13276

--- org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/ConjunctionIntroduction.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/ConjunctionIntroduction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2010 ETH Zurich and others.
+ * Copyright (c) 2007, 2011 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,11 +15,12 @@ import static java.util.Collections.singletonList;
 
 import java.util.List;
 
+import org.eventb.core.ast.IPosition;
 import org.eventb.core.ast.Predicate;
 import org.eventb.core.seqprover.IProofTreeNode;
 import org.eventb.core.seqprover.ITactic;
 import org.eventb.core.seqprover.eventbExtensions.Tactics;
-import org.eventb.ui.prover.DefaultTacticProvider.DefaultPredicateApplication;
+import org.eventb.ui.prover.DefaultTacticProvider.DefaultPositionApplication;
 import org.eventb.ui.prover.ITacticApplication;
 import org.eventb.ui.prover.ITacticProvider;
 
@@ -33,7 +34,11 @@ import org.eventb.ui.prover.ITacticProvider;
 public class ConjunctionIntroduction implements ITacticProvider {
 
 	public static class ConjonctionIntroductionApplication extends
-			DefaultPredicateApplication {
+			DefaultPositionApplication {
+
+		public ConjonctionIntroductionApplication() {
+			super(null, IPosition.ROOT);
+		}
 
 		private static final String TACTIC_ID = "org.eventb.ui.conjI";
 
