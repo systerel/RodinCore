b787af32f6e0da6f5126707c7fad0bd7562ac950
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2011-11-24 17:01:20 +0000

    refactored rule details view: extracted an abstract super class in order to factorize material with upcoming view.
    svn path=/trunk/RodinCore/org.eventb.ui/; revision=13556

---
+++ org.eventb.ui/src/org/eventb/internal/ui/prooftreeui/AbstractProofNodeView.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
+package org.eventb.internal.ui.prooftreeui;
+
+import static org.rodinp.keyboard.preferences.PreferenceConstants.RODIN_MATH_FONT;
+
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.ISelectionService;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.part.ViewPart;
+import org.eventb.core.seqprover.IProofTreeNode;
+
+/**
+ * Abstract class for views that display information about currently selected
+ * proof tree node.
+ * 
+ * @author Nicolas Beauger
+ * 
+ */
+public abstract class AbstractProofNodeView extends ViewPart implements
+		ISelectionListener, IPropertyChangeListener {
+
+	private Composite parentComp = null;
+	private IProofTreeNode currentNode = null;
+	private Font currentFont = null;
+
+	protected abstract void initializeControl(final Composite parent);
+
+	protected abstract void refreshContents(IProofTreeNode node, Font font);
+
+	@Override
+	public void createPartControl(final Composite parent) {
+		this.parentComp = parent;
+		final IWorkbenchWindow workbench = getSite().getWorkbenchWindow();
+		final ISelectionService selectionService = workbench
+				.getSelectionService();
+		// add myself as a global selection listener
+		selectionService.addSelectionListener(this);
+
+		initializeControl(parent);
+
+		currentFont = JFaceResources.getFont(RODIN_MATH_FONT);
+		JFaceResources.getFontRegistry().addListener(this);
+		// prime the selection to display contents
+		refreshOnSelectionChanged(getSite().getPage().getSelection());
+	}
+
+	@Override
+	public void setFocus() {
+		// Nothing to do.
+	}
+
+	private void refreshOnSelectionChanged(ISelection selection) {
+		if (parentComp == null || parentComp.isDisposed()) {
+			return;
+		}
+		if (selection instanceof IStructuredSelection) {
+			final IStructuredSelection ssel = ((IStructuredSelection) selection);
+			final Object element = ssel.getFirstElement();
+			if (currentNode == element) {
+				return;
+			}
+			if (element instanceof IProofTreeNode) {
+				currentNode = (IProofTreeNode) element;
+				refreshContents(currentNode, currentFont);
+			}
+		}
+	}
+
+	@Override
+	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+		if (!(part instanceof ProofTreeUI)) {
+			return;
+		}
+		refreshOnSelectionChanged(selection);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.part.WorkbenchPart#dispose()
+	 */
+	@Override
+	public void dispose() {
+		final IWorkbenchWindow workbench = getSite().getWorkbenchWindow();
+		final ISelectionService selectionService = workbench
+				.getSelectionService();
+		selectionService.removeSelectionListener(this);
+		JFaceResources.getFontRegistry().removeListener(this);
+		super.dispose();
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		if (parentComp == null || parentComp.isDisposed()) {
+			return;
+		}
+		if (event.getProperty().equals(RODIN_MATH_FONT)) {
+			currentFont = JFaceResources.getFont(RODIN_MATH_FONT);
+			if (currentNode != null) {
+				refreshContents(currentNode, currentFont);
+			}
+		}
+	}
+
+}
--- org.eventb.ui/src/org/eventb/internal/ui/prooftreeui/RuleDetailsView.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prooftreeui/RuleDetailsView.java
@@ -11,24 +11,12 @@
  ******************************************************************************/
 package org.eventb.internal.ui.prooftreeui;
 
-import static org.rodinp.keyboard.preferences.PreferenceConstants.RODIN_MATH_FONT;
-
-import org.eclipse.jface.resource.JFaceResources;
-import org.eclipse.jface.util.IPropertyChangeListener;
-import org.eclipse.jface.util.PropertyChangeEvent;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.ui.ISelectionListener;
-import org.eclipse.ui.ISelectionService;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.part.ViewPart;
 import org.eventb.core.seqprover.IProofRule;
 import org.eventb.core.seqprover.IProofTreeNode;
 import org.eventb.ui.EventBUIPlugin;
@@ -40,7 +28,7 @@ import org.eventb.ui.EventBUIPlugin;
  * @author "Thomas Muller"
  */
 
-public class RuleDetailsView extends ViewPart implements ISelectionListener, IPropertyChangeListener {
+public class RuleDetailsView extends AbstractProofNodeView {
 
 	/**
 	 * The identifier of the Rule Details View (value
@@ -52,26 +40,10 @@ public class RuleDetailsView extends ViewPart implements ISelectionListener, IPr
 	public static final int ZERO = 0;
 
 	private ScrolledComposite sc = null;
-	private IProofTreeNode currentNode = null;
 	private RuleDetailsProvider rdp = null;
-	private Font font;
 
 	@Override
-	public void createPartControl(final Composite parent) {
-		final IWorkbenchWindow workbench = getSite().getWorkbenchWindow();
-		final ISelectionService selectionService = workbench.getSelectionService();
-		// add myself as a global selection listener
-        selectionService.addSelectionListener(this);
-
-		initializeControl(parent);
-
-		font = JFaceResources.getFont(RODIN_MATH_FONT);
-		JFaceResources.getFontRegistry().addListener(this);
-		// prime the selection to display contents
-		refreshOnSelectionChanged(getSite().getPage().getSelection());
-	}
-
-	private void initializeControl(final Composite parent) {
+	protected void initializeControl(final Composite parent) {
 		final GridLayout gl = new GridLayout(1, false);
 		gl.marginHeight = ZERO;
 		gl.marginWidth = ZERO;
@@ -82,13 +54,11 @@ public class RuleDetailsView extends ViewPart implements ISelectionListener, IPr
 	}
 
 	@Override
-	public void setFocus() {
-		// Nothing to do.
-	}
-
-	public void refreshContents(IProofTreeNode node) {
+	protected void refreshContents(IProofTreeNode node, Font font) {
 		if (rdp == null) {
 			rdp = new RuleDetailsProvider(sc, font);
+		} else {
+			rdp.setFont(font);
 		}
 		final IProofRule rule = node.getRule();
 		if (rule == null) {
@@ -103,51 +73,10 @@ public class RuleDetailsView extends ViewPart implements ISelectionListener, IPr
 		sc.setRedraw(true);
 	}
 	
-	private void refreshOnSelectionChanged(ISelection selection) {		
-		if (selection instanceof IStructuredSelection) {
-			final IStructuredSelection ssel = ((IStructuredSelection) selection);
-			final Object element = ssel.getFirstElement();
-			if (element instanceof IProofTreeNode) {
-				if (currentNode == element || sc.isDisposed()) {
-					return;
-				}
-				refreshContents((IProofTreeNode) element);
-			}
-		}
-	}
-
-	@Override
-	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-		if (!(part instanceof ProofTreeUI)) {
-			return;
-		}
-		refreshOnSelectionChanged(selection);
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.part.WorkbenchPart#dispose()
-	 */
 	@Override
 	public void dispose() {
-		final IWorkbenchWindow workbench = getSite().getWorkbenchWindow();
-		final ISelectionService selectionService = workbench.getSelectionService();
-        selectionService.removeSelectionListener(this);
-        JFaceResources.getFontRegistry().removeListener(this);
+		sc.dispose();
 		super.dispose();
-	}
-
-	@Override
-	public void propertyChange(PropertyChangeEvent event) {
-		if (event.getProperty().equals(RODIN_MATH_FONT)) {
-			font = JFaceResources.getFont(RODIN_MATH_FONT);
-			if (rdp == null) {
-				return;
-			}
-			rdp.setFont(font);
-			if (currentNode != null) {
-				refreshContents(currentNode);
-			}
-		}
 	}
 
 }
