5d41ee7dd988f26b7f96304771708a05637e94d2
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2011-12-09 11:08:14 +0000

    added simplification test cases for proof trees with 3 nodes
    svn path=/branches/RodinCore/Simplifier/; revision=13720

--- org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/proofSimplifierTests/NodeRemovalTests.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/proofSimplifierTests/NodeRemovalTests.java
@@ -56,9 +56,125 @@ public class NodeRemovalTests extends AbstractSimplificationTests {
 						rn(p("¬¬x=0"), "",
 								trueGoal()),
 						// expected		
-						trueGoal())
+						trueGoal()),
 				
+				///////////////////
+				// 3 nodes tests //
+				///////////////////
+				
+				/**
+				 * Proof tree:
+				 * 0
+				 * 1
+				 * 2
+				 * Dependencies:
+				 * {0->1}
+				 * Expected:
+				 * 2
+				 */
+				test("⊥ |- ¬¬x∈{0}",
+						// initial
+						rn("",
+								rm("",
+										falseHyp())),
+						// expected		
+						falseHyp()),
+								
+				/**
+				 * Proof tree:
+				 * 0
+				 * 1
+				 * 2
+				 * Dependencies:
+				 * {0->2}
+				 * Expected:
+				 * 0
+				 * 2
+				 */
+				test("x=0 ;; ¬¬y=1|- ¬¬x=0",
+						// initial
+						rn("",
+								rn(p("¬¬y=1"), "",
+										hyp())),
+						// expected		
+						rn("",
+								hyp())),
+				
+				/**
+				 * Proof tree:
+				 * 0
+				 * 1
+				 * 2
+				 * Dependencies:
+				 * {0->1, 0->2}
+				 * Expected:
+				 * 0
+				 * 2
+				 */
+				test("|- ¬¬x=0 ⇒ ⊤",
+						// initial
+						impI(
+								rn(p("¬¬x=0"), "",
+										trueGoal())),
+						// expected		
+						impI(
+								trueGoal())),
 
+				/**
+				 * Proof tree:
+				 * 0
+				 * 1
+				 * 2
+				 * Dependencies:
+				 * {1->2}
+				 * Expected:
+				 * 1
+				 * 2
+				 */
+				test("¬¬x=0 |- ⊤ ⇒ ⊤",
+						// initial
+						rn(p("¬¬x=0"), "",
+								impI(
+										trueGoal())),
+						// expected		
+						impI(
+								trueGoal())),
+
+				/**
+				 * Proof tree:
+				 *  0
+				 * 1 2
+				 * Dependencies:
+				 * {}
+				 * Expected:
+				 * 1
+				 */
+				test("⊤ ∨ ⊤ ;; ⊥ |- ⊤",
+						// initial
+						disjE(p("⊤ ∨ ⊤"),
+								trueGoal(),
+								falseHyp()),
+						// expected		
+						trueGoal()),
+
+				/**
+				 * Proof tree:
+				 *  0
+				 * 1 2
+				 * Dependencies:
+				 * {0->1}
+				 * Expected:
+				 * 2
+				 */
+				test("⊥ ∨ ⊤ |- ⊤",
+						// initial
+						disjE(p("⊥ ∨ ⊤"),
+								falseHyp(),
+								trueGoal()),
+						// expected		
+						trueGoal())
+
+						
 				);
 	}
 
