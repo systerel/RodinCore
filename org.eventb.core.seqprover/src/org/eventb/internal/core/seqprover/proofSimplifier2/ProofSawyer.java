12359f9c784775fd1ca88c4f5a4422bbb9bec697
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2011-12-13 16:55:19 +0000

    added a class that applies simplification to a proof tree
    svn path=/branches/RodinCore/Simplifier/; revision=13767

---
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/proofSimplifier2/ProofSawyer.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
+package org.eventb.internal.core.seqprover.proofSimplifier2;
+
+import org.eventb.core.seqprover.IProofTree;
+
+/**
+ * @author Nicolas Beauger
+ *
+ */
+public class ProofSawyer {
+	
+	private final IProofTree proofTree;
+
+	public ProofSawyer(IProofTree proofTree) {
+		this.proofTree = proofTree;
+	}
+	
+	// TODO monitors
+	public IProofTree simplify() {
+		final SawyerTree sawyerTree = new SawyerTree(proofTree.getRoot());
+		sawyerTree.init();
+		sawyerTree.simplify();
+		return null;// FIXME
+	}
+}
