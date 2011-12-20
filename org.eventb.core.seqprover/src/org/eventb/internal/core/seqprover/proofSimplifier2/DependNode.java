c0b4ddcc934dbad11cb3a93ef79a6f4c9104bc8f
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2011-12-20 16:31:31 +0000

    moved hyp action simplification in a new class dedicated to rule processing.
    svn path=/branches/RodinCore/Simplifier/; revision=13896

--- org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/proofSimplifier2/DependNode.java
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/proofSimplifier2/DependNode.java
@@ -10,25 +10,8 @@
  *******************************************************************************/
 package org.eventb.internal.core.seqprover.proofSimplifier2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Set;
-
-import org.eclipse.core.runtime.Assert;
-import org.eventb.core.ast.Predicate;
-import org.eventb.core.seqprover.IHypAction;
-import org.eventb.core.seqprover.IHypAction.IForwardInfHypAction;
-import org.eventb.core.seqprover.IHypAction.ISelectionHypAction;
 import org.eventb.core.seqprover.IProofRule;
-import org.eventb.core.seqprover.IProofRule.IAntecedent;
 import org.eventb.core.seqprover.IProofTreeNode;
-import org.eventb.internal.core.seqprover.IInternalHypAction;
 
 /**
  * A proof tree node type to use for dependence computation and manipulation.
@@ -41,7 +24,7 @@ import org.eventb.internal.core.seqprover.IInternalHypAction;
 public class DependNode {
 
 
-	private IProofRule rule;
+	private final DependRule rule;
 
 	private final RequiredSequent requiredSequent;
 
@@ -51,57 +34,10 @@ public class DependNode {
 	private boolean deleted = false;
 
 	public DependNode(IProofTreeNode node) {
-		final Set<Predicate> reqHyps = new LinkedHashSet<Predicate>();
-		final List<ProducedSequent> prodSeqs = new ArrayList<ProducedSequent>();
-		this.rule = populateSimplify(node, reqHyps, prodSeqs, this);
-		this.producedSequents = prodSeqs.toArray(new ProducedSequent[prodSeqs.size()]);
-		final Predicate reqGoal = node.getRule().getGoal();
-		this.requiredSequent = new RequiredSequent(reqHyps, reqGoal, this);
-	}
-
-	private static IProofRule populateSimplify(IProofTreeNode pNode,
-			Set<Predicate> reqHyps, List<ProducedSequent> prodSeqs,
-			DependNode node) {
-		final IProofRule rule = pNode.getRule();
-		reqHyps.addAll(rule.getNeededHyps());
-		final List<IAntecedent> antecedents = new ArrayList<IAntecedent>(Arrays.asList(rule.getAntecedents()));
-		final ListIterator<IAntecedent> iterAnte = antecedents.listIterator();
-		while(iterAnte.hasNext()) {
-			final IAntecedent antecedent = iterAnte.next();
-			final Set<Predicate> prodHyps = new LinkedHashSet<Predicate>();
-			prodHyps.addAll(antecedent.getAddedHyps());
-			final List<IHypAction> hypActions = new ArrayList<IHypAction>(antecedent.getHypActions());
-			final ListIterator<IHypAction> iterAction = hypActions.listIterator();
-			while (iterAction.hasNext()) {
-				final IInternalHypAction act = (IInternalHypAction) iterAction
-						.next();
-				final List<Predicate> actHyps = new ArrayList<Predicate>(
-						act.getHyps());
-				actHyps.removeAll(prodHyps);
-				final Iterator<Predicate> iterHyp = actHyps.iterator();
-				while (iterHyp.hasNext()) {
-					final Predicate hyp = iterHyp.next();
-					if (!pNode.getSequent().containsHypothesis(hyp)) {
-						iterHyp.remove();
-					}
-				}
-				if (actHyps.isEmpty()) {
-					iterAction.remove();
-					continue;
-				} else {
-					iterAction.set(HypActionCleaner.makeHypAction(act, actHyps));
-				}
-				reqHyps.addAll(actHyps);
-				if (act instanceof IForwardInfHypAction) {
-					final IForwardInfHypAction fwd = (IForwardInfHypAction) act;
-					prodHyps.addAll(fwd.getInferredHyps());
-				}
-			}
-			iterAnte.set(HypActionCleaner.makeAnte(antecedent, hypActions));
-			final Predicate prodGoal = antecedent.getGoal();
-			prodSeqs.add(new ProducedSequent(prodHyps, prodGoal, node));
-		}
-		return HypActionCleaner.makeRule(rule, antecedents);
+		this.rule = new DependRule(node.getRule());
+		this.rule.init(node.getSequent());
+		this.requiredSequent = this.rule.makeRequiredSequent(this);
+		this.producedSequents = this.rule.makeProducedSequents(this);
 	}
 
 	public RequiredSequent getRequiredSequent() {
@@ -145,51 +81,10 @@ public class DependNode {
 	}
 
 	public IProofRule getRule() {
-		return rule;
+		return rule.toProofRule();
 	}
 	
 	public void compressRule() {
-		
-		final IAntecedent[] antecedents = rule.getAntecedents();
-		final ProducedSequent[] sequents = this.getProducedSequents();
-		Assert.isTrue(sequents.length == antecedents.length);
-		
-		for (int i = 0; i < sequents.length; i++) {
-			final ProducedSequent sequent = sequents[i];
-			final Collection<Predicate> usedPredicates = sequent.getUsedPredicates();
-			final IAntecedent antecedent = antecedents[i];
-			final List<IHypAction> hypActions = antecedent.getHypActions();//FIXME no modify
-			final Set<Predicate> skipped = new HashSet<Predicate>(); 
-			final ListIterator<IHypAction> iterAction = hypActions.listIterator();
-			while(iterAction.hasNext()) {
-				final IHypAction hypAction = iterAction.next();
-				if (hypAction instanceof IForwardInfHypAction) {
-					final IForwardInfHypAction fwd = (IForwardInfHypAction) hypAction;
-					final Collection<Predicate> inferredHyps = fwd
-							.getInferredHyps();
-					final List<Predicate> usefulInf = new ArrayList<Predicate>(
-							inferredHyps);
-					usefulInf.retainAll(usedPredicates);
-					if (usefulInf.isEmpty()) {
-						iterAction.remove();
-						skipped.addAll(fwd.getHyps());
-						skipped.addAll(fwd.getInferredHyps());
-					} else if (usefulInf.size() < inferredHyps.size()) {
-						iterAction.set(HypActionCleaner.makeHypAction(fwd,
-								usefulInf));
-					}
-				} else if (hypAction instanceof ISelectionHypAction) {
-					final ISelectionHypAction select = (ISelectionHypAction) hypAction;
-					final Collection<Predicate> hyps = new ArrayList<Predicate>(select.getHyps());
-					hyps.removeAll(skipped);
-					if(hyps.isEmpty()) {
-						iterAction.remove();
-					}
-				}
-			}
-			
-			antecedents[i] = HypActionCleaner.makeAnte(antecedent, hypActions);
-		}
-		rule = HypActionCleaner.makeRule(rule, Arrays.asList(antecedents));
+		rule.compressHypActions(producedSequents);
 	}
 }
