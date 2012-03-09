04bccaa43b46ef705c079c8cf1c682ab1077e3e8
Laurent Voisin <lvoisin@users.sourceforge.net>
2012-03-09 10:57:32 +0000

    Add ListMultimap to the UI plug-in.
    This is a stripped down version of the eponym collection from the
    Guava library.  If more needs like this arise, we should consider
    integrating Guava into the Rodin platform.
    
    svn path=/trunk/RodinCore/org.eventb.ui.tests/; revision=14301

---
+++ org.eventb.ui.tests/src/org/eventb/ui/tests/TestListMultimap.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
+package org.eventb.ui.tests;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
+
+import org.eventb.internal.ui.utils.ListMultimap;
+import org.junit.Test;
+
+/**
+ * Unit test for class {@link ListMultimap}.
+ * 
+ * @author Laurent Voisin
+ */
+public class TestListMultimap {
+
+	private final ListMultimap<Integer, Boolean> map = new ListMultimap<Integer, Boolean>();
+
+	/**
+	 * Ensures that <code>null</code> is returned for a nonexistent key.
+	 */
+	@Test
+	public void getInexistentKey() {
+		assertNull(map.get(0));
+	}
+
+	/**
+	 * Ensures that the appropriate value is returned for a key, and is not
+	 * associated with another key.
+	 */
+	@Test
+	public void getKeyOneImage() {
+		map.put(0, true);
+		assertEquals(asList(true), map.get(0));
+		assertNull(map.get(1));
+	}
+
+	/**
+	 * Ensures that the appropriate values are returned for a key.
+	 */
+	@Test
+	public void getKeyTwoImages() {
+		map.put(0, true);
+		map.put(0, false);
+		assertEquals(asList(true, false), map.get(0));
+	}
+
+	/**
+	 * Ensures that the appropriate values are returned for a key (with
+	 * duplicate values).
+	 */
+	@Test
+	public void getKeyTwiceSameImage() {
+		map.put(0, true);
+		map.put(0, true);
+		assertEquals(asList(true, true), map.get(0));
+	}
+
+	/**
+	 * Ensures that the appropriate values are returned for a key.
+	 */
+	@Test
+	public void putAll() {
+		final List<Boolean> values = asList(true, false);
+		map.putAll(0, values);
+		assertEquals(values, map.get(0));
+	}
+
+}
---
+++ org.eventb.ui/src/org/eventb/internal/ui/utils/ListMultimap.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
+package org.eventb.internal.ui.utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Partial implementation of a multimap, freely inspired from Guava's API. This
+ * is an abstract datatype homogeneous to Map(K, List<V>) and implemented with a
+ * HashMap and an ArrayList. It is very useful for representing the graph of a
+ * relation.
+ * 
+ * @author Laurent Voisin
+ */
+public class ListMultimap<K, V> {
+
+	private final Map<K, List<V>> map;
+
+	public ListMultimap() {
+		map = new HashMap<K, List<V>>();
+	}
+
+	/**
+	 * Returns the list of values associated with the given key. Returns
+	 * <code>null</code> if the given key is not in this multimap.
+	 * 
+	 * @param key
+	 *            some key
+	 * @return the list of values associated with the given key or
+	 *         <code>null</code>
+	 */
+	public List<V> get(K key) {
+		return map.get(key);
+	}
+
+	/**
+	 * Associate a new value with the given key. The new value is added at the
+	 * end of the list of values for the given key. No attempt is made to ensure
+	 * that values in the list are unique.
+	 * 
+	 * @param key
+	 *            some key
+	 * @param value
+	 *            some value
+	 */
+	public void put(K key, V value) {
+		getValueList(key).add(value);
+	}
+
+	/**
+	 * Associates the new values with the given key. The new values are added at
+	 * the end of the list of values for the given key. No attempt is made to
+	 * ensure that values in the list are unique.
+	 * 
+	 * @param key
+	 *            some key
+	 * @param values
+	 *            some values
+	 */
+	public void putAll(K key, Collection<V> values) {
+		final List<V> list = getValueList(key);
+		list.addAll(values);
+	}
+
+	private List<V> getValueList(K key) {
+		List<V> values = map.get(key);
+		if (values == null) {
+			values = new ArrayList<V>();
+			map.put(key, values);
+		}
+		return values;
+	}
+
+}
