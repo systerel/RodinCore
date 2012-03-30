systerel/RodinCore

commit c26fdb309e67162a1c6e36705015575108392c2d

ensure yellow boxes are visible when they get focus or text is input
svn path=/trunk/RodinCore/org.eventb.ui/; revision=14315

org.eventb.ui/src/org/eventb/internal/ui/prover/YellowBoxMaker.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Systerel and others.
+ * Copyright (c) 2011, 2012 Systerel and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,6 +18,7 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eventb.internal.ui.EventBSharedColor;
 import org.eventb.internal.ui.autocompletion.ContentProposalFactory;
 
@@ -63,6 +64,8 @@ public class YellowBoxMaker extends ControlMaker {
 				final PredicateRow row = holder.getRow();
 				row.instantiate();
 			}
+			FormToolkit.ensureVisible(owner);
+			owner.showSelection();
 		}
 	}
 

commit 7772f26f0b83e85dcb5c28daeba97cb66389b101

Fix disposed widget error introduced in commit r14315.
When the instantiation is run, the contents of the predicate row gets
disposed as a side-effect of modifying the proof tree.  It therefore
cannot be made visible.

svn path=/trunk/RodinCore/org.eventb.ui/; revision=14437

org.eventb.ui/src/org/eventb/internal/ui/prover/YellowBoxMaker.java
@@ -63,9 +63,10 @@ public class YellowBoxMaker extends ControlMaker {
 			if (e.character == SWT.CR && owner.isFocusControl()) {
 				final PredicateRow row = holder.getRow();
 				row.instantiate();
+			} else {
+				FormToolkit.ensureVisible(owner);
+				owner.showSelection();
 			}
-			FormToolkit.ensureVisible(owner);
-			owner.showSelection();
 		}
 	}
 