3246ea9979523adf25bcc7926b59f7f05a27b7c3
Laurent Voisin <lvoisin@users.sourceforge.net>
2012-04-08 08:51:56 +0000

    Fix NullTimeTracker
    Prevent the prover UI from dumping illegible messages on the console
    when time tracking is not enabled.
    
    svn path=/trunk/RodinCore/org.eventb.ui/; revision=14522

--- org.eventb.ui/src/org/eventb/internal/ui/prover/TimeTracker.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/TimeTracker.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Systerel and others.
+ * Copyright (c) 2011, 2012 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -56,6 +56,11 @@ public class TimeTracker {
 
 		@Override
 		public void endTask(String msg) {
+			// Do nothing
+		}
+
+		@Override
+		public void trace(String msg) {
 			// Do nothing
 		}
 
