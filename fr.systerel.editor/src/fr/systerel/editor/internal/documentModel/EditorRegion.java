468ab3102715bb2e6a576cec0f8be617f4c2906e
Thomas Muller <thomasmuller@users.sourceforge.net>
2012-05-21 09:17:59 +0000

    Use of boolean instead of Boolean.
    svn path=/trunk/RodinCore/fr.systerel.editor/; revision=14725

--- fr.systerel.editor/src/fr/systerel/editor/internal/documentModel/EditorRegion.java
+++ fr.systerel.editor/src/fr/systerel/editor/internal/documentModel/EditorRegion.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Systerel and others.
+ * Copyright (c) 2011, 2012 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License  v1.0
  * which accompanies this distribution, and is available at
@@ -31,7 +31,7 @@ public class EditorRegion {
 	private final ILElement element;
 	private final ContentType type;
 	private final IAttributeManipulation manipulation;
-	private final Boolean multiline;
+	private final boolean multiline;
 	private final String alignmentStr;
 
 	public EditorRegion(int startOffset, int level, String text,
