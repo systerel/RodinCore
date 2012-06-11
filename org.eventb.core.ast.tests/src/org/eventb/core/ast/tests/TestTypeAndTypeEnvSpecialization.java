d5a95b0d37c25da7fcc2d13a594da5e42edab5e0
Thomas Muller <thomasmuller@users.sourceforge.net>
2012-06-11 10:25:09 +0000

    Finished testing type specialization. Type environment specialization remains.
    svn path=/trunk/RodinCore/org.eventb.core.ast.tests/; revision=14846

--- org.eventb.core.ast.tests/src/org/eventb/core/ast/tests/TestTypeAndTypeEnvSpecialization.java
+++ org.eventb.core.ast.tests/src/org/eventb/core/ast/tests/TestTypeAndTypeEnvSpecialization.java
@@ -10,9 +10,13 @@
  *******************************************************************************/
 package org.eventb.core.ast.tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eventb.core.ast.BooleanType;
 import org.eventb.core.ast.GivenType;
 import org.eventb.core.ast.ISpecialization;
+import org.eventb.core.ast.ParametricType;
 import org.eventb.core.ast.PowerSetType;
 import org.eventb.core.ast.ProductType;
 import org.eventb.core.ast.Type;
@@ -29,14 +33,12 @@ public class TestTypeAndTypeEnvSpecialization extends AbstractTests {
 	private static final PowerSetType POWER_S = ff.makePowerSetType(S);
 
 	private static final GivenType T = ff.makeGivenType("T");
-
+	private static final GivenType U = ff.makeGivenType("U");
+	private static final GivenType V = ff.makeGivenType("V");
+	
 	private static final Type Z = ff.makeIntegerType();
 	private static final PowerSetType POWER_Z = ff.makePowerSetType(Z);
-
-	// private static final FreeIdentifier a = mFreeIdentifier("a", S);
-	// private static final FreeIdentifier b = mFreeIdentifier("b", S);
-	// private static final Expression one = mIntegerLiteral(1);
-	// private static final FreeIdentifier untyped = mFreeIdentifier("untyped");
+	private static final ProductType PRODUCT_T_U = ff.makeProductType(T, U);
 
 	final ISpecialization spec = ff.makeSpecialization();
 
@@ -133,6 +135,19 @@ public class TestTypeAndTypeEnvSpecialization extends AbstractTests {
 	}
 	
 	/**
+	 * Ensures that a given type to product type specialization succeeds.
+	 */
+	public void testGivenTypeToProductTypeSpecialization() {
+		try {
+			spec.put(S, PRODUCT_T_U);
+			final Type specializedS = S.specialize(spec);
+			assertEquals(PRODUCT_T_U, specializedS);
+		} catch (IllegalArgumentException e) {
+			fail("Should not have raised an exception");
+		}
+	}
+	
+	/**
 	 * Ensures that a powerset type to powerset type specialization succeeds.
 	 */
 	public void testPowerSetTypeSpecialization() {
@@ -199,7 +214,81 @@ public class TestTypeAndTypeEnvSpecialization extends AbstractTests {
 			fail("Should not have raised an exception");
 		}
 	}
+	
+	/**
+	 * Ensures that a given type appearing as left member of a product type is
+	 * successfully spezialized.
+	 */
+	public void testProductTypeLeftSpecialization() {
+		try {
+			final ProductType pType = ff.makeProductType(S, T);
+			spec.put(S, U);
+			final Type specialType = pType.specialize(spec);
+			assertEquals(ff.makeProductType(U, T), specialType);
+		} catch (IllegalArgumentException e) {
+			fail("Should not have raised an exception");
+		}
+	}
 
-	// FIXME TO BE COMPLETED...
+	/**
+	 * Ensures that a given type appearing as right member of a product type is
+	 * successfully spezialized.
+	 */
+	public void testProductTypeRightSpecialization() {
+		try {
+			final ProductType pType = ff.makeProductType(T, S);
+			spec.put(S, U);
+			final Type specialType = pType.specialize(spec);
+			assertEquals(ff.makeProductType(T, U), specialType);
+		} catch (IllegalArgumentException e) {
+			fail("Should not have raised an exception");
+		}
+	}
+	
+	/**
+	 * Ensures that given types appearing as left and right members of a product
+	 * type are successfully spezialized.
+	 */
+	public void testProductTypeLeftRightSpecialization() {
+		try {
+			final ProductType pType = ff.makeProductType(S, T);
+			spec.put(S, U);
+			spec.put(T, V);
+			final Type specialType = pType.specialize(spec);
+			assertEquals(ff.makeProductType(U, V), specialType);
+		} catch (IllegalArgumentException e) {
+			fail("should not have raised an exception");
+		}
+	}
+	
+	/**
+	 * Ensures that given types appearing as type parameters of a parametric
+	 * type are successfully spezialized.
+	 */
+	public void testParametricTypeSpecialization() {
+		try {
+			final List<Type> types = new ArrayList<Type>();
+			types.add(S);
+			types.add(T);
+			types.add(U);
+			final Type paramType = LIST_FAC.makeParametricType(types,
+					LIST_DT.getTypeConstructor());
+			spec.put(S, T);
+			spec.put(T, U);
+			spec.put(U, V);
+			final Type s = paramType.specialize(spec);
+			assertTrue(s instanceof ParametricType);
+			final ParametricType spType = (ParametricType) s;
+			final Type[] typeParameters = spType.getTypeParameters();
+			assertTrue(typeParameters.length == 3);
+			assertEquals(T, typeParameters[0]);
+			assertEquals(U, typeParameters[1]);
+			assertEquals(V, typeParameters[2]);
+		} catch (IllegalArgumentException e) {
+			fail("should not have raised an exception");
+		}
+	}
+	
+	// FIXME TO BE COMPLETED BY TYPE ENV TESTS...
 
 }
