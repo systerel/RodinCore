db4e2c62341d10122acae0bb9cf01afa43ba9bd3
Laurent Voisin <lvoisin@users.sourceforge.net>
2012-07-22 16:42:37 +0000

    Refactor class FreshNameSolver
    Restructure the class, getting rid of the singleton instance and making
    it more object-oriented.  No functional change seen from existing
    clients, but potential for using this class more efficiently.
    
    Rewrite the Javadoc of the class and public methods.
    
    svn path=/trunk/RodinCore/org.eventb.core.ast/; revision=14968

--- org.eventb.core.ast/src/org/eventb/internal/core/ast/FreshNameSolver.java
+++ org.eventb.core.ast/src/org/eventb/internal/core/ast/FreshNameSolver.java
@@ -20,10 +20,112 @@ import org.eventb.core.ast.ITypeEnvironment;
 import org.eventb.internal.core.typecheck.TypeEnvironment;
 
 /**
- * Helper class to compute free names.
+ * Helper class to compute fresh names. This class provides two static methods
+ * for the case where only one fresh name needs to be computed. However, when
+ * computing several names in a row, it is more efficient to directly
+ * instantiate this class and use its public method.
+ * <p>
+ * This class does not use any kind of cache, it is therefore possible to modify
+ * the type environment or the set of used names between two calls of the
+ * <code>solve()</code> method.
+ * </p>
+ * 
  */
 public class FreshNameSolver {
 
+	/**
+	 * Returns an identifier name resembling the given name and that does not
+	 * occur in the given type environment. The returned name is guaranteed to
+	 * be a valid identifier name in the mathematical language of the given type
+	 * environment.
+	 * 
+	 * @param name
+	 *            some identifier name
+	 * @param environment
+	 *            the type environment in which the returned name must be fresh
+	 * 
+	 * @return a name fresh in the given type environment
+	 */
+	public static String solve(TypeEnvironment environment, String name) {
+		return new FreshNameSolver(environment).solve(name);
+	}
+
+	/**
+	 * Returns an identifier name resembling the given name and that does not
+	 * occur in the given set of used names. The returned name is guaranteed to
+	 * be a valid identifier name in the mathematical language specified by the
+	 * given formula factory.
+	 * <p>
+	 * This method is an alternative to {{@link #solve(TypeEnvironment, String)}
+	 * as in certain cases, the type environment can not be built (e.g. when
+	 * formulas are untyped).
+	 * </p>
+	 * 
+	 * @param name
+	 *            some identifier name
+	 * @param usedNames
+	 *            identifier names already used
+	 * @param factory
+	 *            the formula factory that defines the mathematical language,
+	 *            and in particular the reserved names of the language
+	 * 
+	 * @return an identifier name that does not occur in the given used names
+	 */
+	public static String solve(String name, Set<String> usedNames,
+			FormulaFactory factory) {
+		return new FreshNameSolver(usedNames, factory).solve(name);
+	}
+
+	private final FormulaFactory factory;
+	private final ITypeEnvironment typeEnvironment;
+	private final Set<String> usedNames;
+
+	public FreshNameSolver(ITypeEnvironment typeEnvironment) {
+		this.factory = typeEnvironment.getFormulaFactory();
+		this.typeEnvironment = typeEnvironment;
+		this.usedNames = null;
+	}
+
+	public FreshNameSolver(Set<String> usedNames, FormulaFactory factory) {
+		this.factory = factory;
+		this.usedNames = usedNames;
+		this.typeEnvironment = null;
+	}
+
+	public String solve(String name) {
+		if (!contains(name)) {
+			// Not used, this name is OK.
+			return name;
+		}
+		// We have a name conflict, so we try with other names
+		final StructuredName sname = new StructuredName(name);
+		String newName;
+		do {
+			sname.increment();
+			newName = sname.toString();
+		} while (contains(newName) || !factory.isValidIdentifierName(newName));
+		return newName;
+	}
+
+	private boolean contains(String name) {
+		if (typeEnvironment != null) {
+			return typeEnvironment.contains(name);
+		}
+		return usedNames.contains(name);
+	}
+
+	/**
+	 * Representation of an identifier name in three components:
+	 * <ul>
+	 * <li>an arbitrary prefix</li>
+	 * <li>an optional numeric suffix</li>
+	 * <li>an optional string of quote characters</li>
+	 * </ul>
+	 * <p>
+	 * The aim of this class is to create new identifier names by incrementing
+	 * the numeric suffix, while keeping the other components unchanged.
+	 * </p>
+	 */
 	private static class StructuredName {
 		private final String prefix;
 		private int suffix;
@@ -33,8 +135,8 @@ public class FreshNameSolver {
 				"^(.*[^\\d'])(\\d*)('*)$", Pattern.DOTALL);
 
 		StructuredName(String name) {
-			Matcher matcher = suffixExtractor.matcher(name);
-			boolean result = matcher.matches();
+			final Matcher matcher = suffixExtractor.matcher(name);
+			final boolean result = matcher.matches();
 			assert result;
 			prefix = matcher.group(1);
 			final String digits = matcher.group(2);
@@ -56,96 +158,6 @@ public class FreshNameSolver {
 			}
 			return prefix + suffix + quotes;
 		}
-	}
-	
-	private FreshNameSolver() {
-		// singleton
-	}
-	
-	private static class NameSolver {
-
-		private FormulaFactory factory;
-		private ITypeEnvironment typeEnvironment;
-		private Set<String> usedNames;
-				
-		public NameSolver() {
-			// Nothing to do
-		}
-
-		private boolean contains(String name) {
-			if (typeEnvironment != null) {
-				return typeEnvironment.contains(name);
-			}
-			return usedNames.contains(name);
-		}
-
-		private FormulaFactory getFormulaFactory() {
-			return factory;
-		}
-		
-		protected String solve(ITypeEnvironment environment, String name) {
-			this.factory = environment.getFormulaFactory();
-			this.typeEnvironment = environment;
-			this.usedNames = null;
-			return solve(name);
-		}
-		
-		protected String solve(FormulaFactory forumlaFactory, Set<String> reservedNames, String name){
-			this.factory = forumlaFactory;
-			this.typeEnvironment = null;
-			this.usedNames = reservedNames;
-			return solve(name);
-		}
-		
-		private String solve(String name) {
-			if (!contains(name)) {
-				// Not used, this name is OK.
-				return name;
-			}
-			// We have a name conflict, so we try with another name
-			final StructuredName sname = new StructuredName(name);
-			String newName;
-			do {
-				sname.increment();
-				newName = sname.toString();
-			} while (contains(newName)
-					|| !getFormulaFactory().isValidIdentifierName(newName));
-			return newName;
-		}
-
-	}
-	
-	private static final NameSolver SOLVER = new NameSolver();
-
-	/**
-	 * Method which returns from the given name a solved free name that does not
-	 * appear in the given type environment.
-	 * 
-	 * @param name
-	 *            the name to solve
-	 * 
-	 * @return a solved name that does not appear in the type environment
-	 */
-	public static String solve(TypeEnvironment environment, String name) {
-		return SOLVER.solve(environment, name);
-	}
-
-	/**
-	 * Method which returns from the given name a solved free name that does not
-	 * appear in the given set of used names, checking that the name is valid by
-	 * using the given formula factory.
-	 * 
-	 * This method is an alternative to {{@link #solve(TypeEnvironment, String)}
-	 * as in certain cases, the type environment can not be given (e.g. when
-	 * simply parsing formulas).
-	 * 
-	 * @param name
-	 *            the name to solve
-	 * @return a solved name that does not appear in the used names
-	 */
-	public static String solve(String name, Set<String> usedNames,
-			FormulaFactory factory) {
-		return SOLVER.solve(factory, usedNames, name);
 	}
 
 }
