ca47ddef5313e1b5d4b4bd81eb611bc80c5086b8
Laurent Voisin <lvoisin@users.sourceforge.net>
2012-12-11 17:46:50 +0000

    Factor out common code for launching New wizard in explorer
    There were two very similar classes, albeit with some unneeded
    differences and that evolved differently for no reason.  Now, only one
    implementation is provided in a common super-class.
    
    svn path=/trunk/RodinCore/fr.systerel.explorer/; revision=15333

--- fr.systerel.explorer/src/fr/systerel/internal/explorer/navigator/actionProviders/AbstractNewActionDelegate.java
+++ fr.systerel.explorer/src/fr/systerel/internal/explorer/navigator/actionProviders/AbstractNewActionDelegate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2010 Systerel and others.
+ * Copyright (c) 2008, 2012 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License  v1.0
  * which accompanies this distribution, and is available at
@@ -7,8 +7,7 @@
  *
  * Contributors:
  *     Systerel - initial API and implementation
-  *******************************************************************************/
-
+ *******************************************************************************/
 package fr.systerel.internal.explorer.navigator.actionProviders;
 
 import org.eclipse.jface.action.IAction;
@@ -17,22 +16,37 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IViewActionDelegate;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.IWorkbench;
+import org.eventb.internal.ui.wizards.NewComponentWizard;
 import org.eventb.internal.ui.wizards.NewProjectWizard;
 
 /**
- * 
- * An action provider for creating new projects
- *
+ * Common implementation of action delegates for creating new projects and new
+ * components.
  */
-public class NewProjectActionDelegate implements IViewActionDelegate {
+public abstract class AbstractNewActionDelegate implements IViewActionDelegate {
+
+	public static class NewComponent extends AbstractNewActionDelegate {
+		@Override
+		protected INewWizard createWizard() {
+			return new NewComponentWizard();
+		}
+	}
+
+	public static class NewProject extends AbstractNewActionDelegate {
+		@Override
+		protected INewWizard createWizard() {
+			return new NewProjectWizard();
+		}
+	}
 
 	private IViewPart view;
-	IStructuredSelection ssel;
-	
+	private IStructuredSelection ssel;
+
 	@Override
 	public void init(IViewPart viewPart) {
 		this.view = viewPart;
@@ -43,22 +57,24 @@ public class NewProjectActionDelegate implements IViewActionDelegate {
 		final IViewSite site = view.getViewSite();
 		final Shell shell = site.getShell();
 		final IWorkbench workbench = site.getWorkbenchWindow().getWorkbench();
+		final INewWizard wizard = createWizard();
+		wizard.init(workbench, ssel);
 		BusyIndicator.showWhile(shell.getDisplay(), new Runnable() {
 			@Override
 			public void run() {
-				NewProjectWizard wizard = new NewProjectWizard();
-				wizard.init(workbench, ssel);
-				WizardDialog dialog = new WizardDialog(shell, wizard);
+				final WizardDialog dialog = new WizardDialog(shell, wizard);
 				dialog.create();
 				dialog.open();
 			}
 		});
 	}
 
+	protected abstract INewWizard createWizard();
+
 	@Override
-	public void selectionChanged(IAction action, ISelection sel) {
-		if (sel instanceof IStructuredSelection) {
-			this.ssel = (IStructuredSelection) sel;
+	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			this.ssel = (IStructuredSelection) selection;
 		} else {
 			this.ssel = null;
 		}
