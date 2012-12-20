6432cc01923d64916ff0ccf27c0a755c2686bb70
Laurent Voisin <lvoisin@users.sourceforge.net>
2012-12-20 12:04:39 +0000

    Cleanup tests for PredicateDecomposer.
    Remove duplicated code and replace it with calls to standard methods
    in class TestLib.
    
    svn path=/trunk/RodinCore/org.eventb.core.seqprover.tests/; revision=15374

--- org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/tests/PredicateDecomposerTests.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/tests/PredicateDecomposerTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Systerel and others.
+ * Copyright (c) 2010, 2012 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,20 +10,17 @@
  *******************************************************************************/
 package org.eventb.core.seqprover.tests;
 
-import static org.eventb.core.ast.LanguageVersion.V2;
-import static org.eventb.core.seqprover.eventbExtensions.DLib.mDLib;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.eventb.core.seqprover.tests.TestLib.genPred;
+import static org.eventb.core.seqprover.tests.TestLib.genPredList;
+import static org.eventb.core.seqprover.tests.TestLib.mTypeEnvironment;
+import static org.junit.Assert.assertEquals;
 
-import org.eventb.core.ast.FormulaFactory;
-import org.eventb.core.ast.IParseResult;
+import java.util.Collection;
+import java.util.List;
+
 import org.eventb.core.ast.ISealedTypeEnvironment;
-import org.eventb.core.ast.ITypeCheckResult;
 import org.eventb.core.ast.ITypeEnvironmentBuilder;
 import org.eventb.core.ast.Predicate;
-import org.eventb.core.seqprover.eventbExtensions.DLib;
 import org.eventb.internal.core.seqprover.proofBuilder.PredicateDecomposer;
 import org.junit.Test;
 
@@ -33,71 +30,14 @@ import org.junit.Test;
  * 
  * @author Hugo De Sa Pereira Pinto
  */
-
 public class PredicateDecomposerTests {
-
-	/**
-	 * The factory to use for parsing
-	 */
-	protected final FormulaFactory factory = FormulaFactory.getDefault();
-
-	protected final DLib lib;
-
-	/**
-	 * Constructor.
-	 * <p>
-	 */
-	public PredicateDecomposerTests() {
-		this.lib = mDLib(factory);
-	}
-
-	protected Predicate makePredicate(String image,
-			ITypeEnvironmentBuilder typenv) {
-		final Predicate pred = lib.parsePredicate(image);
-		if (pred == null)
-			fail("Predicate: \n\t" + image + "\n\tcannot be parsed");
-		final ITypeCheckResult typeCheck = pred.typeCheck(typenv);
-		if (typeCheck.hasProblem())
-			fail("Input predicate: \n\t" + image + "\n\tcannot be type checked");
-		typenv.addAll(typeCheck.getInferredEnvironment());
-		return pred;
-	}
-
-	private ITypeEnvironmentBuilder makeTypeEnvironment(String... env) {
-		assertTrue(env.length % 2 == 0);
-		final ITypeEnvironmentBuilder typenv = factory.makeTypeEnvironment();
-		for (int i = 0; i < env.length; i += 2) {
-			final String name = env[i];
-			final String typeString = env[i + 1];
-			final IParseResult res = factory.parseType(typeString, V2);
-			assertFalse(res.hasProblem());
-			typenv.addName(name, res.getParsedType());
-		}
-		return typenv;
-	}
-
-	protected void predicateTest(ISealedTypeEnvironment stypenv,
-			String inputImage, String... subgoals) {
-		ITypeEnvironmentBuilder typenv = stypenv.makeBuilder();
-		final Predicate input = makePredicate(inputImage, typenv);
-		Predicate[] subgoalsPreds = new Predicate[subgoals.length];
-		PredicateDecomposer decomposer = new PredicateDecomposer(stypenv);
-		Predicate[] result = (Predicate[]) decomposer.decompose(input).toArray(
-				new Predicate[0]);
-		for (int i = 0; i < subgoals.length; i++) {
-			Predicate subGoalsPred = makePredicate(subgoals[i], typenv);
-			subgoalsPreds[i] = subGoalsPred;
-		}
-		assertArrayEquals(subgoalsPreds, result);
-	}
 
 	@Test
 	public void simpleTest() {
-		final ISealedTypeEnvironment env = makeTypeEnvironment().makeSnapshot();
-		predicateTest(env, "x=0", "x=0");
-		predicateTest(env, "∀x,y · x=0∧y=0", "y=0", "x=0");
-		predicateTest(env, "∀x· x=0∧(∀x· x=0)", "x=0 ");
-		predicateTest(env, "∀x· x=0∧(∀x·(∀x·x=0)∧x=0)", "x=0");
+		predicateTest("x=ℤ", "x=0", "x=0");
+		predicateTest("", "∀x,y · x=0∧y=0", "y=0", "x=0");
+		predicateTest("", "∀x· x=0∧(∀x· x=0)", "x=0 ");
+		predicateTest("", "∀x· x=0∧(∀x·(∀x·x=0)∧x=0)", "x=0");
 	}
 
 	/**
@@ -105,11 +45,7 @@ public class PredicateDecomposerTests {
 	 */
 	@Test
 	public void testWithEnv() {
-		final ISealedTypeEnvironment env = makeTypeEnvironment(//
-				"N", "ℙ(N)", //
-				"age", "L ↔ ℤ", //
-				"l_net", "ℤ ↔ L",//
-				"parity", "ℤ ↔ ℤ").makeSnapshot();
+		final String env = "N=ℙ(N); age=L ↔ ℤ; l_net=ℤ ↔ L; parity=ℤ ↔ ℤ";
 		predicateTest(env, "∀ n, l · n∈N ∧ l∈L ⇒ l∈dom(age) ∧ age∈L ⇸ ℤ",
 				"l∈dom(age)", "n∈N", "l∈L", "age∈L ⇸ ℤ");
 		predicateTest(env, "∀l·l∈L ⇒ n_net[N×{l}] ⊆ 0‥age(l)", "l∈L",
@@ -122,4 +58,16 @@ public class PredicateDecomposerTests {
 		predicateTest(env, "x∈dom(parity) ∧ parity∈ℤ ⇸ ℤ", "x∈dom(parity)",
 				"parity∈ℤ ⇸ ℤ");
 	}
+	
+	private void predicateTest(String typenvImage, String inputImage,
+			String... subgoals) {
+		final ITypeEnvironmentBuilder typenv = mTypeEnvironment(typenvImage);
+		final Predicate input = genPred(typenv, inputImage);
+		final ISealedTypeEnvironment stypenv = typenv.makeSnapshot();
+		final PredicateDecomposer decomposer = new PredicateDecomposer(stypenv);
+		final List<Predicate> actual = decomposer.decompose(input);
+		final Collection<Predicate> expected = genPredList(typenv, subgoals);
+		assertEquals(expected, actual);
+	}
+
 }
