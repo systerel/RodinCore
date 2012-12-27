00c7657d2c71cd322a10d9da7a6e6fa957dcb8fc
Laurent Voisin <lvoisin@users.sourceforge.net>
2012-12-27 10:01:15 +0000

    Fix copyright notices in plug-in org.eventb.ui.tests.integration.
    Ensure that all copyright notices are consistent and pass the new checker.
    Add missing notices.
    Update copyright year based on date of last commit to repository.
    
    svn path=/trunk/RodinCore/org.eventb.ui.tests.integration/; revision=15414

--- org.eventb.ui.tests.integration/src/org/eventb/ui/tests/integration/UIIntegrationTestPlugin.java
+++ org.eventb.ui.tests.integration/src/org/eventb/ui/tests/integration/UIIntegrationTestPlugin.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2012 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
 package org.eventb.ui.tests.integration;
 
 import org.osgi.framework.BundleActivator;
