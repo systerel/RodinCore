921f9964fcf4affa05b5010918b97185c5adeed7
Laurent Voisin <lvoisin@users.sourceforge.net>
2012-12-27 09:54:14 +0000

    Fix copyright notices in plug-in org.eventb.pp.
    Ensure that all copyright notices are consistent and pass the new checker.
    Add missing notices.
    Update copyright year based on date of last commit to repository.
    
    svn path=/trunk/RodinCore/org.eventb.pp/; revision=15405

--- org.eventb.pp/src/org/eventb/internal/pp/core/tracing/ExtensionalityOrigin.java
+++ org.eventb.pp/src/org/eventb/internal/pp/core/tracing/ExtensionalityOrigin.java
@@ -1,11 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2006 ETH Zurich.
+ * Copyright (c) 2006, 2012 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ETH Zurich - initial API and implementation
  *******************************************************************************/
-
 package org.eventb.internal.pp.core.tracing;
 
 import java.util.Arrays;
--- org.eventb.pp/src/org/eventb/internal/pp/loader/predicate/AbstractContext.java
+++ org.eventb.pp/src/org/eventb/internal/pp/loader/predicate/AbstractContext.java
@@ -1,15 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2011 ETH Zurich and others.
+ * Copyright (c) 2006, 2012 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     ETH Zurich - initial API and implementation
  *     Systerel - adapted to XProver v2 API
  *******************************************************************************/
-
 package org.eventb.internal.pp.loader.predicate;
 
 import java.util.ArrayList;
