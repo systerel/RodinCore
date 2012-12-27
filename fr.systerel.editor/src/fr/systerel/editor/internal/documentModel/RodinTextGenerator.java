2178a5789b81dc8d5d09bf87bf7f43b7bef68984
Laurent Voisin <lvoisin@users.sourceforge.net>
2012-12-27 09:42:07 +0000

    Fix copyright notices in plug-in fr.systerel.editor.
    Ensure that all copyright notices are consistent and pass the new checker.
    Add missing notices.
    Update copyright year based on date of last commit to repository.
    
    svn path=/trunk/RodinCore/fr.systerel.editor/; revision=15394

--- fr.systerel.editor/src/fr/systerel/editor/internal/documentModel/RodinTextGenerator.java
+++ fr.systerel.editor/src/fr/systerel/editor/internal/documentModel/RodinTextGenerator.java
@@ -1,13 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2008, 2012 Systerel and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License  v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     Systerel - initial API and implementation
- *     Systerel - fixed bug 3392038: no need to filter "other" empty attributes 
  *******************************************************************************/
 package fr.systerel.editor.internal.documentModel;
 
--- fr.systerel.editor/src/fr/systerel/editor/internal/presentation/IRodinColorConstant.java
+++ fr.systerel.editor/src/fr/systerel/editor/internal/presentation/IRodinColorConstant.java
@@ -1,14 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2011 Systerel and others.
+ * Copyright (c) 2008, 2012 Systerel and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License  v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     Systerel - initial API and implementation
  *******************************************************************************/
-
 package fr.systerel.editor.internal.presentation;
 
 import org.eclipse.swt.graphics.Color;
