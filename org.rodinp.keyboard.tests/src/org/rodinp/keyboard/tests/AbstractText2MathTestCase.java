f91a7ce3e3e82951f3ee8a6930850bad25028cc4
Laurent Voisin <lvoisin@users.sourceforge.net>
2012-12-27 10:06:25 +0000

    Fix copyright notices in plug-in org.rodinp.keyboard.tests.
    Ensure that all copyright notices are consistent and pass the new checker.
    Add missing notices.
    Update copyright year based on date of last commit to repository.
    
    svn path=/trunk/RodinCore/org.rodinp.keyboard.tests/; revision=15420

--- org.rodinp.keyboard.tests/src/org/rodinp/keyboard/tests/AbstractText2MathTestCase.java
+++ org.rodinp.keyboard.tests/src/org/rodinp/keyboard/tests/AbstractText2MathTestCase.java
@@ -1,6 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2009 ETH Zurich and others.
- * 
+ * Copyright (c) 2005, 2012 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,8 +7,7 @@
  *
  * Contributors:
  *     ETH Zurich - initial API and implementation
- ******************************************************************************/
-
+ *******************************************************************************/
 package org.rodinp.keyboard.tests;
 
 import junit.framework.TestCase;
