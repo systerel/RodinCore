3aebb625e21d909974a7b0a553d2eb20f944440a
Laurent Voisin <lvoisin@users.sourceforge.net>
2012-12-27 10:05:36 +0000

    Fix copyright notices in plug-in org.rodinp.core.tests.
    Ensure that all copyright notices are consistent and pass the new checker.
    Add missing notices.
    Update copyright year based on date of last commit to repository.
    
    svn path=/trunk/RodinCore/org.rodinp.core.tests/; revision=15418

--- org.rodinp.core.tests/src/org/rodinp/core/tests/CopyMoveResourcesTests.java
+++ org.rodinp.core.tests/src/org/rodinp/core/tests/CopyMoveResourcesTests.java
@@ -1,14 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation as
- *     		org.eclipse.jdt.core.tests.model.CopyMoveResourcesTests
- *     ETH Zurich - adaptation from JDT to Rodin
+ *     IBM Corporation - initial API and implementation
+ *     ETH Zurich - adapted from org.eclipse.jdt.core.tests.model.CopyMoveResourcesTests
  *     Systerel - separation of file and root element
  *******************************************************************************/
 package org.rodinp.core.tests;
--- org.rodinp.core.tests/src/org/rodinp/core/tests/MementoTests.java
+++ org.rodinp.core.tests/src/org/rodinp/core/tests/MementoTests.java
@@ -1,14 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation as
- *     		org.eclipse.jdt.core.tests.model.MementoTests
- *     ETH Zurich - adaptation from JDT to Rodin
+ *     IBM Corporation - initial API and implementation
+ *     ETH Zurich - adapted from org.eclipse.jdt.core.tests.model.MementoTests
  *     Systerel - removed occurrence count
  *     Systerel - separation of file and root element
  *     Systerel - added tests for internal elements named "|"
