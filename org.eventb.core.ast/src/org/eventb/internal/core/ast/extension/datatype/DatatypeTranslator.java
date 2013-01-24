94a5762f049e4561678e5c99c83ca6cb19c12aec
vinmont <vincent.monfort@systerel.fr>
2013-01-24 11:33:49 +0100

    Bugfix in DatatypeTranslator
    The source formula factory was used instead of the target one in
    mTrgProdType

--- org.eventb.core.ast/src/org/eventb/internal/core/ast/extension/datatype/DatatypeTranslator.java
+++ org.eventb.core.ast/src/org/eventb/internal/core/ast/extension/datatype/DatatypeTranslator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Systerel and others.
+ * Copyright (c) 2012, 2013 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -482,7 +482,7 @@ public class DatatypeTranslator {
 	}
 
 	private Type mTrgProdType(Type t1, Type t2) {
-		return srcFactory.makeProductType(t1, t2);
+		return trgFactory.makeProductType(t1, t2);
 	}
 
 	private Type mTrgRelType(Type t1, Type t2) {
