0ccc5203bcfef70e0563443c0fb1eaae534c4fbe
vinmont <vincent.monfort@systerel.fr>
2013-02-04 15:50:53 +0100

    Prepare IFormulaRewriter factory modification
    IFormulaRewriter implementation must not rewrite nodes with a
    different factory than the original formula.
    As a consequence constructors and methods of IFormulaRewriter
    interface and implementors that allow to reference another factory
    than formula factory are marked as deprecated. And it now uses the
    factory of the original formula to rewrite it.
    
    Add a factory to ITypeCheckingRewriter implementations constructors
    and return this factory when needed since it is the correct level of
    rewriting to change factory.

--- org.eventb.core.ast/src/org/eventb/core/ast/IFormulaRewriter.java
+++ org.eventb.core.ast/src/org/eventb/core/ast/IFormulaRewriter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 ETH Zurich and others.
+ * Copyright (c) 2006, 2013 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,8 +18,8 @@ package org.eventb.core.ast;
  * predicate variables. See {@link IFormulaRewriter2}.
  * <p>
  * This interface contains one method for each of the sub-classes of
- * <code>Formula</code>, except assignments which are not covered by
- * sub-formula rewriting.
+ * <code>Formula</code>, except assignments which are not covered by sub-formula
+ * rewriting.
  * </p>
  * <p>
  * Rewriting must be compatible with type-checking. For each
@@ -95,6 +95,7 @@ public interface IFormulaRewriter {
 	 * 
 	 * @return the formula factory to use when rewriting
 	 */
+	@Deprecated
 	FormulaFactory getFactory();
 
 	/**
