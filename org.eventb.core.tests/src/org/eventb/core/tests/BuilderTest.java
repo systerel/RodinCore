63262793230be7d680f802b806dda173c2e75ef9
Laurent Voisin <lvoisin@users.sourceforge.net>
2013-02-15 11:43:57 +0100

    Make constant field final in BuilderTest
    Field "factory" in BuilderTest is never modified after class
    initialization.  It is therefore marked final by this commit.

--- org.eventb.core.tests/src/org/eventb/core/tests/BuilderTest.java
+++ org.eventb.core.tests/src/org/eventb/core/tests/BuilderTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 ETH Zurich and others.
+ * Copyright (c) 2006, 2013 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -68,7 +68,7 @@ public abstract class BuilderTest {
 
 	public static final String PLUGIN_ID = "org.eventb.core.tests";
 
-	protected static FormulaFactory factory = FormulaFactory.getDefault();
+	protected static final FormulaFactory factory = FormulaFactory.getDefault();
 
 	protected IRodinProject rodinProject;
 	protected IEventBProject eventBProject;
