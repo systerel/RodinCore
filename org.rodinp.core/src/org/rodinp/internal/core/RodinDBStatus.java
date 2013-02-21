03afd5c6e24defc09ed6d47b5c0896d4b7ee14e1
muller <thomas.muller@systerel.fr>
2013-02-21 16:14:14 +0100

    Add a DB status constant invalid child types
    Add a RodinDBStatus constant to indicate invalid child types in element
    hierachies to be used when checking item relations.
    No functional change

--- org.rodinp.core/src/org/rodinp/internal/core/RodinDBStatus.java
+++ org.rodinp.core/src/org/rodinp/internal/core/RodinDBStatus.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -358,6 +358,11 @@ public class RodinDBStatus extends Status implements IRodinDBStatus,
 			case ROOT_ELEMENT:
 				return Messages.bind(Messages.status_rootElement,
 						((RodinElement) elements[0]).toStringWithAncestors());
+
+			case INVALID_CHILD_TYPE:
+				final RodinElement parent = (RodinElement) elements[0];
+				return Messages.bind(Messages.status_invalidChildType, string,
+						parent.toStringWithAncestors());
 
 			case INDEXER_ERROR:
 				return Messages.bind(Messages.status_indexerError,
--- org.rodinp.core/src/org/rodinp/internal/core/util/Messages.java
+++ org.rodinp.core/src/org/rodinp/internal/core/util/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -96,6 +96,7 @@ public final class Messages {
 	public static String status_rootElement;
 	public static String status_updateConflict;
 	public static String status_upgradedFile;
+	public static String status_invalidChildType;
 	public static String status_indexerError;
 	
 	public static String type_database;
