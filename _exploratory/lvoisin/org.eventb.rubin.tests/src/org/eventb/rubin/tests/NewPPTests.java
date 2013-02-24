6cb89f95c01948ade0369a2b51e92f9f909b97ea
Laurent Voisin <lvoisin@users.sourceforge.net>
2013-02-24 17:40:11 +0100

    Port Rubin tests to JUnit 4
    Replace all occurrences of "junit.framework" by "org.junit".

--- _exploratory/lvoisin/org.eventb.rubin.tests/src/org/eventb/rubin/tests/NewPPTests.java
+++ _exploratory/lvoisin/org.eventb.rubin.tests/src/org/eventb/rubin/tests/NewPPTests.java
@@ -1,6 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2013 ETH Zurich and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ETH Zurich - initial API and implementation
+ *******************************************************************************/
 package org.eventb.rubin.tests;
 
-import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 
