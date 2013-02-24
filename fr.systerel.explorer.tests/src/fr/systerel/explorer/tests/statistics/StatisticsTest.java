1c955945e477c1f84d258fea7c877137c325e1fa
Laurent Voisin <lvoisin@users.sourceforge.net>
2013-02-24 17:46:55 +0100

    Port Event-B Explorer tests to JUnit 4
    Replace all occurrences of "junit.framework" by "org.junit".

--- fr.systerel.explorer.tests/src/fr/systerel/explorer/tests/statistics/StatisticsTest.java
+++ fr.systerel.explorer.tests/src/fr/systerel/explorer/tests/statistics/StatisticsTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2012 Systerel and others.
+ * Copyright (c) 2008, 2013 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,8 +10,8 @@
  *******************************************************************************/
 package fr.systerel.explorer.tests.statistics;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.eventb.core.IAxiom;
