679b41161f79be5fda6f10f27768666262e3bdd8
Laurent Voisin <lvoisin@users.sourceforge.net>
2013-02-26 23:15:12 +0100

    Port obsolete reasoner tests to common framework
    Port all tests inheriting from class AbstractTests to the common
    framework implemented in AbstractReasonerTests.
    
    Remove class AbstractTests which is no longer used.
    
    All tests have been ported verbatim.

--- org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtentionTests/DisjToImplTests.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtentionTests/DisjToImplTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2012 ETH Zurich and others.
+ * Copyright (c) 2007, 2013 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,230 +7,79 @@
  *
  * Contributors:
  *     ETH Zurich - initial API and implementation
+ *     Systerel - port to AbstractManualRewriterTests
  *******************************************************************************/
 package org.eventb.core.seqprover.eventbExtentionTests;
 
-import static org.eventb.core.ast.FormulaFactory.makePosition;
-import static org.eventb.core.ast.IPosition.ROOT;
-import static org.junit.Assert.assertTrue;
+import static org.eventb.core.seqprover.eventbExtensions.Tactics.disjToImplGetPositions;
 
 import java.util.List;
 
 import org.eventb.core.ast.IPosition;
 import org.eventb.core.ast.Predicate;
-import org.eventb.core.seqprover.IProofRule;
-import org.eventb.core.seqprover.IProverSequent;
-import org.eventb.core.seqprover.IReasoner;
-import org.eventb.core.seqprover.IReasonerFailure;
-import org.eventb.core.seqprover.IReasonerOutput;
-import org.eventb.core.seqprover.eventbExtensions.Tactics;
-import org.eventb.core.seqprover.tests.TestLib;
 import org.eventb.internal.core.seqprover.eventbExtensions.rewriters.DisjunctionToImplicationRewrites;
-import org.junit.Test;
 
 /**
  * Unit tests for the disjunction to implication reasoner
  * 
  * @author htson
  */
-public class DisjToImplTests extends AbstractTests {
+public class DisjToImplTests extends AbstractManualRewriterTests {
 
-	private static final IReasoner dtiReasoner = new DisjunctionToImplicationRewrites();
-
-	Predicate P1 = TestLib.genPred("x = 1 ∨ x = 2 ∨ x = 3");
-
-	Predicate P2 = TestLib.genPred("x = 1 ⇒ x = 2 ∨ x = 3 ∨ x = 4");
-
-	Predicate P3 = TestLib.genPred("∀x·x = 0 ⇒ x = 2 ∨ x = 3 ∨ x = 4");
-
-	Predicate P4 = TestLib.genPred("(x=1 ∨ x=2)∧x=3");
-
-	@Test
-	public void testGoalNotApplicable() {
-		IProverSequent seq;
-		IReasonerOutput output;
-
-		// Goal is not applicable
-		seq = TestLib.genSeq(" ⊤ |- ⊤ ");
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(null,
-				ROOT), null);
-		assertTrue(output instanceof IReasonerFailure);
+	@Override
+	public String getReasonerID() {
+		return DisjunctionToImplicationRewrites.REASONER_ID;
 	}
 
-	@Test
-	public void testPositionGoalIncorrect() {
-		IProverSequent seq;
-		IReasonerOutput output;
-
-		// Position in goal is incorrect
-		seq = TestLib.genSeq(" ⊤ |- " + P1);
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(null,
-				makePosition("0")), null);
-		assertTrue(output instanceof IReasonerFailure);
-
-		// Position in goal is incorrect
-		seq = TestLib.genSeq(" ⊤ |- " + P2);
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(null,
-				makePosition("0.1")), null);
-		assertTrue(output instanceof IReasonerFailure);
-
-		// Position in goal is incorrect
-		seq = TestLib.genSeq(" ⊤ |- " + P3);
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(null,
-				makePosition("0")), null);
-		assertTrue(output instanceof IReasonerFailure);
-
-		// Position in goal is incorrect
-		seq = TestLib.genSeq(" ⊤ |- " + P4);
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(null,
-				makePosition("1")), null);
-		assertTrue(output instanceof IReasonerFailure);
-
+	@Override
+	protected SuccessfulTest[] getSuccessfulTests() {
+		return new SuccessfulTest[] {
+				//
+				new SuccessfulTest(//
+						"x=1 ∨ x=2 ∨ x=3",//
+						"",//
+						"¬x=1 ⇒ (x=2 ∨ x=3)"),//
+				new SuccessfulTest(//
+						"x = 1 ⇒ x = 2 ∨ x = 3 ∨ x = 4",//
+						"1",//
+						"x=1 ⇒ (¬x=2 ⇒ x=3 ∨ x=4)"),//
+				new SuccessfulTest(//
+						"∀x· x=0 ⇒ x=2 ∨ x=3 ∨ x=4",//
+						"1.1",//
+						"∀x· x=0 ⇒ (¬x=2 ⇒ x=3 ∨ x=4)"),//
+				new SuccessfulTest(//
+						"(x=1 ∨ x=2) ∧ x=3",//
+						"0",//
+						"¬x=1 ⇒ x=2", "x=3"), };
 	}
 
-	/**
-	 * Tests for correct reasoner failure
-	 */
-	@Test
-	public void testHypNotPresent() {
-		IProverSequent seq;
-		IReasonerOutput output;
-
-		// Hyp is not present
-		seq = TestLib.genSeq(" ⊤ |- ⊤ ");
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(P1,
-				ROOT), null);
-		assertTrue(output instanceof IReasonerFailure);
+	@Override
+	protected String[] getUnsuccessfulTests() {
+		return new String[] {
+				// Not applicable
+				"⊤", "",//
+				// Incorrect position
+				"x=1 ∨ x=2 ∨ x=3", "0",//
+				"x=1 ⇒ x=2 ∨ x=3 ∨ x=4", "0.1",//
+				"∀x· x=0 ⇒ x=2 ∨ x=3 ∨ x=4", "0",//
+				"(x=1 ∨ x=2) ∧ x=3", "1",//
+		};
 	}
 
-	@Test
-	public void testHypPositionIncorrect() {
-		IProverSequent seq;
-		IReasonerOutput output;
-
-		// Position in hyp is incorrect
-		seq = TestLib.genSeq(P1 + " |- ⊤ ");
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(P1,
-				makePosition("0")), null);
-		assertTrue(output instanceof IReasonerFailure);
-
-		// Position in hyp is incorrect
-		seq = TestLib.genSeq(P2 + " |- ⊤ ");
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(P2,
-				makePosition("0.1")), null);
-		assertTrue(output instanceof IReasonerFailure);
-
-		// Position in hyp is incorrect
-		seq = TestLib.genSeq(P3 + " |- ⊤ ");
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(P3,
-				makePosition("0")), null);
-		assertTrue(output instanceof IReasonerFailure);
-
-		// Position in hyp is incorrect
-		seq = TestLib.genSeq(P4 + " |- ⊤ ");
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(P4,
-				makePosition("1")), null);
-		assertTrue(output instanceof IReasonerFailure);
-
+	@Override
+	protected String[] getTestGetPositions() {
+		return new String[] {
+				//
+				"x=1 ∨ x=2 ∨ x=3", "ROOT",//
+				"x=1 ⇒ x=2 ∨ x=3 ∨ x=4", "1",//
+				"∀x· x=0 ⇒ x=2 ∨ x=3 ∨ x=4", "1.1",//
+				"(x=1 ∨ x=2) ∧ x=3", "0",//
+		};
 	}
 
-	/**
-	 * Tests for applicable positions
-	 */
-	@Test
-	public void testGetPositions() {
-		List<IPosition> positions;
-		positions = Tactics.disjToImplGetPositions(P1);
-		assertPositions("Position found for P1 ", "", positions);
-		positions = Tactics.disjToImplGetPositions(P2);
-		assertPositions("Position found for P2 ", "1", positions);
-		positions = Tactics.disjToImplGetPositions(P3);
-		assertPositions("Position found for P3 ", "1.1", positions);
-		positions = Tactics.disjToImplGetPositions(P4);
-		assertPositions("Position found for P4 ", "0", positions);
+	@Override
+	protected List<IPosition> getPositions(Predicate predicate) {
+		return disjToImplGetPositions(predicate);
 	}
-
-	/**
-	 * Tests for reasoner success
-	 */
-	@Test
-	public void testSuccess() {
-
-		IProverSequent seq;
-		IProverSequent[] newSeqs;
-		IReasonerOutput output;
-
-		seq = TestLib.genSeq(" ⊤ |- " + P1);
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(null,
-				ROOT), null);
-		assertTrue(output instanceof IProofRule);
-		newSeqs = ((IProofRule) output).apply(seq);
-		assertSequents("Applied successfully goal P1 ",
-				"{x=ℤ}[][][⊤] |- ¬x=1⇒x=2∨x=3", newSeqs);
-
-		seq = TestLib.genSeq(P1 + " |- ⊤ ");
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(P1,
-				makePosition("")), null);
-		assertTrue(output instanceof IProofRule);
-		newSeqs = ((IProofRule) output).apply(seq);
-		assertSequents("Applied successfully hyp P1 ",
-				"{x=ℤ}[x=1∨x=2∨x=3][][¬x=1⇒x=2∨x=3] |- ⊤", newSeqs);
-
-		seq = TestLib.genSeq(" ⊤ |- " + P2);
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(null,
-				makePosition("1")), null);
-		assertTrue(output instanceof IProofRule);
-		newSeqs = ((IProofRule) output).apply(seq);
-		assertSequents("Applied successfully goal P2 ",
-				"{x=ℤ}[][][⊤] |- x=1⇒(¬x=2⇒x=3∨x=4)", newSeqs);
-
-		seq = TestLib.genSeq(P2 + " |- ⊤ ");
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(P2,
-				makePosition("1")), null);
-		assertTrue(output instanceof IProofRule);
-		newSeqs = ((IProofRule) output).apply(seq);
-		assertSequents("Applied successfully hyp P2 ",
-				"{x=ℤ}[x=1⇒x=2∨x=3∨x=4][][x=1⇒(¬x=2⇒x=3∨x=4)] |- ⊤",
-				newSeqs);
-
-		seq = TestLib.genSeq(" ⊤ |- " + P3);
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(null,
-				makePosition("1.1")), null);
-		assertTrue(output instanceof IProofRule);
-		newSeqs = ((IProofRule) output).apply(seq);
-		assertSequents("Applied successfully goal P3 ",
-				"{}[][][⊤] |- ∀x·x=0⇒(¬x=2⇒x=3∨x=4)", newSeqs);
-
-		seq = TestLib.genSeq(P3 + " |- ⊤ ");
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(P3,
-				makePosition("1.1")), null);
-		assertTrue(output instanceof IProofRule);
-		newSeqs = ((IProofRule) output).apply(seq);
-		assertSequents("Applied successfully hyp P3 ",
-				"{}[∀x·x=0⇒x=2∨x=3∨x=4][][∀x·x=0⇒(¬x=2⇒x=3∨x=4)] |- ⊤", newSeqs);
-
-		seq = TestLib.genSeq(" ⊤ |- " + P4);
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(null,
-				makePosition("0")), null);
-		assertTrue(output instanceof IProofRule);
-		newSeqs = ((IProofRule) output).apply(seq);
-		assertSequents("Applied successfully goal P4 ",
-				"{x=ℤ}[][][⊤] |- ¬x=1⇒x=2\n{x=ℤ}[][][⊤] |- x=3", newSeqs);
-
-		seq = TestLib.genSeq(P4 + " |- ⊤ ");
-		output = dtiReasoner.apply(seq, new DisjunctionToImplicationRewrites.Input(P4,
-				makePosition("0")), null);
-		assertTrue(output instanceof IProofRule);
-		newSeqs = ((IProofRule) output).apply(seq);
-		assertSequents("Applied successfully hyp P4 ",
-				"{x=ℤ}[(x=1∨x=2)∧x=3][][¬x=1⇒x=2, x=3] |- ⊤", newSeqs);
-
-	}
-
-	// Commented out, but makes the tests succeed
-//	@Override
-//	public ITactic getJustDischTactic() {
-//		return B4freeCore.externalPP(false);
-//	}
 
 }
