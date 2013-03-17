af3715f0cc5642b466605cca1c03da349f460eb1
Laurent Voisin <lvoisin@users.sourceforge.net>
2013-03-17 11:20:56 +0100

    Clean up AbstractPPTests from Rubin
    Remove now unused methods and other small cleanup.

--- _exploratory/lvoisin/org.eventb.rubin.tests/src/org/eventb/rubin/tests/AbstractPPTests.java
+++ _exploratory/lvoisin/org.eventb.rubin.tests/src/org/eventb/rubin/tests/AbstractPPTests.java
@@ -12,13 +12,10 @@
 package org.eventb.rubin.tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.IOException;
-
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Platform;
 import org.eventb.core.ast.FormulaFactory;
 import org.eventb.core.ast.ITypeCheckResult;
 import org.eventb.core.ast.ITypeEnvironment;
@@ -26,12 +23,10 @@ import org.eventb.core.ast.ITypeEnvironmentBuilder;
 import org.eventb.core.ast.Predicate;
 import org.eventb.core.seqprover.transformer.ISimpleSequent;
 import org.eventb.core.seqprover.transformer.SimpleSequents;
-import org.eventb.core.seqprover.xprover.BundledFileExtractor;
 import org.eventb.pp.PPProof;
 import org.eventb.pp.PPResult;
 import org.eventb.pp.PPResult.Result;
 import org.eventb.rubin.Sequent;
-import org.osgi.framework.Bundle;
 
 public class AbstractPPTests {
 
@@ -41,20 +36,9 @@ public class AbstractPPTests {
 
 	private static final boolean PERF = false;
 
-	/*
-	 * Returns a resolved local path for a file distributed as part of this
-	 * plugin or a fragment of it.
-	 */
-	protected static String getLocalPath(IPath relativePath) throws IOException {
-		final Bundle bundle = Platform.getBundle(PLUGIN_ID);
-		final IPath path = BundledFileExtractor.extractFile(bundle,
-				relativePath, false);
-		return path.toOSString();
-	}
-	
 	protected final ITypeEnvironment typeCheck(Sequent sequent) {
 		final ITypeEnvironmentBuilder typenv = ff.makeTypeEnvironment();
-		for (Predicate hyp: sequent.getHypotheses()) {
+		for (final Predicate hyp : sequent.getHypotheses()) {
 			typeCheck(hyp, typenv);
 		}
 		typeCheck(sequent.getGoal(), typenv);
@@ -70,47 +54,14 @@ public class AbstractPPTests {
 	 * @param typenv
 	 *            initial type environment. Will be extended with inferred types
 	 */
-	protected final void typeCheck(Predicate pred, ITypeEnvironmentBuilder typenv) {
-		ITypeCheckResult result = pred.typeCheck(typenv);
-		assertTrue("TypeChecker failed on predicate " + pred, result.isSuccess());
+	protected final void typeCheck(Predicate pred,
+			ITypeEnvironmentBuilder typenv) {
+		final ITypeCheckResult result = pred.typeCheck(typenv);
+		assertTrue("TypeChecker failed on predicate " + pred,
+				result.isSuccess());
 		typenv.addAll(result.getInferredEnvironment());
-		assertTrue("PredicateFormula should be type-checked", pred.isTypeChecked());
-	}
-
-	protected void testSequent(Sequent sequent) throws IOException {
-		final String name = sequent.getName();
-		final long start, end;
-		if (PERF) {
-			start = System.currentTimeMillis();
-			System.out.println("-------------------");
-			System.out.println("Proving: " + name);
-		}
-
-		typeCheck(sequent);
-		final ISimpleSequent ss = SimpleSequents.make(sequent.getHypotheses(),
-				sequent.getGoal(), ff);
-		final PPProof proof = new PPProof(ss, null);
-		proof.translate();
-		proof.load();
-		proof.prove(400);
-		final PPResult ppr = proof.getResult();
-
-		if (name.startsWith("VALIDPPFAILS")) {
-			// Test for an valid sequent that PP fails to discharge
-			assertEquals(name, Result.valid, ppr.getResult());
-		} else if (name.startsWith("VALID")) {
-			// Test for a valid sequent
-			assertEquals(name, Result.valid, ppr.getResult());
-		} else if (name.startsWith("INVALID")) {
-			// Test for an invalid sequent
-			assertTrue(name, !ppr.getResult().equals(Result.valid));
-		} else {
-			fail("Invalid name for sequent:\n" + sequent);
-		}
-		if (PERF) {
-			end = System.currentTimeMillis();
-			System.out.println("Time: " + (end - start) + " ms");
-		}
+		assertTrue("PredicateFormula should be type-checked",
+				pred.isTypeChecked());
 	}
 
 	protected static interface IProblemFilter {
@@ -149,10 +100,12 @@ public class AbstractPPTests {
 		switch (problem.status()) {
 		case VALID:
 		case VALIDPPFAILS:
-			assertEquals("" + sequent, Result.valid, ppr.getResult());
+			assertEquals("Should have succeeded" + sequent, Result.valid,
+					ppr.getResult());
 			break;
 		case INVALID:
-			assertTrue("" + sequent, !ppr.getResult().equals(Result.valid));
+			assertFalse("Should have failed" + sequent,
+					ppr.getResult().equals(Result.valid));
 			break;
 		default:
 			fail("Invalid status for problem " + name);
