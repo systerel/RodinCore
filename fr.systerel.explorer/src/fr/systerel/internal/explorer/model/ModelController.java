16703bf53544a0c63004bcbd8e969f40b2b9cfb4
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-04-23 17:55:23 +0200

    Enforce model refresh upon navigator global refresh.
    Applies when refresh element is workspace root, which is the case when
    refreshing with the new button or from the navigator context menu.
    
    Add a test showing that this fixes de-synchronization of the model.

--- fr.systerel.explorer/src/fr/systerel/internal/explorer/model/ModelController.java
+++ fr.systerel.explorer/src/fr/systerel/internal/explorer/model/ModelController.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2010 Systerel and others.
+ * Copyright (c) 2008, 2013 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License  v1.0
  * which accompanies this distribution, and is available at
@@ -13,6 +13,7 @@
 package fr.systerel.internal.explorer.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -370,6 +371,21 @@ public class ModelController implements IElementChangedListener {
 		notifyListeners(toRefresh);
 	}
 
+	
+	/**
+	 * Refreshes explorer model for all Rodin projects.
+	 */
+	public void refreshModel() {
+		try {
+			final IRodinProject[] rProjects = RodinCore.getRodinDB().getRodinProjects();
+			for (IRodinProject project : rProjects) {
+				refreshModel(project);
+			}
+			notifyListeners(Arrays.<IRodinElement>asList(rProjects));
+		} catch (RodinDBException e) {
+			ExplorerUtils.log(e, "while refreshing explorer model");
+		}
+	}
 	
 	/**
 	 * Refreshes the model
--- fr.systerel.explorer/src/fr/systerel/internal/explorer/navigator/EventBNavigator.java
+++ fr.systerel.explorer/src/fr/systerel/internal/explorer/navigator/EventBNavigator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Systerel and others.
+ * Copyright (c) 2010, 2013 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License  v1.0
  * which accompanies this distribution, and is available at
@@ -10,11 +10,29 @@
  *******************************************************************************/
 package fr.systerel.internal.explorer.navigator;
 
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.navigator.CommonNavigator;
 import org.eclipse.ui.navigator.CommonViewer;
 
+import fr.systerel.internal.explorer.model.ModelController;
+
 public class EventBNavigator extends CommonNavigator {
+
+	@Override
+	protected CommonViewer createCommonViewerObject(Composite aParent) {
+		return new CommonViewer(getViewSite().getId(), aParent, SWT.MULTI
+				| SWT.H_SCROLL | SWT.V_SCROLL) {
+			@Override
+			public void refresh(Object element, boolean updateLabels) {
+				super.refresh(element, updateLabels);
+				if (element instanceof IWorkspaceRoot) {
+					ModelController.getInstance().refreshModel();
+				}
+			}
+		};
+	}
 
 	@Override
 	protected CommonViewer createCommonViewer(Composite aParent) {
