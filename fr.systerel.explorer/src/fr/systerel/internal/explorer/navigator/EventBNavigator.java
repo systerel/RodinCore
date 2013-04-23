3577e6b595c09a2d54ee48c79aa7796223f8872a
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-04-23 17:55:23 +0200

    Enforce model refresh upon navigator global refresh.
    Applies when refresh element is workspace root, which is the case when
    refreshing with the new button or from navigator context menu.

--- fr.systerel.explorer/src/fr/systerel/internal/explorer/navigator/EventBNavigator.java
+++ fr.systerel.explorer/src/fr/systerel/internal/explorer/navigator/EventBNavigator.java
@@ -10,11 +10,40 @@
  *******************************************************************************/
 package fr.systerel.internal.explorer.navigator;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.navigator.CommonNavigator;
 import org.eclipse.ui.navigator.CommonViewer;
+import org.rodinp.core.IRodinProject;
+import org.rodinp.core.RodinCore;
+
+import fr.systerel.internal.explorer.model.ModelController;
 
 public class EventBNavigator extends CommonNavigator {
+
+	@Override
+	protected CommonViewer createCommonViewerObject(Composite aParent) {
+		return new CommonViewer(getViewSite().getId(), aParent, SWT.MULTI
+				| SWT.H_SCROLL | SWT.V_SCROLL) {
+			@Override
+			public void refresh(Object element, boolean updateLabels) {
+				if (element instanceof IWorkspaceRoot) {
+					final IProject[] projects = ((IWorkspaceRoot) element)
+							.getProjects();
+					for (IProject project : projects) {
+						final IRodinProject rProject = RodinCore
+								.valueOf(project);
+						ModelController.getInstance().refreshModel(rProject);
+					}
+				}
+
+				super.refresh(element, updateLabels);
+			}
+		};
+
+	}
 
 	@Override
 	protected CommonViewer createCommonViewer(Composite aParent) {
