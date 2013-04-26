71267730d722c4449a6f2a2bfa7a9f5919aee322
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-04-26 19:13:30 +0200

    Batch process of EMF notifications
    Avoid processing each EMF notification separately, but try to process
    several of them at once.
    
    Extract some code to separate methods.

--- fr.systerel.editor/src/fr/systerel/editor/internal/presentation/updaters/PresentationUpdater.java
+++ fr.systerel.editor/src/fr/systerel/editor/internal/presentation/updaters/PresentationUpdater.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Systerel and others.
+ * Copyright (c) 2011, 2013 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License  v1.0
  * which accompanies this distribution, and is available at
@@ -10,8 +10,14 @@
  *******************************************************************************/
 package fr.systerel.editor.internal.presentation.updaters;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.util.EContentAdapter;
+import org.eclipse.swt.widgets.Display;
 import org.rodinp.core.emf.api.itf.ILAttribute;
 import org.rodinp.core.emf.api.itf.ILElement;
 
@@ -19,8 +25,15 @@ import fr.systerel.editor.internal.documentModel.DocumentMapper;
 import fr.systerel.editor.internal.editors.RodinEditor;
 
 /**
- * This updater manage the notifications for the ILElements.
- * Implicit elements are treated separately by {@link ImplicitPresentationUpdater}.
+ * This updater manages the notifications for the ILElements. Implicit elements
+ * are treated separately by {@link ImplicitPresentationUpdater}.
+ * <p>
+ * For each notification received by {@link #notifyChanged(Notification)}, we
+ * push the notification to a queue and trigger an asynchronous processing in
+ * the UI thread. Consequently, several notifications can be processed at once,
+ * at the cost of having asynchronous processing that does nothing (because the
+ * notification has already been processed by a previous one).
+ * </p>
  * 
  * @author "Thomas Muller"
  */
@@ -29,55 +42,97 @@ public class PresentationUpdater extends EContentAdapter {
 	private final DocumentMapper mapper;
 	private final RodinEditor editor;
 
+	private final BlockingQueue<Notification> notifications = new LinkedBlockingQueue<Notification>();
+	
+	private final Runnable processNotifications = new Runnable() {
+		
+		@Override
+		public void run() {
+			Collection<Notification> currentNotifications = new ArrayList<Notification>();
+			notifications.drainTo(currentNotifications);
+			
+			if (currentNotifications.isEmpty()) {
+				return;
+			}
+
+			/*
+			 * Cases where the editor shall be completely resynchronized.
+			 */
+			for (Notification notification : currentNotifications) {
+				if (performFullResyncIfNeeded(notification)) {
+					return;
+				}
+			}
+			
+			/*
+			 * Other cases where the editor can be finely synchronized:
+			 * attribute update. THIS IS A SECURITY TO MAINTAIN CONSISTENCY,
+			 * IF OTHER THINGS THAN THE EDITOR MODIFY THE RODIN DB.
+			 */
+			for (Notification notification : currentNotifications) {
+				final Object oldObject = notification.getOldValue();
+				final Object notifier = notification.getNotifier();
+				final boolean wasILElement = !(oldObject instanceof ILElement);
+				if (notifier instanceof ILElement && wasILElement) {
+					mapper.elementChanged((ILElement) notifier);
+				}
+				if (notifier instanceof ILAttribute) {
+					mapper.elementChanged(((ILAttribute) notifier)
+							.getOwner());
+				}
+				if (oldObject instanceof ILElement) {
+					mapper.elementChanged((ILElement) oldObject);
+				}
+				if (oldObject instanceof ILAttribute) {
+					mapper.elementChanged(((ILAttribute) oldObject)
+							.getOwner());
+				}
+			}
+		}
+	};
+
 	public PresentationUpdater(RodinEditor editor, DocumentMapper mapper) {
 		this.editor = editor;
 		this.mapper = mapper;
 	}
-
-	@Override
-	public void notifyChanged(Notification notification) {
+	
+	private static boolean isAboutImplicitElement(Notification notification) {
 		if (notification.getNewValue() instanceof ILElement
 				&& ((ILElement) notification.getNewValue()).isImplicit()) {
-			return; // we don't care about implicit elements
+			return true;
 		}
 		if (notification.getNotifier() instanceof ILElement
 				&& ((ILElement) notification.getNotifier()).isImplicit()) {
-			return; // we don't care about implicit elements
+			return true;
 		}
-		/*
-		 * Cases where the editor shall be completely resynchronized.
-		 */
+		return false;
+	}
+	
+	private boolean performFullResyncIfNeeded(Notification notification) {
 		if ((notification.getEventType() == Notification.REMOVE && notification
 				.getOldValue() instanceof ILElement)
 				|| (notification.getEventType() == Notification.MOVE)) {
 			editor.resync(null, false);
-			return;
+			return true;
 		}
 		if (notification.getEventType() == Notification.ADD
 				&& notification.getNewValue() instanceof ILElement) {
 			editor.resync(null, false, (ILElement) notification.getNewValue());
-			return;
+			return true;
 		}
-		/*
-		 * Other cases where the editor can be finely synchronized: attribute
-		 * update. THIS IS A SECURITY TO MAINTAIN CONSISTENCY, IF OTHER THINGS
-		 * THAT THE EDITOR MODIFY THE RODIN DB.
-		 */
-		final Object oldObject = notification.getOldValue();
-		final Object notifier = notification.getNotifier();
-		final boolean isILElement = !(oldObject instanceof ILElement);
-		if (notifier instanceof ILElement && isILElement) {
-			mapper.elementChanged((ILElement) notifier);
+		return false;
+	}
+
+	@Override
+	public void notifyChanged(Notification notification) {
+		
+		if (isAboutImplicitElement(notification)) {
+			return; // we don't care about implicit elements
 		}
-		if (notifier instanceof ILAttribute) {
-			mapper.elementChanged(((ILAttribute) notifier).getOwner());
-		}
-		if (oldObject instanceof ILElement) {
-			mapper.elementChanged((ILElement) oldObject);
-		}
-		if (oldObject instanceof ILAttribute) {
-			mapper.elementChanged(((ILAttribute) oldObject).getOwner());
-		}
+
+		notifications.add(notification);
+		
+		Display.getDefault().asyncExec(processNotifications);
 	}
 
 }
