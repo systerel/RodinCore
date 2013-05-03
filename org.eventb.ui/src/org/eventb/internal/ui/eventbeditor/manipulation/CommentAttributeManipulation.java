cb9ff96aaa118ef41e0dd561903c1c04404d3e50
muller <thomas.muller@systerel.fr>
2013-05-03 18:20:19 +0200

    Fix bug #658 - Do not add empty comment attributes
    Do not add empty comment attributes to elements when editing. In case of empty
    comments the attribute comment is deleted.

--- org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/manipulation/CommentAttributeManipulation.java
+++ org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/manipulation/CommentAttributeManipulation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 ETH Zurich and others.
+ * Copyright (c) 2007, 2013 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,7 @@
  *     ETH Zurich - initial API and implementation
  *     Systerel - added history support
  *     Systerel - made IAttributeFactory generic
+ *     Systerel - remove empty comment attributes within setValue()
  *******************************************************************************/
 package org.eventb.internal.ui.eventbeditor.manipulation;
 
@@ -39,6 +40,10 @@ public class CommentAttributeManipulation extends AbstractAttributeManipulation 
 	@Override
 	public void setValue(IRodinElement element, String newValue,
 			IProgressMonitor monitor) throws RodinDBException {
+		if (newValue.isEmpty()) {
+			asCommented(element).removeAttribute(COMMENT_ATTRIBUTE, monitor);
+			return;
+		}
 		asCommented(element).setComment(newValue, monitor);
 	}
 
