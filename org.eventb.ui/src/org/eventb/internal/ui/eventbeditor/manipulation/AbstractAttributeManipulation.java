72e342cf22fa8362d315f9f84fa30e15a7600dc4
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-06-18 17:35:04 +0200

    Remove logging on GetPossibleValues, deprecate logging method.

--- org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/manipulation/AbstractAttributeManipulation.java
+++ org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/manipulation/AbstractAttributeManipulation.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2013 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
 package org.eventb.internal.ui.eventbeditor.manipulation;
 
 import org.eventb.internal.ui.UIUtils;
@@ -6,6 +16,13 @@ import org.rodinp.core.IAttributeType;
 public abstract class AbstractAttributeManipulation implements
 		IAttributeManipulation {
 
+	/**
+	 * @deprecated Logging each time GetPossibleValues is called produces
+	 *             unnecessary log entries. GetPossibleValues is allowed to
+	 *             return <code>null</code>, this is part of the API and needs
+	 *             not be logged whatsoever.
+	 */
+	@Deprecated
 	protected void logCantGetPossibleValues(IAttributeType attribute) {
 		UIUtils.log(null,
 				"The method GetPossibleValues cannot be called for attribute "
