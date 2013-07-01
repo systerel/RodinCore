8bb38551931b725ba7e16da67322e6b35e6ae728
muller <thomas.muller@systerel.fr>
2013-07-01 16:26:45 +0200

    Replacing "NewVariable" editor action by a command
    Use a command, menu contribution and handler.
    Add a description and a mnemonic.
    Remove unused NewVariable action delegate class.

--- org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/create/handlers/NewVariableHandler.java
+++ org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/create/handlers/NewVariableHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2011 ETH Zurich and others.
+ * Copyright (c) 2006, 2013 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,17 +10,16 @@
  *     Systerel - separation of file and root element
  *     Systerel - introduced read only elements through new super class
  *     Systerel - refactored to use NewVariablesWizard
+ *     Systerel - refactored using AbstractCreationWizardHandler
  *******************************************************************************/
-package org.eventb.internal.ui.eventbeditor.actions;
+package org.eventb.internal.ui.eventbeditor.create.handlers;
 
-import org.eclipse.jface.action.IAction;
-import org.eventb.core.IMachineRoot;
 import org.eventb.internal.ui.eventbeditor.wizards.EventBCreationWizards;
 
-public class NewVariable extends AbstractNewActionDelegate<IMachineRoot> {
+public class NewVariableHandler extends AbstractCreationWizardHandler {
 
 	@Override
-	public void runAction(IAction action) {
+	protected void openCreationWizard() {
 		new EventBCreationWizards.NewVariablesWizard().openDialog(editor);
 	}
 
