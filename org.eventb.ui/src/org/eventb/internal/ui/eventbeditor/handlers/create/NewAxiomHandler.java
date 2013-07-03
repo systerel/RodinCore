800ae601ff0d3030bd50fe93b963921e34c89985
muller <thomas.muller@systerel.fr>
2013-07-03 18:05:17 +0200

    Replacing "NewAxioms" editor action by a command
    Use a command, menu contribution and handler.
    Add a description and a mnemonic.
    Remove unused NewAxioms action delegate class.
    Remove messages for "New Theorem" action which is no longer used.

--- org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/handlers/create/NewAxiomHandler.java
+++ org.eventb.ui/src/org/eventb/internal/ui/eventbeditor/handlers/create/NewAxiomHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2011 ETH Zurich and others.
+ * Copyright (c) 2006, 2013 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,17 +10,17 @@
  *     Systerel - separation of file and root element
  *     Systerel - introduced read only elements through new super class
  *     Systerel - now using wizard class NewAxiomsWizard
+ *     Systerel - refactored using AbstractCreationWizardHandler
  *******************************************************************************/
-package org.eventb.internal.ui.eventbeditor.actions;
+package org.eventb.internal.ui.eventbeditor.handlers.create;
 
-import org.eclipse.jface.action.IAction;
-import org.eventb.core.IContextRoot;
 import org.eventb.internal.ui.eventbeditor.wizards.EventBCreationWizards;
 
-public class NewAxioms extends AbstractNewActionDelegate<IContextRoot> {
+
+public class NewAxiomHandler extends AbstractCreationWizardHandler {
 
 	@Override
-	public void runAction(IAction action) {
+	protected void openCreationWizard() {
 		new EventBCreationWizards.NewAxiomsWizard().openDialog(editor);
 	}
 
