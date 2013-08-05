6b19c4a3f473e7cbc0f6af71c77ec91d9346aaab
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-08-05 15:44:23 +0200

    Fix inverted exception messages about mutable/immutable states.
    Also improve message about state that should be immutable.

--- org.eventb.core/src/org/eventb/internal/core/tool/state/State.java
+++ org.eventb.core/src/org/eventb/internal/core/tool/state/State.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 ETH Zurich and others.
+ * Copyright (c) 2006, 2013 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,14 +30,14 @@ public abstract class State implements IState {
 	protected final void assertImmutable() throws CoreException {
 		if ( ! immutable)
 			throw Util.newCoreException(
-					Messages.bind(Messages.tool_ImmutableStateModificationFailure, 
+					Messages.bind(Messages.tool_MutableStateNotUnmodifiableFailure,
 							getStateType()));
 	}
 
 	protected final void assertMutable() throws CoreException {
 		if (immutable)
 			throw Util.newCoreException(
-					Messages.bind(Messages.tool_MutableStateNotUnmodifiableFailure, 
+					Messages.bind(Messages.tool_ImmutableStateModificationFailure,
 							getStateType()));
 	}
 
