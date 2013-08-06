69e0ae43a1bd5d47bbf1d98c96599b8e41a471e4
Laurent Voisin <lvoisin@users.sourceforge.net>
2013-08-06 15:40:59 +0200

    Fix unresolved merge conflicts
    Resolve the merge conflicts that were committed unresolved in some
    copyright headers.  There is no other unresolved merge conflict in the
    whole tree at the moment of this commit.

--- fr.systerel.editor/src/fr/systerel/editor/internal/documentModel/RodinTextStream.java
+++ fr.systerel.editor/src/fr/systerel/editor/internal/documentModel/RodinTextStream.java
@@ -1,13 +1,7 @@
 /*******************************************************************************
-<<<<<<< HEAD
- * Copyright (c) 2011, 2012 Systerel and others.
+ * Copyright (c) 2011, 2013 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
-=======
- * Copyright (c) 2011, 2013 Systerel and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
->>>>>>> b
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
