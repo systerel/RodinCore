b9b97721e93bdd811847f94975e38e1e123b79c6
Laurent Voisin <lvoisin@users.sourceforge.net>
2013-09-04 18:59:26 +0200

    Document the type-checking API

--- org.eventb.core.ast/src/org/eventb/core/ast/extension/ITypeCheckMediator.java
+++ org.eventb.core.ast/src/org/eventb/core/ast/extension/ITypeCheckMediator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2012 Systerel and others.
+ * Copyright (c) 2010, 2013 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,13 +13,31 @@ package org.eventb.core.ast.extension;
 import org.eventb.core.ast.Type;
 
 /**
+ * Common protocol for type-checking formula extensions.
+ * 
  * @author Nicolas Beauger
  * @since 2.0
  * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface ITypeCheckMediator extends ITypeMediator {
 
+	/**
+	 * Returns a fresh type variable that can be used as a place-holder when
+	 * checking types.
+	 * 
+	 * @return a fresh type variable
+	 */
 	Type newTypeVariable();
-	
+
+	/**
+	 * Declares a type-checking constraint stating that two types must be
+	 * unifiable each with the other.
+	 * 
+	 * @param left
+	 *            some type
+	 * @param right
+	 *            some type
+	 */
 	void sameType(Type left, Type right);
+
 }
--- org.eventb.core.ast/src/org/eventb/core/ast/extension/ITypeMediator.java
+++ org.eventb.core.ast/src/org/eventb/core/ast/extension/ITypeMediator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2012 Systerel and others.
+ * Copyright (c) 2010, 2013 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,8 +22,9 @@ import org.eventb.core.ast.ProductType;
 import org.eventb.core.ast.Type;
 
 /**
- */
-/**
+ * Common protocol for synthesizing the type of or type-checking formula
+ * extensions.
+ * 
  * @author Nicolas Beauger
  * @since 2.0
  * @noimplement This interface is not intended to be implemented by clients.
@@ -31,20 +32,75 @@ import org.eventb.core.ast.Type;
  */
 public interface ITypeMediator {
 
+	/**
+	 * Returns the factory to use for type-checking.
+	 * 
+	 * @return the factory to use for type-checking
+	 */
+	FormulaFactory getFactory();
+
+	/**
+	 * This is a short-hand method fully equivalent to
+	 * 
+	 * <pre>
+	 * getFactory().makeBooleanType()
+	 * </pre>
+	 */
 	BooleanType makeBooleanType();
 
+	/**
+	 * This is a short-hand method fully equivalent to
+	 * 
+	 * <pre>
+	 * getFactory().makeParametricType(typePrms, typeConstr)
+	 * </pre>
+	 */
 	ParametricType makeParametricType(List<Type> typePrms,
 			IExpressionExtension typeConstr);
 
+	/**
+	 * This is a short-hand method fully equivalent to
+	 * 
+	 * <pre>
+	 * getFactory().makeGivenType(name)
+	 * </pre>
+	 */
 	GivenType makeGivenType(String name);
 
+	/**
+	 * This is a short-hand method fully equivalent to
+	 * 
+	 * <pre>
+	 * getFactory().makeIntegerType()
+	 * </pre>
+	 */
 	IntegerType makeIntegerType();
 
+	/**
+	 * This is a short-hand method fully equivalent to
+	 * 
+	 * <pre>
+	 * getFactory().makePowerSetType(base)
+	 * </pre>
+	 */
 	PowerSetType makePowerSetType(Type base);
 
+	/**
+	 * This is a short-hand method fully equivalent to
+	 * 
+	 * <pre>
+	 * getFactory().makeProductType(left, right)
+	 * </pre>
+	 */
 	ProductType makeProductType(Type left, Type right);
 
+	/**
+	 * This is a short-hand method fully equivalent to
+	 * 
+	 * <pre>
+	 * getFactory().makeRelationalType(left, right)
+	 * </pre>
+	 */
 	PowerSetType makeRelationalType(Type left, Type right);
 
-	FormulaFactory getFactory();
 }
