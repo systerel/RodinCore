c5a97fa471badbbc7debbe21c7a8fe9d862582c2
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-09-10 14:52:21 +0200

    Fix hashcode() computation based on array field: use Arrays.hashcode().

--- org.eventb.ui/src/org/eventb/internal/ui/wizards/WorkingSetSelection.java
+++ org.eventb.ui/src/org/eventb/internal/ui/wizards/WorkingSetSelection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2012 Systerel and others.
+ * Copyright (c) 2008, 2013 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -69,7 +69,7 @@ public class WorkingSetSelection {
 	
 	@Override
 	public int hashCode() {
-		return workingSets.hashCode();
+		return Arrays.hashCode(workingSets);
 	}
 	
 }
