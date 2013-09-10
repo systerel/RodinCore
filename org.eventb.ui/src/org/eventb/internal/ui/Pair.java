b7259d8192c83d4130a0973545f20dc36b430e14
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-09-10 12:39:39 +0200

    Add hashcode() to Pair, that was only implementing equals().
    Use standard generated implementation for both equals() and hashcode().

--- org.eventb.ui/src/org/eventb/internal/ui/Pair.java
+++ org.eventb.ui/src/org/eventb/internal/ui/Pair.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2012 ETH Zurich and others.
+ * Copyright (c) 2007, 2013 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,15 +30,41 @@ public class Pair<S extends Object, T extends Object> {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((obj1 == null) ? 0 : obj1.hashCode());
+		result = prime * result + ((obj2 == null) ? 0 : obj2.hashCode());
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
-		if (obj == null)
-			return this == null;
-		if (obj instanceof Pair<?, ?>) {
-			Pair<?, ?> pair = (Pair<?, ?>) obj;
-			return pair.getFirst().equals(obj1)
-					&& pair.getSecond().equals(obj2);
+		if (this == obj) {
+			return true;
 		}
-		return super.equals(obj);
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Pair<?, ?> other = (Pair<?, ?>) obj;
+		if (obj1 == null) {
+			if (other.obj1 != null) {
+				return false;
+			}
+		} else if (!obj1.equals(other.obj1)) {
+			return false;
+		}
+		if (obj2 == null) {
+			if (other.obj2 != null) {
+				return false;
+			}
+		} else if (!obj2.equals(other.obj2)) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
