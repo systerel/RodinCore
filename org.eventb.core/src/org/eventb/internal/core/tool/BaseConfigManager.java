40b9f47b976927b9fdd71e5f0e7e30f7ecb3197f
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-09-10 12:15:05 +0200

    Fix call to toString() on an array, transform array as list.

--- org.eventb.core/src/org/eventb/internal/core/tool/BaseConfigManager.java
+++ org.eventb.core/src/org/eventb/internal/core/tool/BaseConfigManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 ETH Zurich and others.
+ * Copyright (c) 2006, 2013 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,8 @@
  *     ETH Zurich - initial API and implementation
  *******************************************************************************/
 package org.eventb.internal.core.tool;
+
+import static java.util.Arrays.asList;
 
 import java.util.List;
 
@@ -56,7 +58,7 @@ public class BaseConfigManager extends ConfigManager<String, BaseConfig> {
 	protected void printConfig(BaseConfig config) {
 		System.out.println("  " + config.getId());
 		System.out.println("    name: " + config.getName());
-		System.out.println("    included: " + config.getIncluded());		
+		System.out.println("    included: " + asList(config.getIncluded()));		
 	}
 
 }
