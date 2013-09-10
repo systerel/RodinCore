0c5ab5a584bbdf54c0d52c69c167b9ddabb2e995
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-09-10 15:30:42 +0200

    Explicitly throw NPE instead of writing code that causes it.

--- org.rodinp.core.emf.tests/src/org/rodinp/core/emf/tests/basis/TestBuggyImplicitChildProvider.java
+++ org.rodinp.core.emf.tests/src/org/rodinp/core/emf/tests/basis/TestBuggyImplicitChildProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2012 Systerel and others.
+ * Copyright (c) 2011, 2013 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.rodinp.core.emf.tests.basis;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.rodinp.core.IInternalElement;
@@ -22,14 +21,11 @@ import org.rodinp.core.emf.api.itf.ICoreImplicitChildProvider;
  */
 public class TestBuggyImplicitChildProvider implements ICoreImplicitChildProvider {
 
-	@SuppressWarnings("null")
 	@Override
 	public List<? extends IInternalElement> getImplicitChildren(
 			IInternalElement parent) {
 		// Oh noooo !
-		Object o = null;
-		o.hashCode();
-		return Collections.emptyList();
+		throw new NullPointerException();
 	}
 	
 }
