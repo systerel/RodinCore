666080c8d37a5676d0820c298605ebf9a4197bf4
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-09-10 15:29:13 +0200

    Fix resource removal: call map remove() with key instead of value.

--- org.rodinp.core.emf/src/org/rodinp/core/emf/lightcore/RodinResourceFactory.java
+++ org.rodinp.core.emf/src/org/rodinp/core/emf/lightcore/RodinResourceFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2012 University of Southampton and others.
+ * Copyright (c) 2011, 2013 University of Southampton and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -54,7 +54,7 @@ public class RodinResourceFactory implements Resource.Factory, ILFileFactory {
 			collection.remove(resource);
 
 			if (collection.isEmpty()) {
-				handledResources.remove(collection);
+				handledResources.remove(key);
 			}
 		}
 	}
