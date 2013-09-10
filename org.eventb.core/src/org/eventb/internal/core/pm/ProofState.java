0b655a4830c59767717ff7999cf92374e22a657d
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-09-10 11:54:52 +0200

    Add hashcode() to ProofState that was only implementing equals().
    Use standard generated code for hashcode() and equals().

--- org.eventb.core/src/org/eventb/internal/core/pm/ProofState.java
+++ org.eventb.core/src/org/eventb/internal/core/pm/ProofState.java
@@ -499,20 +499,34 @@ public class ProofState implements IProofState {
 			this.dirty = dirty;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eventb.core.pm.IProofState#equals(java.lang.Object)
-	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
-		if (!(obj instanceof ProofState))
-			return false;
-		else {
-			IProofState proofState = (IProofState) obj;
-			return proofState.getPSStatus().equals(status);
+		if (this == obj) {
+			return true;
 		}
-
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		ProofState other = (ProofState) obj;
+		if (status == null) {
+			if (other.status != null) {
+				return false;
+			}
+		} else if (!status.equals(other.status)) {
+			return false;
+		}
+		return true;
 	}
 
 	/*
