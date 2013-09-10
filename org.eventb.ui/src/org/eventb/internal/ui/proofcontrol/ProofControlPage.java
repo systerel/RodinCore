825c245fd608b376de7c573071ab59a83ac17bdf
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-09-10 14:39:25 +0200

    Fix reference comparisons with empty string. Use isEmpty() instead.

--- org.eventb.ui/src/org/eventb/internal/ui/proofcontrol/ProofControlPage.java
+++ org.eventb.ui/src/org/eventb/internal/ui/proofcontrol/ProofControlPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2012 ETH Zurich and others.
+ * Copyright (c) 2005, 2013 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -250,7 +250,7 @@ public class ProofControlPage extends Page implements IProofControlPage,
 						if (!currentInput.equals("")) {
 							historyCombo.add(currentInput, 0);
 						}
-						if (textWidget.getText() != "") {
+						if (!textWidget.getText().isEmpty()) {
 							textWidget.setText("");
 						}
 						currentInput = "";
@@ -316,7 +316,7 @@ public class ProofControlPage extends Page implements IProofControlPage,
 					if (!currentInput.equals("")) {
 						historyCombo.add(currentInput, 0);
 					}
-					if (textWidget.getText() != "") {
+					if (!textWidget.getText().isEmpty()) {
 						textWidget.setText("");
 					}
 				}
