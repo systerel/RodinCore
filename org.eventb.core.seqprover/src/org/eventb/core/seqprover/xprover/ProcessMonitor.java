ef38f2e99889c73e454f7be60edc118bc456f589
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-09-10 12:24:49 +0200

    Add @noextend to ProcessMonitor to prevent clients from subclassing.
    As the constructor starts threads, calling it from subclasses would
    result in code being executed while the object is not fully initialized.

--- org.eventb.core.seqprover/src/org/eventb/core/seqprover/xprover/ProcessMonitor.java
+++ org.eventb.core.seqprover/src/org/eventb/core/seqprover/xprover/ProcessMonitor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2012 ETH Zurich and others.
+ * Copyright (c) 2007, 2013 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -29,6 +29,7 @@ import org.osgi.framework.Bundle;
  * 
  * @author Laurent Voisin
  * @since 1.0
+ * @noextend This class is not intended to be subclassed by clients.
  */
 public class ProcessMonitor {
 
