2872e0b42b51d7f5c0ef213a1b5231bd4ad01729
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-09-10 14:51:26 +0200

    Add check for hypotheses parameter nullity when printing sequents.

--- org.eventb.ui/src/org/eventb/internal/ui/prooftreeui/RuleDetailsProvider.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prooftreeui/RuleDetailsProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2012 Systerel and others.
+ * Copyright (c) 2010, 2013 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -266,7 +266,9 @@ public class RuleDetailsProvider {
 
 	private boolean printSequent(int level, List<String> hypotheses,
 			List<String> goal) {
-		printHypotheses(level, hypotheses);
+		if (hypotheses != null) {
+			printHypotheses(level, hypotheses);
+		}
 		if (goal != null) {
 			appendTurnstile(level + 1);
 			printGoalNotNull(level, goal);
