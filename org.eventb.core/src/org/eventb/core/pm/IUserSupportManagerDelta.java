2ca448d2a85734d6d1789f525b8ccc4b35859cc0
muller <thomas.muller@systerel.fr>
2013-09-17 19:00:51 +0200

    Fix javadoc using @noimplement annotation. No code change

--- org.eventb.core/src/org/eventb/core/pm/IUserSupportManagerDelta.java
+++ org.eventb.core/src/org/eventb/core/pm/IUserSupportManagerDelta.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 ETH Zurich and others.
+ * Copyright (c) 2006, 2013 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,10 +20,8 @@ package org.eventb.core.pm;
  *         <code>IUserSupportManagerDelta</code> object are not valid outside
  *         the dynamic scope of the notification.
  *         </p>
- *         <p>
- *         This interface is not intended to be implemented by clients.
- *         </p>
  * @since 1.0
+ * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface IUserSupportManagerDelta {
 
