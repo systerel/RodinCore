ef8fcc7b847d06c638e8d0391e9e97e582918699
Thomas Muller <thomas.muller@systerel.fr>
2013-12-11 19:23:51 +0100

    Add @noextend & @noinstantiate on PPCore (Activator)
    No functional change.

--- org.eventb.pp/src/org/eventb/pp/PPCore.java
+++ org.eventb.pp/src/org/eventb/pp/PPCore.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 ETH Zurich and others.
+ * Copyright (c) 2006, 2013s ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,6 +32,8 @@ import org.osgi.framework.BundleContext;
 
 /**
  * @since 0.5
+ * @noextend This class is not intended to be subclassed by clients.
+ * @noinstantiate This class is not intended to be instantiated by clients.
  */
 public class PPCore extends Plugin {
 
