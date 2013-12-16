9f6e8113f484f5fc1a66cce16e1df7ed7fd7b190
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2013-12-16 11:23:39 +0100

    Rewrite collection.size()==0 into isEmpty().

--- org.eventb.ui/src/org/eventb/internal/ui/utils/ListMultimap.java
+++ org.eventb.ui/src/org/eventb/internal/ui/utils/ListMultimap.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Systerel and others.
+ * Copyright (c) 2012, 2013 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -89,7 +89,7 @@ public class ListMultimap<K, V> {
 	 *            some values
 	 */
 	public void putAll(K key, Collection<V> values) {
-		if (values.size() == 0) {
+		if (values.isEmpty()) {
 			return;
 		}
 		final List<V> list = getValueList(key);
