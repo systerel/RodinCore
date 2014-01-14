beaca397c7b774273a1aba6702571f9d09003040
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2014-01-14 12:54:45 +0100

    Implement factory translation in rewrite hyp action

--- org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/RewriteHypAction.java
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/RewriteHypAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 Systerel and others.
+ * Copyright (c) 2013, 2014 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,8 +13,10 @@ package org.eventb.internal.core.seqprover;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.eventb.core.ast.FormulaFactory;
 import org.eventb.core.ast.FreeIdentifier;
 import org.eventb.core.ast.Predicate;
+import org.eventb.core.seqprover.IHypAction;
 import org.eventb.core.seqprover.IHypAction.IRewriteHypAction;
 
 /**
@@ -53,4 +55,18 @@ public class RewriteHypAction extends ForwardInfHypAction implements
 		return sequent;
 	}
 
+	@Override
+	public IHypAction translate(FormulaFactory factory) {
+		final IForwardInfHypAction trSuper = (IForwardInfHypAction) super
+				.translate(factory);
+		final Collection<Predicate> trDisapHyps = new ArrayList<Predicate>(
+				disappearingHyps.size());
+		for (Predicate hyp : disappearingHyps) {
+			trDisapHyps.add(hyp.translate(factory));
+		}
+
+		return new RewriteHypAction(trSuper.getHyps(),
+				trSuper.getAddedFreeIdents(), trSuper.getInferredHyps(),
+				trDisapHyps);
+	}
 }
