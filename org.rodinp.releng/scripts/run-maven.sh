7bb0c40664cdc577f71eb6f78d4a405a72abfd93
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2014-01-15 10:48:27 +0100

    Release engineering: discard version patch after build

--- org.rodinp.releng/scripts/run-maven.sh
+++ org.rodinp.releng/scripts/run-maven.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ###############################################################################
-# Copyright (c) 2013 Systerel and others.
+# Copyright (c) 2013, 2014 Systerel and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
@@ -18,4 +18,5 @@ cd $MAIN_DIR
 GIT_COMMIT=`git log -1 --format='%h'`
 echo "Building Rodin with Maven for git commit: $GIT_COMMIT"
 
-mvn clean install -Dgit-commit=$GIT_COMMIT $@ # -DskipTests -fae
+mvn clean install -Dgit-commit=$GIT_COMMIT $@ # -DskipTests -fae|-fn
+git checkout -- $MAIN_DIR/org.rodinp.platform/plugin.properties
