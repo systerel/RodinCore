c4c4bfc7b76d1248e910eedffffe693084c0c4d9
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2014-01-22 14:38:33 +0100

    Fix potential stack overflow in combined descriptor
    Combined descriptors were able to make self references to the list of
    combined tactics inside the combined tactics.
    Break this by copying the list in combined tactic descriptor.

--- org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/TacticDescriptors.java
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/TacticDescriptors.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2012 Systerel and others.
+ * Copyright (c) 2011, 2014 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -326,7 +326,8 @@ public class TacticDescriptors {
 			super(id, name, description);
 			this.combinator = combinator;
 			this.combinatorId = combinatorId;
-			this.combinedDescs = combinedDescs;
+			// copy list to avoid self references in combined descriptors
+			this.combinedDescs = new ArrayList<ITacticDescriptor>(combinedDescs);
 			this.combined = new ArrayList<ITactic>(combinedDescs.size());
 		}
 
