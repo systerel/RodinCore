f80f1d45277d1c69c4888aa84b9233d2ffea00d2
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2014-01-30 15:54:31 +0100

    Rework Maven build script
    Add suffix if Git repository is dirty
    
    Change default target to "verify", as installation is not required to
    produce the build output.
    
    Make it compatible with Mac OS X.

--- org.rodinp.releng/scripts/run-maven.sh
+++ org.rodinp.releng/scripts/run-maven.sh
@@ -1,29 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 ###############################################################################
 # Copyright (c) 2013, 2014 Systerel and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
-# 
+#
 # Contributors:
 #     Systerel - initial API and implementation
 ###############################################################################
-SCRIPT=`readlink -m -n $0`
-SCRIPTS_DIR=`dirname $SCRIPT`
-MAIN_DIR=`readlink -m -n $SCRIPTS_DIR/../..`
+#
+#  This script must be run from a working copy of the Rodin platform sources.
+#
 
-cd $MAIN_DIR
+if ! git rev-parse --is-inside-work-tree > /dev/null; then
+    echo "This script must be run from a Git working copy." >&2
+    exit 1
+fi
 
-GIT_COMMIT=`git log -1 --format='%h'`
+MAIN_DIR=$(git rev-parse --show-toplevel)
 
-case $1 in 
-    RC* ) RC=$1_
-	  shift ;;
+git_is_dirty() {
+    test -n "$(git status --porcelain)"
+}
+
+case "$1" in
+    RC*) RC="$1"_; shift ;;
 esac
 
-SUFFIX=$RC$GIT_COMMIT
+GIT_COMMIT=$(git log -1 --format='%h')
+
+if git_is_dirty; then DIRTY="-dirty"; fi
+
+SUFFIX="$RC$GIT_COMMIT$DIRTY"
 echo "Building Rodin $SUFFIX"
 
-mvn clean install -Dversion-suffix=$SUFFIX $@ # -DskipTests -fae|-fn
-git checkout -- $MAIN_DIR/org.rodinp.platform/plugin.properties
+mvn clean verify -Dversion-suffix="$SUFFIX" "$@" # -DskipTests -fae|-fn
+git checkout -- "$MAIN_DIR"/org.rodinp.platform/plugin.properties
