7103a1e705fc59e8115adeb3b84e00bbbe362dc4
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2014-02-12 17:12:39 +0100

    Remove unused import in test class

--- org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/autoTacticExtentionTests/AutoTacticRegistryTest.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/autoTacticExtentionTests/AutoTacticRegistryTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2013 ETH Zurich and others.
+ * Copyright (c) 2007, 2014 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,7 +19,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
