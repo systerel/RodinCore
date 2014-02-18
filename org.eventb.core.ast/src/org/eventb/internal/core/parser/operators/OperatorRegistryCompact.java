3ed2ac42e94a40cb8fe4ec934f3e629c42a80357
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2014-02-18 19:53:18 +0100

    Make operators incompatible when group priority is undefined
    Used to default to left associative, which was incorrect.

--- org.eventb.core.ast/src/org/eventb/internal/core/parser/operators/OperatorRegistryCompact.java
+++ org.eventb.core.ast/src/org/eventb/internal/core/parser/operators/OperatorRegistryCompact.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Systerel and others.
+ * Copyright (c) 2011, 2014 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -113,7 +113,7 @@ public class OperatorRegistryCompact {
 				return INCOMPATIBLE;
 			}
 		} else {
-			return LEFT_PRIORITY;
+			return INCOMPATIBLE;
 		}
 
 	}
