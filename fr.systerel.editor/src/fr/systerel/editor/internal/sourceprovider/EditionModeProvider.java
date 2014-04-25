828406b964bb3e3522991817b8360832ba96d946
Thomas Muller <thomas.muller@systerel.fr>
2014-04-25 20:21:31 +0200

    Provide an edition mode status for command/handler expressions
    Add a source provider for the edition mode with a (high) priority
    level equivalent to the active editor Id source. Edition modes are:
    structural or overlay.
    Modify the overlay editor to update the edition mode when it is
    activated or deactivated.

---
+++ fr.systerel.editor/src/fr/systerel/editor/internal/sourceprovider/EditionModeProvider.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
+package fr.systerel.editor.internal.sourceprovider;
+
+import static org.eclipse.ui.ISources.ACTIVE_EDITOR;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.ui.AbstractSourceProvider;
+
+/**
+ * Class providing a variable which denotes the current edition mode and can be
+ * used within command enablement conditions.
+ * 
+ * @author Thomas Muller
+ */
+public class EditionModeProvider extends AbstractSourceProvider {
+
+	public static final String EDITION_MODE = "fr.systerel.editor.sourceprovider.editionMode";
+
+	// structural mode is the default value
+	private EditionMode currentMode = EditionMode.STRUCTURAL;
+
+	public EditionModeProvider() {
+		// Nothing to do
+	}
+
+	@Override
+	public void dispose() {
+		// Nothing to do
+	}
+
+	public void setCurrentMode(EditionMode newMode) {
+		if (newMode != currentMode) {
+			currentMode = newMode;
+			fireSourceChanged(ACTIVE_EDITOR, EDITION_MODE, currentMode.getId());
+		}
+	}
+
+	@Override
+	public Map getCurrentState() {
+		final Map<String, Object> map = new HashMap<String, Object>();
+		map.put(EDITION_MODE, currentMode.getId());
+		return map;
+	}
+
+	@Override
+	public String[] getProvidedSourceNames() {
+		return new String[] { EDITION_MODE };
+	}
+
+	public static enum EditionMode {
+
+		STRUCTURAL("structural"), //
+		OVERLAY("overlay"), //
+		;
+
+		private String id;
+
+		private EditionMode(String id) {
+			this.id = id;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+	}
+
+}
