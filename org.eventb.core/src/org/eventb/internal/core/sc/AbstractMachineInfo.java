4485df6cc1a741111785a2e160f33456e5aab632
Laurent Voisin <lvoisin@users.sourceforge.net>
2014-05-08 18:27:38 +0200

    Fix toString() methods
    Protect against null values.

--- org.eventb.core/src/org/eventb/internal/core/sc/AbstractMachineInfo.java
+++ org.eventb.core/src/org/eventb/internal/core/sc/AbstractMachineInfo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2010 ETH Zurich and others.
+ * Copyright (c) 2006, 2014 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -26,7 +26,7 @@ public class AbstractMachineInfo extends State implements
 	
 	@Override
 	public String toString() {
-		return machineFile.getComponentName();
+		return machineFile == null ? "null" : machineFile.getComponentName();
 	}
 
 	private final ISCMachineRoot machineFile;
