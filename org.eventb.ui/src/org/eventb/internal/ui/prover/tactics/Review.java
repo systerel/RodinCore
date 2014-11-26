9a88cb716d1b2c25a67b021a54d385b9de4cc21d
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2014-11-26 12:40:38 +0100

    Fix Review tactic confidence level

--- org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/Review.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/Review.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2010 ETH Zurich and others.
+ * Copyright (c) 2006, 2014 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eventb.core.ast.Predicate;
 import org.eventb.core.pm.IProofState;
 import org.eventb.core.pm.IUserSupport;
+import org.eventb.core.seqprover.IConfidence;
 import org.eventb.core.seqprover.IProofTreeNode;
 import org.eventb.core.seqprover.eventbExtensions.Tactics;
 import org.eventb.ui.prover.IProofCommand;
@@ -24,7 +25,7 @@ public class Review implements IProofCommand {
 	@Override
 	public void apply(IUserSupport us, Predicate hyp, String[] inputs,
 			IProgressMonitor monitor) throws RodinDBException {
-		us.applyTactic(Tactics.review(1), false, null);
+		us.applyTactic(Tactics.review(IConfidence.REVIEWED_MAX), false, null);
 	}
 
 	@Override
