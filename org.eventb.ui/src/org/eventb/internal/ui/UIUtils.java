c896f034bf64130c61194ec6f574d2b18930874c
Thomas Muller <thomas.muller@systerel.fr>
2015-06-10 11:35:18 +0200

    Remove root element selection when opening an editor
    Linking with an editor shall never make the root element to be selected.

--- org.eventb.ui/src/org/eventb/internal/ui/UIUtils.java
+++ org.eventb.ui/src/org/eventb/internal/ui/UIUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2014 ETH Zurich and others.
+ * Copyright (c) 2005, 2015 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -436,7 +436,7 @@ public class UIUtils {
 				return;
 			}
 			final ISelectionProvider sp = editor.getSite().getSelectionProvider();
-			if (sp == null) {
+			if (sp == null || component.getRoot().equals(obj)) {
 				return;
 			}
 			sp.setSelection(new StructuredSelection(obj));
