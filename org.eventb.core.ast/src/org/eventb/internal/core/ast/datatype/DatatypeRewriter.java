582a15d9ea225b866fb62c116c46d0d9fafc9d1a
Laurent Voisin <lvoisin@users.sourceforge.net>
2016-09-27 18:24:38 +0200

    Change DefaultTypeCheckingRewriter to allow for type rewriting
    There are already several implementation of type-checking rewriters
    that also can change the types on the fly.  Update there common
    abstraction to also capture this facet, rather than duplicating the
    same code everywhere.
    
    No functional change for clients.

--- org.eventb.core.ast/src/org/eventb/internal/core/ast/datatype/DatatypeRewriter.java
+++ org.eventb.core.ast/src/org/eventb/internal/core/ast/datatype/DatatypeRewriter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 Systerel and others.
+ * Copyright (c) 2013, 2016 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -40,7 +40,8 @@ public class DatatypeRewriter extends DefaultTypeCheckingRewriter {
 	private final DatatypeTranslation translation;
 
 	public DatatypeRewriter(DatatypeTranslation translation) {
-		super(translation.getTargetFormulaFactory());
+		super(translation.getTargetFormulaFactory(),
+				translation.getTypeRewriter());
 		this.translation = translation;
 	}
 
