5468d7b47dc4e0b74dd921cf610c6293d4329a98
Nicolas Beauger <nicolas_beauger@users.sourceforge.net>
2016-11-16 12:47:39 +0100

    Build upon Eclipse Neon
    It requires a more recent Tycho version and Java 8.

--- org.rodinp.releng/scripts/run-maven.sh
+++ org.rodinp.releng/scripts/run-maven.sh
@@ -11,8 +11,27 @@
 ###############################################################################
 #
 #  This script must be run from a working copy of the Rodin platform sources.
+#  Since Rodin 3.3, Java 8 is required for building.
 #
 
+# Check Java version, Maven searches in JAVA_HOME first
+if [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo "Found java executable in JAVA_HOME"
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "Using java from PATH"
+    _java=java
+fi
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | grep 'version' | sed 's/.*version .*\.\(.*\)\..*/\1/; 1q')
+    echo "Java version: $version"
+    if [[ "$version" -lt "8" ]]; then
+        echo "Java 8 is required"
+	exit 1
+    fi
+fi
+
+# Check Git repository
 if ! git rev-parse --is-inside-work-tree > /dev/null; then
     echo "This script must be run from a Git working copy." >&2
     exit 1
@@ -35,4 +54,5 @@ if git_is_dirty; then DIRTY="-dirty"; fi
 SUFFIX="$RC-$GIT_COMMIT$DIRTY"
 echo "Building Rodin $SUFFIX"
 
+cd "$MAIN_DIR"
 mvn clean verify -Dversion-suffix="$SUFFIX" "$@" # -DskipTests -fae|-fn
