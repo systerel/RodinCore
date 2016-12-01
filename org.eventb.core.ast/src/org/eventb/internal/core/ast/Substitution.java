1895f3586e038311fe2b1ac70fd23ca066cd11e3
Laurent Voisin <lvoisin@users.sourceforge.net>
2016-12-01 14:30:55 +0100

    Add constructor to the Substitution class
    This will allow to reuse the new implementation of
    DefaultTypeCheckingRewriter that rewrites types at the same time as
    formulas.
    
    No functional change.

--- org.eventb.core.ast/src/org/eventb/internal/core/ast/Substitution.java
+++ org.eventb.core.ast/src/org/eventb/internal/core/ast/Substitution.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 ETH Zurich and others.
+ * Copyright (c) 2006, 2016 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -21,6 +21,10 @@ public abstract class Substitution extends DefaultTypeCheckingRewriter {
 
 	public Substitution(FormulaFactory ff) {
 		super(ff);
+	}
+
+	public Substitution(TypeRewriter typeRewriter) {
+		super(typeRewriter);
 	}
 
 }
