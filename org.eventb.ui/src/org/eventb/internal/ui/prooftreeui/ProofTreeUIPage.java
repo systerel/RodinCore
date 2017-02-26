6a8cf4ce445af714580c863141c056856b271a27
Laurent Voisin <lvoisin@users.sourceforge.net>
2017-02-26 18:36:18 +0100

    Remove double test about change event property
    The property is constant, so it will not change between two calls.

--- org.eventb.ui/src/org/eventb/internal/ui/prooftreeui/ProofTreeUIPage.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prooftreeui/ProofTreeUIPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2016 ETH Zurich and others.
+ * Copyright (c) 2005, 2017 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -125,14 +125,10 @@ public class ProofTreeUIPage extends Page implements IProofTreeUIPage,
 
 		@Override
 		public void propertyChange(PropertyChangeEvent event) {
-			if (event.getProperty()
-					.equals(RODIN_MATH_FONT)) {
-				if (event.getProperty().equals(
-						RODIN_MATH_FONT)) {
-					final Font font = JFaceResources.getFont(RODIN_MATH_FONT);
-					viewer.getControl().setFont(font);
-					viewer.refresh();
-				}
+			if (event.getProperty().equals(RODIN_MATH_FONT)) {
+				final Font font = JFaceResources.getFont(RODIN_MATH_FONT);
+				viewer.getControl().setFont(font);
+				viewer.refresh();
 			}
 		}
 
