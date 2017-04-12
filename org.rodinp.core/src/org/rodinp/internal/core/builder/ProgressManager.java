14a5793fd115e4f07406cc5918bcc50a3877802c
Guillaume Babin <guillaume.babin@irit.fr>
2017-04-12 17:47:04 +0200

    Remove useless code

--- org.rodinp.core/src/org/rodinp/internal/core/builder/ProgressManager.java
+++ org.rodinp.core/src/org/rodinp/internal/core/builder/ProgressManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 ETH Zurich and others.
+ * Copyright (c) 2006, 2017 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -49,7 +49,6 @@ public class ProgressManager {
 			if (!node.isDerived())
 				slices++;
 		}
-		slices *= 1;
 		
 		if (graph.size() > slices)
 			slices = graph.size();
