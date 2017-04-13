f5a2a60cd7a8ddf0dcd97822ba80d3934edd2a36
Guillaume Babin <guillaume.babin@irit.fr>
2017-04-13 11:02:52 +0200

    Classes that override equals() should override hashCode()

--- org.eventb.pp/src/org/eventb/internal/pp/loader/predicate/IntermediateResult.java
+++ org.eventb.pp/src/org/eventb/internal/pp/loader/predicate/IntermediateResult.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 ETH Zurich and others.
+ * Copyright (c) 2006, 2017 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -48,6 +48,12 @@ public class IntermediateResult implements IIntermediateResult {
 			return false;
 		final IntermediateResult other = (IntermediateResult) obj;
 		return Arrays.equals(this.terms, other.terms);
+	}
+	
+	@Override
+	public int hashCode() {
+		assert false : "hashCode not designed";
+		return 0;
 	}
 
 	@Override
