04afa7fd80488a18abd434d0953a91b4aa090e17
Guillaume Babin <guillaume.babin@irit.fr>
2017-04-14 20:30:30 +0200

    Remove useless assignement
    At this line, we know that "selectionCount > 2" ; and "enableExport" was already initialized at "true"

--- org.eventb.ui/src/org/eventb/internal/ui/preferences/tactics/PostAutoTacticPreferencePage.java
+++ org.eventb.ui/src/org/eventb/internal/ui/preferences/tactics/PostAutoTacticPreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2015 Systerel and others.
+ * Copyright (c) 2010, 2017 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -345,8 +345,6 @@ public class PostAutoTacticPreferencePage extends
 				final int selectionCount = tacticList.getSelectionCount();
 				if (selectionCount <= 2) {
 					enableExport = !containsDefaultProfiles(Arrays.asList(tacticList.getSelection()));
-				} else {
-					enableExport = selectionCount > 0;
 				}
 				exportAction.setEnabled(enableExport);
 			}
