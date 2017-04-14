416466567784ab2d90bf9db817fc400ef204d5e0
Guillaume Babin <guillaume.babin@irit.fr>
2017-04-14 18:30:46 +0200

    Remove repeated conditional tests

--- org.eventb.pp/src/org/eventb/internal/pp/core/elements/terms/Mod.java
+++ org.eventb.pp/src/org/eventb/internal/pp/core/elements/terms/Mod.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 ETH Zurich and others.
+ * Copyright (c) 2006, 2017 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -31,8 +31,7 @@ public final class Mod extends BinaryTerm {
 	@Override
 	public boolean equals(Object obj) {
 		if (obj instanceof Mod) {
-			Mod temp = (Mod) obj;
-			return super.equals(temp) && super.equals(temp);
+			return super.equals(obj);
 		}
 		return false;
 	}
