26a87402a5451d3ee9f5ed92eb3880ce6e7c4a2b
Nicolas Beauger <nicolas.beauger@systerel.fr>
2017-08-07 16:19:02 +0200

    Fix RodinDBStatus error message
    The message was about invalid attribute kind instead of value.

--- org.rodinp.core/src/org/rodinp/internal/core/RodinDBStatus.java
+++ org.rodinp.core/src/org/rodinp/internal/core/RodinDBStatus.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -340,7 +340,7 @@ public class RodinDBStatus extends Status implements IRodinDBStatus,
 						string);
 
 			case INVALID_ATTRIBUTE_VALUE:
-				return Messages.bind(Messages.status_invalidAttributeKind,
+				return Messages.bind(Messages.status_invalidAttributeValue,
 						string);
 
 			case INVALID_VERSION_NUMBER:
