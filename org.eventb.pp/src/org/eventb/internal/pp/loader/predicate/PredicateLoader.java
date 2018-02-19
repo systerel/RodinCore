7282593934965e3650464c7099d87d54d52d15da
Nicolas Beauger <nicolas.beauger@systerel.fr>
2018-02-19 20:04:58 +0100

    Add @SuppressWarnings in PP (raw types, synthetic access)

--- org.eventb.pp/src/org/eventb/internal/pp/loader/predicate/PredicateLoader.java
+++ org.eventb.pp/src/org/eventb/internal/pp/loader/predicate/PredicateLoader.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 ETH Zurich and others.
+ * Copyright (c) 2006, 2018 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -490,7 +490,7 @@ public class PredicateLoader {
 		}
 
 		// we create the new signature
-		@SuppressWarnings("unchecked")
+		@SuppressWarnings({ "unchecked", "rawtypes" })
 		final SignedFormula<?> lit = new SignedFormula(sig, isPositive);
 
 		// we append the new literal to the result before
