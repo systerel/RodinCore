dffdcae12f2a502c3201c824410665d1957024ab
Laurent Voisin <lvoisin@users.sourceforge.net>
2018-09-18 16:44:45 +0200

    Fix bug #777 Missing refresh in Rodin Editor for event convergence status
    As we now produce `SET` notifications, we process them the same way as
    the `ADD` notifications we were receiving before the fix of the Rodin
    EMF plug-in.

--- fr.systerel.editor/src/fr/systerel/editor/internal/presentation/updaters/PresentationUpdater.java
+++ fr.systerel.editor/src/fr/systerel/editor/internal/presentation/updaters/PresentationUpdater.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2017 Systerel and others.
+ * Copyright (c) 2011, 2018 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -89,6 +89,7 @@ public class PresentationUpdater extends EContentAdapter implements
 			final int eventType = notification.getEventType();
 			final RodinEditor editor = updater.getEditor();
 			switch (eventType) {
+			case Notification.SET:
 			case Notification.ADD:
 				if (notification.getNewValue() instanceof ILElement) {
 					return new EditorResynchronizer(editor, null,
