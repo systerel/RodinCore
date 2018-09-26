f85a1bce96588204e46f48c605039874d0505c08
Laurent Voisin <lvoisin@users.sourceforge.net>
2018-09-26 16:41:39 +0200

    Add @noimplement annotation to descendants of IInternalLocation
    These interfaces are not implementable like their parent interface.
    They shall therefore not leak any API.
    
    Remove the now obsolete problem filter.
    
    No functional change.

--- org.rodinp.core/src/org/rodinp/core/location/IAttributeLocation.java
+++ org.rodinp.core/src/org/rodinp/core/location/IAttributeLocation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2012 Systerel and others.
+ * Copyright (c) 2008, 2018 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,6 +22,7 @@ import org.rodinp.core.IAttributeType;
  * 
  * @author Laurent Voisin
  * @since 1.0
+ * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface IAttributeLocation extends IInternalLocation {
 
