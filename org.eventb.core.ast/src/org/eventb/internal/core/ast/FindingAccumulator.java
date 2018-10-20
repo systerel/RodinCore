a17d5f381483328c78974eb817552bd5b50d72ac
Laurent Voisin <lvoisin@users.sourceforge.net>
2018-10-20 16:54:01 +0200

    Fix varargs warning in Event-B AST
    All uses are safe, but the Java compiler cannot know that there is
    exactly one implementation of IAccumulator, thanks to the @noimplement
    Javadoc tag.

--- org.eventb.core.ast/src/org/eventb/internal/core/ast/FindingAccumulator.java
+++ org.eventb.core.ast/src/org/eventb/internal/core/ast/FindingAccumulator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 Systerel and others.
+ * Copyright (c) 2010, 2018 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -77,7 +77,8 @@ public class FindingAccumulator<F> implements IAccumulator<F> {
 	}
 
 	@Override
-	public void add(F... items) {
+	@SafeVarargs
+	public final void add(F... items) {
 		for (F item : items) {
 			findings.add(item);
 		}
