4c2ef078110cfbb93aa6dc1c5bb38e9f8b94cf5b
Laurent Voisin <lvoisin@users.sourceforge.net>
2018-10-21 18:56:07 +0200

    Remove useless attribute
    The tableValues attribute is not necessary.  It can be replaced by
    calling the values() method of the HashTable.
    
    The modification does not change anything because all values are
    different by construction (they all have different symbols).  There is
    no risk to have two equal values in the result and we can thus replace
    the set by a collection.
    
    The corresponding unit tests needed to be updated, because the new
    result is not a proper set anymore (which was not guaranteed by the API,
    anyhow). We therefore create an intermediate set to get a proper
    comparison.
    
    No functional change.

--- org.eventb.core/src/org/eventb/internal/core/sc/symbolTable/IdentifierSymbolTable.java
+++ org.eventb.core/src/org/eventb/internal/core/sc/symbolTable/IdentifierSymbolTable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 ETH Zurich and others.
+ * Copyright (c) 2006, 2018 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,7 +14,6 @@
 package org.eventb.internal.core.sc.symbolTable;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -76,7 +75,7 @@ public class IdentifierSymbolTable
 
 	@Override
 	public Collection<IIdentifierSymbolInfo> getSymbolInfosFromTop() {
-		return Collections.unmodifiableSet(tableValues);
+		return getSymbolInfos();
 	}
 
 }
--- org.eventb.core/src/org/eventb/internal/core/sc/symbolTable/SymbolTable.java
+++ org.eventb.core/src/org/eventb/internal/core/sc/symbolTable/SymbolTable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 ETH Zurich and others.
+ * Copyright (c) 2006, 2018 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,9 +12,10 @@
  *******************************************************************************/
 package org.eventb.internal.core.sc.symbolTable;
 
+import static java.util.Collections.unmodifiableCollection;
+
+import java.util.Collection;
 import java.util.Hashtable;
-import java.util.Set;
-import java.util.TreeSet;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eventb.core.sc.state.ISymbolInfo;
@@ -33,13 +34,8 @@ public abstract class SymbolTable<E extends IInternalElement, T extends IInterna
 
 	private final Hashtable<String, I> table;
 
-	// the tableValues variable is a cache that holds the value of
-	// table.values()
-	protected final Set<I> tableValues;
-
 	public SymbolTable(int size) {
 		table = new Hashtable<String, I>(size);
-		tableValues = new TreeSet<I>();
 	}
 
 	@Override
@@ -74,16 +70,19 @@ public abstract class SymbolTable<E extends IInternalElement, T extends IInterna
 			table.put(key, oldSymbol);
 			return false;
 		}
-		tableValues.add(symbolInfo);
 		return true;
 	}
 
 	@Override
 	public void makeImmutable() {
-		for (I info : tableValues) {
+		for (I info : table.values()) {
 			info.makeImmutable();
 		}
 		super.makeImmutable();
+	}
+
+	public Collection<I> getSymbolInfos() {
+		return unmodifiableCollection(table.values());
 	}
 
 	/*
