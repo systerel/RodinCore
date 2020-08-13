49269c097069a3c66b8533bf66bd748f431bdc0e
Thái Sơn Hoàng <T.S.Hoang@ecs.soton.ac.uk>
2020-08-13 11:00:38 +0100

    Removed unused import in org.rodinp.keyboard.core

--- org.rodinp.keyboard.core/src/org/rodinp/keyboard/core/ISymbolsProvider.java
+++ org.rodinp.keyboard.core/src/org/rodinp/keyboard/core/ISymbolsProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2013 Systerel and others.
+ * Copyright (c) 2010, 2020 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,12 +9,11 @@
  * 
  * Contributors:
  *     Systerel - initial API and implementation
+ *     University of Southampton - Remove unused import
  *******************************************************************************/
 package org.rodinp.keyboard.core;
 
 import java.util.List;
-
-import org.rodinp.keyboard.core.ExtensionSymbol;
 
 /**
  * Interface used to contribute programatically the new symbols to the keyboard.
