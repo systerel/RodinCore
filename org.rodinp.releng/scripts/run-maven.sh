71117576764cc95a070867cba72803abaee71a84
Thái Sơn Hoàng <T.S.Hoang@ecs.soton.ac.uk>
2020-08-14 22:25:07 +0100

    Added command line options to the build script

--- org.rodinp.releng/scripts/run-maven.sh
+++ org.rodinp.releng/scripts/run-maven.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ###############################################################################
-# Copyright (c) 2013, 2014 Systerel and others.
+# Copyright (c) 2013,2020 Systerel and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
@@ -8,11 +8,109 @@
 #
 # Contributors:
 #     Systerel - initial API and implementation
+#     University of Southampton - Added various command line options
 ###############################################################################
 #
 #  This script must be run from a working copy of the Rodin platform sources.
 #  Since Rodin 3.3, Java 8 is required for building.
 #
+
+# Use the template script from https://gist.github.com/neatshell/5283811
+# Declare the number of mandatory args
+margs=0 # We do not have any mandatory arguments at the moment.
+
+# Script name should be the 0th-argument
+script=$0
+
+function usage {
+    echo -e "usage: $script [OPTION]\n"
+}
+
+function example {
+    echo -e "example: $script -rc 1"
+    echo -e "               Build a release candidate RC1"
+}
+
+function help {
+  usage
+    echo -e "MANDATORY:"
+    echo -e "OPTION:"
+    echo -e "  -e,  --errors                    Produce Maven execution error messages"
+    echo -e "  -h,  --help                      Prints this help"
+    echo -e "  -rc,  --release-candidate  n     Build a release candidate RCn"
+    echo -e "  -st,  --skip-tests               Skip tests"
+    echo -e "  -X,  --debug                     Produce Maven execution debug output"
+    echo -e ""
+  example
+}
+
+# Copied from https://gist.github.com/neatshell/5283811
+# Ensures that the number of passed args are at least equals
+# to the declared number of mandatory args.
+# It also handles the special case of the -h or --help arg.
+function margs_precheck {
+    if [ $2 ] && [ $1 -lt $margs ]; then
+        if [ $2 == "--help" ] || [ $2 == "-h" ]; then
+            help
+            exit
+        else
+            usage
+            example
+            exit 1 # error
+        fi
+    fi
+}
+
+# Ensures that all the mandatory args are not empty
+function margs_check {
+    if [ $# -lt $margs ]; then
+        usage
+        example
+        exit 1 # error
+    fi
+}
+
+margs_precheck $# $1
+
+# Default values for optional arguments
+RC=""
+DEBUG=""
+ERROR=""
+
+# Args while-loop
+while [ "$1" != "" ];
+do
+    case $1 in
+    -e   | --errors  )
+                                    ERROR="-e"
+                                    ;;
+    -h   | --help  )
+                                    help
+                                    exit
+                                    ;;
+    -rc  | --release-candidate  )
+                                    shift
+                                    RC="-RC$1"
+                                    ;;
+    -st  | --skip-tests  )
+                                    shift
+                                    SKIP_TESTS="-DskipTests"
+                                    ;;
+    -X   | --debug  )
+                                    DEBUG="-X"
+                                    ;;
+    *)
+                                    echo "$script: illegal option $1"
+                                    usage
+                                    example
+                                    exit 1 # error
+                                    ;;
+    esac
+    shift
+done
+
+# Pass here your mandatory args for check
+margs_check
 
 # Check Java version, Maven searches in JAVA_HOME first
 if [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
@@ -32,6 +130,9 @@ if [[ "$_java" ]]; then
         echo "Java 8 is required"
         exit 1
     fi
+else
+    echo "No Java executable found in PATH or JAVA_HOME"
+    exit 1
 fi
 
 # Check Git repository
@@ -46,10 +147,6 @@ git_is_dirty() {
     test -n "$(git status --porcelain)"
 }
 
-case "$1" in
-    RC*) RC="-$1"; shift ;;
-esac
-
 GIT_COMMIT=$(git log -1 --format='%h')
 
 if git_is_dirty; then DIRTY="-dirty"; fi
@@ -58,4 +155,4 @@ SUFFIX="$RC-$GIT_COMMIT$DIRTY"
 echo "Building Rodin $SUFFIX"
 
 cd "$MAIN_DIR"
-mvn clean verify -Dversion-suffix="$SUFFIX" "$@" # -DskipTests -fae|-fn
+mvn clean verify $ERROR $DEBUG $SKIP_TESTS -Dversion-suffix="$SUFFIX" "$@" # -fae|-fn
