7106c8a32dc65f8df33bb0fbe01abcb5b7c20454
Guillaume Verdier <gverdier@users.sourceforge.net>
2022-04-06 15:37:16 +0200

    Fix bug #801: Hypothesis checkbox not refreshed when clicked
    The bug was caused by setting the background color of the checkbox: with
    some desktop environments, it causes the checkmark to not be displayed
    anymore when the checkbox is checked. Removing the background color on
    the checkbox fixes the bug and does not seem to cause particular issues
    with any other system. The predicate row that contains the checkbox
    still has its background color around the checkbox.
    
    The dropdown button for tactic application does not suffer from the same
    bug, so its background color is not removed.

--- org.eventb.ui/src/org/eventb/internal/ui/prover/PredicateRow.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/PredicateRow.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 ETH Zurich and others.
+ * Copyright (c) 2006, 2022 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -143,7 +143,7 @@ public class PredicateRow {
 		final Color bgColor = odd ? SOFT_BG_COLOR : WHITE;
 		if (!isGoal) {
 			checkBoxHolder = new ControlHolder(this, checkBoxMaker,
-					checkBoxOffset, false, bgColor);
+					checkBoxOffset, false);
 			checkBoxHolder.addListener(checkboxListener);
 		}
 		final int menuOffset = checkBoxOffset + 1;
