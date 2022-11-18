be4602e4dc6a6005552319e5d70e5eb0b8299a3c
Guillaume Verdier <gverdier@users.sourceforge.net>
2022-11-18 14:43:08 +0100

    Remove overridden methods that are not needed anymore
    It appears that getHyperlinkBounds and getOperatorPosition are now
    implemented in a generic way in a super class and don't need to be
    overridden in the concrete tactic application implementation.

--- org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/DisjunctionElimination.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/DisjunctionElimination.java
@@ -16,15 +16,10 @@ import static java.util.Collections.singletonList;
 
 import java.util.List;
 
-import org.eclipse.swt.graphics.Point;
-import org.eventb.core.ast.AssociativePredicate;
-import org.eventb.core.ast.Formula;
 import org.eventb.core.ast.IPosition;
 import org.eventb.core.ast.Predicate;
-import org.eventb.core.ast.RelationalPredicate;
 import org.eventb.core.seqprover.IProofTreeNode;
 import org.eventb.core.seqprover.ITactic;
-import org.eventb.core.seqprover.eventbExtensions.Lib;
 import org.eventb.core.seqprover.eventbExtensions.Tactics;
 import org.eventb.ui.prover.DefaultTacticProvider.DefaultPositionApplication;
 import org.eventb.ui.prover.ITacticApplication;
@@ -56,31 +51,6 @@ public class DisjunctionElimination implements ITacticProvider {
 		@Override
 		public String getTacticID() {
 			return TACTIC_ID;
-		}
-
-		@Override
-		public Point getHyperlinkBounds(String parsedString,
-				Predicate parsedPredicate) {
-			return getOperatorPosition(parsedPredicate, parsedString);
-		}
-
-		@Override
-		public Point getOperatorPosition(Predicate predicate, String predStr) {
-			Predicate subFormula = (Predicate) predicate.getSubFormula(position);
-			Formula<?> first, second;
-			if (Lib.isDisj(subFormula)) {
-				Predicate[] children = ((AssociativePredicate) subFormula).getChildren();
-				first = children[0];
-				second = children[1];
-			} else {
-				// subFormula is x ∈ ...
-				var rel = (RelationalPredicate) subFormula;
-				first = rel.getLeft();
-				second = rel.getRight();
-			}
-			// Return the operator between the first and second child
-			return getOperatorPosition(predStr, first.getSourceLocation()
-					.getEnd() + 1, second.getSourceLocation().getStart());
 		}
 
 	}
