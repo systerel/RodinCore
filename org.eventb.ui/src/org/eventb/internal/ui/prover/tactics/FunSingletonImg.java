ca3f8894d88d4541245e5f8c78b48cabc214f5ad
Guillaume Verdier <gverdier@users.sourceforge.net>
2022-12-06 09:35:47 +0100

    Use new adapter to simplify fun. img. tactic providers

--- org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/FunSingletonImg.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/FunSingletonImg.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2012 ETH Zurich and others.
+ * Copyright (c) 2007, 2022 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,15 +10,12 @@
  *******************************************************************************/
 package org.eventb.internal.ui.prover.tactics;
 
+import static org.eventb.ui.prover.TacticProviderUtils.adaptPositionsToApplications;
+
 import java.util.List;
 
-import org.eventb.core.ast.BinaryExpression;
-import org.eventb.core.ast.Expression;
-import org.eventb.core.ast.Formula;
-import org.eventb.core.ast.IAccumulator;
 import org.eventb.core.ast.IPosition;
 import org.eventb.core.ast.Predicate;
-import org.eventb.core.ast.SetExtension;
 import org.eventb.core.seqprover.IProofTreeNode;
 import org.eventb.core.seqprover.ITactic;
 import org.eventb.core.seqprover.eventbExtensions.Tactics;
@@ -55,38 +52,12 @@ public class FunSingletonImg extends AbstractHypGoalTacticProvider {
 
 	}
 
-	public static class FunSingletonImgAppliInspector extends
-			DefaultApplicationInspector {
-
-		public FunSingletonImgAppliInspector(Predicate hyp) {
-			super(hyp);
-		}
-
-		@Override
-		public void inspect(BinaryExpression expression,
-				IAccumulator<ITacticApplication> accumulator) {
-			if (!(expression.getTag() == Formula.RELIMAGE)) {
-				return;
-			}
-			final Expression right = expression.getRight();
-			if (!(right.getTag() == Formula.SETEXT)) {
-				return;
-			}
-			final SetExtension setExtension = ((SetExtension) right);
-			// singleton
-			if (setExtension.getMembers().length == 1) {
-				final IPosition position = accumulator.getCurrentPosition();
-				accumulator.add(new FunSingletonImgApplication(hyp, position));
-			}
-		}
-
-	}
-
 	@Override
 	protected List<ITacticApplication> getApplicationsOnPredicate(
 			IProofTreeNode node, Predicate hyp, String globalInput,
 			Predicate predicate) {
-		return predicate.inspect(new FunSingletonImgAppliInspector(hyp));
+		return adaptPositionsToApplications(hyp, predicate, Tactics::funSingletonImgGetPositions,
+				FunSingletonImgApplication::new);
 	}
 
 }
