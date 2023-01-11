089fe850703a73490b4c72e952782055fcc3d040
Guillaume Verdier <gverdier@users.sourceforge.net>
2023-01-11 15:34:48 +0100

    Simplify tactic providers for distinct case/induction
    The shared implementation is deleted and simpler, more modern
    implementations are adopted for both providers.

--- org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/DTInduction.java
+++ org.eventb.ui/src/org/eventb/internal/ui/prover/tactics/DTInduction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Systerel and others.
+ * Copyright (c) 2010, 2023 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,42 +10,56 @@
  *******************************************************************************/
 package org.eventb.internal.ui.prover.tactics;
 
+import static java.util.Collections.emptyList;
+import static org.eventb.ui.prover.TacticProviderUtils.adaptPositionsToApplications;
+
 import java.util.List;
 
 import org.eventb.core.ast.IPosition;
 import org.eventb.core.ast.Predicate;
+import org.eventb.core.seqprover.IProofTreeNode;
 import org.eventb.core.seqprover.ITactic;
 import org.eventb.core.seqprover.eventbExtensions.Tactics;
+import org.eventb.ui.prover.DefaultTacticProvider.DefaultPositionApplication;
 import org.eventb.ui.prover.ITacticApplication;
+import org.eventb.ui.prover.ITacticProvider;
 
 /**
  * @author Nicolas Beauger
  * 
  */
-public class DTInduction extends DTTactic {
+public class DTInduction implements ITacticProvider {
 
 	private static final String TACTIC_ID = "org.eventb.ui.dtInduction";
 
-	private static class DCApplication extends DTApplication {
+	private static class DCApplication extends DefaultPositionApplication {
 
 		public DCApplication(Predicate hyp, IPosition position) {
-			super(TACTIC_ID, hyp, position);
+			super(hyp, position);
 		}
 
 		@Override
 		public ITactic getTactic(String[] inputs, String gInput) {
 			return Tactics.dtInduction(hyp, position);
 		}
+
+		@Override
+		public String getTacticID() {
+			return TACTIC_ID;
+		}
 	}
 
-	@Override
-	protected List<IPosition> getPositions(Predicate pred) {
-		return Tactics.dtDCInducGetPositions(pred);
-	}
+	private static final List<ITacticApplication> NO_APPLICATIONS = emptyList();
 
 	@Override
-	protected ITacticApplication makeApplication(Predicate hyp, IPosition p) {
-		return new DCApplication(hyp, p);
+	public List<ITacticApplication> getPossibleApplications(IProofTreeNode node, Predicate hyp, String globalInput) {
+		// Induction is only applicable on goal
+		if (hyp == null) {
+			return adaptPositionsToApplications(null, node.getSequent().goal(), Tactics::dtDCInducGetPositions,
+					DCApplication::new);
+		} else {
+			return NO_APPLICATIONS;
+		}
 	}
 
 }
