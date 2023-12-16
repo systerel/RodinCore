f17e394527ce517c6b92b3d3b0bc9ae9a10b736c
Laurent Voisin <lvoisin@users.sourceforge.net>
2023-12-16 16:58:01 +0100

    Extract tests for DEF_IN_RANSUB

--- org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtensionTests/RemoveMembershipTests.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtensionTests/RemoveMembershipTests.java
@@ -245,12 +245,15 @@ public abstract class RemoveMembershipTests extends AbstractManualRewriterTests 
 		noRewriteRoot("a ∈ {1 ↦ 2} ▷ A");
 	}
 
+	// E |-> F : r |>> T == E |-> F : r & F /: T
+	@Test
+	public void testDEF_IN_RANSUB() throws Exception {
+		rewriteRoot("1 ↦ 2 ∈ R ⩥ A", "1 ↦ 2 ∈ R ∧ 2 ∉ A");
+		noRewriteRoot("a ∈ {1 ↦ 2} ⩥ A");
+	}
+
 	@Test
 	public void testSuccessful() throws Exception {
-		// E |-> F : r |>> T == E |-> F : r & F /: T
-		assertReasonerSuccess("(0 = 1) ⇒ (1 ↦ 0 ∈ {1 ↦ 0} ⩥ {0})", "1", "0=1⇒1 ↦ 0∈{1 ↦ 0}∧0∉{0}");
-		assertReasonerSuccess("∀x·x = 0 ⇒ (1 ↦ x ∈ {1 ↦ x, x ↦ 2} ⩥ {x})", "1.1", "∀x·x=0⇒1 ↦ x∈{1 ↦ x,x ↦ 2}∧x∉{x}");
-
 		// F : r[w] = #x.x : w & x |-> F : r
 		assertReasonerSuccess("(0 = 1) ⇒ 1 ∈ r[{0, 1}]", "1", "0=1⇒(∃x·x∈{0,1}∧x ↦ 1∈r)");
 		assertReasonerSuccess("∀x·x = 0 ⇒ x ∈ r[{0, x}]", "1.1", "∀x·x=0⇒(∃x0·x0∈{0,x}∧x0 ↦ x∈r)");
@@ -371,12 +374,6 @@ public abstract class RemoveMembershipTests extends AbstractManualRewriterTests 
 
 	@Test
 	public void testUnsuccessful() {
-		assertReasonerFailure("e ∈ {1 ↦ 0} ⩥ {0}", "");
-
-		// E |-> F : r |>> T == E |-> F : r & F /: T
-		assertReasonerFailure("(0 = 1) ⇒ (1 ↦ 0 ∈ {1 ↦ 0} ⩥ {0})", "0");
-		assertReasonerFailure("∀x·x = 0 ⇒ (1 ↦ x ∈ {1 ↦ x, x ↦ 2} ⩥ {x})", "1.0");
-
 		// F : r[w] = #x.x : w & x |-> F : r
 		assertReasonerFailure("(0 = 1) ⇒ 1 ∈ r[{0, 1}]", "0");
 		assertReasonerFailure("∀x·x = 0 ⇒ x ∈ r[{0, x}]", "1.0");
