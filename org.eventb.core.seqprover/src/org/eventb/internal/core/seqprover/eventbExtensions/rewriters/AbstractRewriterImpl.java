bbb145c2b84408f9d0f07bf5df958d96c959ebbf
Laurent Voisin <lvoisin@users.sourceforge.net>
2023-12-17 22:13:49 +0100

    Move more methods in AbstractRewriterImpl
    We now move utility methods from AutoRewriterImpl to
    AbstractRewriterImpl.

--- org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/rewriters/AbstractRewriterImpl.java
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/rewriters/AbstractRewriterImpl.java
@@ -12,14 +12,31 @@
  *******************************************************************************/
 package org.eventb.internal.core.seqprover.eventbExtensions.rewriters;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.eventb.core.ast.AssociativeExpression;
 import org.eventb.core.ast.AssociativePredicate;
+import org.eventb.core.ast.AtomicExpression;
+import org.eventb.core.ast.BinaryExpression;
 import org.eventb.core.ast.BinaryPredicate;
 import org.eventb.core.ast.BoundIdentDecl;
+import org.eventb.core.ast.BoundIdentifier;
 import org.eventb.core.ast.DefaultRewriter;
+import org.eventb.core.ast.Expression;
 import org.eventb.core.ast.Formula;
 import org.eventb.core.ast.FormulaFactory;
 import org.eventb.core.ast.Predicate;
+import org.eventb.core.ast.QuantifiedExpression;
+import org.eventb.core.ast.QuantifiedExpression.Form;
 import org.eventb.core.ast.QuantifiedPredicate;
+import org.eventb.core.ast.RelationalPredicate;
+import org.eventb.core.ast.SetExtension;
+import org.eventb.core.ast.SimplePredicate;
+import org.eventb.core.ast.Type;
+import org.eventb.core.ast.UnaryExpression;
+import org.eventb.core.ast.UnaryPredicate;
 
 /**
  * Abstract implementation of a formula rewriter. The purpose of this class is
@@ -79,6 +96,115 @@ public abstract class AbstractRewriterImpl extends DefaultRewriter {
 			BoundIdentDecl[] boundIdentifiers, Predicate child) {
 		final FormulaFactory ff = child.getFactory();
 		return ff.makeQuantifiedPredicate(tag, boundIdentifiers, child, null);
+	}
+
+	protected UnaryPredicate makeUnaryPredicate(int tag, Predicate child) {
+		final FormulaFactory ff = child.getFactory();
+		return ff.makeUnaryPredicate(tag, child, null);
+	}
+
+	protected RelationalPredicate makeRelationalPredicate(int tag, Expression left, Expression right) {
+		final FormulaFactory ff = left.getFactory();
+		return ff.makeRelationalPredicate(tag, left, right, null);
+	}
+
+	protected SetExtension makeSetExtension(Collection<Expression> expressions) {
+		final FormulaFactory ff = expressions.iterator().next().getFactory();
+		return ff.makeSetExtension(expressions, null);
+	}
+
+	protected SetExtension makeSetExtension(Expression... expressions) {
+		final FormulaFactory ff = expressions[0].getFactory();
+		return ff.makeSetExtension(expressions, null);
+	}
+
+	protected UnaryExpression makeUnaryExpression(int tag, Expression child) {
+		final FormulaFactory ff = child.getFactory();
+		return ff.makeUnaryExpression(tag, child, null);
+	}
+
+	protected BinaryExpression makeBinaryExpression(int tag, Expression left, Expression right) {
+		final FormulaFactory ff = left.getFactory();
+		return ff.makeBinaryExpression(tag, left, right, null);
+	}
+
+	protected AtomicExpression makeEmptySet(FormulaFactory ff, Type type) {
+		return ff.makeEmptySet(type, null);
+	}
+
+	protected Expression makeAssociativeExpression(int tag, Expression... children) {
+		if (children.length == 1) {
+			return children[0];
+		}
+		final FormulaFactory ff = children[0].getFactory();
+		return ff.makeAssociativeExpression(tag, children, null);
+	}
+
+	protected Expression makeAssociativeExpression(int tag, List<Expression> children) {
+		if (children.size() == 1) {
+			return children.get(0);
+		}
+		final FormulaFactory ff = children.get(0).getFactory();
+		return ff.makeAssociativeExpression(tag, children, null);
+	}
+
+	protected SimplePredicate makeSimplePredicate(int tag, Expression expression) {
+		final FormulaFactory ff = expression.getFactory();
+		return ff.makeSimplePredicate(tag, expression, null);
+	}
+
+	protected QuantifiedExpression makeQuantifiedExpression(int tag, //
+			BoundIdentDecl[] boundIdentifiers, Predicate pred, Expression expr, Form form) {
+		final FormulaFactory ff = pred.getFactory();
+		return ff.makeQuantifiedExpression(tag, boundIdentifiers, pred, expr, null, form);
+	}
+
+	protected BoundIdentifier makeBoundIdentifier(FormulaFactory ff, int index, Type type) {
+		return ff.makeBoundIdentifier(index, null, type);
+	}
+
+	protected AtomicExpression makeAtomicExpression(FormulaFactory ff, int tag) {
+		return ff.makeAtomicExpression(tag, null);
+	}
+
+	protected boolean notLocallyBound(Formula<?> form, int nbBound) {
+		for (BoundIdentifier ident : form.getBoundIdentifiers()) {
+			if (ident.getBoundIndex() < nbBound) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// Removes exactly one occurrence of the given child from the given
+	// associative expression. If not possible raises an error.
+	protected Expression removeChild(AssociativeExpression parent, Expression toRemove) {
+		final int tag = parent.getTag();
+		final Expression[] children = parent.getChildren();
+		final int length = children.length;
+		if (length == 2) {
+			if (toRemove.equals(children[0])) {
+				return children[1];
+			}
+			if (toRemove.equals(children[1])) {
+				return children[0];
+			}
+			assert false;
+		}
+		final Expression[] newChildren = remove(toRemove, children);
+		return makeAssociativeExpression(tag, newChildren);
+	}
+
+	// Removes exactly one occurrence of the given expression from the given
+	// expressions. If not possible raises an error.
+	protected Expression[] remove(Expression toRemove, Expression[] exprs) {
+		final int index = Arrays.asList(exprs).indexOf(toRemove);
+		assert 0 <= index;
+		final int length = exprs.length;
+		final Expression[] newExprs = new Expression[length - 1];
+		System.arraycopy(exprs, 0, newExprs, 0, index);
+		System.arraycopy(exprs, index + 1, newExprs, index, length - index - 1);
+		return newExprs;
 	}
 
 }
