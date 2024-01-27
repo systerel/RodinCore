5f20b3d410bda3993fe2513f68238833edf0928a
Laurent Voisin <lvoisin@users.sourceforge.net>
2024-01-27 19:01:37 +0100

    Add missing Javadoc to the AutoRewrites class
    No functional change.

--- org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/rewriters/AutoRewrites.java
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/rewriters/AutoRewrites.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2023 ETH Zurich and others.
+ * Copyright (c) 2006, 2024 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -25,6 +25,12 @@ import org.eventb.core.seqprover.IVersionedReasoner;
 import org.eventb.core.seqprover.SequentProver;
 import org.eventb.core.seqprover.eventbExtensions.DLib;
 
+/**
+ * Common implementation of the {@code org.eventb.core.seqprover.autoRewrites}
+ * reasoner.
+ * 
+ * There are as many sub-classes as levels of this reasoner.
+ */
 public abstract class AutoRewrites extends AbstractAutoRewrites implements
 		IVersionedReasoner {
 
