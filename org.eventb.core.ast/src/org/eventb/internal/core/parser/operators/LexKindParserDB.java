9e89e9013179c3128228ae06411cd63a402bfc37
Laurent Voisin <lvoisin@users.sourceforge.net>
2024-02-04 18:44:52 +0100

    Reimplement LexKindParserDB
    We now can greatly simplify the LexKindParserDB class, as we now have
    at most one parser per token kind.
    
    No functional change.

--- org.eventb.core.ast/src/org/eventb/internal/core/parser/operators/LexKindParserDB.java
+++ org.eventb.core.ast/src/org/eventb/internal/core/parser/operators/LexKindParserDB.java
@@ -10,9 +10,7 @@
  *******************************************************************************/
 package org.eventb.internal.core.parser.operators;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -29,89 +27,36 @@ import org.eventb.internal.core.parser.operators.ExternalViewUtils.Instantiator;
  */
 public class LexKindParserDB {
 
-	private static class KindParsers {
-		private final List<ILedParser<? extends Formula<?>>> ledParsers = new ArrayList<ILedParser<? extends Formula<?>>>();
-		private final List<INudParser<? extends Formula<?>>> nudParsers = new ArrayList<INudParser<? extends Formula<?>>>();
+	private final Map<Integer, INudParser<? extends Formula<?>>> nudParsers = new HashMap<>();
+	private final Map<Integer, ILedParser<? extends Formula<?>>> ledParsers = new HashMap<>();
 
-		public KindParsers() {
-			// nothing to do
-		}
-
-		public void addLed(ILedParser<? extends Formula<?>> subParser) throws OverrideException {
-			if (!ledParsers.isEmpty()) {
-				throw new OverrideException(
-						"Cannot add several led parsers for one kind (led backtracking is not supported)");
-			}
-			ledParsers.add(subParser);
-		}
-
-		public void addNud(INudParser<? extends Formula<?>> subParser) throws OverrideException {
-			if (!nudParsers.isEmpty()) {
-				throw new OverrideException(
-						"Cannot add several nud parsers for one kind (nud backtracking is not supported)");
-			}
-			if (!nudParsers.contains(subParser)) {
-				nudParsers.add(subParser);
-			}
-		}
-
-		public List<ILedParser<? extends Formula<?>>> getLedParsers() {
-			return ledParsers;
-		}
-
-		public List<INudParser<? extends Formula<?>>> getNudParsers() {
-			return nudParsers;
-		}
-	}
-
-	private final Map<Integer, KindParsers> kindParsers = new HashMap<Integer, KindParsers>();
-	
 	public INudParser<? extends Formula<?>> getNudParser(Token token) {
-		final KindParsers parsers = kindParsers.get(token.kind);
-		if (parsers == null) {
-			return null;
-		}
-		final List<INudParser<? extends Formula<?>>> nudParsers = parsers.getNudParsers();
-		if (nudParsers.isEmpty()) {
-			return null;
-		}
-		return nudParsers.get(0); 
+		return nudParsers.get(token.kind);
 	}
-	
+
 	public ILedParser<? extends Formula<?>> getLedParser(Token token) {
-		final KindParsers parsers = kindParsers.get(token.kind);
-		if (parsers == null) {
-			return null;
-		}
-		final List<ILedParser<? extends Formula<?>>> ledParsers = parsers
-				.getLedParsers();
-		if (ledParsers.isEmpty()) {
-			return null;
-		}
-		return ledParsers.get(0);
+		return ledParsers.get(token.kind);
 	}
-	
+
 	public void addNud(int kind, INudParser<? extends Formula<?>> subParser) throws OverrideException {
-		final KindParsers parsers = fetchParsers(kind);
-		parsers.addNud(subParser);
+		var old = nudParsers.put(kind, subParser);
+		if (old != null) {
+			throw new OverrideException(
+					"Cannot add several nud parsers for one kind (nud backtracking is not supported)");
+		}
 	}
 
 	public void addLed(int kind, ILedParser<? extends Formula<?>> subParser) throws OverrideException {
-		final KindParsers parsers = fetchParsers(kind);
-		parsers.addLed(subParser);
-	}
-
-	private KindParsers fetchParsers(int kind) {
-		KindParsers parsers = kindParsers.get(kind);
-		if (parsers == null) {
-			parsers = new KindParsers();
-			kindParsers.put(kind, parsers);
+		var old = ledParsers.put(kind, subParser);
+		if (old != null) {
+			throw new OverrideException(
+					"Cannot add several led parsers for one kind (led backtracking is not supported)");
 		}
-		return parsers;
 	}
 
 	public void redistribute(Instantiator<Integer, Integer> opKindInst) {
-		remapKinds(opKindInst, kindParsers);
+		remapKinds(opKindInst, nudParsers);
+		remapKinds(opKindInst, ledParsers);
 	}
 
 	private static <T> void remapKinds(Instantiator<Integer, Integer> opKinInst, Map<Integer, T> map) {
