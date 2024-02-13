3a7de0fa2fe29060616ace3f308a02538484019b
Guillaume Verdier <gverdier@users.sourceforge.net>
2024-02-13 11:10:26 +0100

    Simplify tests for He reasoner
    He and Eh have the same implementation, except for which side of the
    equality hypothesis is rewritten. We can have only a few integration
    tests for He and leave all the complex test cases for Eh, rather than
    writing all tests twice.

--- org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtensionTests/HeTests.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtensionTests/HeTests.java
@@ -11,10 +11,7 @@
  *******************************************************************************/
 package org.eventb.core.seqprover.eventbExtensionTests;
 
-import static org.eventb.core.seqprover.tests.TestLib.genFullSeq;
 import static org.eventb.internal.core.seqprover.eventbExtensions.EqHe.Level.L0;
-import static org.eventb.internal.core.seqprover.eventbExtensions.EqHe.Level.L1;
-import static org.eventb.internal.core.seqprover.eventbExtensions.EqHe.Level.L2;
 import static org.junit.Assert.fail;
 
 import org.eventb.core.ast.Predicate;
@@ -27,6 +24,9 @@ import org.junit.Test;
 
 /**
  * Unit tests for the He reasoner
+ *
+ * The He and Eh reasoners share the same implementation. This class only has a
+ * few integration tests for He. Most tests should be added to {@link EhTests}.
  * 
  * @author htson
  */
@@ -61,33 +61,6 @@ public class HeTests extends AbstractReasonerTests {
 	public void testSuccess() throws UntranslatableException {
 		assertReasonerSuccess("0 = 1 ;; 0+1 = 2 |- 1+0+1 = 3 ", makeInput("0 = 1"),
 				"{}[][0+1=2][0=1 ;; 0+0=2] |- 0+0+0=3");
-		assertReasonerSuccess("1 = 0 + 1 ;; 0+1 = 2 |- 2+0+1 = 3 ", makeInput("1 = 0 + 1"),
-				"{}[][0+1=2][1=0+1 ;; 1=2] |- 2+1=3");
-		assertReasonerSuccess("2 + 1 = 0 + 1 + 2 ;; 0+1 = 0+1+2 |- 2+0+1 = 0+1+2+3 ", makeInput("2 + 1 = 0 + 1 + 2"),
-				"{}[][0+1=0+1+2][2+1=0+1+2 ;; 0+1=2+1] |- 2+0+1 = 2+1+3");
-		// FR #371
-		if (level.from(L1)) {
-			// Hypothesis is hidden when rewriting an identifier
-			assertReasonerSuccess("y + 1 = x |- x + 1 = 1 + y + 1", makeInput("y + 1 = x"),
-					"{}[y + 1 = x][][] |- y + 1 + 1 = 1 + y + 1");
-		} else {
-			assertReasonerSuccess("y + 1 = x |- x + 1 = 1 + y + 1", makeInput("y + 1 = x"),
-					"{}[][][y + 1 = x] |- y + 1 + 1 = 1 + y + 1");
-		}
-		// Bug #818
-		if (level.from(L2)) {
-			// Hypothesis is deselected (not hidden) when rewriting an identifier that is
-			// used in deselected hypotheses
-			assertReasonerSuccess(genFullSeq("y + 1 = x ;; x > 0 ;H; ;S; y + 1 = x |- x + 1 = 1 + y + 1", ff),
-					makeInput("y + 1 = x"), "{}[][y + 1 = x ;; x > 0][] |- y + 1 + 1 = 1 + y + 1");
-		} else if (level.from(L1)) {
-			assertReasonerSuccess(genFullSeq("y + 1 = x ;; x > 0 ;H; ;S; y + 1 = x |- x + 1 = 1 + y + 1", ff),
-					makeInput("y + 1 = x"), "{}[y + 1 = x][x > 0][] |- y + 1 + 1 = 1 + y + 1");
-		} else {
-			assertReasonerSuccess(genFullSeq("y + 1 = x ;; x > 0 ;H; ;S; y + 1 = x |- x + 1 = 1 + y + 1", ff),
-					makeInput("y + 1 = x"), "{}[][x > 0][y + 1 = x] |- y + 1 + 1 = 1 + y + 1");
-		}
-		// Behavior is not modified when rewriting a complex expression
 		assertReasonerSuccess("x = y + 1 |- x + 1 = 1 + y + 1", makeInput("x = y + 1"),
 				"{}[][][x = y + 1] |- x + 1 = 1 + x");
 	}
@@ -98,8 +71,6 @@ public class HeTests extends AbstractReasonerTests {
 		assertReasonerFailure("1 = 2 ⇒ 2 = 3 |- ⊤", makeInput("1 = 2 ⇒ 2 = 3"), "Unsupported hypothesis: 1=2⇒2=3");
 		// Nothing to do
 		assertReasonerFailure("0 = 1 ;; ⊤ |- ⊤", makeInput("0 = 1"), "Nothing to rewrite");
-		// Hyp is not present
-		assertReasonerFailure(" ⊤ |- ⊤ ", makeInput("0 = 1"), "Nonexistent hypothesis: 0=1");
 	}
 
 	private HypothesisReasoner.Input makeInput(String predImage) {
