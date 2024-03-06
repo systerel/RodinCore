089ce35da6a1e66b71c3660041b2d25e392d000d
Guillaume Verdier <gverdier@users.sourceforge.net>
2024-03-06 14:36:58 +0100

    Use a more modern method to test positions of DTDistinctCase

--- org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtensionTests/DTDistinctCaseTests.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtensionTests/DTDistinctCaseTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2023 Systerel and others.
+ * Copyright (c) 2010, 2024 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -59,12 +59,10 @@ public class DTDistinctCaseTests extends AbstractManualReasonerTests {
 		return Tactics.dtDCGetPositions(predicate);
 	}
 
-	@Override
-	protected String[] getTestGetPositions() {
-		return new String [] {
-				"∀ l⦂SD · l=l1", "1.1",
-				"∀ l ⦂ SD · destr1(l) = 0", "",
-		};
+	@Test
+	public void testGetPositions() {
+		assertGetPositions("∀ l⦂SD · l=l1", "1.1");
+		assertGetPositions("∀ l ⦂ SD · destr1(l) = 0");
 	}
 
 	@Test
