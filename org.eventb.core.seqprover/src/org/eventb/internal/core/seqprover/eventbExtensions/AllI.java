110e3855fd73fc17560a64fed21de2832720d7cc
Guillaume Verdier <gverdier@users.sourceforge.net>
2024-03-06 10:24:33 +0100

    Fix typo in AllI documentation

--- org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/AllI.java
+++ org.eventb.core.seqprover/src/org/eventb/internal/core/seqprover/eventbExtensions/AllI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 ETH Zurich and others.
+ * Copyright (c) 2006, 2024 ETH Zurich and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -31,7 +31,7 @@ import org.eventb.core.seqprover.reasonerInputs.EmptyInputReasoner;
 import org.eventb.internal.core.seqprover.eventbExtensions.utils.FreshInstantiation;
 
 /**
- * Generates the introduntion rule for universal quantification.
+ * Generates the introduction rule for universal quantification.
  * 
  * <p>
  * This reasoner frees all universally quantified variables in a goal.
