352e254089ecb5e581a0984f09c4377f8f89076e
Guillaume Verdier <gverdier@users.sourceforge.net>
2024-03-06 10:43:54 +0100

    Use more modern methods to test AllI

--- org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtensionTests/AllITests.java
+++ org.eventb.core.seqprover.tests/src/org/eventb/core/seqprover/eventbExtensionTests/AllITests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Systerel and others.
+ * Copyright (c) 2012, 2024 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,11 +10,10 @@
  *******************************************************************************/
 package org.eventb.core.seqprover.eventbExtensionTests;
 
-import static org.eventb.core.seqprover.tests.TestLib.genSeq;
-
 import org.eventb.core.seqprover.reasonerExtensionTests.AbstractReasonerTests;
 import org.eventb.core.seqprover.reasonerInputs.EmptyInput;
 import org.eventb.internal.core.seqprover.eventbExtensions.AllI;
+import org.junit.Test;
 
 /**
  * Acceptance tests for reasoner AllI.
@@ -30,53 +29,26 @@ public class AllITests extends AbstractReasonerTests {
 		return AllI.REASONER_ID;
 	}
 
-	@Override
-	public SuccessfullReasonerApplication[] getSuccessfulReasonerApplications() {
-		return new SuccessfullReasonerApplication[]{
-				// one bound variable
-				new SuccessfullReasonerApplication(genSeq("|- ∀x·x=1"), 
-						NO_INPUT,
-						"{}[][][] |- x=1"
-				),
-				// two predicates generated
-				new SuccessfullReasonerApplication(genSeq("|- ∀x·x=1 ∧ x=2"), 
-						NO_INPUT,
-						"{}[][][] |- x=1 ∧ x=2"
-				),
-				// two bound variables
-				new SuccessfullReasonerApplication(genSeq("|- ∀x,y·x↦y = 1↦2"), 
-						NO_INPUT,
-						"{}[][][] |- x↦y = 1↦2"
-				),
-				// name collision
-				new SuccessfullReasonerApplication(genSeq("x=3 |- ∀x·x=1"), 
-						NO_INPUT,
-						"{}[][][x=3] |- x0=1"
-				),
-				// name collision, different type
-				new SuccessfullReasonerApplication(genSeq("x=TRUE |- ∀x·x=1"), 
-						NO_INPUT,
-						"{}[][][x=TRUE] |- x0=1"
-				),
-		};
+	@Test
+	public void success() throws Exception {
+		// one bound variable
+		assertReasonerSuccess("|- ∀x·x=1", NO_INPUT, "{}[][][] |- x=1");
+		// two predicates generated
+		assertReasonerSuccess("|- ∀x·x=1 ∧ x=2", NO_INPUT, "{}[][][] |- x=1 ∧ x=2");
+		// two bound variables
+		assertReasonerSuccess("|- ∀x,y·x↦y = 1↦2", NO_INPUT, "{}[][][] |- x↦y = 1↦2");
+		// name collision
+		assertReasonerSuccess("x=3 |- ∀x·x=1", NO_INPUT, "{}[][][x=3] |- x0=1");
+		// name collision, different type
+		assertReasonerSuccess("x=TRUE |- ∀x·x=1", NO_INPUT, "{}[][][x=TRUE] |- x0=1");
 	}
 
-	@Override
-	public UnsuccessfullReasonerApplication[] getUnsuccessfullReasonerApplications() {
-		return new UnsuccessfullReasonerApplication[] {
-				// goal not quantified
-				new UnsuccessfullReasonerApplication(
-						genSeq(" ⊤ |- ⊥ "),
-						NO_INPUT,
-						"Goal is not universally quantified"
-				),	
-				// goal not universally quantified
-				new UnsuccessfullReasonerApplication(
-						genSeq(" ⊤ |- ∃x·x=1 "),
-						NO_INPUT,
-						"Goal is not universally quantified"
-				),	
-		};
+	@Test
+	public void failure() throws Exception {
+		// goal not quantified
+		assertReasonerFailure(" ⊤ |- ⊥ ", NO_INPUT, "Goal is not universally quantified");
+		// goal not universally quantified
+		assertReasonerFailure(" ⊤ |- ∃x·x=1 ", NO_INPUT, "Goal is not universally quantified");
 	}
 
 }
