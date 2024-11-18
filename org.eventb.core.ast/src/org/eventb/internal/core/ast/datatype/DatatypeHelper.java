b88a153f4ebc6fbbc6b512c1a48450ec44752bba
Guillaume Verdier <gverdier@users.sourceforge.net>
2024-11-18 10:55:16 +0100

    Prevent race conditions on DatatypeHelper.computeId
    This method uses a static counter attribute, so it needs to be protected
    against concurrent calls.
    
    The computeGroup and computeKind methods of the same class do not access
    shared attributes, so they are safe to call concurrently.

--- org.eventb.core.ast/src/org/eventb/internal/core/ast/datatype/DatatypeHelper.java
+++ org.eventb.core.ast/src/org/eventb/internal/core/ast/datatype/DatatypeHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 Systerel and others.
+ * Copyright (c) 2013, 2024 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -43,7 +43,7 @@ public class DatatypeHelper {
 
 	private static long uniqueId = 0;
 
-	public static String computeId(String name) {
+	public static synchronized String computeId(String name) {
 		if (name == null) {
 			name = "Id";
 		}
