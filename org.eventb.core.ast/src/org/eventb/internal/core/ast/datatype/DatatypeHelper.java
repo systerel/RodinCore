2fb546024e260425ca284625d06aab23ec5ec510
Guillaume Verdier <gverdier@users.sourceforge.net>
2024-11-18 11:14:55 +0100

    Remove unused method in DatatypeHelper
    No functional change.

--- org.eventb.core.ast/src/org/eventb/internal/core/ast/datatype/DatatypeHelper.java
+++ org.eventb.core.ast/src/org/eventb/internal/core/ast/datatype/DatatypeHelper.java
@@ -18,23 +18,13 @@ import static org.eventb.core.ast.extension.IOperatorProperties.FormulaType.EXPR
 import static org.eventb.core.ast.extension.StandardGroup.ATOMIC_EXPR;
 import static org.eventb.core.ast.extension.StandardGroup.CLOSED;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.eventb.core.ast.GivenType;
-import org.eventb.core.ast.Type;
 import org.eventb.core.ast.extension.IExtensionKind;
-import org.eventb.internal.core.typecheck.TypeCheckResult;
-import org.eventb.internal.core.typecheck.TypeVariable;
 
 /**
  * This class provides services for building and defining a datatype.
  * <p>
- * First it provides the way to have unique ID for datatype extensions, as well
- * as correct group ID and extension kind regarding extensions arguments. Then
- * to instantiate type parameters and datatype type for a datatype using a given
- * type which represents the datatype type.
+ * It provides the way to have unique ID for datatype extensions, as well as
+ * correct group ID and extension kind regarding extensions arguments.
  * </p>
  * 
  * @author Vincent Monfort
@@ -69,59 +59,6 @@ public class DatatypeHelper {
 					makeAllExpr(makeFixedArity(nbArgs)));
 		}
 		return kind;
-	}
-
-	/**
-	 * Returns the map representing the instantiated values for the given types
-	 * which were representing the datatype type parameters and its type during
-	 * the datatype construction. The datatype type constructor extension and a
-	 * given type representing the instantiated datatype type must be provided
-	 * in order to calculate this map values.
-	 * 
-	 * @param typeCons
-	 *            the datatype type constructor extension
-	 * @param datatypeType
-	 *            the given type that was representing the datatype type during
-	 *            datatype construction
-	 * @param typeParams
-	 *            the type parameters of the datatype
-	 * @param givenDTType
-	 *            the type representing the datatype type instantiated
-	 * @return the map representing the instantiated values for the given types
-	 *         which were representing the datatype type parameters and its type
-	 *         itself
-	 */
-	public static Map<GivenType, Type> instantiate(
-			TypeConstructorExtension typeCons, GivenType datatypeType,
-			List<GivenType> typeParams, Type givenDTType) {
-		// FIXME: Do not use type check result and use "manual" instantiation
-		final TypeCheckResult result = new TypeCheckResult(givenDTType
-				.getFactory().makeTypeEnvironment().makeSnapshot());
-
-		TypeVariable[] instantiations = new TypeVariable[typeParams.size()];
-
-		for (int i = 0; i < typeParams.size(); i++) {
-			instantiations[i] = result.newFreshVariable(null);
-		}
-
-		Type resultType = result.makeParametricType(typeCons, instantiations);
-		result.unify(givenDTType, resultType, givenDTType.toExpression());
-		result.solveTypeVariables();
-
-		if (result.hasProblem()) {
-			return null;
-		}
-
-		HashMap<GivenType, Type> instantiated = new HashMap<GivenType, Type>(
-				typeParams.size());
-		// Add the type parameters instantiations
-		for (int i = 0; i < typeParams.size(); i++) {
-			instantiated.put(typeParams.get(i), instantiations[i].getValue());
-		}
-		// Add the datatype type instantiation
-		instantiated.put(datatypeType, givenDTType);
-
-		return instantiated;
 	}
 
 }
